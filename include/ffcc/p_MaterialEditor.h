e2474a680f8279f2452106ce2c0c90b6b4973bfd
Zachary Canann <zacharycanann@gmail.com>
2026-01-31 12:48:57 -0800

    zlist: stage-aware placement new for AddTail

--- include/ffcc/p_MaterialEditor.h
+++ include/ffcc/p_MaterialEditor.h
@@ -2,6 +2,7 @@
 #define _FFCC_P_MATERIALEDITOR_H_
 
 #include "ffcc/system.h"
+#include "ffcc/memory.h"
 
 struct Vec;
 
@@ -24,6 +25,11 @@ public:
     void drawViewer();
 
     void CreateBoundaryBox(Vec&, Vec&, long, const Vec*);
+
+    // guessed from uses (e.g. ZLIST::AddTail): lwz r4, 0x4(MaterialEditorPcs)
+    CMemory::CStage* m_stage; // 0x04
 };
+
+extern __declspec(section ".data") CMaterialEditorPcs MaterialEditorPcs;
 
 #endif // _FFCC_P_MATERIALEDITOR_H_

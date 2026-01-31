dc92f5afd22780ddb44201fbfad19ad2cdd1f2bb
Zachary Canann <zacharycanann@gmail.com>
2026-01-31 09:43:14 -0800

    stopwatch: implement CStopWatch/CProfile

--- include/ffcc/stopwatch.h
+++ include/ffcc/stopwatch.h
@@ -2,29 +2,36 @@
 #define _FFCC_STOPWATCH_H
 
 #include <Dolphin/types.h>
+#include <dolphin/os.h>
 
 #include "ffcc/manager.h"
 
-class CStopWatch
+class CStopWatch : public OSStopwatch
 {
 public:
-	CStopWatch(char*);
+	CStopWatch(char* name);
 	~CStopWatch();
 
 	void Reset();
 	void Start();
 	void Stop();
-	void Get();
+	float Get();
 };
 
-class CProfile
+class CProfile : public OSStopwatch
 {
 public:
-	CProfile(char*);
+	CProfile(char* name);
 	~CProfile();
 
 	void ProfStart();
 	void ProfEnd();
+
+	// OSStopwatch is 0x30. These pad bytes model the real class layout up to 0x60.
+	u8 _pad_0x30[0x30];
+	float m_lastTime; // 0x60
+	float m_maxTime;  // 0x64
+	u32 m_frame;      // 0x68
 };
 
 #endif // _FFCC_STOPWATCH_H
--- src/stopwatch.cpp
+++ src/stopwatch.cpp
@@ -1,13 +1,19 @@
 #include "ffcc/stopwatch.h"
 
+extern "C" float __cvt_sll_flt(u32 lo, u32 hi);
+
+extern char lbl_8032F860[]; // small-data string used by target
+extern float lbl_8032F854;  // 0.0f
+
 /*
  * --INFO--
  * Address:	TODO
  * Size:	TODO
  */
-CStopWatch::CStopWatch(char*)
+CStopWatch::CStopWatch(char* name)
 {
-	// TODO
+	OSInitStopwatch(this, name);
+	OSResetStopwatch(this);
 }
 
 /*
@@ -15,9 +21,43 @@ CStopWatch::CStopWatch(char*)
  * Address:	TODO
  * Size:	TODO
  */
-CStopWatch::~CStopWatch()
+CStopWatch::~CStopWatch() {}
+
+/*
+ * --INFO--
+ * Address:	TODO
+ * Size:	TODO
+ */
+void CStopWatch::Reset() { OSResetStopwatch(this); }
+
+/*
+ * --INFO--
+ * Address:	TODO
+ * Size:	TODO
+ */
+void CStopWatch::Start() { OSStartStopwatch(this); }
+
+/*
+ * --INFO--
+ * Address:	TODO
+ * Size:	TODO
+ */
+void CStopWatch::Stop() { OSStopStopwatch(this); }
+
+/*
+ * --INFO--
+ * Address:	TODO
+ * Size:	TODO
+ */
+float CStopWatch::Get()
 {
-	// TODO
+	// Match target: convert accumulated ticks (total) into microseconds as float.
+	u32* p = (u32*)&this->total;
+	u32 lo = p[0];
+	u32 hi = p[1];
+	float ticks = __cvt_sll_flt(lo, hi);
+	float denom = (float)(OS_TIMER_CLOCK / 125000);
+	return (8.0f * ticks) / denom;
 }
 
 /*
@@ -25,9 +65,18 @@ CStopWatch::~CStopWatch()
  * Address:	TODO
  * Size:	TODO
  */
-void CStopWatch::Reset()
+CProfile::CProfile(char* name)
 {
-	// TODO
+	OSInitStopwatch(this, lbl_8032F860);
+	OSResetStopwatch(this);
+
+	OSStopwatch tmp;
+	OSInitStopwatch(&tmp, name);
+	OSResetStopwatch(&tmp);
+
+	m_maxTime = lbl_8032F854;
+	m_lastTime = lbl_8032F854;
+	m_frame = 0;
 }
 
 /*
@@ -35,60 +84,14 @@ void CStopWatch::Reset()
  * Address:	TODO
  * Size:	TODO
  */
-void CStopWatch::Start()
-{
-	// TODO
-}
+CProfile::~CProfile() {}
 
 /*
  * --INFO--
  * Address:	TODO
  * Size:	TODO
  */
-void CStopWatch::Stop()
-{
-	// TODO
-}
-
-/*
- * --INFO--
- * Address:	TODO
- * Size:	TODO
- */
-void CStopWatch::Get()
-{
-	// TODO
-}
-
-/*
- * --INFO--
- * Address:	TODO
- * Size:	TODO
- */
-CProfile::CProfile(char*)
-{
-	// TODO
-}
-
-/*
- * --INFO--
- * Address:	TODO
- * Size:	TODO
- */
-CProfile::~CProfile()
-{
-	// TODO
-}
-
-/*
- * --INFO--
- * Address:	TODO
- * Size:	TODO
- */
-void CProfile::ProfStart()
-{
-	// TODO
-}
+void CProfile::ProfStart() { OSResetStopwatch(this); }
 
 /*
  * --INFO--
@@ -97,5 +100,24 @@ void CProfile::ProfStart()
  */
 void CProfile::ProfEnd()
 {
-	// TODO
+	u32* p = (u32*)&this->total;
+	u32 lo = p[0];
+	u32 hi = p[1];
+	float ticks = __cvt_sll_flt(lo, hi);
+	float denom = (float)(OS_TIMER_CLOCK / 125000);
+	m_lastTime = (8.0f * ticks) / denom;
+
+	u32 next = m_frame + 1;
+	m_frame = next;
+	if (next == 0x5A)
+	{
+		m_maxTime = lbl_8032F854;
+		m_frame = 0;
+	}
+
+	if (m_maxTime < m_lastTime)
+	{
+		m_maxTime = m_lastTime;
+		m_frame = 0;
+	}
 }

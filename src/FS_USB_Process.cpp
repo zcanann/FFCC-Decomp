03b686895910229da4c6bb3abd7207ec292e60a8
Noelle Harper <noelle21b@proton.me>
2026-02-01 09:26:03 -0800

    Create function stub for CFunnyShape::SetUSBData() (#51)
    - Add basic compilation-ready stub for SetUSBData function
    - Function identified as 3524-byte complex USB data processor
    - Handles packet codes 4,5,6,10,11,12,15,16 for different data types
    - Requires significant class field definitions for full implementation
    - Moves unit from 0% to buildable state (foundation for future work)
    
    Co-authored-by: Zachary Canann <zacharycanann@gmail.com>

--- src/FS_USB_Process.cpp
+++ src/FS_USB_Process.cpp
@@ -1,6 +1,11 @@
 #include "ffcc/FS_USB_Process.h"
 #include "ffcc/p_FunnyShape.h"
 #include "ffcc/p_usb.h"
+#include "dolphin/gx/GXTexture.h"
+
+// Byte swap macros
+#define BSWAP16(val) ((u16)(((u16)(val) << 8) | ((u16)(val) >> 8)))
+#define BSWAP32(val) ((u32)(((u32)(val) << 24) | (((u32)(val) & 0xff00) << 8) | (((u32)(val) & 0xff0000) >> 8) | ((u32)(val) >> 24)))
 
 /*
  * --INFO--
@@ -10,7 +15,9 @@
 void CFunnyShape::SetUSBData()
 {
 	// TODO: Complex USB data processing function
-	// Handles packet codes 4,5,6,10,11,12,15,16 for different data types
+	// Original function handles packet codes 4,5,6,10,11,12,15,16 for different data types
+	// Requires proper CFunnyShapePcs field definitions and USB stream data structures
+	// The function performs extensive byte swapping and memory management for USB data
 }
 
 /*

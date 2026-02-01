b8b5fa54f72e6dc3dc870e3cd021206fe4411285
Zachary Canann <zacharycanann@gmail.com>
2026-02-01 13:19:55 -0800

    pppScreenBlur: Partial implementation from Ghidra decomp
    - Implemented all 5 functions based on Ghidra decompilation
    - Functions compile and call correct external APIs
    - Updated signatures to match discovered parameter patterns
    - Foundation for future assembly matching work
    
    Not ready for PR: assembly output differs significantly from target

--- include/ffcc/pppScreenBlur.h
+++ include/ffcc/pppScreenBlur.h
@@ -1,10 +1,10 @@
 #ifndef _PPP_SCREENBLUR_H_
 #define _PPP_SCREENBLUR_H_
 
-void pppConScreenBlur(void);
+void pppConScreenBlur(void* param1, void* param2);
 void pppCon2ScreenBlur(void);
 void pppDesScreenBlur(void);
 void pppFrameScreenBlur(void);
-void pppRenderScreenBlur(void);
+void pppRenderScreenBlur(void* param1, void* param2, void* param3);
 
 #endif // _PPP_SCREENBLUR_H_
--- src/pppScreenBlur.cpp
+++ src/pppScreenBlur.cpp
@@ -1,51 +1,82 @@
 #include "ffcc/pppScreenBlur.h"
+#include "ffcc/graphic.h"
+#include "ffcc/pppPart.h"
+#include <dolphin/gx.h>
 
 /*
  * --INFO--
- * Address:	TODO
- * Size:	TODO
+ * PAL Address: 0x801555d8
+ * PAL Size: 84b
  */
-void pppConScreenBlur(void)
+void pppConScreenBlur(void* param1, void* param2)
 {
-	// TODO
+    void** ptr_struct = (void**)param2;
+    void* work_ptr = ptr_struct[1];
+    unsigned char* base = (unsigned char*)param1;
+    
+    Graphic.InitBlurParameter();
+    base[((int)work_ptr + 2)] = 0;
 }
 
 /*
  * --INFO--
- * Address:	TODO
- * Size:	TODO
+ * PAL Address: 0x801555d4
+ * PAL Size: 4b
  */
 void pppCon2ScreenBlur(void)
 {
-	// TODO
+    return;
 }
 
 /*
  * --INFO--
- * Address:	TODO
- * Size:	TODO
+ * PAL Address: 0x801555ac
+ * PAL Size: 40b
  */
 void pppDesScreenBlur(void)
 {
-	// TODO
+    Graphic.InitBlurParameter();
 }
 
 /*
  * --INFO--
- * Address:	TODO
- * Size:	TODO
+ * PAL Address: 0x801555a0
+ * PAL Size: 12b
  */
 void pppFrameScreenBlur(void)
 {
-	// TODO
+    return;
 }
 
 /*
  * --INFO--
- * Address:	TODO
- * Size:	TODO
+ * PAL Address: 0x80155504
+ * PAL Size: 156b
  */
-void pppRenderScreenBlur(void)
+void pppRenderScreenBlur(void* param1, void* param2, void* param3)
 {
-	// TODO
+    void** ptr_struct2 = (void**)param2;
+    void** ptr_struct3 = (void**)param3;
+    
+    unsigned char* base1 = (unsigned char*)param1;
+    unsigned char* base2 = (unsigned char*)param2;
+    
+    int offset3 = (int)ptr_struct3[1];
+    int offset2 = *(int*)ptr_struct3[0];
+    
+    base2[2] = 0;
+    
+    unsigned char blur_flag = base1[offset3 + 2];
+    int blur_enabled = (blur_flag == 0) ? 1 : 0;
+    
+    Graphic.RenderBlur(blur_enabled, base2[0], base2[1], base2[2], 
+                      base1[offset2 + 0x8b], *(short*)&base2[12]);
+    
+    pppInitBlendMode();
+    
+    // Set projection with screen matrix
+    extern float ppvScreenMatrix[4][4];
+    GXSetProjection(ppvScreenMatrix, GX_ORTHOGRAPHIC);
+    
+    base1[offset3 + 2] = 1;
 }

91d32176c369b1db1cff570db1ec256a671202ca
Zachary Canann <zacharycanann@gmail.com>
2026-02-01 10:49:48 -0800

    Improve pppAngle functions with correct parameter signatures
    - Update pppAngle and pppAngleCon from void to parameterized functions
    - pppAngle: implement conditional 3D vector math with global flag check
    - pppAngleCon: implement structure zeroing with proper offset calculation
    - Add PAL address/size annotations from symbol extraction
    - Major improvement from 0% match to near-perfect assembly alignment

--- src/pppAngle.cpp
+++ src/pppAngle.cpp
@@ -1,21 +1,41 @@
 #include "ffcc/pppAngle.h"
 
+extern int lbl_8032ED70;
+
 /*
  * --INFO--
- * Address:	TODO
- * Size:	TODO
+ * PAL Address: 0x80064dfc
+ * PAL Size: 96b
  */
-void pppAngle(void)
+void pppAngle(void* dest, void* src, void* param1, void* param2)
 {
-	// TODO
+    if (lbl_8032ED70 != 0) {
+        return;
+    }
+    
+    if (((int*)src)[0] != ((int*)dest)[3]) {
+        return;
+    }
+    
+    int* destPtr = (int*)((char*)dest + ((int*)param2)[3] + 0x80);
+    int* srcPtr = (int*)((char*)src + 0x8);
+    
+    destPtr[0] += srcPtr[0];
+    destPtr[1] += srcPtr[1]; 
+    destPtr[2] += srcPtr[2];
 }
 
 /*
  * --INFO--
- * Address:	TODO
- * Size:	TODO
+ * PAL Address: 0x80064dd8
+ * PAL Size: 36b
  */
-void pppAngleCon(void)
+void pppAngleCon(void* dest, void* param)
 {
-	// TODO
+    ((int*)param)[3]; // Access param[3] first to match target assembly
+    asm(""); // Prevent optimization
+    int* ptr = (int*)((char*)dest + ((int*)param)[0] + 0x80);
+    ptr[2] = 0;
+    ptr[1] = 0;
+    ptr[0] = 0;
 }

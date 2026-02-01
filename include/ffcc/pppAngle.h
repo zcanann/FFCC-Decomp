b562f227894ff44ae95a02ce27e76d7311fee604
Noelle Harper <noelle21b@proton.me>
2026-02-01 10:53:37 -0800

    Improve pppAngle functions with correct parameter signatures (#60)
    - Update pppAngle and pppAngleCon from void to parameterized functions
    - pppAngle: implement conditional 3D vector math with global flag check
    - pppAngleCon: implement structure zeroing with proper offset calculation
    - Add PAL address/size annotations from symbol extraction
    - Major improvement from 0% match to near-perfect assembly alignment
    
    Co-authored-by: Zachary Canann <zacharycanann@gmail.com>

--- include/ffcc/pppAngle.h
+++ include/ffcc/pppAngle.h
@@ -1,7 +1,7 @@
 #ifndef _FFCC_PPPANGLE_H_
 #define _FFCC_PPPANGLE_H_
 
-void pppAngle(void);
-void pppAngleCon(void);
+void pppAngle(void* dest, void* src, void* param1, void* param2);
+void pppAngleCon(void* dest, void* param);
 
 #endif

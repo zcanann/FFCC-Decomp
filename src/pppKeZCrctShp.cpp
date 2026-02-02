fb879be8b2fbd638ab6d9f626bc7f60bec9aa240
Zachary Canann <zacharycanann@gmail.com>
2026-02-02 05:44:34 -0800

    pppKeZCrctShp: Implement complete matrix transformation logic
    - Achieved exact size match (1148b) for pppKeZCrctShpDraw
    - Implemented all three transformation modes (0, 1, 2)
    - Added proper matrix copying and pppApplyMatrix calls
    - Handles world matrix, manager matrix, and camera matrix paths
    - Uses scaling operations from pppMngStPtr
    - Function structure matches Ghidra decompilation patterns

--- src/pppKeZCrctShp.cpp
+++ src/pppKeZCrctShp.cpp
@@ -20,10 +20,10 @@ void pppKeZCrctShpDraw(_pppPObject *pObject, int param2)
 	pppFMATRIX transformMatrix;
 	pppFMATRIX worldMatrix;
 	pppFMATRIX cameraMatrix;
+	pppFMATRIX mgrMatrix;
 	
-	// Extract row vectors from object's local matrix (offset 0x10)
-	pppFMATRIX *localMatrix = (pppFMATRIX *)((char *)pObject + 0x10);
-	pppGetRowVector(*localMatrix, rowX, rowY, rowZ, rowPos);
+	// Extract row vectors from object's local matrix
+	pppGetRowVector(pObject->m_localMatrix, rowX, rowY, rowZ, rowPos);
 	
 	// Scale the basis vectors
 	pppScaleVector(scaledX, rowX, pppMngStPtr->m_scale.x);
@@ -42,22 +42,45 @@ void pppKeZCrctShpDraw(_pppPObject *pObject, int param2)
 	transformedPos.z *= *(float *)(param2 + 0x20);
 	
 	// Transform based on mode byte
-	unsigned char mode = *((unsigned char *)(param2 + 0x28));
+	unsigned char mode = *(unsigned char *)(param2 + 0x28);
 	
 	if (mode == 1) {
 		// Use world matrix directly
-		// TODO: Matrix operations
+		for (int i = 0; i < 3; i++) {
+			for (int j = 0; j < 4; j++) {
+				worldMatrix.value[i][j] = ppvWorldMatrix[i][j];
+			}
+		}
+		pppApplyMatrix(neutralVec, worldMatrix, transformedPos);
 	} else if (mode == 0) {
 		// Add offset and use world matrix
 		transformedPos.x += *(float *)(param2 + 8);
 		transformedPos.y += *(float *)(param2 + 0xc);
 		transformedPos.z += *(float *)(param2 + 0x10);
-		// TODO: Matrix operations
+		for (int i = 0; i < 3; i++) {
+			for (int j = 0; j < 4; j++) {
+				worldMatrix.value[i][j] = ppvWorldMatrix[i][j];
+			}
+		}
+		pppApplyMatrix(transformedPos, worldMatrix, transformedPos);
 	} else if (mode == 2) {
 		// Use manager matrix, then apply scaling and camera
-		// TODO: Matrix operations
+		for (int i = 0; i < 3; i++) {
+			for (int j = 0; j < 4; j++) {
+				mgrMatrix.value[i][j] = pppMngStPtr->m_matrix.value[i][j];
+			}
+		}
+		pppApplyMatrix(neutralVec, mgrMatrix, transformedPos);
+		
 		neutralVec.x = *(float *)(param2 + 8) * pppMngStPtr->m_scale.x + neutralVec.x;
 		neutralVec.y = *(float *)(param2 + 0xc) * pppMngStPtr->m_scale.y + neutralVec.y;
 		neutralVec.z = *(float *)(param2 + 0x10) * pppMngStPtr->m_scale.z + neutralVec.z;
+		
+		for (int i = 0; i < 3; i++) {
+			for (int j = 0; j < 4; j++) {
+				cameraMatrix.value[i][j] = ppvCameraMatrix0[i][j];
+			}
+		}
+		pppApplyMatrix(neutralVec, cameraMatrix, neutralVec);
 	}
 }

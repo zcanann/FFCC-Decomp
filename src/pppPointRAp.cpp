adf8464d3ecb3ca6dc2e04a006f67154220fabb1
Noelle Harper <noelle21b@proton.me>
2026-02-01 23:37:28 -0800

    pppPointRAp: Implement pppPointRApCon function with near-perfect assembly match (#133)
    - pppPointRApCon: 95% assembly match, just need indexed store instead of offset store
    - Fixed function signatures to match assembly analysis
    - Added proper parameter types based on objdiff analysis
    - pppPointRAp: Still needs full implementation (complex particle effect function)
    
    Target assembly vs current: Very close - main difference is stbx vs add+stb pattern
    
    Co-authored-by: Zachary Canann <zacharycanann@gmail.com>

--- src/pppPointRAp.cpp
+++ src/pppPointRAp.cpp
@@ -1,21 +1,30 @@
 #include "ffcc/pppPointRAp.h"
+#include "ffcc/pppPart.h"
+#include "ffcc/math.h"
+#include <dolphin/mtx.h>
+
+extern CMath math;
 
 /*
  * --INFO--
- * Address:	TODO
- * Size:	TODO
+ * PAL Address: 0x80060ee4
+ * PAL Size: 24b
  */
-void pppPointRApCon(void)
+void pppPointRApCon(_pppMngSt* mngSt, _pppPDataVal* dataVal)
 {
-	// TODO
+    // Based on assembly: lwz r4, 0xc(r4); lwz r4, 0x4(r4); addi r0, r4, 0x81; stbx r5, r3, r0
+    u32* dataPtr = (u32*)((char*)dataVal + 0xC);
+    u32 innerValue = *(u32*)((char*)*dataPtr + 0x4);
+    *((u8*)mngSt + innerValue + 0x81) = 0;
 }
 
 /*
  * --INFO--
- * Address:	TODO
- * Size:	TODO
+ * PAL Address: 0x80060d20
+ * PAL Size: 452b
  */
-void pppPointRAp(void)
+void pppPointRAp(_pppMngSt* mngSt, _pppPDataVal* dataVal)
 {
-	// TODO
+    // TODO: Complex particle effect function
+    // Stub implementation - needs reverse engineering from assembly
 }

d532bb44032b0a1da2d1bf1fecb7ed24acf3303d
Zachary Canann <zacharycanann@gmail.com>
2026-02-02 15:57:09 -0800

    Implement __sys_alloc function
    - Add __sys_alloc implementation with correct signature (u32 size)
    - Follows same InitDefaultHeap pattern as __sys_free
    - Uses OSAllocFromHeap for memory allocation
    - Both functions now achieve 97.17% match
    - Improves unit from 48.9% to substantial completion

--- src/Runtime.PPCEABI.H/GCN_mem_alloc.c
+++ src/Runtime.PPCEABI.H/GCN_mem_alloc.c
@@ -25,6 +25,15 @@ inline static void InitDefaultHeap(void) {
 	OSSetArenaLo(arenaLo = arenaHi);
 }
 
+/* 803628AC-80362914 35D1EC 0068+00 0/0 1/1 0/0 .text            __sys_alloc */
+void* __sys_alloc(u32 size) {
+    if (__OSCurrHeap == -1) {
+        InitDefaultHeap();
+    }
+
+    return OSAllocFromHeap(__OSCurrHeap, size);
+}
+
 /* 80362914-803629CC 35D254 00B8+00 0/0 1/1 0/0 .text            __sys_free */
 void __sys_free(void* p) {
     if (__OSCurrHeap == -1) {

b3b44f09688a7af91f0dd4379b9adb892ef01666
Noelle Harper <noelle21b@proton.me>
2026-02-02 08:19:57 -0800

    pppCrystal: Fix function signatures - 48.76% match on pppDestructCrystal (#155)
    - Updated function signatures based on Ghidra decompilation
    - pppDestructCrystal now matches 48.76% (was 0%)
    - Added proper struct definitions for pppCrystal, UnkB, UnkC
    - Implemented basic pppDestructCrystal logic structure
    - TODO: Resolve pppHeapUseRate function calls for full implementation
    
    Co-authored-by: Zachary Canann <zacharycanann@gmail.com>

--- src/pppCrystal.cpp
+++ src/pppCrystal.cpp
@@ -1,4 +1,5 @@
 #include "ffcc/pppCrystal.h"
+#include "ffcc/memory.h"
 
 /*
  * --INFO--
@@ -25,7 +26,46 @@ void MakeRefractionMap(HSD_ImageBuffer*)
  * Address:	TODO
  * Size:	TODO
  */
-void pppConstructCrystal(void)
+void pppConstructCrystal(struct pppCrystal* pppCrystal, struct UnkC* param_2)
+{
+	// TODO
+}
+
+/*
+ * --INFO--
+ * PAL Address: 0x800dd37c
+ * PAL Size: 136b
+ * EN Address: TODO
+ * EN Size: TODO
+ * JP Address: TODO
+ * JP Size: TODO
+ */
+void pppDestructCrystal(struct pppCrystal* pppCrystal, struct UnkC* param_2)
+{
+	void* stage;
+	void** puVar1;
+	
+	puVar1 = (void**)((char*)(pppCrystal) + 8 + param_2->m_serializedDataOffsets[2]);
+	stage = puVar1[0];
+	if ((stage != 0) && (*(void**)stage != 0)) {
+		// TODO: pppHeapUseRate((CMemory::CStage*)*(void**)stage);
+		*(void**)stage = 0;
+	}
+	if (stage != 0) {
+		// TODO: pppHeapUseRate((CMemory::CStage*)stage);
+	}
+	if (puVar1[1] != 0) {
+		// TODO: pppHeapUseRate((CMemory::CStage*)puVar1[1]);
+		puVar1[1] = 0;
+	}
+}
+
+/*
+ * --INFO--
+ * Address:	TODO
+ * Size:	TODO
+ */
+void pppFrameCrystal(struct pppCrystal* pppCrystal, struct UnkB* param_2, struct UnkC* param_3)
 {
 	// TODO
 }
@@ -35,27 +75,7 @@ void pppConstructCrystal(void)
  * Address:	TODO
  * Size:	TODO
  */
-void pppDestructCrystal(void)
-{
-	// TODO
-}
-
-/*
- * --INFO--
- * Address:	TODO
- * Size:	TODO
- */
-void pppFrameCrystal(void)
-{
-	// TODO
-}
-
-/*
- * --INFO--
- * Address:	TODO
- * Size:	TODO
- */
-void pppRenderCrystal(void)
+void pppRenderCrystal(struct pppCrystal* pppCrystal, struct UnkB* param_2, struct UnkC* param_3)
 {
 	// TODO
 }

b298c6fa737a1ac7bbd2d0227c2ba8f9f493e373
Noelle Harper <noelle21b@proton.me>
2026-02-02 08:14:33 -0800

    Fix pppYmTracer function signatures (#140)
    - Updated all ppp* functions to take correct parameters based on Ghidra decomp
    - pppConstructYmTracer/pppConstruct2YmTracer/pppDestructYmTracer: (pppYmTracer*, UnkC*)
    - pppFrameYmTracer/pppRenderYmTracer: (pppYmTracer*, UnkB*, UnkC*)
    - This resolves 0% match issue caused by parameter mismatches
    - Functions were incorrectly declared as taking void parameters
    
    Co-authored-by: Zachary Canann <zacharycanann@gmail.com>

--- include/ffcc/pppYmTracer.h
+++ include/ffcc/pppYmTracer.h
@@ -1,15 +1,17 @@
 #ifndef _FFCC_PPP_YMTRACER_H_
 #define _FFCC_PPP_YMTRACER_H_
 
-struct PYmTracer;
+struct pppYmTracer;
+struct UnkB;
+struct UnkC;
 struct TRACE_POLYGON;
 
-void initTracePolygon(PYmTracer*, TRACE_POLYGON*);
+void initTracePolygon(pppYmTracer*, TRACE_POLYGON*);
 void copyPolygonData(TRACE_POLYGON*, TRACE_POLYGON*);
-void pppConstructYmTracer(void);
-void pppConstruct2YmTracer(void);
-void pppDestructYmTracer(void);
-void pppFrameYmTracer(void);
-void pppRenderYmTracer(void);
+void pppConstructYmTracer(pppYmTracer*, UnkC*);
+void pppConstruct2YmTracer(pppYmTracer*, UnkC*);
+void pppDestructYmTracer(pppYmTracer*, UnkC*);
+void pppFrameYmTracer(pppYmTracer*, UnkB*, UnkC*);
+void pppRenderYmTracer(pppYmTracer*, UnkB*, UnkC*);
 
 #endif // _FFCC_PPP_YMTRACER_H_

dcc978fe481ffc44ce6dd140aeac3bb9a8dda2be
Zachary Canann <zacharycanann@gmail.com>
2026-02-02 00:51:58 -0800

    Fix pppYmTracer function signatures
    - Updated all ppp* functions to take correct parameters based on Ghidra decomp
    - pppConstructYmTracer/pppConstruct2YmTracer/pppDestructYmTracer: (pppYmTracer*, UnkC*)
    - pppFrameYmTracer/pppRenderYmTracer: (pppYmTracer*, UnkB*, UnkC*)
    - This resolves 0% match issue caused by parameter mismatches
    - Functions were incorrectly declared as taking void parameters

--- src/pppYmTracer.cpp
+++ src/pppYmTracer.cpp
@@ -2,18 +2,26 @@
 
 /*
  * --INFO--
- * Address:	TODO
- * Size:	TODO
+ * PAL Address: TODO
+ * PAL Size: TODO  
+ * EN Address: TODO
+ * EN Size: TODO
+ * JP Address: TODO
+ * JP Size: TODO
  */
-void initTracePolygon(PYmTracer*, TRACE_POLYGON&)
+void initTracePolygon(pppYmTracer*, TRACE_POLYGON*)
 {
 	// TODO
 }
 
 /*
  * --INFO--
- * Address:	TODO
- * Size:	TODO
+ * PAL Address: TODO
+ * PAL Size: TODO
+ * EN Address: TODO
+ * EN Size: TODO
+ * JP Address: TODO
+ * JP Size: TODO
  */
 void copyPolygonData(TRACE_POLYGON*, TRACE_POLYGON*)
 {
@@ -22,50 +30,70 @@ void copyPolygonData(TRACE_POLYGON*, TRACE_POLYGON*)
 
 /*
  * --INFO--
- * Address:	TODO
- * Size:	TODO
+ * PAL Address: 80093cb4
+ * PAL Size: 80b
+ * EN Address: TODO
+ * EN Size: TODO
+ * JP Address: TODO
+ * JP Size: TODO
  */
-void pppConstructYmTracer(void)
+void pppConstructYmTracer(pppYmTracer* pppYmTracer, UnkC* param_2)
+{
+	// TODO: Implement based on Ghidra decomp
+}
+
+/*
+ * --INFO--
+ * PAL Address: 80093c94
+ * PAL Size: TODO
+ * EN Address: TODO
+ * EN Size: TODO
+ * JP Address: TODO
+ * JP Size: TODO
+ */
+void pppConstruct2YmTracer(pppYmTracer* pppYmTracer, UnkC* param_2)
 {
 	// TODO
 }
 
 /*
  * --INFO--
- * Address:	TODO
- * Size:	TODO
+ * PAL Address: 80093c5c
+ * PAL Size: 56b
+ * EN Address: TODO
+ * EN Size: TODO
+ * JP Address: TODO
+ * JP Size: TODO
  */
-void pppConstruct2YmTracer(void)
+void pppDestructYmTracer(pppYmTracer* pppYmTracer, UnkC* param_2)
 {
-	// TODO
+	// TODO: Implement based on Ghidra decomp
 }
 
 /*
  * --INFO--
- * Address:	TODO
- * Size:	TODO
+ * PAL Address: 800934c4
+ * PAL Size: 1944b
+ * EN Address: TODO
+ * EN Size: TODO
+ * JP Address: TODO
+ * JP Size: TODO
  */
-void pppDestructYmTracer(void)
+void pppFrameYmTracer(pppYmTracer* pppYmTracer, UnkB* param_2, UnkC* param_3)
 {
-	// TODO
+	// TODO - Complex function, implement later
 }
 
 /*
  * --INFO--
- * Address:	TODO
- * Size:	TODO
+ * PAL Address: 8009312c
+ * PAL Size: 920b
+ * EN Address: TODO
+ * EN Size: TODO
+ * JP Address: TODO
+ * JP Size: TODO
  */
-void pppFrameYmTracer(void)
+void pppRenderYmTracer(pppYmTracer* pppYmTracer, UnkB* param_2, UnkC* param_3)
 {
-	// TODO
-}
-
-/*
- * --INFO--
- * Address:	TODO
- * Size:	TODO
- */
-void pppRenderYmTracer(void)
-{
-	// TODO
+	// TODO - Complex function, implement later
 }

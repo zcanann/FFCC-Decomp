09676f2accdc1b1f0dfe034ccefa75408b8a641f
Noelle Harper <noelle21b@proton.me>
2026-02-01 16:07:52 -0800

    pppYmCallBack: Fix function signatures and implement Construct/Destruct (#86)
    - Correct all function signatures to take no parameters
    - Implement pppConstructYmCallBack as simple return (4b, should be 100% match)
    - Implement pppDestructYmCallBack as simple return (4b, should be 100% match)
    - Update function documentation with PAL addresses from Ghidra decomp
    - TODO: pppFrameYmCallBack complex implementation (192b target)
    
    Co-authored-by: Zachary Canann <zacharycanann@gmail.com>

--- src/pppYmCallBack.cpp
+++ src/pppYmCallBack.cpp
@@ -1,31 +1,42 @@
 #include "ffcc/pppYmCallBack.h"
+#include "ffcc/partMng.h"
+#include "ffcc/game.h"
+
+#include <dolphin/mtx.h>
 
 /*
  * --INFO--
- * Address:	TODO
- * Size:	TODO
+ * PAL Address: 0x800a6090
+ * PAL Size: 4b
  */
 void pppConstructYmCallBack(void)
 {
-	// TODO
+	return;
 }
 
 /*
  * --INFO--
- * Address:	TODO
- * Size:	TODO
+ * PAL Address: TODO
+ * PAL Size: 4b
  */
 void pppDestructYmCallBack(void)
 {
-	// TODO
+	return;
 }
 
 /*
  * --INFO--
- * Address:	TODO
- * Size:	TODO
+ * PAL Address: 0x800a5fcc
+ * PAL Size: 192b
  */
 void pppFrameYmCallBack(void)
 {
-	// TODO
+	_pppMngSt* p_Var1;
+	int iVar2;
+	unsigned int uVar3;
+	Vec local_18;
+	
+	p_Var1 = pppMngStPtr;
+	// Complex graph ID comparison and matrix operations
+	// TODO: Need to understand the structure better to complete implementation
 }

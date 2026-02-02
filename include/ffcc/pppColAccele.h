1a5af2aa9c22b526529563a3800286030c55bb94
Zachary Canann <zacharycanann@gmail.com>
2026-02-02 04:08:20 -0800

    Fix pppColAccele function signatures with C linkage
    - Added extern C linkage to pppColAccele header functions
    - Achieved 100% match on pppColAccele function
    - Achieved 99.6% match on pppColAcceleCon function
    - Resolved 0% match issue caused by C++ name mangling

--- include/ffcc/pppColAccele.h
+++ include/ffcc/pppColAccele.h
@@ -3,7 +3,15 @@
 
 struct _pppPObject;
 
+#ifdef __cplusplus
+extern "C" {
+#endif
+
 void pppColAccele(_pppPObject* obj1, void* data1, _pppPObject* obj2);
 void pppColAcceleCon(_pppPObject* obj, void* data);
+
+#ifdef __cplusplus
+}
+#endif
 
 #endif // _FFCC_PPPCOLACCELE_H_

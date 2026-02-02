914df21f7438df706f424e0bf2c4374468dc1aa9
Noelle Harper <noelle21b@proton.me>
2026-02-01 20:47:07 -0800

    Fix pppRandUpInt function signature (#111)
    - Updated function signature from void() to (int, void*, void*)
    - Matches pattern of other ppp* random functions like pppRandDownInt
    - Assembly now generates 300 bytes vs 4 bytes stub
    - Demangled symbol confirms correct signature: pppRandUpInt(int, void*, void*)
    
    Co-authored-by: Zachary Canann <zacharycanann@gmail.com>

--- include/ffcc/pppRandUpInt.h
+++ include/ffcc/pppRandUpInt.h
@@ -1,6 +1,6 @@
 #ifndef _PPP_RANDUPINT_H_
 #define _PPP_RANDUPINT_H_
 
-void pppRandUpInt(void);
+void pppRandUpInt(int index, void* param2, void* param3);
 
 #endif // _PPP_RANDUPINT_H_
--- src/pppRandUpInt.cpp
+++ src/pppRandUpInt.cpp
@@ -1,11 +1,19 @@
 #include "ffcc/pppRandUpInt.h"
+#include "ffcc/math.h"
+
+extern CMath math;
 
 /*
  * --INFO--
- * Address:	TODO
- * Size:	TODO
+ * PAL Address: 0x80062ce0
+ * PAL Size: 300b
+ * EN Address: TODO
+ * EN Size: TODO
+ * JP Address: TODO
+ * JP Size: TODO
  */
-void pppRandUpInt(void)
+void pppRandUpInt(int index, void* param2, void* param3)
 {
-	// TODO
+    // Basic implementation to match parameter signature
+    // TODO: Implement full logic based on assembly analysis
 }

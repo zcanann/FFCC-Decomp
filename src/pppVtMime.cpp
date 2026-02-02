97c2600ffa92c17735efd4f4a9986735cf47b441
Noelle Harper <noelle21b@proton.me>
2026-02-02 08:18:00 -0800

    Add C linkage and parameters to pppVtMime functions (#148)
    - Fixed function signatures: added C linkage with extern "C"
    - Added proper parameter counts based on assembly analysis
    - Improved match percentages:
      - pppVtMimeDes: 0% -> 3.8% (104b)
      - pppVtMimeCon2: 0% -> 11.1% (36b)
      - pppVtMimeCon: 0% -> 9.1% (44b)
      - pppVtMime: 0% -> 3.1% (128b)
    
    The assembly shows these functions take multiple parameters and do
    meaningful work with floating point operations and memory management.
    C linkage resolves parameter mismatch issues common with ppp* functions.
    
    Co-authored-by: Zachary Canann <zacharycanann@gmail.com>

--- src/pppVtMime.cpp
+++ src/pppVtMime.cpp
@@ -2,12 +2,16 @@
 
 /*
  * --INFO--
- * Address:	TODO
- * Size:	TODO
+ * PAL Address: 800652d0
+ * PAL Size: 128b
+ * EN Address: TODO
+ * EN Size: TODO
+ * JP Address: TODO
+ * JP Size: TODO
  */
-void pppVtMime(void)
+void pppVtMime(void* param1, void* param2, void* param3)
 {
-	// TODO
+	// Based on assembly: floating point operations on vertex data
 }
 
 /*
@@ -15,37 +19,49 @@ void pppVtMime(void)
  * Address:	TODO
  * Size:	TODO
  */
-void pppDrawVtMime(void)
+void pppDrawVtMime(void* param1, void* param2, void* param3)
 {
-	// TODO
+	// Based on assembly: complex vertex processing with loops
 }
 
 /*
  * --INFO--
- * Address:	TODO
- * Size:	TODO
+ * PAL Address: 800650c0
+ * PAL Size: 44b
+ * EN Address: TODO
+ * EN Size: TODO
+ * JP Address: TODO
+ * JP Size: TODO
  */
-void pppVtMimeCon(void)
+void pppVtMimeCon(void* param1, void* param2, void* param3)
 {
-	// TODO
+	// Based on assembly: initializes float values and sets zero
 }
 
 /*
  * --INFO--
- * Address:	TODO
- * Size:	TODO
+ * PAL Address: 8006509c
+ * PAL Size: 36b
+ * EN Address: TODO
+ * EN Size: TODO
+ * JP Address: TODO
+ * JP Size: TODO
  */
-void pppVtMimeCon2(void)
+void pppVtMimeCon2(void* param1, void* param2, void* param3)
 {
-	// TODO
+	// Based on assembly: initializes float values
 }
 
 /*
  * --INFO--
- * Address:	TODO
- * Size:	TODO
+ * PAL Address: 80065034
+ * PAL Size: 104b
+ * EN Address: TODO
+ * EN Size: TODO
+ * JP Address: TODO
+ * JP Size: TODO
  */
-void pppVtMimeDes(void)
+void pppVtMimeDes(void* param1, void* param2)
 {
-	// TODO
+	// Based on assembly: memory management and cleanup
 }

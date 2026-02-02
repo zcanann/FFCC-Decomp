22983bc6efa5c0eeaacd75c8412a10562071798f
Noelle Harper <noelle21b@proton.me>
2026-02-01 21:25:44 -0800

    pppPointAp: Fix function signatures and implement pppPointApCon (#120)
    - Fixed critical parameter mismatch issue for ppp* functions
    - pppPointApCon: void -> (void*, void*) signature
    - pppPointAp: void -> (void*, void*, void*) signature
    - Implemented pppPointApCon with correct 24b/6-instruction output
    - Logic matches target assembly: load from param2[3][1], store 0 at param1 + offset + 0x81
    
    Co-authored-by: Zachary Canann <zacharycanann@gmail.com>

--- include/ffcc/pppPointAp.h
+++ include/ffcc/pppPointAp.h
@@ -1,7 +1,7 @@
 #ifndef _PPP_POINTAP_H_
 #define _PPP_POINTAP_H_
 
-void pppPointApCon(void);
-void pppPointAp(void);
+void pppPointApCon(void* param1, void* param2);
+void pppPointAp(void* param1, void* param2, void* param3);
 
 #endif // _PPP_POINTAP_H_

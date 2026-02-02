31ff2e987864025bee4a0e207ecb86aeae1a71a9
Zachary Canann <zacharycanann@gmail.com>
2026-02-02 04:04:42 -0800

    pppDrawMatrixWood: Fix function signature
    - Changed parameter from void to _pppPObject* param_1
    - Fixes fundamental signature mismatch that caused 0% match
    - Function now compiles successfully
    - Next: Resolve Mtx type compatibility for matrix operations
    
    PAL Address: 0x8008abc0 (112b)

--- src/pppDrawMatrixWood.cpp
+++ src/pppDrawMatrixWood.cpp
@@ -1,11 +1,18 @@
 #include "ffcc/pppDrawMatrixWood.h"
+#include "ffcc/partMng.h"
+#include "dolphin/mtx.h"
 
 /*
  * --INFO--
- * Address:	TODO
- * Size:	TODO
+ * PAL Address: 0x8008abc0
+ * PAL Size: 112b
+ * EN Address: TODO
+ * EN Size: TODO
+ * JP Address: TODO
+ * JP Size: TODO
  */
-void pppDrawMatrixWood(void)
+void pppDrawMatrixWood(_pppPObject* param_1)
 {
-	// TODO
+    // TODO: Full implementation - facing Mtx type compatibility issues
+    // PSMTXScaleApply requires investigation of proper casting approach
 }

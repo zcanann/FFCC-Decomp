1a6d5ea1a4e25783c07012e90945b9bd30b842ca
Zachary Canann <zacharycanann@gmail.com>
2026-02-01 21:18:10 -0800

    pppPointAp: Fix function signatures and implement pppPointApCon
    - Fixed critical parameter mismatch issue for ppp* functions
    - pppPointApCon: void -> (void*, void*) signature
    - pppPointAp: void -> (void*, void*, void*) signature
    - Implemented pppPointApCon with correct 24b/6-instruction output
    - Logic matches target assembly: load from param2[3][1], store 0 at param1 + offset + 0x81

--- src/pppPointAp.cpp
+++ src/pppPointAp.cpp
@@ -2,20 +2,30 @@
 
 /*
  * --INFO--
- * Address:	TODO
- * Size:	TODO
+ * PAL Address: 0x80060d08
+ * PAL Size: 24b
+ * EN Address: TODO
+ * EN Size: TODO
+ * JP Address: TODO
+ * JP Size: TODO
  */
-void pppPointApCon(void)
+void pppPointApCon(void* param1, void* param2)
 {
-	// TODO
+    int* data = (int*)((int*)param2)[3];
+    int value = data[1];
+    *((char*)param1 + value + 0x81) = 0;
 }
 
 /*
  * --INFO--
- * Address:	TODO
- * Size:	TODO
+ * PAL Address: 0x80060c04
+ * PAL Size: 260b
+ * EN Address: TODO
+ * EN Size: TODO
+ * JP Address: TODO
+ * JP Size: TODO
  */
-void pppPointAp(void)
+void pppPointAp(void* param1, void* param2, void* param3)
 {
-	// TODO
+    // TODO - Complex function with matrix operations and object creation
 }

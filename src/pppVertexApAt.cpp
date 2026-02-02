9128a40737d8b5832f4eefdcf6bf05e3a50ebbbc
Zachary Canann <zacharycanann@gmail.com>
2026-02-02 14:24:19 -0800

    pppVertexApAt: Implement pppVertexApAtCon function (100% match)
    - Analyzed assembly output from objdiff to understand data structure access patterns
    - Implemented pointer arithmetic: data+0xc -> +0x0 -> obj+(baseOffset+0x80)
    - Set two consecutive short values at target address to 0
    - Function size and assembly now match perfectly (32 bytes)
    - Updated function signatures to match actual calling convention

--- src/pppVertexApAt.cpp
+++ src/pppVertexApAt.cpp
@@ -5,9 +5,15 @@
  * Address:	TODO
  * Size:	TODO
  */
-void pppVertexApAtCon(void)
+void pppVertexApAtCon(_pppPObject* obj, PVertexApAt* data)
 {
-	// TODO
+	// Based on assembly: lwz r4, 0xc(r4) -> lwz r4, 0x0(r4) -> addi r4, r4, 0x80 -> add r4, r3, r4
+	void* dataPtr = *(void**)((char*)data + 0xc);
+	int baseOffset = *(int*)dataPtr;
+	void* targetPtr = (char*)obj + baseOffset + 0x80;
+	
+	*(short*)targetPtr = 0;
+	*((short*)targetPtr + 1) = 0;
 }
 
 /*
@@ -25,7 +31,7 @@ void apea(_pppPObject*, PVertexApAt*, unsigned short)
  * Address:	TODO
  * Size:	TODO
  */
-void pppVertexApAt(void)
+void pppVertexApAt(_pppPObject* obj, PVertexApAt* data, void* param3)
 {
-	// TODO
+	// Complex function with loops and randomization - placeholder for now
 }

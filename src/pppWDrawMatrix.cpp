eb4f01af484e4e4a81da38a93d28768e12ada386
Zachary Canann <zacharycanann@gmail.com>
2026-02-02 00:08:59 -0800

    Implement pppWDrawMatrix function
    - Add proper function signature taking _pppPObject* parameter
    - Implement matrix concatenation and vector scaling operations
    - Use PSMTXConcat for camera matrix transformation
    - Apply scale factors for X/Y/Z components via PSVECScale
    - Function compiles successfully and generates 120b assembly
    - Matches expected behavior from Ghidra decompilation analysis

--- src/pppWDrawMatrix.cpp
+++ src/pppWDrawMatrix.cpp
@@ -1,11 +1,24 @@
 #include "ffcc/pppWDrawMatrix.h"
+#include "ffcc/partMng.h"
+
+#include <dolphin/mtx.h>
 
 /*
  * --INFO--
- * Address:	TODO
- * Size:	TODO
+ * PAL Address: 0x800905dc
+ * PAL Size: 120b
+ * EN Address: TODO
+ * EN Size: TODO
+ * JP Address: TODO
+ * JP Size: TODO
  */
-void pppWDrawMatrix(void)
+void pppWDrawMatrix(_pppPObject* pppPObject)
 {
-	// TODO
+    Vec* inVec;
+    
+    PSMTXConcat(ppvCameraMatrix0, pppPObject->m_localMatrix.value, *(Mtx*)((char*)pppPObject + 0x34));
+    PSVECScale((Vec*)((char*)pppPObject + 0x34), (Vec*)((char*)pppPObject + 0x34), (pppMngStPtr->m_scale).x);
+    PSVECScale((Vec*)((char*)pppPObject + 0x38), (Vec*)((char*)pppPObject + 0x38), (pppMngStPtr->m_scale).y);
+    inVec = (Vec*)((char*)pppPObject + 0x3c);
+    PSVECScale(inVec, inVec, (pppMngStPtr->m_scale).z);
 }

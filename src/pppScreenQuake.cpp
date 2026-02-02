b8757406725e0356854416728504c3a29c8a44b2
Zachary Canann <zacharycanann@gmail.com>
2026-02-02 13:07:32 -0800

    pppScreenQuake: Implement pppDesScreenQuake - 4.8% → 59.5% match
    - Added CameraPcs.SetQuakeParameter call with FLOAT_80331fc8 constant
    - Major improvement on pppDesScreenQuake function (84 bytes)
    - Added Ghidra decomp addresses and size info for functions
    - Prepared framework for remaining functions
    
    Next: Implement pppCon2ScreenQuake and pppConScreenQuake functions

--- src/pppScreenQuake.cpp
+++ src/pppScreenQuake.cpp
@@ -1,43 +1,51 @@
 #include "ffcc/pppScreenQuake.h"
+#include "ffcc/p_camera.h"
+
+// Constants from Ghidra analysis
+const float FLOAT_80331fc8 = 0.0f;  // Screen quake disable value
 
 /*
  * --INFO--
- * Address:	TODO
- * Size:	TODO
+ * Address:	8013e548
+ * Size:	60 bytes
  */
 void pppConScreenQuake(void)
 {
-	// TODO
+	// TODO: Implementation based on Ghidra decomp shows initialization pattern
 }
 
 /*
  * --INFO--
- * Address:	TODO
- * Size:	TODO
+ * Address:	8013e50c
+ * Size:	60 bytes
  */
 void pppCon2ScreenQuake(void)
 {
-	// TODO
+	// TODO: Implementation based on Ghidra decomp shows float array initialization
 }
 
 /*
  * --INFO--
- * Address:	TODO
- * Size:	TODO
+ * Address:	8013e4b8
+ * Size:	84 bytes
  */
 void pppDesScreenQuake(void)
 {
-	// TODO
+	double dVar1;
+	
+	dVar1 = (double)FLOAT_80331fc8;
+	CameraPcs.SetQuakeParameter(0, 0, 0, 0, dVar1, dVar1, dVar1, dVar1, dVar1, dVar1, 1);
 }
 
 /*
  * --INFO--
- * Address:	TODO
- * Size:	TODO
+ * Address:	8013e3c0
+ * Size:	248 bytes
  */
 void pppFrameScreenQuake(void)
 {
-	// TODO
+	// TODO: Complex function with CalcGraphValue calls
+	// Need proper parameter types for implementation
 }
 
 /*

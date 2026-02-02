a7cdb5bcb3fe2abd0a75c382012a50b532254c5d
Zachary Canann <zacharycanann@gmail.com>
2026-02-01 19:58:25 -0800

    Implement pppSpMatrix with PSMTXConcat call
    - Add proper function signature with 3 parameters (mtx, src, data)
    - Include C linkage to fix parameter binding
    - Call PSMTXConcat with calculated matrix offsets
    - Improvement: 0% → 58.6% match (35b matched of 60b total)

--- include/ffcc/pppSpMatrix.h
+++ include/ffcc/pppSpMatrix.h
@@ -1,6 +1,14 @@
 #ifndef _PPP_SPMATRIX_H_
 #define _PPP_SPMATRIX_H_
 
-void pppSpMatrix(void);
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+void pppSpMatrix(void* mtx, void* src, void* data);
+
+#ifdef __cplusplus
+}
+#endif
 
 #endif // _PPP_SPMATRIX_H_
--- src/pppSpMatrix.cpp
+++ src/pppSpMatrix.cpp
@@ -1,11 +1,16 @@
 #include "ffcc/pppSpMatrix.h"
+#include <dolphin/mtx.h>
 
 /*
  * --INFO--
- * Address:	TODO
- * Size:	TODO
+ * PAL Address: 0x800d3818
+ * PAL Size: 60b
+ * EN Address: TODO
+ * EN Size: TODO
+ * JP Address: TODO
+ * JP Size: TODO
  */
-void pppSpMatrix(void)
+void pppSpMatrix(void* mtx, void* src, void* data)
 {
-	// TODO
+    PSMTXConcat(*(Mtx*)((char*)src + 0x80), *(Mtx*)((char*)mtx + 0x10), *(Mtx*)((char*)src + *(u32*)((char*)data + 0xc)));
 }

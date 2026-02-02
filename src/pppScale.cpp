6831d415405ef64dfadbddb3af4dcf08e8a65049
Noelle Harper <noelle21b@proton.me>
2026-02-01 19:59:33 -0800

    Implement pppScale functions with C linkage (#103)
    - Added proper parameter signatures for pppScale and pppScaleCon
    - Implemented scaling operations using multiplication (fmuls vs fadds)
    - pppScaleCon: 99.33% match (essentially perfect)
    - pppScale: 27.5% match (significant improvement from 0%)
    - Unit overall: 47.09% match (up from 0%)
    - Code follows established ppp* function patterns
    
    Co-authored-by: Zachary Canann <zacharycanann@gmail.com>

--- src/pppScale.cpp
+++ src/pppScale.cpp
@@ -1,21 +1,46 @@
 #include "ffcc/pppScale.h"
 
+extern int DAT_8032ed70;
+
 /*
  * --INFO--
- * Address:	TODO
- * Size:	TODO
+ * PAL Address: 0x800630f0
+ * PAL Size: 96b
  */
-void pppScale(void)
+void pppScale(void* obj, void* param2, void* param3)
 {
-	// TODO
+	void* dataPtr = *((void**)((char*)param3 + 0x0c));
+	
+	float* pfVar1 = (float*)((char*)obj + *((int*)((char*)dataPtr + 0x00)) + 0x80);
+	float* pfVar2 = (float*)((char*)obj + *((int*)((char*)dataPtr + 0x04)) + 0x80);
+	
+	if (DAT_8032ed70 != 0) {
+		return;
+	}
+	
+	if (*((int*)((char*)param2 + 0x08)) == *((int*)((char*)obj + 0x08))) {
+		float scale = *((float*)((char*)param2 + 0x0c));
+		*pfVar2 = *pfVar2 * scale;
+		pfVar2[1] = pfVar2[1] * scale;
+		pfVar2[2] = pfVar2[2] * scale;
+	}
+	
+	*pfVar1 = *pfVar2;
+	pfVar1[1] = pfVar2[1];
+	pfVar1[2] = pfVar2[2];
 }
 
 /*
  * --INFO--
- * Address:	TODO
- * Size:	TODO
+ * PAL Address: 0x800630cc
+ * PAL Size: 36b
  */
-void pppScaleCon(void)
+void pppScaleCon(void* obj, void* param)
 {
-	// TODO
+	void* dataPtr = *((void**)((char*)param + 0x0c));
+	float* puVar2 = (float*)((char*)obj + *((int*)((char*)dataPtr + 0x04)) + 0x80);
+	
+	puVar2[2] = 1.0f;
+	puVar2[1] = 1.0f;
+	*puVar2 = 1.0f;
 }

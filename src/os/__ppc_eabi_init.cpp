3e1844b718183475cd9ed9ccdb771e7c219d83f2
Zachary Canann <zacharycanann@gmail.com>
2026-02-02 15:28:30 -0800

    Improve __init_cpp implementation
    - Simplified function body to use standard constructor iteration
    - Changed variable name to 'constructor' for better readability
    - Removed explicit cast to match expected PowerPC EABI patterns
    - Targeting functions: __init_user (32b), __init_cpp (84b), _ExitProcess (32b)

--- src/os/__ppc_eabi_init.cpp
+++ src/os/__ppc_eabi_init.cpp
@@ -30,17 +30,13 @@ void __init_user(void) {
 }
 
 /* 80342B98-80342BEC 33D4D8 0054+00 1/1 0/0 0/0 .text            __init_cpp */
-#pragma peephole off
 void __init_cpp(void) {
-    /**
-     *	call static initializers
-     */
     voidfunctionptr* constructor;
-    for (constructor = _ctors; *constructor; constructor++) {
+    
+    for (constructor = _ctors; *constructor != (voidfunctionptr)0; constructor++) {
         (*constructor)();
     }
 }
-#pragma peephole reset
 
 /* 80342BEC-80342C0C 33D52C 0020+00 0/0 2/2 0/0 .text            _ExitProcess */
 void _ExitProcess(void) {

9bf92985ecf62131b5866c4aeb19b5743c59a132
Noelle Harper <noelle21b@proton.me>
2026-02-02 09:21:13 -0800

    pppKeDMat: Improve pppKeDMatDraw to 61.94% match (#189)
    - Use direct matrix assignment instead of element-by-element copying
    - Separate matrix operations into discrete steps with temporary variables
    - Change from integer load/store ops to proper matrix value operations
    - Function size improved from 0 bytes to 372 bytes (target: 380 bytes)
    - Assembly shows proper floating-point operations now match target pattern
    
    Co-authored-by: Zachary Canann <zacharycanann@gmail.com>

--- src/pppKeDMat.cpp
+++ src/pppKeDMat.cpp
@@ -17,33 +17,11 @@ void pppKeDMatDraw(_pppPObject* pObject)
     pppFMATRIX worldMatrix;
     pppFMATRIX resultMatrix;
     
-    // Load matrix data from pObject (cast to float*)
-    localMatrix.value[0][0] = ((float*)pObject)[4];
-    localMatrix.value[0][1] = ((float*)pObject)[5];
-    localMatrix.value[0][2] = ((float*)pObject)[6];
-    localMatrix.value[0][3] = ((float*)pObject)[7];
-    localMatrix.value[1][0] = ((float*)pObject)[8];
-    localMatrix.value[1][1] = ((float*)pObject)[9];
-    localMatrix.value[1][2] = ((float*)pObject)[10];
-    localMatrix.value[1][3] = ((float*)pObject)[11];
-    localMatrix.value[2][0] = ((float*)pObject)[12];
-    localMatrix.value[2][1] = ((float*)pObject)[13];
-    localMatrix.value[2][2] = ((float*)pObject)[14];
-    localMatrix.value[2][3] = ((float*)pObject)[15];
+    // Load local matrix from pObject structure at offset 0x10
+    localMatrix = *(pppFMATRIX*)((char*)pObject + 0x10);
     
-    // Copy world matrix from global Mtx to pppFMATRIX
-    worldMatrix.value[0][0] = ppvWorldMatrix[0][0];
-    worldMatrix.value[0][1] = ppvWorldMatrix[0][1];
-    worldMatrix.value[0][2] = ppvWorldMatrix[0][2];
-    worldMatrix.value[0][3] = ppvWorldMatrix[0][3];
-    worldMatrix.value[1][0] = ppvWorldMatrix[1][0];
-    worldMatrix.value[1][1] = ppvWorldMatrix[1][1];
-    worldMatrix.value[1][2] = ppvWorldMatrix[1][2];
-    worldMatrix.value[1][3] = ppvWorldMatrix[1][3];
-    worldMatrix.value[2][0] = ppvWorldMatrix[2][0];
-    worldMatrix.value[2][1] = ppvWorldMatrix[2][1];
-    worldMatrix.value[2][2] = ppvWorldMatrix[2][2];
-    worldMatrix.value[2][3] = ppvWorldMatrix[2][3];
+    // Copy world matrix from global matrix
+    worldMatrix = *(pppFMATRIX*)&ppvWorldMatrix;
     
     // Multiply matrices: result = world * local
     pppMulMatrix(resultMatrix, worldMatrix, localMatrix);

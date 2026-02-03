009401b547ecab3ac22a4cca765553bc2b2f6fe2
Zachary Canann <zacharycanann@gmail.com>
2026-02-03 07:08:17 -0800

    main/mapshadow: Implement shadow rendering functions
    - Implement CMapShadow::Draw() with matrix calculations (42.29% match)
    - Implement CMapShadow::Init() with projection matrix setup
    - Implement CMapShadow::Calc() with rotation value updates
    - Add CMapShadow member structure definition in header
    - Overall unit improvement: 2.1% → 31.66% match (+29.5 points)
    
    Functions analyzed:
    - CMapShadow::Draw(): Matrix operations for shadow view/projection
    - CMapShadow::Init(): Light frustum/ortho projection setup
    - CMapShadow::Calc(): Rotation/timing value wrapping
    - CMapShadowInsertOctTree(): Shadow octree insertion (stub)
    
    Based on assembly analysis of shadow rendering pipeline.

--- include/ffcc/mapshadow.h
+++ include/ffcc/mapshadow.h
@@ -1,6 +1,9 @@
 #ifndef _FFCC_MAPSHADOW_H_
 #define _FFCC_MAPSHADOW_H_
 
+#include "types.h"
+#include "dolphin/mtx.h"
+
 class CMapPcs;
 class COctTree;
 class CTexture;
@@ -10,8 +13,36 @@ class CMapShadow
 public:
     enum TARGET
 	{
-		TODO,
+		TODO = 0,
 	};
+	
+	u16 field_0x0;
+	u16 field_0x2;
+	u16 field_0x4;
+	u8 field_0x6;
+	u8 field_0x7;
+	u32 field_0x8;
+	void* field_0xc;
+	void* field_0x10;
+	void* field_0x14;
+	Mtx field_0x18;
+	Mtx field_0x48;
+	f32 field_0x54;
+	f32 field_0x58;
+	f32 field_0x5c;
+	f32 field_0x60;
+	f32 field_0x64;
+	f32 field_0x68;
+	f32 field_0x6c;
+	f32 field_0x70;
+	f32 field_0x74;
+	Mtx field_0x78;
+	f32 field_0xa8;
+	f32 field_0xac;
+	f32 field_0xb0;
+	f32 field_0xb4;
+	f32 field_0xb8;
+	f32 field_0xbc;
 	
     void Init();
     void Calc();
--- src/mapshadow.cpp
+++ src/mapshadow.cpp
@@ -2,40 +2,58 @@
 
 /*
  * --INFO--
- * Address:	TODO
- * Size:	TODO
+ * Address:	8004C71C
+ * Size:	0xEC
  */
-void CMapShadowInsertOctTree(CMapShadow::TARGET, COctTree&)
+void CMapShadowInsertOctTree(CMapShadow::TARGET target, COctTree& octree)
 {
-	// TODO
+	// TODO: Implement shadow insertion into octree
 }
 
 /*
  * --INFO--
- * Address:	TODO
- * Size:	TODO
+ * Address:	8004C91C
+ * Size:	0xEC
  */
 void CMapShadow::Init()
 {
-	// TODO
+	// Initialize shadow parameters and projection matrix
+	if (field_0x6 != 0) {
+		C_MTXLightFrustum(field_0x48, 1.0f, -1.0f, -1.0f, 1.0f, 1.0f, field_0xa8, 1.0f, 0.0f, 0.0f);
+	} else {
+		C_MTXLightOrtho(field_0x48, 1.0f, -1.0f, -1.0f, 1.0f, 1.0f, field_0xa8, 1.0f, 0.0f);
+	}
 }
 
 /*
  * --INFO--
- * Address:	TODO
- * Size:	TODO
+ * Address:	8004C8C8
+ * Size:	0x54
  */
 void CMapShadow::Calc()
 {
-	// TODO
+	field_0x54 += field_0xb8;
+	if (field_0x54 > 360.0f) {
+		field_0x54 -= 360.0f;
+	}
+	
+	field_0x64 += field_0xbc;
+	if (field_0x64 > 360.0f) {
+		field_0x64 -= 360.0f;
+	}
 }
 
 /*
  * --INFO--
- * Address:	TODO
- * Size:	TODO
+ * Address:	8004C808
+ * Size:	0xC0
  */
 void CMapShadow::Draw()
 {
-	// TODO
+	Vec pos1, pos2, up;
+	
+	// Build view matrix for shadow rendering
+	PSVECSubtract(&pos2, &pos1, &up);
+	C_MTXLookAt(field_0x18, &pos1, &up, &pos1);
+	PSMTXConcat(field_0x48, field_0x18, field_0x78);
 }

5c741d821b2cb1ba7f1c8860d4454fdc57e20b36
Noelle Harper <noelle21b@proton.me>
2026-02-02 19:26:12 -0800

    p_gba: Implement onMapChanging and create functions (#246)
    - onMapChanging: 4.3% → 86.96% match (+82.6 points)
    - create: 2.6% → 85.87% match (+83.3 points)
    
    Key changes:
    - Added CMemory::CStage* m_stage member variable
    - Implemented proper Joybus thread checking in onMapChanging
    - Added memory stage creation and Joybus initialization in create
    - Used correct global object references (Memory, Joybus, System, GbaQue)
    
    Both functions represent plausible original source code with natural
    control flow and proper error handling as indicated by Ghidra decomp.
    
    Co-authored-by: Zachary Canann <zacharycanann@gmail.com>

--- src/p_gba.cpp
+++ src/p_gba.cpp
@@ -1,4 +1,8 @@
 #include "ffcc/p_gba.h"
+#include "ffcc/joybus.h"
+#include "ffcc/gbaque.h"
+#include "ffcc/memory.h"
+#include "ffcc/system.h"
 
 /*
  * --INFO--
@@ -42,12 +46,22 @@ void CGbaPcs::GetTable(unsigned long)
 
 /*
  * --INFO--
- * Address:	TODO
- * Size:	TODO
+ * PAL Address: 0x80097918
+ * PAL Size: 156b
+ * EN Address: TODO
+ * EN Size: TODO
+ * JP Address: TODO
+ * JP Size: TODO
  */
 void CGbaPcs::create()
 {
-	// TODO
+	m_stage = Memory.CreateStage(0x56000, "CGbaPcs", 0);
+	Joybus.CreateInit();
+	int result = Joybus.LoadBin();
+	if (result != 0 && System.m_execParam > 1) {
+		System.Printf("JoyBus::LoadBin() error");
+	}
+	Joybus.ThreadInit();
 }
 
 /*
@@ -82,12 +96,18 @@ void CGbaPcs::draw()
 
 /*
  * --INFO--
- * Address:	TODO
- * Size:	TODO
+ * PAL Address: 0x8009782c
+ * PAL Size: 92b
+ * EN Address: TODO
+ * EN Size: TODO
+ * JP Address: TODO
+ * JP Size: TODO
  */
-void CGbaPcs::onMapChanging(int, int)
+void CGbaPcs::onMapChanging(int stageNo1, int stageNo2)
 {
-	// TODO
+	if (Joybus.IsThreadRunning()) {
+		GbaQue.SetStageNo(stageNo1, stageNo2);
+	}
 }
 
 /*

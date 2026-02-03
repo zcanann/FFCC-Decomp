3bc1e17c4b558ad08385b5ae02e14d43037f17fc
Zachary Canann <zacharycanann@gmail.com>
2026-02-03 02:08:05 -0800

    wind: Implement CWind::ClearAll and CWind::Draw from Ghidra decomp
    - ClearAll: Complete implementation clearing object arrays and setting defaults
    - Draw: Implement wind particle sphere rendering with proper GX state setup
    - Added WindObject structure definition with 100-byte stride
    - Functions based on Ghidra decompilation analysis

--- include/ffcc/wind.h
+++ include/ffcc/wind.h
@@ -1,7 +1,21 @@
 #ifndef _FFCC_PPP_WIND_H_
 #define _FFCC_PPP_WIND_H_
 
-struct Vec;
+#include "ffcc/types.h"
+
+struct WindObject
+{
+    u8 flags;
+    u8 pad1[3];
+    Vec pos;
+    u8 pad2[16];
+    s32 type;
+    u8 pad3[16];
+    f32 radius;
+    u8 pad4[4];
+    f32 intensity;
+    u8 pad5[56]; // Total size: 100 bytes
+};
 
 class CWind
 {
@@ -16,6 +30,9 @@ public:
     void AddDiffuse(const Vec*, float, float, float);
     void AddSphere(const Vec*, float, float, int);
     void ChangePower(int, float);
+
+private:
+    WindObject m_objects[32];
 };
 
 #endif // _FFCC_PPP_WIND_H_
--- src/wind.cpp
+++ src/wind.cpp
@@ -1,13 +1,25 @@
 #include "ffcc/wind.h"
 
+#include "ffcc/graphic.h"
+#include "ffcc/color.h"
+#include "ffcc/vector.h"
+#include "ffcc/camera.h"
+
+extern CGraphic Graphic;
+extern CCameraPcs CameraPcs;
+extern u32 CFlat; // Temporary - needs proper declaration
+
 /*
  * --INFO--
- * Address:	TODO
- * Size:	TODO
+ * Address:	800da05c
+ * Size:	88
  */
 void CWind::ClearAll()
 {
-	// TODO
+	memset(this, 0, 0xc80);
+	*(u32*)((char*)this + 0xc80) = 1;
+	memset((char*)this + 0xc84, 0, 0x7000);
+	*(u32*)((char*)this + 0x7c84) = 10000000;
 }
 
 /*
@@ -22,12 +34,52 @@ void CWind::Frame()
 
 /*
  * --INFO--
- * Address:	TODO
- * Size:	TODO
+ * Address:	800d9b2c
+ * Size:	564
  */
 void CWind::Draw()
 {
-	// TODO
+	Mtx cameraMatrix;
+	PSMTXCopy(&CameraPcs.m_cameraMatrix, &cameraMatrix);
+	
+	GXSetBlendMode(GX_BM_BLEND, GX_BL_SRCALPHA, GX_BL_INVSRCALPHA, GX_LO_NOOP);
+	GXSetZCompLoc(GX_FALSE);
+	GXSetAlphaCompare(GX_LEQUAL, 1, GX_AOP_AND, GX_ALWAYS, 0);
+	GXSetZMode(GX_TRUE, GX_LEQUAL, GX_TRUE);
+	GXSetCullMode(GX_CULL_BACK);
+	
+	GXSetNumTevStages(1);
+	GXSetTevOp(GX_TEVSTAGE0, GX_MODULATE);
+	GXSetTevOrder(GX_TEVSTAGE0, GX_TEXCOORDNULL, GX_TEXMAP_NULL, GX_COLOR0A0);
+	
+	GXSetNumChans(1);
+	GXSetChanCtrl(GX_COLOR0A0, GX_FALSE, GX_SRC_REG, GX_SRC_VTX, GX_LIGHT_NULL, GX_DF_CLAMP, GX_AF_SPEC);
+	GXSetChanCtrl(GX_COLOR1A1, GX_FALSE, GX_SRC_REG, GX_SRC_VTX, GX_LIGHT_NULL, GX_DF_CLAMP, GX_AF_NONE);
+	
+	GXClearVtxDesc();
+	GXSetVtxDesc(GX_VA_POS, GX_DIRECT);
+	GXSetVtxAttrFmt(GX_VTXFMT0, GX_VA_POS, GX_POS_XYZ, GX_F32, 0);
+	
+	if (CFlat._4764_4_ & 0x800000) {
+		for (int i = 0; i < 32; i++) {
+			WindObject* obj = &m_objects[i];
+			if (obj->flags & 0x80) {
+				CColor color;
+				CVector position;
+				
+				if (obj->type == 1) {
+					color = CColor(0xff, 0xff, 0x00, 0xff);
+					position = CVector(obj->pos.x, 100.0f, obj->pos.z);
+					Graphic.DrawSphere(&cameraMatrix, &position, obj->radius, &color);
+				} else {
+					int alpha = (int)(255.0f * (1.0f - obj->intensity));
+					color = CColor(0xff, 0xff, 0x80, alpha);
+					position = CVector(obj->pos.x, 100.0f, obj->pos.z);
+					Graphic.DrawSphere(&cameraMatrix, &position, obj->radius, &color);
+				}
+			}
+		}
+	}
 }
 
 /*

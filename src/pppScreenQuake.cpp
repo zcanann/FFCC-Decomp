4cd61efe2124205469159fedfdad26be7d5aa35f
Zachary Canann <zacharycanann@gmail.com>
2026-02-03 09:19:33 -0800

    Improve pppScreenQuake: pppDesScreenQuake 36.0% → 44.6%
    - Fix pppDesScreenQuake to use double variable matching Ghidra decomp pattern
    - Improve CalcGraphValue function calls in pppFrameScreenQuake to match Ghidra parameter order
    - Fix offset calculation for data access using (&field0_0x0 + 2) pattern
    - Match SetQuakeParameter call structure with proper type casting
    - Add partMng.h include for _pppPObject type
    
    Overall unit progress: 46.4% → 48.0%

--- src/pppScreenQuake.cpp
+++ src/pppScreenQuake.cpp
@@ -1,10 +1,11 @@
 #include "ffcc/pppScreenQuake.h"
 #include "ffcc/p_camera.h"
+#include "ffcc/partMng.h"
 
 extern float FLOAT_80331fc8;
 extern int DAT_8032ed70;
 
-void CalcGraphValue(float param1, pppScreenQuake *param2, int param3, float *param4, float *param5, float *param6, float *param7, float *param8);
+void CalcGraphValue(float param1, _pppPObject *param2, int param3, float *param4, float *param5, float *param6, float *param7, float *param8);
 
 /*
  * --INFO--
@@ -67,9 +68,10 @@ void pppCon2ScreenQuake(pppScreenQuake *quake, UnkC *param2)
  */
 void pppDesScreenQuake(void)
 {
-	float val = FLOAT_80331fc8;
-	CameraPcs.SetQuakeParameter((int)val, (int)val, (short)val, (short)val, 
-	                            val, val, val, val, val, val, 1);
+	double dVar1 = (double)FLOAT_80331fc8;
+	CameraPcs.SetQuakeParameter((int)dVar1, (int)dVar1, (short)dVar1, (short)dVar1, 
+	                            (float)dVar1, (float)dVar1, (float)dVar1, (float)dVar1, 
+	                            (float)dVar1, (float)dVar1, 1);
 }
 
 /*
@@ -84,20 +86,26 @@ void pppDesScreenQuake(void)
 void pppFrameScreenQuake(pppScreenQuake *quake, UnkB *param2, UnkC *param3)
 {
 	if (DAT_8032ed70 == 0) {
-		float *data = (float *)((char *)&quake->field0_0x0 + 128 + *param3->m_serializedDataOffsets);
+		float *value = (float *)((int)(&quake->field0_0x0 + 2) + *param3->m_serializedDataOffsets);
 		
-		CalcGraphValue((float)param2->m_dataValIndex, quake, param2->m_graphId, 
-		               &data[0], &data[1], &data[2], &param2->m_initWOrk, &param2->m_stepValue);
+		CalcGraphValue((float)param2->m_dataValIndex, (_pppPObject*)&quake->field0_0x0, param2->m_graphId, 
+		               value, value + 1, value + 2, &param2->m_initWOrk, &param2->m_stepValue);
 		               
-		CalcGraphValue((float)param2->m_arg3, quake, param2->m_graphId,
-		               &data[3], &data[4], &data[5], &param2->m_payload[0], &param2->m_payload[1]);
+		CalcGraphValue((float)param2->m_arg3, (_pppPObject*)&quake->field0_0x0, param2->m_graphId,
+		               value + 3, value + 4, value + 5, param2->m_payload, param2->m_payload + 4);
 		               
-		CalcGraphValue(param2->m_payload[2], quake, param2->m_graphId,
-		               &data[6], &data[7], &data[8], &param2->m_payload[3], &param2->m_payload[4]);
+		CalcGraphValue(*(float*)(param2->m_payload + 8), (_pppPObject*)&quake->field0_0x0, param2->m_graphId,
+		               value + 6, value + 7, value + 8, param2->m_payload + 0xc, param2->m_payload + 0x10);
 		               
-		CameraPcs.SetQuakeParameter((int)data[0], (int)data[3], (short)data[6], (short)param2->m_payload[5],
-		                            param2->m_payload[6], param2->m_payload[7], param2->m_payload[8], param2->m_payload[9],
-		                            param2->m_payload[10], param2->m_payload[11], 1);
+		CameraPcs.SetQuakeParameter((int)*value, (int)value[3], (short)value[6], 
+		                            (short)*(float*)(param2->m_payload + 0x14),
+		                            *(float*)(param2->m_payload + 0x18),
+		                            *(float*)(param2->m_payload + 0x1c),
+		                            *(float*)(param2->m_payload + 0x20),
+		                            *(float*)(param2->m_payload + 0x24),
+		                            *(float*)(param2->m_payload + 0x28),
+		                            *(float*)(param2->m_payload + 0x2c),
+		                            1);
 	}
 }
 

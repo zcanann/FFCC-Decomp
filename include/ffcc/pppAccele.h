014dc7f306c5cea1c837529e09dfbdae67043be9
Zachary Canann <zacharycanann@gmail.com>
2026-02-02 20:22:27 -0800

    Fix c linkage

--- include/ffcc/pppAccele.h
+++ include/ffcc/pppAccele.h
@@ -1,7 +1,15 @@
 #ifndef _FFCC_PPPACCELE_H_
 #define _FFCC_PPPACCELE_H_
 
+#ifdef __cplusplus
+extern "C" {
+#endif
+
 void pppAccele(void* obj, void* param2, void* param3);
 void pppAcceleCon(void* obj, void* param);
+
+#ifdef __cplusplus
+}
+#endif
 
 #endif
--- include/ffcc/pppAlignmentScale.h
+++ include/ffcc/pppAlignmentScale.h
@@ -4,6 +4,7 @@
 #ifdef __cplusplus
 extern "C" {
 #endif
+
 void pppConstructAlignmentScale(void);
 void pppFrameAlignmentScale(void);
 

65e868fd251e9f8610663d76d8d792e0033c7652
Noelle Harper <noelle21b@proton.me>
2026-02-02 19:20:06 -0800

    maplight: Implement GetLightHolder function to 100% match (#245)
    - Added proper _GXColor mColor and Vec mVec member variables to CMapLightHolder
    - Implemented function with null checks and member variable copying
    - Includes proper headers for dolphin/gx/GXStruct.h and dolphin/mtx.h
    - Function now perfectly matches original assembly (5.3% -> 100.0%)
    
    Co-authored-by: Zachary Canann <zacharycanann@gmail.com>

--- src/maplight.cpp
+++ src/maplight.cpp
@@ -2,10 +2,19 @@
 
 /*
  * --INFO--
- * Address:	TODO
- * Size:	TODO
+ * PAL Address: 0x8004b1d0
+ * PAL Size: 76b
+ * EN Address: TODO
+ * EN Size: TODO
+ * JP Address: TODO
+ * JP Size: TODO
  */
-void CMapLightHolder::GetLightHolder(_GXColor*, Vec*)
+void CMapLightHolder::GetLightHolder(_GXColor* color, Vec* vec)
 {
-	// TODO
+    if (color != nullptr) {
+        *color = mColor;
+    }
+    if (vec != nullptr) {
+        *vec = mVec;
+    }
 }

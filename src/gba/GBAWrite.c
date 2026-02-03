e1b30fa70655032208096eb898921094277a661d
Zachary Canann <zacharycanann@gmail.com>
2026-02-03 02:27:19 -0800

    Improve GBAWrite functions: remove convenience variables
    - Remove GBAControl* abstraction in WriteProc, GBAWriteAsync, GBAWrite
    - Access __GBA[chan] directly to match Ghidra decompilation patterns
    - Add separate ret variable declaration in GBAWrite
    - Changes represent plausible original low-level source style

--- src/gba/GBAWrite.c
+++ src/gba/GBAWrite.c
@@ -2,31 +2,28 @@
 #include "string.h"
 
 static void WriteProc(s32 chan) {
-    GBAControl* gba = &__GBA[chan];
-
-    if (gba->ret != 0) {
+    if (__GBA[chan].ret != 0) {
         return;
     }
 
-    gba->status[0] = gba->input[0] & GBA_JSTAT_MASK;
+    __GBA[chan].status[0] = __GBA[chan].input[0] & GBA_JSTAT_MASK;
 }
 
 s32 GBAWriteAsync(s32 chan, u8* src, u8* status, GBACallback callback) {
-    GBAControl* gba = &__GBA[chan];
-
-    if (gba->callback != NULL) {
+    if (__GBA[chan].callback != NULL) {
         return GBA_BUSY;
     }
-    gba->output[0] = 0x15;
-    memcpy(&gba->output[1], src, 4);
-    gba->ptr = src;
-    gba->status = status;
-    gba->callback = callback;
+    __GBA[chan].output[0] = 0x15;
+    memcpy(&__GBA[chan].output[1], src, 4);
+    __GBA[chan].ptr = src;
+    __GBA[chan].status = status;
+    __GBA[chan].callback = callback;
     return __GBATransfer(chan, 5, 1, WriteProc);
 }
 
 s32 GBAWrite(s32 chan, u8* src, u8* status) {
-    s32 ret = GBAWriteAsync(chan, src, status, __GBASyncCallback);
+    s32 ret;
+    ret = GBAWriteAsync(chan, src, status, __GBASyncCallback);
     if (ret != GBA_READY) {
         return ret;
     }

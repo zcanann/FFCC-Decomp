cdb492f179fbe84f7a25a7892eaf1b385b5b5863
Zachary Canann <zacharycanann@gmail.com>
2026-02-03 22:16:51 -0800

    Ref function info

--- src/ref.cpp
+++ src/ref.cpp
@@ -2,10 +2,27 @@
 
 /*
  * --INFO--
- * Address:	TODO
- * Size:	TODO
+ * PAL Address: 0x80043d58
+ * PAL Size: 24b
+ * EN Address: TODO
+ * EN Size: TODO
+ * JP Address: TODO
+ * JP Size: TODO
  */
 CRef::CRef()
 {
 	this->refCount = 1;
+}
+
+/*
+ * --INFO--
+ * PAL Address: 0x80043d10
+ * PAL Size: 72b
+ * EN Address: TODO
+ * EN Size: TODO
+ * JP Address: TODO
+ * JP Size: TODO
+ */
+CRef::~CRef()
+{
 }

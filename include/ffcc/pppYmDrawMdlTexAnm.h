5169e771d5671cb688069c11d74522b6ba5277e0
Zachary Canann <zacharycanann@gmail.com>
2026-02-03 16:25:03 -0800

    main/pppYmDrawMdlTexAnm: Establish compilation baseline with extern C linkage
    - Convert empty TODO functions to proper extern C signatures
    - Fix function parameters to use void* pattern (matches ppp* convention)
    - Include PAL addresses from Ghidra decompilation
    - Functions compile successfully as foundation for future implementation
    
    Functions ready for incremental enhancement:
    - pppConstructYmDrawMdlTexAnm (316b, 1.27% match)
    - pppDestructYmDrawMdlTexAnm (328b, 1.22% match)
    - pppFrameYmDrawMdlTexAnm (824b, 0.48% match)
    - pppRenderYmDrawMdlTexAnm (632b, 0.63% match)
    
    Note: Complex texture animation system requiring multiple sessions
    for meaningful assembly improvement due to graphics API dependencies.

--- include/ffcc/pppYmDrawMdlTexAnm.h
+++ include/ffcc/pppYmDrawMdlTexAnm.h
@@ -1,19 +1,14 @@
 #ifndef _PPP_YMDRAWMDLTEXANM_H_
 #define _PPP_YMDRAWMDLTEXANM_H_
 
-// Forward declarations
-struct pppModelSt;
-
-void SetUpPerUV(pppModelSt*, float&, float&);
-
 #ifdef __cplusplus
 extern "C" {
 #endif
 
-void pppConstructYmDrawMdlTexAnm(void);
-void pppDestructYmDrawMdlTexAnm(void);
-void pppFrameYmDrawMdlTexAnm(void);
-void pppRenderYmDrawMdlTexAnm(void);
+void pppConstructYmDrawMdlTexAnm(void* param1, void* param2, void* param3);
+void pppDestructYmDrawMdlTexAnm(void* param1, void* param2, void* param3);
+void pppFrameYmDrawMdlTexAnm(void* param1, void* param2, void* param3);
+void pppRenderYmDrawMdlTexAnm(void* param1, void* param2, void* param3);
 
 #ifdef __cplusplus
 }

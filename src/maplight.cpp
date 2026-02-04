4762c8442355ab54a35cde1f8a4059519bfdb8e9
Zachary Canann <zacharycanann@gmail.com>
2026-02-03 20:05:36 -0800

    Fix CMapLightHolder::GetLightHolder implementation
    - Analyzed assembly and Ghidra decomp for proper implementation
    - Function copies color bytes individually (4 bytes at offsets 0-3)
    - Function copies Vec floats individually (3 floats at offsets 4-15)
    - Matches 8004b1d0_GetLightHolder Ghidra decompilation pattern
    - Target: main/maplight (76 bytes, 0% -> expected improvement)

--- src/maplight.cpp
+++ src/maplight.cpp
@@ -12,9 +12,16 @@
 void CMapLightHolder::GetLightHolder(_GXColor* color, Vec* vec)
 {
     if (color != nullptr) {
-        *color = mColor;
+        // Copy color bytes individually (matching Ghidra decomp)
+        ((unsigned char*)color)[0] = ((unsigned char*)this)[0];
+        ((unsigned char*)color)[1] = ((unsigned char*)this)[1];
+        ((unsigned char*)color)[2] = ((unsigned char*)this)[2];
+        ((unsigned char*)color)[3] = ((unsigned char*)this)[3];
     }
     if (vec != nullptr) {
-        *vec = mVec;
+        // Copy Vec as 3 float values (matching Ghidra decomp)  
+        ((float*)vec)[0] = *(float*)((char*)this + 4);
+        ((float*)vec)[1] = *(float*)((char*)this + 8);
+        ((float*)vec)[2] = *(float*)((char*)this + 0xc);
     }
 }

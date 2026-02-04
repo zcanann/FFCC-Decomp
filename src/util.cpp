096861261987412b4abe369aec71ff80ed2f3cec
Noelle Harper <noelle21b@proton.me>
2026-02-03 21:33:08 -0800

    Implement main/util rendering and vector functions (#402)
    - GetDirectVector: Implemented vector cross product calculation with up vector constant
    - RenderQuad: Implemented single texture coordinate quad rendering
    - RenderQuadTex2: Implemented dual texture coordinate quad rendering
    - Added Vec2d struct definition for 2D float vectors
    
    Co-authored-by: Zachary Canann <zacharycanann@gmail.com>

--- src/util.cpp
+++ src/util.cpp
@@ -1,5 +1,10 @@
 #include "ffcc/util.h"
 
+// Vec2d definition 
+struct Vec2d {
+	float x, y;
+};
+
 /*
  * --INFO--
  * Address:	TODO
@@ -142,22 +147,110 @@ void CUtil::RenderQuadNoTex(Vec&, Vec&, _GXColor)
 
 /*
  * --INFO--
- * Address:	TODO
- * Size:	TODO
+ * PAL Address: 0x80024608
+ * PAL Size: 320b
+ * EN Address: TODO
+ * EN Size: TODO
+ * JP Address: TODO
+ * JP Size: TODO
  */
-void CUtil::RenderQuad(Vec&, Vec&, _GXColor, Vec2d*, Vec2d*)
+void CUtil::RenderQuad(Vec& pos1, Vec& pos2, _GXColor color, Vec2d* uv1, Vec2d* uv2)
 {
-	// TODO
+	float u1, v1, u2, v2;
+	
+	// Default UV coordinates if null
+	extern float lbl_8032f888;  // 0.0f
+	extern float lbl_8032f88c;  // 1.0f
+	
+	if (uv1 == NULL || uv2 == NULL) {
+		u1 = lbl_8032f888;  // 0.0f
+		v1 = lbl_8032f888;  // 0.0f  
+		u2 = lbl_8032f88c;  // 1.0f
+		v2 = lbl_8032f88c;  // 1.0f
+	} else {
+		u1 = uv1->x;
+		v1 = uv1->y;
+		u2 = uv2->x;
+		v2 = uv2->y;
+	}
+	
+	GXBegin(GX_TRIANGLESTRIP, GX_VTXFMT7, 4);
+	
+	// Vertex 1: pos1.x, pos1.y, pos1.z, color, u1, v1
+	GXPosition3f32(pos1.x, pos1.y, pos1.z);
+	GXColor1u32(*(u32*)&color);
+	GXTexCoord2f32(u1, v1);
+	
+	// Vertex 2: pos2.x, pos1.y, pos1.z, color, u2, v1  
+	GXPosition3f32(pos2.x, pos1.y, pos1.z);
+	GXColor1u32(*(u32*)&color);
+	GXTexCoord2f32(u2, v1);
+	
+	// Vertex 3: pos2.x, pos2.y, pos1.z, color, u2, v2
+	GXPosition3f32(pos2.x, pos2.y, pos1.z);
+	GXColor1u32(*(u32*)&color);
+	GXTexCoord2f32(u2, v2);
+	
+	// Vertex 4: pos1.x, pos2.y, pos1.z, color, u1, v2
+	GXPosition3f32(pos1.x, pos2.y, pos1.z);
+	GXColor1u32(*(u32*)&color);
+	GXTexCoord2f32(u1, v2);
 }
 
 /*
  * --INFO--
- * Address:	TODO
- * Size:	TODO
+ * PAL Address: 0x800244a8
+ * PAL Size: 352b
+ * EN Address: TODO
+ * EN Size: TODO
+ * JP Address: TODO
+ * JP Size: TODO
  */
-void CUtil::RenderQuadTex2(Vec&, Vec&, _GXColor, Vec2d*, Vec2d*)
+void CUtil::RenderQuadTex2(Vec& pos1, Vec& pos2, _GXColor color, Vec2d* uv1, Vec2d* uv2)
 {
-	// TODO
+	float u1, v1, u2, v2;
+	
+	// Default UV coordinates if null
+	extern float lbl_8032f888;  // 0.0f
+	extern float lbl_8032f88c;  // 1.0f
+	
+	if (uv1 == NULL || uv2 == NULL) {
+		u1 = lbl_8032f888;  // 0.0f
+		v1 = lbl_8032f888;  // 0.0f
+		u2 = lbl_8032f88c;  // 1.0f
+		v2 = lbl_8032f88c;  // 1.0f
+	} else {
+		u1 = uv1->x;
+		v1 = uv1->y;
+		u2 = uv2->x;
+		v2 = uv2->y;
+	}
+	
+	GXBegin(GX_TRIANGLESTRIP, GX_VTXFMT7, 4);
+	
+	// Vertex 1: pos1.x, pos1.y, pos1.z, color, u1,v1, u1,v1
+	GXPosition3f32(pos1.x, pos1.y, pos1.z);
+	GXColor1u32(*(u32*)&color);
+	GXTexCoord2f32(u1, v1);  // TEX0
+	GXTexCoord2f32(u1, v1);  // TEX1
+	
+	// Vertex 2: pos2.x, pos1.y, pos1.z, color, u2,v1, u2,v1
+	GXPosition3f32(pos2.x, pos1.y, pos1.z);
+	GXColor1u32(*(u32*)&color);
+	GXTexCoord2f32(u2, v1);  // TEX0
+	GXTexCoord2f32(u2, v1);  // TEX1
+	
+	// Vertex 3: pos2.x, pos2.y, pos1.z, color, u2,v2, u2,v2
+	GXPosition3f32(pos2.x, pos2.y, pos1.z);
+	GXColor1u32(*(u32*)&color);
+	GXTexCoord2f32(u2, v2);  // TEX0
+	GXTexCoord2f32(u2, v2);  // TEX1
+	
+	// Vertex 4: pos1.x, pos2.y, pos1.z, color, u1,v2, u1,v2
+	GXPosition3f32(pos1.x, pos2.y, pos1.z);
+	GXColor1u32(*(u32*)&color);
+	GXTexCoord2f32(u1, v2);  // TEX0
+	GXTexCoord2f32(u1, v2);  // TEX1
 }
 
 /*
@@ -302,12 +395,30 @@ void CUtil::GetNumPolygonFromDL(void*, unsigned long)
 
 /*
  * --INFO--
- * Address:	TODO
- * Size:	TODO
+ * PAL Address: 0x80022780
+ * PAL Size: 152b
+ * EN Address: TODO
+ * EN Size: TODO
+ * JP Address: TODO
+ * JP Size: TODO
  */
-void CUtil::GetDirectVector(Vec*, Vec*, Vec&)
+void CUtil::GetDirectVector(Vec* param_2, Vec* param_3, Vec& param_4)
 {
-	// TODO
+	Vec local_vec;
+	
+	// Use up vector constant
+	extern float lbl_801d7070;  // x component
+	extern float lbl_801d7074;  // y component  
+	extern float lbl_801d7078;  // z component
+	
+	local_vec.x = lbl_801d7070;
+	local_vec.y = lbl_801d7074;
+	local_vec.z = lbl_801d7078;
+	
+	PSVECCrossProduct(&param_4, &local_vec, param_2);
+	PSVECNormalize(param_2, param_2);
+	PSVECCrossProduct(param_2, &param_4, param_3);
+	PSVECNormalize(param_3, param_3);
 }
 
 /*

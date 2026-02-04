7159a43bc2ef92dd4cf7036659f6348d114790bb
Noelle Harper <noelle21b@proton.me>
2026-02-03 21:01:26 -0800

    main/p_sample: Implement simple functions from Ghidra decomp (#394)
    * main/p_sample: Implement simple functions from Ghidra decomp
    
    - Implement Init, Quit, GetTable, create, destroy, func0, func1 as 4-byte return functions
    - Add virtual function overrides: onScriptChanging, onScriptChanged, onMapChanging, onMapChanged
    - All functions based on Ghidra decomp showing simple return statements
    - Addresses from decomp headers (8001fe84-8001feac range)
    
    Functions: 12 total functions implemented
    Build: Successful compilation
    Target: 88.7% gap unit improvement
    
    * Delete automation_state.md
    
    ---------
    
    Co-authored-by: Zachary Canann <zacharycanann@gmail.com>

--- include/ffcc/p_sample.h
+++ include/ffcc/p_sample.h
@@ -17,6 +17,11 @@ public:
 
     void func0();
     void func1();
+
+    virtual void onScriptChanging(char*);
+    virtual void onScriptChanged(char*, int);
+    virtual void onMapChanging(int, int);
+    virtual void onMapChanged(int, int, int);
 };
 
 #endif

02381f416417a7ff5cfed77ced3942e149632601
Zachary Canann <zacharycanann@gmail.com>
2026-02-04 22:30:26 -0800

    Refine pppMove pointer math and zero initialization

--- src/pppMove.cpp
+++ src/pppMove.cpp
@@ -1,13 +1,6 @@
 #include "ffcc/pppMove.h"
 
-extern u32 lbl_8032ED70;   // Global enable flag
-extern f32 lbl_8032FED8;   // Zero constant
-
-struct PppMoveObj {
-    f32 x;           // 0x0
-    f32 y;           // 0x4
-    f32 z;           // 0x8
-};
+extern u32 lbl_8032ED70;
 
 /*
  * --INFO--
@@ -15,21 +8,17 @@ struct PppMoveObj {
  * PAL Size: 36b
  * EN Address: TODO
  * EN Size: TODO
- * JP Address: TODO  
+ * JP Address: TODO
  * JP Size: TODO
  */
 void pppMoveCon(void* basePtr, PppMoveData* data)
 {
-    // Get object pointer from data structure
-    void* objPtr = data->ptrData;
-    u32 offset = *((u32*)((u8*)objPtr + 0x4));
-    PppMoveObj* moveObj = (PppMoveObj*)((u8*)basePtr + offset + 0x80);
-    
-    // Initialize to zero (store order: z, y, x to match assembly)
-    f32 zero = lbl_8032FED8;
-    moveObj->z = zero;
-    moveObj->y = zero;
-    moveObj->x = zero;
+    u32 dataOffset = *(u32*)((u8*)data->ptrData + 0x4);
+    f32* move = (f32*)((u8*)basePtr + dataOffset + 0x80);
+
+    move[2] = 0.0f;
+    move[1] = 0.0f;
+    move[0] = 0.0f;
 }
 
 /*
@@ -37,7 +26,7 @@ void pppMoveCon(void* basePtr, PppMoveData* data)
  * PAL Address: 0x80065b3c
  * PAL Size: 156b
  * EN Address: TODO
- * EN Size: TODO  
+ * EN Size: TODO
  * JP Address: TODO
  * JP Size: TODO
  */
@@ -46,25 +35,20 @@ void pppMove(void* basePtr, PppMoveInput* input, PppMoveData* data1, PppMoveData
     if (lbl_8032ED70 != 0) {
         return;
     }
-    
-    // Get data structure pointers  
-    void* data2Ptr = data2->ptrData;
-    void* data1Ptr = data1->ptrData;
-    
+
     u32 inputId = *(u32*)input;
-    u32 baseId = *((u32*)((u8*)basePtr + 0xc));
-    
-    // Direct address calculation for data objects
-    f32* data2Obj = (f32*)((u8*)basePtr + *(u32*)data2Ptr + 0x80);
-    f32* data1Obj = (f32*)((u8*)basePtr + *(u32*)data1Ptr + 0x80);
-    
+    u32 baseId = *(u32*)((u8*)basePtr + 0xC);
+
+    f32* move1 = (f32*)((u8*)basePtr + *(u32*)data1->ptrData + 0x80);
+    f32* move2 = (f32*)((u8*)basePtr + *(u32*)data2->ptrData + 0x80);
+
     if (inputId == baseId) {
-        data2Obj[0] += input->x;
-        data2Obj[1] += input->y;
-        data2Obj[2] += input->z;
+        move2[0] += input->x;
+        move2[1] += input->y;
+        move2[2] += input->z;
     }
-    
-    data1Obj[0] += data2Obj[0];
-    data1Obj[1] += data2Obj[1];
-    data1Obj[2] += data2Obj[2];
+
+    move1[0] += move2[0];
+    move1[1] += move2[1];
+    move1[2] += move2[2];
 }

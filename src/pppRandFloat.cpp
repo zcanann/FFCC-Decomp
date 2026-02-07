898f3283b59820fac97b4b5ea7ecd2566d75d8e9
Zachary Canann <zacharycanann@gmail.com>
2026-02-07 05:24:20 -0800

    Improve pppRandFloat matching with offset-based param access

--- src/pppRandFloat.cpp
+++ src/pppRandFloat.cpp
@@ -7,35 +7,21 @@ extern float lbl_8032FF88;
 extern float lbl_801EADC8;
 extern "C" float RandF__5CMathFv(CMath* instance);
 
-struct RandFloatParam {
-    int targetId;
-    int sourceOffset;
-    float blend;
-    unsigned char randomTwice;
-};
-
-struct RandFloatCtx {
-    void* unk0;
-    void* unk4;
-    void* unk8;
-    int* outputOffset;
-};
-
 /*
  * --INFO--
  * PAL Address: 0x80061d48
  * PAL Size: 268b
  * EN Address: TODO
  * EN Size: TODO
- * JP Address: TODO  
+ * JP Address: TODO
  * JP Size: TODO
  */
 void pppRandFloat(void* param1, void* param2, void* param3)
 {
-    int* base = (int*)param1;
-    RandFloatParam* data = (RandFloatParam*)param2;
-    RandFloatCtx* ctx = (RandFloatCtx*)param3;
-    int index = base[3];
+    char* base = (char*)param1;
+    int* param = (int*)param2;
+    void* ctx = param3;
+    int index = *(int*)(base + 0xC);
 
     if (lbl_8032ED70 != 0) {
         return;
@@ -44,28 +30,29 @@ void pppRandFloat(void* param1, void* param2, void* param3)
     if (index == 0) {
         float out = RandF__5CMathFv(&math);
 
-        if (data->randomTwice != 0) {
+        if (*(unsigned char*)((char*)param + 0xC) != 0) {
             out += RandF__5CMathFv(&math);
         } else {
             out *= lbl_8032FF88;
         }
 
-        int outputOffset = *ctx->outputOffset;
-        *(float*)((char*)param1 + outputOffset + 0x80) = out;
+        int outputOffset = *(int*)*(int**)((char*)ctx + 0xC);
+        *(float*)(base + outputOffset + 0x80) = out;
         return;
     }
 
-    if (data->targetId == index) {
-        int outputOffset = *ctx->outputOffset;
-        float* outputValue = (float*)((char*)param1 + outputOffset + 0x80);
+    if (param[0] == index) {
+        int outputOffset = *(int*)*(int**)((char*)ctx + 0xC);
         float* source;
 
-        if (data->sourceOffset == -1) {
+        if (param[1] == -1) {
             source = &lbl_801EADC8;
         } else {
-            source = (float*)((char*)param1 + data->sourceOffset + 0x80);
+            source = (float*)(base + param[1] + 0x80);
         }
 
-        *source = *source + (data->blend * *outputValue - data->blend);
+        float blend = *(float*)((char*)param + 0x8);
+        float output = *(float*)(base + outputOffset + 0x80);
+        *source = *source + (blend * output - blend);
     }
 }

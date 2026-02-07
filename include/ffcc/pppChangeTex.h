437566ba8ea0296576ce49db4cfcd21e393f3d82
Zachary Canann <zacharycanann@gmail.com>
2026-02-07 12:13:43 -0800

    pppChangeTex: implement construct/destruct/render paths

--- include/ffcc/pppChangeTex.h
+++ include/ffcc/pppChangeTex.h
@@ -3,8 +3,35 @@
 
 #include "ffcc/chara.h"
 #include "ffcc/materialman.h"
+#include "ffcc/mapmesh.h"
 #include "dolphin/gx/GXDispList.h"
 #include "dolphin/gx/GXVert.h"
+#include <dolphin/types.h>
+
+struct pppChangeTex {
+    union {
+        void* ptr;
+        struct {
+            u32 m_graphId;
+        };
+    } field0_0x0;
+};
+
+struct UnkB {
+    s32 m_graphId;
+    s32 m_dataValIndex;
+    u16 m_initWOrk;
+    u8 _pad0[2];
+    float m_stepValue;
+    u8 m_arg3;
+    u8 m_payload[6];
+    u8 _pad1[1];
+};
+
+struct UnkC {
+    u8 _pad0[0xC];
+    s32* m_serializedDataOffsets;
+};
 
 void ChangeTex_DrawMeshDLCallback(CChara::CModel*, void*, void*, int, int, float (*)[4]);
 void ChangeTex_AfterDrawMeshCallback(CChara::CModel*, void*, void*, int, float (*)[4]);
@@ -13,11 +40,11 @@ void ChangeTex_AfterDrawMeshCallback(CChara::CModel*, void*, void*, int, float (
 extern "C" {
 #endif
 
-void pppConstructChangeTex(void);
-void pppConstruct2ChangeTex(void);
-void pppDestructChangeTex(void);
-void pppFrameChangeTex(void);
-void pppRenderChangeTex(void);
+void pppConstructChangeTex(pppChangeTex*, UnkC*);
+void pppConstruct2ChangeTex(pppChangeTex*, UnkC*);
+void pppDestructChangeTex(pppChangeTex*, UnkC*);
+void pppFrameChangeTex(pppChangeTex*, UnkB*, UnkC*);
+void pppRenderChangeTex(pppChangeTex*, UnkB*, UnkC*);
 
 #ifdef __cplusplus
 }

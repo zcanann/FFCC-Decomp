aee3d05494153dee9e061f77a192603e3c310cc3
Zachary Canann <zacharycanann@gmail.com>
2026-02-07 02:18:48 -0800

    Improve TRK_fill_mem match in mem_TRK

--- src/TRK_MINNOW_DOLPHIN/mem_TRK.c
+++ src/TRK_MINNOW_DOLPHIN/mem_TRK.c
@@ -6,55 +6,65 @@
 #include "TRK_MINNOW_DOLPHIN/MetroTRK/Portable/mem_TRK.h"
 
 #pragma dont_inline on
+/*
+ * --INFO--
+ * PAL Address: 0x801C6080
+ * PAL Size: 184b
+ * EN Address: TODO
+ * EN Size: TODO
+ * JP Address: TODO
+ * JP Size: TODO
+ */
 /* 8036F580-8036F638 369EC0 00B8+00 0/0 1/1 0/0 .text            TRK_fill_mem */
 void TRK_fill_mem(void* dst, int val, u32 n) {
+    u8* d;
+    u32* w;
     u32 v, i, j;
-    v = (u8)val;
 
-    ((u8*)dst) = ((u8*)dst) - 1;
+    v = (u8)val;
+    d = (u8*)dst - 1;
 
     if (n >= 32) {
-        i = (~(unsigned int)dst) & 3;
+        i = (~(u32)d) & 3;
 
         if (i) {
             n -= i;
 
             do {
-                *++(((u8*)dst)) = v;
+                *++d = v;
             } while (--i);
         }
 
         if (v)
             v |= v << 24 | v << 16 | v << 8;
 
-        ((u32*)dst) = ((u32*)(((u8*)dst) + 4)) - 1;
-        ((u32*)dst) = ((u32*)(((u8*)dst) + 1)) - 1;
+        w = (u32*)(d + 4) - 1;
 
-        i = n / 32;
+        i = n >> 5;
 
         if (i) {
             do {
                 for (j = 0; j < 8; j++)
-                    *++((u32*)dst) = v;
+                    *++w = v;
             } while (--i);
         }
 
-        i = (n / 4) % 8;
+        i = (n >> 2) & 7;
 
         if (i) {
             do {
-                *++((u32*)dst) = v;
+                *++w = v;
             } while (--i);
         }
 
-        ((u8*)dst) = ((u8*)(((u32*)dst) + 1)) - 1;
+        d = (u8*)(w + 1) - 1;
 
-        n %= 4;
+        n &= 3;
     }
 
     if (n)
         do {
-            *++((u8*)dst) = v;
+            *++d = v;
         } while (--n);
 }
 #pragma dont_inline reset

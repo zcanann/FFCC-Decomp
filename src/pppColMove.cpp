193964e704b601af3295b8613d317e8f6738affd
Zachary Canann <zacharycanann@gmail.com>
2026-02-07 08:41:55 -0800

    pppColMove: align conditional update flow with target

--- src/pppColMove.cpp
+++ src/pppColMove.cpp
@@ -29,11 +29,7 @@ void pppColMove(void* param1, void* param2, void* param3)
     
     int** ptr_array = (int**)param3;
     int* ptr0 = ptr_array[3];  // Load from offset 0xC
-    
-    if (lbl_8032ED70 != 0) {
-        return;
-    }
-    
+
     int* ptr_src = (int*)ptr0[0]; // Load from offset 0x0
     int* ptr_dest = (int*)ptr0[1]; // Load from offset 0x4
     
@@ -42,25 +38,24 @@ void pppColMove(void* param1, void* param2, void* param3)
     ptr_dest = (int*)((char*)ptr_dest + 0x80);
     short* src = (short*)((char*)param1 + (int)ptr_src);
     short* dest = (short*)((char*)param1 + (int)ptr_dest);
+
+    if (lbl_8032ED70 != 0) {
+        return;
+    }
     
     int* param2_int = (int*)param2;
     int* param1_int = (int*)param1;
     
-    if (param2_int[0] == param1_int[3]) {  // Inverted comparison
-        // Skip the movement update section
-        goto do_final_add;
+    if (param2_int[0] == param1_int[3]) {
+        // Update movement values
+        short* movement = (short*)((char*)param2 + 0x8);
+        
+        dest[0] += movement[0];  // x
+        dest[1] += movement[1];  // y
+        dest[2] += movement[2];  // z
+        dest[3] += movement[3];  // w
     }
-    
-    // Update movement values
-    short* movement = (short*)((char*)param2 + 0x8);
-    
-    dest[0] += movement[0];  // x
-    dest[1] += movement[1];  // y
-    dest[2] += movement[2];  // z
-    dest[3] += movement[3];  // w
 
-do_final_add:
-    // Always perform this addition
     src[0] += dest[0];  // x
     src[1] += dest[1];  // y  
     src[2] += dest[2];  // z

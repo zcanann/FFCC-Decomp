71032233138bd7187a7790704424efa14fa43d86
Noelle Harper <noelle21b@proton.me>
2026-02-07 10:06:19 -0800

    Restore powf wrapper in math_ppc (#553)
    Co-authored-by: Zachary Canann <zacharycanann@gmail.com>

--- src/MSL_C/PPCEABI/bare/H/math_ppc.c
+++ src/MSL_C/PPCEABI/bare/H/math_ppc.c
@@ -1,20 +1,22 @@
 #include "PowerPC_EABI_Support/Msl/MSL_C/MSL_Common/math_ppc.h"
-float acosf(float arg0) {
-    return (float) acos(arg0);
-}
+double pow(double, double);
 
-float atan2f(float arg0) {
-    return (float) atan2(arg0);
+float acosf(float arg0) {
+    return (float)acos(arg0);
 }
 
 float cosf(float arg0) {
-    return (float) cos(arg0);
+    return (float)cos(arg0);
 }
 
 float sinf(float arg0) {
-    return (float) sin(arg0);
+    return (float)sin(arg0);
 }
 
 float tanf(float arg0) {
-    return (float) tan(arg0);
+    return (float)tan(arg0);
+}
+
+float powf(float arg0, float arg1) {
+    return (float)pow(arg0, arg1);
 }

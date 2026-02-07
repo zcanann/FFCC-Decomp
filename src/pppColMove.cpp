7e99ec45eeafaf2c3d82661f27a6880b649eac20
Zachary Canann <zacharycanann@gmail.com>
2026-02-06 20:28:46 -0800

    pppColMove: simplify control flow and pointer offset setup

--- src/pppColMove.cpp
+++ src/pppColMove.cpp
@@ -26,43 +26,34 @@ void pppColMoveCon(void* param1, void* param2)
 void pppColMove(void* param1, void* param2, void* param3)
 {
     extern int lbl_8032ED70;
-    
-    int** ptr_array = (int**)param3;
-    int* ptr0 = ptr_array[3];  // Load from offset 0xC
-    
+
     if (lbl_8032ED70 != 0) {
         return;
     }
-    
-    int* ptr_src = (int*)ptr0[0]; // Load from offset 0x0
-    int* ptr_dest = (int*)ptr0[1]; // Load from offset 0x4
-    
-    // Calculate offsets
-    ptr_src = (int*)((char*)ptr_src + 0x80);
-    ptr_dest = (int*)((char*)ptr_dest + 0x80);
-    short* src = (short*)((char*)param1 + (int)ptr_src);
-    short* dest = (short*)((char*)param1 + (int)ptr_dest);
-    
+
     int* param2_int = (int*)param2;
     int* param1_int = (int*)param1;
-    
-    if (param2_int[0] == param1_int[3]) {  // Inverted comparison
-        // Skip the movement update section
-        goto do_final_add;
-    }
-    
-    // Update movement values
-    short* movement = (short*)((char*)param2 + 0x8);
-    
-    dest[0] += movement[0];  // x
-    dest[1] += movement[1];  // y
-    dest[2] += movement[2];  // z
-    dest[3] += movement[3];  // w
+    int same_target = (param2_int[0] == param1_int[3]);
 
-do_final_add:
-    // Always perform this addition
-    src[0] += dest[0];  // x
-    src[1] += dest[1];  // y  
-    src[2] += dest[2];  // z
-    src[3] += dest[3];  // w
+    int** ptr_array = (int**)param3;
+    int* ptr0 = ptr_array[3];
+
+    int src_offset = ptr0[0];
+    int dest_offset = ptr0[1];
+    short* src = (short*)((char*)param1 + src_offset + 0x80);
+    short* dest = (short*)((char*)param1 + dest_offset + 0x80);
+
+    if (!same_target) {
+        short* movement = (short*)((char*)param2 + 0x8);
+
+        dest[0] += movement[0];
+        dest[1] += movement[1];
+        dest[2] += movement[2];
+        dest[3] += movement[3];
+    }
+
+    src[0] += dest[0];
+    src[1] += dest[1];
+    src[2] += dest[2];
+    src[3] += dest[3];
 }

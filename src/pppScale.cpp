db1cefc2f6ee8d3f096270a3347f5024815c4ab6
Noelle Harper <noelle21b@proton.me>
2026-02-07 09:16:20 -0800

    main/pppScale: correct field indices and early-return flow (#547)
    Co-authored-by: Zachary Canann <zacharycanann@gmail.com>

--- src/pppScale.cpp
+++ src/pppScale.cpp
@@ -6,36 +6,43 @@ extern int DAT_8032ed70;
  * --INFO--
  * PAL Address: 0x800630f0
  * PAL Size: 96b
+ * EN Address: TODO
+ * EN Size: TODO
+ * JP Address: TODO
+ * JP Size: TODO
  */
 void pppScale(void* obj, void* param2, void* param3)
 {
 	if (DAT_8032ed70 != 0) {
 		return;
 	}
-	
+
 	if (*((int*)((char*)param2 + 0x00)) != *((int*)((char*)obj + 0x0c))) {
 		return;
 	}
 
-	void* dataPtr = *((void**)((char*)param3 + 0x0c));
-	float* dst = (float*)((char*)obj + *((int*)((char*)dataPtr + 0x00)) + 0x80);
-
-	dst[0] += *((float*)((char*)param2 + 0x08));
-	dst[1] += *((float*)((char*)param2 + 0x0c));
-	dst[2] += *((float*)((char*)param2 + 0x10));
+	int* data = *((int**)((char*)param3 + 0x0c));
+	float* scale = (float*)((char*)obj + data[0] + 0x80);
+	scale[0] += *((float*)((char*)param2 + 0x08));
+	scale[1] += *((float*)((char*)param2 + 0x0c));
+	scale[2] += *((float*)((char*)param2 + 0x10));
 }
 
 /*
  * --INFO--
  * PAL Address: 0x800630cc
  * PAL Size: 36b
+ * EN Address: TODO
+ * EN Size: TODO
+ * JP Address: TODO
+ * JP Size: TODO
  */
 void pppScaleCon(void* obj, void* param)
 {
-	void* dataPtr = *((void**)((char*)param + 0x0c));
-	float* puVar2 = (float*)((char*)obj + *((int*)((char*)dataPtr + 0x04)) + 0x80);
+	int* data = *((int**)((char*)param + 0x0c));
+	float* value = (float*)((char*)obj + data[0] + 0x80);
 	
-	puVar2[2] = 1.0f;
-	puVar2[1] = 1.0f;
-	*puVar2 = 1.0f;
+	value[2] = 1.0f;
+	value[1] = 1.0f;
+	value[0] = 1.0f;
 }

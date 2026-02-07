6910f3afec345eb0c703be005f008f19bd99e800
Zachary Canann <zacharycanann@gmail.com>
2026-02-07 04:56:35 -0800

    p_system: match GetTable to table-base symbol arithmetic

--- src/p_system.cpp
+++ src/p_system.cpp
@@ -2,6 +2,10 @@
 #include "ffcc/pad.h"
 #include "ffcc/p_dbgmenu.h"
 
+extern "C" {
+extern int lbl_801EA0F4[];
+}
+
 /*
  * --INFO--
  * PAL Address: 0x80047d7c
@@ -57,7 +61,9 @@ void CSystemPcs::Quit()
  */
 int CSystemPcs::GetTable(unsigned long index)
 {
-	return index * 0x15c + -0x7fe15f0c;
+	int offset = index * 0x15c;
+	char* table = (char*)lbl_801EA0F4;
+	return (int)(table + offset);
 }
 
 /*

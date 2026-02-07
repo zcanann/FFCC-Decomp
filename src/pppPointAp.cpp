5172cd21517ef7427468d62726c208f4832595a1
Zachary Canann <zacharycanann@gmail.com>
2026-02-07 15:31:35 -0800

    Refine pppPointAp offset handling for better match

--- src/pppPointAp.cpp
+++ src/pppPointAp.cpp
@@ -5,6 +5,10 @@
 
 struct _pppMngSt;
 struct _pppPDataVal;
+struct _pppPointApOffsets {
+    u32 srcOffset;
+    u32 targetOffset;
+};
 
 struct _pppPObject {
     void* unk0;
@@ -26,9 +30,9 @@ extern _pppPObject* pppCreatePObject(_pppMngSt*, _pppPDataVal*);
  */
 void pppPointApCon(void* param1, void* param2)
 {
-    u32* data = *(u32**)((u8*)param2 + 0xC);
-    param1 = (u8*)param1 + data[1];
-    *((u8*)param1 + 0x81) = 0;
+    _pppPointApOffsets* data = *(_pppPointApOffsets**)((u8*)param2 + 0xC);
+    u32 offset = data->targetOffset + 0x81;
+    ((u8*)param1)[offset] = 0;
 }
 
 /*
@@ -42,31 +46,35 @@ void pppPointApCon(void* param1, void* param2)
  */
 void pppPointAp(void* param1, void* param2, void* param3)
 {
-    Vec* src = *(Vec**)((u8*)param3 + 0xC);
-    s32 index = (s32)src->y;
-    u8* target = (u8*)param1 + index + 0x80;
+    _pppPointApOffsets* data = *(_pppPointApOffsets**)((u8*)param3 + 0xC);
+    Vec* src = (Vec*)((u8*)param1 + data->srcOffset + 0x80);
+    u8* target = (u8*)param1 + data->targetOffset + 0x80;
 
     if (lbl_8032ED70 == 0) {
-        if ((s8)target[1] == 0) {
+        if (target[1] == 0) {
             u32 objId = *(u32*)((u8*)param2 + 0x4);
             if ((objId + 0x10000) != 0xFFFF) {
                 _pppPDataVal* objData = (_pppPDataVal*)(*(u32*)((u8*)lbl_8032ED50 + 0xD4) + (objId << 4));
+                _pppPObject* obj;
 
-                if (objData != 0) {
-                    _pppPObject* obj = pppCreatePObject(lbl_8032ED50, objData);
-                    obj->owner = param1;
-
-                    Vec* dst = (Vec*)((u8*)obj + *(u32*)((u8*)param2 + 0x8) + 0x80);
-                    if (*(u8*)((u8*)param2 + 0xD) == 0) {
-                        dst->x = src->x;
-                        dst->y = src->y;
-                        dst->z = src->z;
-                    } else {
-                        PSMTXMultVec((MtxPtr)((u8*)lbl_8032ED50 + 0x78), src, dst);
-                    }
-
-                    target[1] = *(u8*)((u8*)param2 + 0xC);
+                if (objData == 0) {
+                    obj = 0;
+                } else {
+                    obj = pppCreatePObject(lbl_8032ED50, objData);
                 }
+
+                obj->owner = param1;
+
+                Vec* dst = (Vec*)((u8*)obj + *(u32*)((u8*)param2 + 0x8) + 0x80);
+                if (*(u8*)((u8*)param2 + 0xD) == 0) {
+                    dst->x = src->x;
+                    dst->y = src->y;
+                    dst->z = src->z;
+                } else {
+                    PSMTXMultVec((MtxPtr)((u8*)lbl_8032ED50 + 0x78), src, dst);
+                }
+
+                target[1] = *(u8*)((u8*)param2 + 0xC);
             }
         }
 

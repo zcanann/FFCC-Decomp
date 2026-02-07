e899963a8975ae3cad3da5e095fc31a767da46d6
Zachary Canann <zacharycanann@gmail.com>
2026-02-07 11:09:49 -0800

    Match pppAngMove argument/data layout

--- src/pppAngMove.cpp
+++ src/pppAngMove.cpp
@@ -2,6 +2,32 @@
 
 extern int lbl_8032ED70;
 
+struct PppAngMoveObj {
+    int x;
+    int y;
+    int z;
+};
+
+struct PppAngMoveOffsets {
+    int a;
+    int b;
+};
+
+struct PppAngMoveData {
+    void* field_0;
+    void* field_4;
+    int field_8;
+    void* ptrData;
+};
+
+struct PppAngMoveInput {
+    int field_0;
+    int field_4;
+    int x;
+    int y;
+    int z;
+};
+
 /*
  * --INFO--
  * PAL Address: 0x80064e80
@@ -11,30 +37,31 @@ extern int lbl_8032ED70;
  * JP Address: TODO
  * JP Size: TODO
  */
-void pppAngMove(void* dest, void* src, void* param1, void* param2)
+void pppAngMove(void* basePtr, void* input, void* data1, void* data2)
 {
+    PppAngMoveOffsets* offsets = (PppAngMoveOffsets*)((PppAngMoveData*)data1)->ptrData;
+    PppAngMoveObj* a = (PppAngMoveObj*)((char*)basePtr + offsets->a + 0x80);
+    PppAngMoveObj* b = (PppAngMoveObj*)((char*)basePtr + offsets->b + 0x80);
+    PppAngMoveInput* inputData = (PppAngMoveInput*)input;
+
+    (void)data2;
+
     if (lbl_8032ED70 != 0) {
         return;
     }
 
-    int* param2Ptr = (int*)param2;
-    int destOffset = param2Ptr[0];
-    int srcOffset = param2Ptr[1];
-    int* destPtr = (int*)((char*)dest + destOffset + 0x80);
-    int* srcPtr = (int*)((char*)dest + srcOffset + 0x80);
+    int inputId = *(int*)inputData;
+    int baseId = *(int*)((char*)basePtr + 0xc);
 
-    if (param1 != 0) {
-        int* param1Ptr = (int*)param1;
-        if (param1Ptr[3] == ((int*)dest)[3]) {
-            srcPtr[0] += param1Ptr[2];
-            srcPtr[1] += param1Ptr[3];
-            srcPtr[2] += param1Ptr[4];
-        }
+    if (inputId == baseId) {
+        b->x += inputData->x;
+        b->y += inputData->y;
+        b->z += inputData->z;
     }
 
-    destPtr[0] += srcPtr[0];
-    destPtr[1] += srcPtr[1];
-    destPtr[2] += srcPtr[2];
+    a->x += b->x;
+    a->y += b->y;
+    a->z += b->z;
 }
 
 /*

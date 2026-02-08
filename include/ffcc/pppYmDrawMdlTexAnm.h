5b6006679dce5ef8deb39b10cf14989edcc508b1
Zachary Canann <zacharycanann@gmail.com>
2026-02-08 10:49:29 -0800

    Implement first-pass pppConstructYmDrawMdlTexAnm

--- include/ffcc/pppYmDrawMdlTexAnm.h
+++ include/ffcc/pppYmDrawMdlTexAnm.h
@@ -1,14 +1,35 @@
 #ifndef _PPP_YMDRAWMDLTEXANM_H_
 #define _PPP_YMDRAWMDLTEXANM_H_
 
+#include "ffcc/partMng.h"
+
+struct pppYmDrawMdlTexAnm {
+    _pppPObject field0_0x0;
+};
+
+struct pppYmDrawMdlTexAnmStep {
+    s32 m_graphId;
+    s32 m_dataValIndex;
+    s16 m_initWOrk;
+    u16 _pad0;
+    f32 m_stepValue;
+    f32 m_arg3;
+    u8 m_payload[8];
+};
+
+struct pppYmDrawMdlTexAnmOffsets {
+    u8 _pad0[0x0C];
+    s32* m_serializedDataOffsets;
+};
+
 #ifdef __cplusplus
 extern "C" {
 #endif
 
-void pppConstructYmDrawMdlTexAnm(void* param1, void* param2, void* param3);
-void pppDestructYmDrawMdlTexAnm(void* param1, void* param2, void* param3);
-void pppFrameYmDrawMdlTexAnm(void* param1, void* param2, void* param3);
-void pppRenderYmDrawMdlTexAnm(void* param1, void* param2, void* param3);
+void pppConstructYmDrawMdlTexAnm(pppYmDrawMdlTexAnm* param1, pppYmDrawMdlTexAnmOffsets* param2, void* param3);
+void pppDestructYmDrawMdlTexAnm(pppYmDrawMdlTexAnm* param1, pppYmDrawMdlTexAnmOffsets* param2, void* param3);
+void pppFrameYmDrawMdlTexAnm(pppYmDrawMdlTexAnm* param1, pppYmDrawMdlTexAnmStep* param2, pppYmDrawMdlTexAnmOffsets* param3);
+void pppRenderYmDrawMdlTexAnm(pppYmDrawMdlTexAnm* param1, pppYmDrawMdlTexAnmStep* param2, pppYmDrawMdlTexAnmOffsets* param3);
 
 #ifdef __cplusplus
 }

7a4e13f8d1b6671245afd2f86db77f9cc79485a2
Noelle Harper <noelle21b@proton.me>
2026-02-07 16:02:04 -0800

    Implement pppAlignmentScale frame logic (#616)
    Co-authored-by: Zachary Canann <zacharycanann@gmail.com>

--- include/ffcc/pppAlignmentScale.h
+++ include/ffcc/pppAlignmentScale.h
@@ -1,11 +1,20 @@
 #ifndef _FFCC_PPPALIGNMENTSCALE_H_
 #define _FFCC_PPPALIGNMENTSCALE_H_
 
+struct pppAlignmentScale;
+
+struct pppAlignmentScaleData
+{
+    float m_unk0x0;
+    float m_unk0x4;
+    float m_unk0x8;
+};
+
 #ifdef __cplusplus
 extern "C" {
 #endif
 void pppConstructAlignmentScale(void);
-void pppFrameAlignmentScale(void);
+struct pppAlignmentScale* pppFrameAlignmentScale(struct pppAlignmentScale*, struct pppAlignmentScaleData*);
 
 #ifdef __cplusplus
 }

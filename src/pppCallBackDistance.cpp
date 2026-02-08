0a35413260449f27f606ec427c6f7e3ac8ac39e9
Zachary Canann <zacharycanann@gmail.com>
2026-02-08 12:41:34 -0800

    Improve pppFrameCallBackDistance match shape

--- src/pppCallBackDistance.cpp
+++ src/pppCallBackDistance.cpp
@@ -4,6 +4,8 @@
 #include "ffcc/p_game.h"
 #include <dolphin/mtx.h>
 
+extern CPartMng PartMng;
+
 /*
  * --INFO--
  * PAL Address: 0x80141318
@@ -57,45 +59,34 @@ void pppDestructCallBackDistance(void)
  */
 void pppFrameCallBackDistance(pppCallBackDistance* param1, UnkB* param2, UnkC* param3)
 {
-    _pppMngSt* mngSt;
-    u8* mngBytes;
+    _pppMngSt* pppMngSt;
     u32 graphId;
     s32 dataOffset;
-    s32 frameIndex;
-    s32 graphChunk;
-    s32 graphAdjust;
+    f64 distance;
     Vec worldPos;
     Vec particlePos;
-    f32 distance;
 
-    mngSt = pppMngStPtr;
-    mngBytes = (u8*)mngSt;
-    particlePos.x = *(f32*)(mngBytes + 0x10);
+    particlePos.x = pppMngStPtr->m_matrix.value[0][3];
     dataOffset = *param3->m_serializedDataOffsets;
-    particlePos.y = *(f32*)(mngBytes + 0x20);
-    particlePos.z = *(f32*)(mngBytes + 0x30);
-    distance = PSVECDistance(&particlePos, (Vec*)(mngBytes + 0x64));
-    mngSt = pppMngStPtr;
-    mngBytes = (u8*)mngSt;
+    particlePos.y = pppMngStPtr->m_matrix.value[1][3];
+    particlePos.z = pppMngStPtr->m_matrix.value[2][3];
+    distance = (f64)PSVECDistance(&particlePos, (Vec*)((u8*)pppMngStPtr + 0x64));
+    pppMngSt = pppMngStPtr;
 
-    if ((distance <= param2->m_dataValIndex) || (*(f32*)((u8*)param1 + dataOffset + 8) <= distance)) {
-        worldPos.x = *(f32*)(mngBytes + 0x10);
-        worldPos.y = *(f32*)(mngBytes + 0x20);
-        worldPos.z = *(f32*)(mngBytes + 0x30);
+    if ((distance <= (double)(f32)param2->m_dataValIndex) ||
+        ((double)*(f32*)((u8*)(&param1->field0_0x0 + 2) + dataOffset) <= distance)) {
+        worldPos.x = pppMngStPtr->m_matrix.value[0][3];
+        worldPos.y = pppMngStPtr->m_matrix.value[1][3];
+        worldPos.z = pppMngStPtr->m_matrix.value[2][3];
         PSMTXMultVec(ppvWorldMatrix, &worldPos, &worldPos);
 
-        frameIndex = (s32)(mngBytes + 0x42E55C58) / 0x158 + ((s32)(mngBytes + 0x42E55C58) >> 0x1f);
-        frameIndex = frameIndex - (frameIndex >> 0x1f);
-
         graphId = *(u32*)&param1->field0_0x0;
-        graphChunk = (s32)graphId >> 0xc;
-        graphAdjust = 0;
-        if (((s32)graphId < 0) && ((graphId & 0xfff) != 0)) {
-            graphAdjust = 1;
-        }
+        dataOffset = ((s32)((u8*)pppMngSt - ((u8*)&PartMng + 0x2A18))) / 0x158;
 
-        Game.game.ParticleFrameCallback(frameIndex, (s32)*(s16*)(mngBytes + 0xb0),
-                                        (s32)*(s16*)(mngBytes + 0xb2), (s32)param2->m_initWOrk,
-                                        graphChunk + graphAdjust, &worldPos);
+        Game.game.ParticleFrameCallback(dataOffset, (s32)pppMngSt->m_kind, (s32)pppMngSt->m_nodeIndex,
+                                        (s32)*(s16*)&param2->m_initWOrk,
+                                        ((s32)graphId >> 0xC) +
+                                            (((s32)graphId < 0) && ((graphId & 0xFFF) != 0)),
+                                        &worldPos);
     }
 }

69084408d6aa01c0523554008e55ff5b775baddd
Zachary Canann <zacharycanann@gmail.com>
2026-02-08 06:02:16 -0800

    Implement first-pass CFunnyShapePcs::SetUSBData

--- include/ffcc/FS_USB_Process.h
+++ include/ffcc/FS_USB_Process.h
@@ -1,15 +1,8 @@
 #ifndef _FFCC_FS_USB_PROCESS_H_
 #define _FFCC_FS_USB_PROCESS_H_
 
-// Forward declarations
-class CFunnyShapePcs;
 class CDataHeader;
-
-class OSFS_SHAPE_ST
-{
-public:
-	OSFS_SHAPE_ST& operator=(const OSFS_SHAPE_ST&);
-};
+class CFunnyShapePcs;
 
 class FS_DISPLAY_STATUS
 {
@@ -17,12 +10,10 @@ public:
 	FS_DISPLAY_STATUS& operator=(const FS_DISPLAY_STATUS&);
 };
 
-class CFunnyShape
+class OSFS_SHAPE_ST
 {
 public:
-	void SetDisplay(FS_DISPLAY_STATUS);
-	void SetUSBData();
-	void USBDataCallback(CDataHeader*);
+	OSFS_SHAPE_ST& operator=(const OSFS_SHAPE_ST&);
 };
 
 #endif // _FFCC_FS_USB_PROCESS_H_

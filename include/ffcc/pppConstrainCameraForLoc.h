1f5337b47caa277ca12c34f5efa7da80fa668379
Noelle Harper <noelle21b@proton.me>
2026-02-07 16:01:41 -0800

    Match ConstrainCameraForLoc constructor helper signatures (#615)
    Co-authored-by: Zachary Canann <zacharycanann@gmail.com>

--- include/ffcc/pppConstrainCameraForLoc.h
+++ include/ffcc/pppConstrainCameraForLoc.h
@@ -7,6 +7,24 @@
 
 class CGObject;
 
+typedef struct pppConstrainCameraForLoc {
+    float field0_0x0;
+} pppConstrainCameraForLoc;
+
+typedef struct pppConstrainCameraForLocParams {
+    int m_graphId;
+    float m_dataValIndex;
+    float m_initWork;
+    float m_stepValue;
+} pppConstrainCameraForLocParams;
+
+typedef struct pppConstrainCameraForLocData {
+    int m_unk0;
+    float m_unk4;
+    float m_unk8;
+    int* m_serializedDataOffsets;
+} pppConstrainCameraForLocData;
+
 void CC_BeforeCalcMatrixCallback(CChara::CModel*, void*, void*);
 
 #ifdef __cplusplus
@@ -14,8 +32,10 @@ extern "C" {
 #endif
 
 void pppConstructConstrainCameraForLoc(void);
-void pppConstruct2ConstrainCameraForLoc(void);
-void pppDestructConstrainCameraForLoc(void);
+void pppConstruct2ConstrainCameraForLoc(pppConstrainCameraForLoc*, pppConstrainCameraForLocData*);
+void pppDestructConstrainCameraForLoc(pppConstrainCameraForLoc*, pppConstrainCameraForLocParams*,
+                                      pppConstrainCameraForLocData*);
+void fn_80167EC4(pppConstrainCameraForLoc*, pppConstrainCameraForLocData*);
 void pppFrameConstrainCameraForLoc(void);
 
 #ifdef __cplusplus

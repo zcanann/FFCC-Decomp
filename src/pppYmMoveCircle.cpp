8fc6e1daa0ff6949c59b49f9ff9164cee6f377e4
Noelle Harper <noelle21b@proton.me>
2026-02-08 15:47:08 -0800

    Rewrite pppYmMoveCircle frame/construct with inferred data layout (#805)

--- src/pppYmMoveCircle.cpp
+++ src/pppYmMoveCircle.cpp
@@ -4,42 +4,116 @@
 #include "dolphin/mtx.h"
 #include <math.h>
 
+struct pppYmMoveCircleWork {
+    f32 m_angle;
+    f32 m_angleStep;
+    f32 m_angleStepStep;
+    f32 m_angleStepStepStep;
+    f32 m_radius;
+    f32 m_radiusStep;
+    f32 m_radiusStepStep;
+    Vec m_center;
+    u8 m_hasInit;
+};
+
+extern s32 DAT_8032ed70;
+extern unsigned char* lbl_8032ED50;
+extern f32 lbl_801EC9F0[];
+extern f32 lbl_80330D78;
+extern f32 lbl_80330D7C;
+extern f32 lbl_80330D80;
+extern f32 lbl_80330D84;
+extern f32 lbl_80330D88;
+extern f32 lbl_80330D8C;
+extern f32 lbl_80330D90;
+
 /*
  * --INFO--
  * PAL Address: 0x800d183c
  * PAL Size: 300b
+ * EN Address: TODO
+ * EN Size: TODO
+ * JP Address: TODO
+ * JP Size: TODO
  */
-void pppConstructYmMoveCircle(void)
+extern "C" void pppConstructYmMoveCircle(pppYmMoveCircle* basePtr, pppYmMoveCircleOffsets* offsetData)
 {
     Vec temp1, temp2;
-    float angle;
-    
-    // PSVECSubtract call observed in assembly
+    pppYmMoveCircleWork* work = (pppYmMoveCircleWork*)((u8*)basePtr + *offsetData->m_serializedDataOffsets + 0x80);
+
     PSVECSubtract(&temp1, &temp2, &temp1);
-    
-    // PSVECNormalize call observed
     PSVECNormalize(&temp1, &temp1);
-    
-    // PSVECDotProduct and acos calls for angle calculation
-    angle = PSVECDotProduct(&temp1, &temp2);
-    angle = acos(angle);
+    work->m_angle = lbl_80330D90 * (f32)acos(PSVECDotProduct(&temp1, &temp2));
+
+    if (!((temp1.x <= lbl_80330D7C && temp1.z >= lbl_80330D7C) ||
+          (temp1.x > lbl_80330D7C && temp1.z < lbl_80330D7C))) {
+        work->m_angle = lbl_80330D78 - work->m_angle;
+    }
+
+    work->m_angleStep = lbl_80330D7C;
+    work->m_angleStepStep = lbl_80330D7C;
+    work->m_angleStepStepStep = lbl_80330D7C;
+    work->m_radius = lbl_80330D7C;
+    work->m_radiusStep = lbl_80330D7C;
+    work->m_radiusStepStep = lbl_80330D7C;
+    pppCopyVector(work->m_center, *(Vec*)((u8*)lbl_8032ED50 + 0x58));
+    work->m_hasInit = 0;
 }
 
 /*
  * --INFO--
- * PAL Address: 0x800d160c  
+ * PAL Address: 0x800d160c
  * PAL Size: 560b
+ * EN Address: TODO
+ * EN Size: TODO
+ * JP Address: TODO
+ * JP Size: TODO
  */
-void pppFrameYmMoveCircle(void)
+extern "C" void pppFrameYmMoveCircle(pppYmMoveCircle* basePtr, pppYmMoveCircleStep* stepData, pppYmMoveCircleOffsets* offsetData)
 {
-    Vec position;
-    float angleStep;
-    
-    // Vector operations observed in assembly
-    pppCopyVector(position, position);
-    
-    // Floating point math for circular movement
-    position.x += angleStep;
-    position.y += angleStep; 
-    position.z += angleStep;
+    pppYmMoveCircleWork* work;
+    Vec currentPos;
+    Vec nextPos;
+    s32 tableIndex;
+
+    if (DAT_8032ed70 != 0) {
+        return;
+    }
+
+    work = (pppYmMoveCircleWork*)((u8*)basePtr + *offsetData->m_serializedDataOffsets + 0x80);
+
+    work->m_radiusStep += work->m_radiusStepStep;
+    work->m_radius += work->m_radiusStep;
+    work->m_angleStep += work->m_angleStepStep;
+    work->m_angle += work->m_angleStep;
+
+    if (stepData->m_graphId == basePtr->m_graphId) {
+        work->m_radius += stepData->m_radius;
+        work->m_radiusStep += stepData->m_radiusStep;
+        work->m_radiusStepStep += stepData->m_radiusStepStep;
+        work->m_angle += stepData->m_angle;
+        work->m_angleStep += stepData->m_angleStep;
+        work->m_angleStepStep += stepData->m_angleStepStep;
+    }
+
+    if (work->m_angle > lbl_80330D78) {
+        work->m_angle -= lbl_80330D78;
+    }
+    if (work->m_angle < lbl_80330D7C) {
+        work->m_angle += lbl_80330D78;
+    }
+
+    tableIndex = (s32)((lbl_80330D80 * (lbl_80330D84 * work->m_angle)) / lbl_80330D88);
+    nextPos.x = (work->m_radius * *(f32*)((u8*)lbl_801EC9F0 + ((tableIndex + 0x4000) & 0xFFFC))) + work->m_center.x;
+    nextPos.y = *(f32*)((u8*)lbl_8032ED50 + 0xC);
+    nextPos.z = (work->m_radius * -(*(f32*)((u8*)lbl_801EC9F0 + (tableIndex & 0xFFFC)))) + work->m_center.z;
+
+    pppCopyVector(currentPos, *(Vec*)((u8*)lbl_8032ED50 + 0x8));
+    pppCopyVector(*(Vec*)((u8*)lbl_8032ED50 + 0x48), currentPos);
+    pppCopyVector(*(Vec*)((u8*)lbl_8032ED50 + 0x8), nextPos);
+
+    *(f32*)((u8*)lbl_8032ED50 + 0x84) = nextPos.x;
+    *(f32*)((u8*)lbl_8032ED50 + 0x94) = nextPos.y;
+    *(f32*)((u8*)lbl_8032ED50 + 0xA4) = nextPos.z;
+    pppSetFpMatrix((_pppMngSt*)lbl_8032ED50);
 }

a1865b77bf5a0f254ca66a75899b059b67360f43
Zachary Canann <zacharycanann@gmail.com>
2026-02-08 15:27:24 -0800

    Rewrite pppYmMoveCircle frame/construct with inferred data layout

--- include/ffcc/pppYmMoveCircle.h
+++ include/ffcc/pppYmMoveCircle.h
@@ -1,12 +1,34 @@
 #ifndef _FFCC_PPP_YMMOVECIRCLE_H_
 #define _FFCC_PPP_YMMOVECIRCLE_H_
 
+struct pppYmMoveCircle {
+    char m_padding[0xC];
+    int m_graphId;
+};
+
+struct pppYmMoveCircleStep {
+    int m_graphId;
+    float m_angle;
+    float m_angleStep;
+    float m_angleStepStep;
+    float m_radius;
+    float m_radiusStep;
+    float m_radiusStepStep;
+};
+
+struct pppYmMoveCircleOffsets {
+    int m_pad0;
+    int m_pad1;
+    int m_pad2;
+    int* m_serializedDataOffsets;
+};
+
 #ifdef __cplusplus
 extern "C" {
 #endif
 
-void pppConstructYmMoveCircle(void);
-void pppFrameYmMoveCircle(void);
+void pppConstructYmMoveCircle(struct pppYmMoveCircle* basePtr, struct pppYmMoveCircleOffsets* offsetData);
+void pppFrameYmMoveCircle(struct pppYmMoveCircle* basePtr, struct pppYmMoveCircleStep* stepData, struct pppYmMoveCircleOffsets* offsetData);
 
 #ifdef __cplusplus
 }

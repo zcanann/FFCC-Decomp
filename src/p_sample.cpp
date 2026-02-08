9c0743bbb48fc1bfa768909d4b0bb507e82b3cf7
Zachary Canann <zacharycanann@gmail.com>
2026-02-07 21:36:48 -0800

    main/p_sample: implement GetTable and static init wiring

--- src/p_sample.cpp
+++ src/p_sample.cpp
@@ -1,18 +1,21 @@
 #include "ffcc/p_sample.h"
 
-/*
- * --INFO--
- * Address:	TODO
- * Size:	TODO
- */
-CSamplePcs::CSamplePcs()
-{
-}
+extern unsigned int lbl_801E8498[];
+extern unsigned int lbl_801E84A4[];
+extern unsigned int lbl_801E84B0[];
+extern unsigned int lbl_801E84BC[];
+extern unsigned char lbl_801E84C8[];
+extern unsigned int lbl_801E8644[];
+extern unsigned int lbl_8032EC60;
 
 /*
  * --INFO--
- * Address:	8001feac
- * Size:	4
+ * PAL Address: 0x8001FEAC
+ * PAL Size: 4b
+ * EN Address: TODO
+ * EN Size: TODO
+ * JP Address: TODO
+ * JP Size: TODO
  */
 void CSamplePcs::Init()
 {
@@ -21,8 +24,12 @@ void CSamplePcs::Init()
 
 /*
  * --INFO--
- * Address:	8001fea8
- * Size:	4
+ * PAL Address: 0x8001FEA8
+ * PAL Size: 4b
+ * EN Address: TODO
+ * EN Size: TODO
+ * JP Address: TODO
+ * JP Size: TODO
  */
 void CSamplePcs::Quit()
 {
@@ -31,18 +38,26 @@ void CSamplePcs::Quit()
 
 /*
  * --INFO--
- * Address:	8001fe94
- * Size:	4
+ * PAL Address: 0x8001FE94
+ * PAL Size: 20b
+ * EN Address: TODO
+ * EN Size: TODO
+ * JP Address: TODO
+ * JP Size: TODO
  */
-void CSamplePcs::GetTable(unsigned long)
+int CSamplePcs::GetTable(unsigned long index)
 {
-	return;
+	return (int)(lbl_801E84C8 + index * 0x15C);
 }
 
 /*
  * --INFO--
- * Address:	8001fe90
- * Size:	4
+ * PAL Address: 0x8001FE90
+ * PAL Size: 4b
+ * EN Address: TODO
+ * EN Size: TODO
+ * JP Address: TODO
+ * JP Size: TODO
  */
 void CSamplePcs::create()
 {
@@ -51,8 +66,12 @@ void CSamplePcs::create()
 
 /*
  * --INFO--
- * Address:	8001fe8c
- * Size:	4
+ * PAL Address: 0x8001FE8C
+ * PAL Size: 4b
+ * EN Address: TODO
+ * EN Size: TODO
+ * JP Address: TODO
+ * JP Size: TODO
  */
 void CSamplePcs::destroy()
 {
@@ -61,8 +80,12 @@ void CSamplePcs::destroy()
 
 /*
  * --INFO--
- * Address:	8001fe88
- * Size:	4
+ * PAL Address: 0x8001FE88
+ * PAL Size: 4b
+ * EN Address: TODO
+ * EN Size: TODO
+ * JP Address: TODO
+ * JP Size: TODO
  */
 void CSamplePcs::func0()
 {
@@ -71,12 +94,57 @@ void CSamplePcs::func0()
 
 /*
  * --INFO--
- * Address:	8001fe84
- * Size:	4
+ * PAL Address: 0x8001FE84
+ * PAL Size: 4b
+ * EN Address: TODO
+ * EN Size: TODO
+ * JP Address: TODO
+ * JP Size: TODO
  */
 void CSamplePcs::func1()
 {
 	return;
+}
+
+/*
+ * --INFO--
+ * PAL Address: 0x8001FEB0
+ * PAL Size: 188b
+ * EN Address: TODO
+ * EN Size: TODO
+ * JP Address: TODO
+ * JP Size: TODO
+ */
+extern "C" void __sinit_p_sample_cpp(void)
+{
+	unsigned int a0 = lbl_801E8498[0];
+	unsigned int a1 = lbl_801E8498[1];
+	unsigned int a2 = lbl_801E8498[2];
+	unsigned int b0 = lbl_801E84A4[0];
+	unsigned int b1 = lbl_801E84A4[1];
+	unsigned int b2 = lbl_801E84A4[2];
+	unsigned int c0 = lbl_801E84B0[0];
+	unsigned int c1 = lbl_801E84B0[1];
+	unsigned int c2 = lbl_801E84B0[2];
+	unsigned int d0 = lbl_801E84BC[0];
+	unsigned int d1 = lbl_801E84BC[1];
+	unsigned int d2 = lbl_801E84BC[2];
+	unsigned int* table = (unsigned int*)lbl_801E84C8;
+
+	lbl_8032EC60 = (unsigned int)&lbl_801E8644;
+
+	table[1] = a0;
+	table[2] = a1;
+	table[3] = a2;
+	table[4] = b0;
+	table[5] = b1;
+	table[6] = b2;
+	table[7] = c0;
+	table[8] = c1;
+	table[9] = c2;
+	table[12] = d0;
+	table[13] = d1;
+	table[14] = d2;
 }
 
 /*

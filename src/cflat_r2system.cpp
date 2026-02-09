0dfb84ce1a2929f8aa08063a065e9d01a8dfa3e7
Noelle Harper <noelle21b@proton.me>
2026-02-08 17:00:44 -0800

    Implement cflat_r2system system wrapper accessors (#821)
    Co-authored-by: Zachary Canann <zacharycanann@gmail.com>

--- src/cflat_r2system.cpp
+++ src/cflat_r2system.cpp
@@ -1,5 +1,8 @@
 #include "ffcc/cflat_r2system.h"
 #include "ffcc/game.h"
+#include "ffcc/p_camera.h"
+#include "ffcc/p_menu.h"
+#include "ffcc/sound.h"
 
 /*
  * --INFO--
@@ -13,6 +16,48 @@
 void CGame::SetNextScriptNewGame()
 {
     m_nextScript.m_flags = 1;
+}
+
+/*
+ * --INFO--
+ * PAL Address: 0x800B8F90
+ * PAL Size: 8b
+ * EN Address: TODO
+ * EN Size: TODO
+ * JP Address: TODO
+ * JP Size: TODO
+ */
+void CCameraPcs::SetFullScreenShadowCamLen(float len)
+{
+    *(float*)((char*)this + 0x430) = len;
+}
+
+/*
+ * --INFO--
+ * PAL Address: 0x800B8F98
+ * PAL Size: 8b
+ * EN Address: TODO
+ * EN Size: TODO
+ * JP Address: TODO
+ * JP Size: TODO
+ */
+CFont* CMenuPcs::GetFont22()
+{
+    return *(CFont**)((char*)this + 0xF8);
+}
+
+/*
+ * --INFO--
+ * PAL Address: 0x800B8FA0
+ * PAL Size: 8b
+ * EN Address: TODO
+ * EN Size: TODO
+ * JP Address: TODO
+ * JP Size: TODO
+ */
+void CSound::SeMaxVolume(int volume)
+{
+    *(int*)((char*)this + 0x22BC) = volume;
 }
 
 /*

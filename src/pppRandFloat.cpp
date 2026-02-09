aebb9408ab36fa7e2380d52a72e7c7f6e0dad7a8
Zachary Canann <zacharycanann@gmail.com>
2026-02-08 19:00:08 -0800

    main/pppRandFloat: improve decomp match via pointer/offset typing

--- src/pppRandFloat.cpp
+++ src/pppRandFloat.cpp
@@ -1,7 +1,7 @@
 #include "ffcc/pppRandFloat.h"
 #include "ffcc/math.h"
 
-extern CMath math;
+extern CMath math[];
 extern int lbl_8032ED70;
 extern float lbl_8032FF88;
 extern float lbl_801EADC8;
@@ -34,10 +34,9 @@ void pppRandFloat(void* param1, void* param2, void* param3)
 {
     int index;
     int outputOffset;
-    RandFloatCtx* ctx;
     RandFloatParam* data;
+    RandFloatCtx* ctx;
     float* source;
-    float* output;
     char* base;
     float value;
 
@@ -46,32 +45,33 @@ void pppRandFloat(void* param1, void* param2, void* param3)
     }
 
     base = (char*)param1;
-    index = ((int*)base)[3];
     ctx = (RandFloatCtx*)param3;
     data = (RandFloatParam*)param2;
+    index = ((int*)base)[3];
 
     if (index == 0) {
-        value = RandF__5CMathFv(&math);
+        value = RandF__5CMathFv(math);
         if (data->randomTwice != 0) {
-            value += RandF__5CMathFv(&math);
+            value += RandF__5CMathFv(math);
         } else {
             value *= lbl_8032FF88;
         }
 
         outputOffset = *ctx->outputOffset;
-        output = (float*)(base + outputOffset + 0x80);
-        *output = value;
+        *(float*)(base + outputOffset + 0x80) = value;
         return;
     }
 
     if (data->targetId == index) {
+        int sourceOffset;
+
         outputOffset = *ctx->outputOffset;
-        output = (float*)(base + outputOffset + 0x80);
+        sourceOffset = data->sourceOffset;
         if (data->sourceOffset == -1) {
             source = &lbl_801EADC8;
         } else {
-            source = (float*)(base + data->sourceOffset + 0x80);
+            source = (float*)(base + sourceOffset + 0x80);
         }
-        *source = *source + (data->blend * *output - data->blend);
+        *source = *source + (data->blend * *(float*)(base + outputOffset + 0x80) - data->blend);
     }
 }

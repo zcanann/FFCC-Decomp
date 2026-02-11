3571ba04d3e3ba5d3b6f337870f9ae91bd715ad0
Noelle Harper <noelle21b@proton.me>
2026-02-11 14:17:20 -0800

    main/pppKeShpTail2X: implement constructor/destructor init path (#998)
    Co-authored-by: Zachary Canann <zacharycanann@gmail.com>

--- src/pppKeShpTail2X.cpp
+++ src/pppKeShpTail2X.cpp
@@ -1,9 +1,15 @@
 #include "ffcc/pppKeShpTail2X.h"
+#include <dolphin/types.h>
+#include <string.h>
 
 /*
  * --INFO--
- * Address:	TODO
- * Size:	TODO
+ * PAL Address: 0x80088e4c
+ * PAL Size: 992b
+ * EN Address: TODO
+ * EN Size: TODO
+ * JP Address: TODO
+ * JP Size: TODO
  */
 void pppKeShpTail2X(void)
 {
@@ -12,8 +18,12 @@ void pppKeShpTail2X(void)
 
 /*
  * --INFO--
- * Address:	TODO
- * Size:	TODO
+ * PAL Address: 0x80088748
+ * PAL Size: 1796b
+ * EN Address: TODO
+ * EN Size: TODO
+ * JP Address: TODO
+ * JP Size: TODO
  */
 void pppKeShpTail2XDraw(void)
 {
@@ -22,28 +32,56 @@ void pppKeShpTail2XDraw(void)
 
 /*
  * --INFO--
- * Address:	TODO
- * Size:	TODO
+ * PAL Address: 0x800886f0
+ * PAL Size: 88b
+ * EN Address: TODO
+ * EN Size: TODO
+ * JP Address: TODO
+ * JP Size: TODO
  */
-void pppKeShpTail2XCon(void)
+void pppKeShpTail2XCon(void* obj, void* param_2)
 {
-	// TODO
+	u32 serializedOffset = **(u32**)((u8*)param_2 + 0xc);
+	u8* tail = (u8*)obj + serializedOffset + 0x80;
+
+	*(u16*)(tail + 2) = 0;
+	*(u16*)(tail + 4) = 0;
+	*(u16*)(tail + 6) = 0;
+	tail[1] = 0;
+	tail[0] = 0x1f;
+	memset(tail + 8, 0, 0x174);
 }
 
 /*
  * --INFO--
- * Address:	TODO
- * Size:	TODO
+ * PAL Address: 0x80088698
+ * PAL Size: 88b
+ * EN Address: TODO
+ * EN Size: TODO
+ * JP Address: TODO
+ * JP Size: TODO
  */
-void pppKeShpTail2XDes(void)
+void pppKeShpTail2XDes(void* obj, void* param_2)
 {
-	// TODO
+	u32 serializedOffset = **(u32**)((u8*)param_2 + 0xc);
+	u8* tail = (u8*)obj + serializedOffset + 0x80;
+
+	*(u16*)(tail + 2) = 0;
+	*(u16*)(tail + 4) = 0;
+	*(u16*)(tail + 6) = 0;
+	tail[1] = 0;
+	tail[0] = 0x1f;
+	memset(tail + 8, 0, 0x174);
 }
 
 /*
  * --INFO--
- * Address:	TODO
- * Size:	TODO
+ * PAL Address: TODO
+ * PAL Size: TODO
+ * EN Address: TODO
+ * EN Size: TODO
+ * JP Address: TODO
+ * JP Size: TODO
  */
 void U8ToF32(pppFVECTOR4*, unsigned char*)
 {

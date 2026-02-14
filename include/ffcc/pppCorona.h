f876859762b598c79e8fcc324f605cd4af919f16
Noelle Harper <noelle21b@proton.me>
2026-02-14 11:52:16 -0800

    pppCorona: implement frame/render logic and correct signatures (#1276)
    Co-authored-by: Zachary Canann <zacharycanann@gmail.com>

--- include/ffcc/pppCorona.h
+++ include/ffcc/pppCorona.h
@@ -17,14 +17,32 @@ struct UnkC {
     s32* m_serializedDataOffsets;
 };
 
+struct CoronaParam {
+    s32 m_graphId;
+    u16 m_dataValIndex;
+    u8 m_colorR;
+    u8 m_colorG;
+    u8 m_colorB;
+    s16 m_shapeStep;
+    float m_distMin;
+    float m_distMax;
+    float m_distRange;
+    float m_addX;
+    float m_addY;
+    float m_addZ;
+    u8 m_blendMode;
+    u8 m_drawB;
+    u8 m_drawA;
+};
+
 #ifdef __cplusplus
 extern "C" {
 #endif
 
 void pppConstructCorona(pppCorona* param1, UnkC* param2);
 void pppDestructCorona(void);
-void pppFrameCorona(pppCorona* param1, UnkC* param2);
-void pppRenderCorona(pppCorona* param1, UnkC* param2);
+void pppFrameCorona(pppCorona* param1, CoronaParam* param2, UnkC* param3);
+void pppRenderCorona(pppCorona* param1, CoronaParam* param2, UnkC* param3);
 
 #ifdef __cplusplus
 }

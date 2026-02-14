8012d47b169ec0837ab0fa8dba315f1760ba9650
Zachary Canann <zacharycanann@gmail.com>
2026-02-14 11:38:31 -0800

    RedCommand: implement _EraseTime and fix return type

--- src/RedSound/RedCommand.cpp
+++ src/RedSound/RedCommand.cpp
@@ -1,4 +1,11 @@
 #include "ffcc/RedSound/RedCommand.h"
+#include "ffcc/RedSound/RedEntry.h"
+#include "ffcc/RedSound/RedMidiCtrl.h"
+
+extern CRedEntry DAT_8032e154;
+extern void* DAT_8032f3f0;
+extern void* DAT_8032f3fc;
+extern unsigned int* DAT_8032f444;
 
 /*
  * --INFO--
@@ -12,12 +19,81 @@ void _EraseAttribute(int, int)
 
 /*
  * --INFO--
- * Address:	TODO
- * Size:	TODO
+ * PAL Address: 0x801ca1a4
+ * PAL Size: 536b
+ * EN Address: TODO
+ * EN Size: TODO
+ * JP Address: TODO
+ * JP Size: TODO
  */
-void _EraseTime(int)
+int _EraseTime(int eraseTrack)
 {
-	// TODO
+	unsigned int minTrack = 0x100;
+	int* trackBasePtr = (int*)((char*)DAT_8032f3f0 + 0xdbc);
+	int* track = (int*)*trackBasePtr;
+
+	do {
+		if ((*track != 0) && (*(char*)(track + 0x54) == '\0') &&
+		    ((unsigned int)*(unsigned char*)((char*)track + 0x14f) < minTrack)) {
+			minTrack = *(unsigned char*)((char*)track + 0x14f);
+		}
+		track += 0x55;
+	} while (track < (int*)(*trackBasePtr + 0x2a80));
+
+	if ((int)minTrack < eraseTrack) {
+		eraseTrack = (int)minTrack;
+	}
+
+	track = (int*)*trackBasePtr;
+	int maxWait = 0;
+	int sepId = 0;
+	do {
+		if ((*track != 0) && (*(char*)(track + 0x54) == '\0') &&
+		    ((int)*(unsigned char*)((char*)track + 0x14f) <= eraseTrack) &&
+		    (maxWait < track[0x43])) {
+			maxWait = track[0x43];
+			sepId = track[0x3d];
+		}
+		track += 0x55;
+	} while (track < (int*)(*trackBasePtr + 0x2a80));
+
+	track = (int*)*trackBasePtr;
+	int erasedCount = 0;
+	do {
+		if ((*track != 0) && (*(char*)(track + 0x54) == '\0') &&
+		    ((int)*(unsigned char*)((char*)track + 0x14f) <= eraseTrack) &&
+		    (track[0x43] == maxWait)) {
+			unsigned char trackNo;
+			unsigned int* seTrack;
+
+			KeyOnReserveClear((RedKeyOnDATA*)DAT_8032f3fc, (RedTrackDATA*)track);
+			track[0x3e] = 0;
+			track[0x41] = 0;
+			*track = 0;
+			track[0x16] = 0;
+
+			trackNo = *(unsigned char*)((char*)track + 0x14e);
+			((unsigned char*)DAT_8032f444)[trackNo * 0xc0 + 0x1a] &=
+			    (unsigned char)0xfa;
+			seTrack = (unsigned int*)((unsigned char*)DAT_8032f444 + trackNo * 0xc0);
+			seTrack[0x25] &= 0xfffffff7;
+			seTrack[0x24] &= 0xfffffffe;
+			seTrack[0x24] |= 2;
+			seTrack[0x23] = 0;
+
+			if (track[6] != 0) {
+				DAT_8032e154.WaveHistoryManager(0, *(short*)(track[6] + 2));
+			}
+			erasedCount++;
+		}
+		track += 0x55;
+	} while (track < (int*)(*trackBasePtr + 0x2a80));
+
+	if (erasedCount != 0) {
+		DAT_8032e154.SeSepHistoryManager(0, sepId);
+	}
+
+	return erasedCount;
 }
 
 /*

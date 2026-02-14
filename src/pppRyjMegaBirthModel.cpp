c5c2e97316d8e177e65233e207da2f53282a4786
Noelle Harper <noelle21b@proton.me>
2026-02-14 10:34:08 -0800

    Fix pppRyjMegaBirthModel mangled signatures for objdiff matching (#1254)
    Co-authored-by: Zachary Canann <zacharycanann@gmail.com>

--- src/pppRyjMegaBirthModel.cpp
+++ src/pppRyjMegaBirthModel.cpp
@@ -118,7 +118,7 @@ void init_matrix(_pppPObject*, pppFMATRIX&, PRyjMegaBirthModel*, VRyjMegaBirthMo
  * Address:	TODO
  * Size:	TODO
  */
-void set_matrix(_pppPObject*, pppFMATRIX&, pppFMATRIX&, PRyjMegaBirthModel*, _PARTICLE_DATA*, _PARTICLE_WMAT*, pppFMATRIX&, unsigned char)
+void set_matrix(_pppPObject*, pppFMATRIX, pppFMATRIX, PRyjMegaBirthModel*, _PARTICLE_DATA*, _PARTICLE_WMAT*, pppFMATRIX&, unsigned char)
 {
 	// TODO
 }

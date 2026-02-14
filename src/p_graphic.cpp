fdf3feebeb11d4a6f34ffb18529976d44496aba2
Noelle Harper <noelle21b@proton.me>
2026-02-14 12:01:47 -0800

    p_graphic: implement __sinit_p_graphic_cpp table init (#1278)
    Co-authored-by: Zachary Canann <zacharycanann@gmail.com>

--- src/p_graphic.cpp
+++ src/p_graphic.cpp
@@ -1,4 +1,71 @@
 #include "ffcc/p_graphic.h"
+#include "types.h"
+
+extern void* __vt__8CManager;
+extern void* lbl_801E8668;
+extern void* lbl_801E9E9C;
+
+extern u32 lbl_801E9C90[];
+extern u32 lbl_801E9C9C[];
+extern u32 lbl_801E9CA8[];
+extern u32 lbl_801E9CB4[];
+extern u32 lbl_801E9CC0[];
+extern u32 lbl_801E9CCC[];
+extern u32 lbl_801E9CD8[];
+extern u32 lbl_801E9CE4[];
+extern u32 lbl_801E9CF0[];
+extern u32 lbl_801E9CFC[];
+extern u32 lbl_801E9D08[];
+extern CGraphicPcs GraphicsPcs;
+
+/*
+ * --INFO--
+ * PAL Address: 0x80047788
+ * PAL Size: 424b
+ * EN Address: TODO
+ * EN Size: TODO
+ * JP Address: TODO
+ * JP Size: TODO
+ */
+extern "C" void __sinit_p_graphic_cpp(void)
+{
+    volatile void** base = (volatile void**)&GraphicsPcs;
+    *base = &__vt__8CManager;
+    *base = &lbl_801E8668;
+    *base = &lbl_801E9E9C;
+
+    u32* dst = lbl_801E9D08;
+    dst[1] = lbl_801E9C90[0];
+    dst[2] = lbl_801E9C90[1];
+    dst[3] = lbl_801E9C90[2];
+    dst[4] = lbl_801E9C9C[0];
+    dst[5] = lbl_801E9C9C[1];
+    dst[6] = lbl_801E9C9C[2];
+    dst[7] = lbl_801E9CA8[0];
+    dst[8] = lbl_801E9CA8[1];
+    dst[9] = lbl_801E9CA8[2];
+    dst[12] = lbl_801E9CB4[0];
+    dst[13] = lbl_801E9CB4[1];
+    dst[14] = lbl_801E9CB4[2];
+    dst[17] = lbl_801E9CC0[0];
+    dst[18] = lbl_801E9CC0[1];
+    dst[19] = lbl_801E9CC0[2];
+    dst[22] = lbl_801E9CCC[0];
+    dst[23] = lbl_801E9CCC[1];
+    dst[24] = lbl_801E9CCC[2];
+    dst[27] = lbl_801E9CD8[0];
+    dst[28] = lbl_801E9CD8[1];
+    dst[29] = lbl_801E9CD8[2];
+    dst[32] = lbl_801E9CE4[0];
+    dst[33] = lbl_801E9CE4[1];
+    dst[34] = lbl_801E9CE4[2];
+    dst[37] = lbl_801E9CF0[0];
+    dst[38] = lbl_801E9CF0[1];
+    dst[39] = lbl_801E9CF0[2];
+    dst[42] = lbl_801E9CFC[0];
+    dst[43] = lbl_801E9CFC[1];
+    dst[44] = lbl_801E9CFC[2];
+}
 
 /*
  * --INFO--

0eacebe0c58f2215bfd4710edb01cb2d4646fc97
Zachary Canann <zacharycanann@gmail.com>
2026-02-14 18:05:45 -0800

    menu_item: implement ItemCtrl control flow

--- src/menu_item.cpp
+++ src/menu_item.cpp
@@ -1,5 +1,8 @@
 #include "ffcc/menu_item.h"
 
+typedef signed short s16;
+typedef unsigned char u8;
+
 /*
  * --INFO--
  * Address:	TODO
@@ -32,12 +35,41 @@ void CMenuPcs::ItemOpen()
 
 /*
  * --INFO--
- * Address:	TODO
- * Size:	TODO
+ * PAL Address: 0x8015a994
+ * PAL Size: 260b
+ * EN Address: TODO
+ * EN Size: TODO
+ * JP Address: TODO
+ * JP Size: TODO
  */
-void CMenuPcs::ItemCtrl()
+int CMenuPcs::ItemCtrl()
 {
-	// TODO
+    s16* itemState = *(s16**)((u8*)this + 0x82C);
+    s16* selectState = *(s16**)((u8*)this + 0x848);
+    int result = 0;
+
+    itemState[0x19] = itemState[0x18];
+
+    if ((itemState[0x18] == 0) || ((itemState[0x18] != 0) && (itemState[0x9] == 1))) {
+        result = ItemCtrlCur();
+    } else if ((itemState[0x18] == 1) && (itemState[0x9] == 0)) {
+        if (selectState[5] == 1) {
+            result = 0;
+            itemState[0x9] = 1;
+        }
+    } else if ((itemState[0x18] == 1) && (itemState[0x9] == 2) && (selectState[5] == 3)) {
+        result = 0;
+        itemState[0x9] = 0;
+        itemState[0x18] = 0;
+        itemState[0x11] = 0;
+    }
+
+    if (result != 0) {
+        SingLifeInit(-1);
+        ItemInit1();
+    }
+
+    return result;
 }
 
 /*
@@ -65,7 +97,7 @@ void CMenuPcs::ItemDraw()
  * Address:	TODO
  * Size:	TODO
  */
-void CMenuPcs::ItemCtrlCur()
+int CMenuPcs::ItemCtrlCur()
 {
-	// TODO
+    return 0;
 }

e4f1e956a409d221bc96301cef243a292def521c
Zachary Canann <zacharycanann@gmail.com>
2026-02-15 00:25:39 -0800

    Implement first-pass calc for pppYmMegaBirthShpTail2

--- include/ffcc/pppYmMegaBirthShpTail2.h
+++ include/ffcc/pppYmMegaBirthShpTail2.h
@@ -38,7 +38,6 @@ void get_rand(void);
 void U8ToF32(pppFVECTOR4*, unsigned char*);
 void alloc_check(VYmMegaBirthShpTail2*, PYmMegaBirthShpTail2*);
 void birth(_pppPObject*, VYmMegaBirthShpTail2*, PYmMegaBirthShpTail2*, VColor*, _PARTICLE_DATA*, _PARTICLE_WMAT*, _PARTICLE_COLOR*);
-void calc(_pppPObject*, VYmMegaBirthShpTail2*, PYmMegaBirthShpTail2*, _PARTICLE_DATA*, VColor*, _PARTICLE_COLOR*);
 void calc_particle(_pppPObject*, VYmMegaBirthShpTail2*, PYmMegaBirthShpTail2*, VColor*);
 
 #ifdef __cplusplus

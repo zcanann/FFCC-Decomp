41b59f6c9613c3a0623c5b05088a600705c9837f
Zachary Canann <zacharycanann@gmail.com>
2026-02-22 03:36:21 -0800

    ringmenu: first-pass decomp for CRingMenu::drawGBA

--- src/ringmenu.cpp
+++ src/ringmenu.cpp
@@ -1,4 +1,50 @@
 #include "ffcc/ringmenu.h"
+#include "ffcc/p_game.h"
+
+#include <math.h>
+
+extern "C" {
+void* __ct__6CColorFUcUcUcUc(void* color, unsigned char r, unsigned char g, unsigned char b, unsigned char a);
+void SetAttrFmt__8CMenuPcsFQ28CMenuPcs3FMT(void* menuPcs, int fmt);
+void SetTexture__8CMenuPcsFQ28CMenuPcs3TEX(void* menuPcs, int tex);
+void SetColor__8CMenuPcsFR6CColor(void* menuPcs, void* color);
+void DrawRect__8CMenuPcsFUlfffffffff(
+    void* menuPcs, unsigned long flags, float x, float y, float w, float h, float u, float v, float uvScaleX, float uvScaleY,
+    float rot);
+void DrawInit__8CMenuPcsFv(void* menuPcs);
+
+bool GetGBAStart__6JoyBusFi(void* joyBus, int portIndex);
+int IsInitSend__6JoyBusFi(void* joyBus, int portIndex);
+
+extern unsigned char MenuPcs[];
+extern unsigned char Joybus[];
+extern unsigned char CFlat[];
+
+extern float FLOAT_803309c0;
+extern float FLOAT_803309c4;
+extern float FLOAT_803309cc;
+extern float FLOAT_80330a08;
+extern float FLOAT_80330a0c;
+extern float FLOAT_80330a10;
+extern float FLOAT_80330a14;
+extern float FLOAT_80330a18;
+extern float FLOAT_80330a28;
+extern float FLOAT_80330a2c;
+extern float FLOAT_80330a30;
+extern float FLOAT_80330a34;
+extern float FLOAT_80330a38;
+extern float FLOAT_80330a3c;
+extern float FLOAT_80330a40;
+extern float FLOAT_80330a44;
+extern float FLOAT_80330a48;
+extern float FLOAT_80330a4c;
+extern float FLOAT_80330a54;
+extern float FLOAT_80330a58;
+extern float FLOAT_80330a5c;
+
+extern double DOUBLE_80330a00;
+extern double DOUBLE_80330a20;
+}
 
 /*
  * --INFO--
@@ -131,12 +177,113 @@ void CRingMenu::onDraw()
 
 /*
  * --INFO--
- * Address:	TODO
- * Size:	TODO
+ * PAL Address: 0x800a3404
+ * PAL Size: 1472b
+ * EN Address: TODO
+ * EN Size: TODO
+ * JP Address: TODO
+ * JP Size: TODO
  */
 void CRingMenu::drawGBA()
 {
-	// TODO
+    int menuIndex = *reinterpret_cast<int*>(reinterpret_cast<char*>(this) + 0x8);
+    if (!((Game.game.m_gameWork.m_menuStageMode == 0 || menuIndex < 1) && (Game.game.m_scriptFoodBase[menuIndex] != 0))) {
+        return;
+    }
+
+    int displayCounter = *reinterpret_cast<int*>(reinterpret_cast<char*>(this) + 0x500);
+    double anim = (double)(FLOAT_80330a08 * (float)((double)(displayCounter + 0x80000000ULL) - DOUBLE_80330a00));
+    if (*reinterpret_cast<int*>(reinterpret_cast<char*>(this) + 0x504) != 0) {
+        anim = (double)(FLOAT_803309cc - (float)anim);
+    }
+    if ((double)FLOAT_803309c0 == anim) {
+        return;
+    }
+
+    SetTexture__8CMenuPcsFQ28CMenuPcs3TEX(MenuPcs, 0x16);
+
+    int gbaAnimCounter = *reinterpret_cast<int*>(reinterpret_cast<char*>(this) + 0x508);
+    double phaseSin =
+        (double)(float)sin((double)((FLOAT_80330a0c * (float)((double)(gbaAnimCounter + 0x80000000ULL) - DOUBLE_80330a00)) /
+                              FLOAT_80330a10));
+    if (*reinterpret_cast<int*>(reinterpret_cast<char*>(this) + 0x4FC) == 1) {
+        phaseSin = (double)(FLOAT_803309cc - (float)phaseSin);
+    }
+
+    float menuX = (menuIndex & 1) != 0 ? 592.0f : 48.0f;
+    float menuY = (menuIndex & 2) != 0 ? 400.0f : 48.0f;
+
+    SetAttrFmt__8CMenuPcsFQ28CMenuPcs3FMT(MenuPcs, 0);
+
+    int frameCounter = *reinterpret_cast<int*>(reinterpret_cast<char*>(this) + 0x4EC);
+    double pulseScale = (double)(FLOAT_80330a14 * (FLOAT_803309cc - (float)phaseSin) + FLOAT_803309cc);
+    double wave = (double)fmod(
+        (double)(FLOAT_80330a18 * (float)((double)(frameCounter + 0x80000000ULL) - DOUBLE_80330a00)),
+        DOUBLE_80330a20);
+    float waveF = (float)wave;
+    if (FLOAT_803309cc < waveF) {
+        waveF = FLOAT_80330a28 - waveF;
+    }
+
+    double phase = (double)(FLOAT_80330a2c * waveF);
+    double sinA = (double)(float)sin(phase);
+    double sinB = (double)(float)sin((double)(FLOAT_80330a30 + (float)phase));
+
+    SetTexture__8CMenuPcsFQ28CMenuPcs3TEX(MenuPcs, 0x1E);
+
+    double alphaScale = (double)(FLOAT_80330a34 * (float)phaseSin);
+    int shadowAlpha = (int)(FLOAT_803309c4 * (float)alphaScale * (float)anim);
+    unsigned char colorShadow[8];
+    __ct__6CColorFUcUcUcUc(colorShadow, 0, 0, 0, static_cast<unsigned char>(shadowAlpha));
+    SetColor__8CMenuPcsFR6CColor(MenuPcs, colorShadow);
+
+    double invPulse = (double)(FLOAT_803309cc - (float)pulseScale);
+    double drawX = (double)(menuX + FLOAT_80330a3c * (float)(pulseScale * sinB));
+    double drawY = (double)(menuY - FLOAT_80330a40 * (float)(pulseScale * sinA));
+    float v = static_cast<float>(menuIndex * 0x30);
+    DrawRect__8CMenuPcsFUlfffffffff(
+        MenuPcs, 3, (float)(drawX + FLOAT_80330a38), (float)(drawY + FLOAT_80330a38), FLOAT_80330a44, FLOAT_80330a48,
+        FLOAT_803309c0, v, FLOAT_80330a4c * (FLOAT_803309cc + (float)invPulse), FLOAT_80330a4c * ((float)pulseScale + (float)invPulse),
+        0.0f);
+
+    int iconAlpha = (int)((float)sinB * (float)anim);
+    unsigned char colorIcon[8];
+    __ct__6CColorFUcUcUcUc(colorIcon, 0xFF, 0xFF, 0xFF, static_cast<unsigned char>(iconAlpha));
+    SetColor__8CMenuPcsFR6CColor(MenuPcs, colorIcon);
+    DrawRect__8CMenuPcsFUlfffffffff(
+        MenuPcs, 3, (float)drawX, (float)drawY, FLOAT_80330a44, FLOAT_80330a48, FLOAT_803309c0, v,
+        FLOAT_80330a4c * (float)pulseScale, FLOAT_80330a4c * (float)pulseScale, 0.0f);
+
+    if (((*reinterpret_cast<unsigned int*>(CFlat + 0x12A0) & *reinterpret_cast<unsigned int*>(CFlat + 0x12A4) & 8) != 0) &&
+        !GetGBAStart__6JoyBusFi(Joybus, menuIndex)) {
+        if (IsInitSend__6JoyBusFi(Joybus, menuIndex) == 0) {
+            SetTexture__8CMenuPcsFQ28CMenuPcs3TEX(MenuPcs, 0x1D);
+            double blink = (double)sin(
+                (double)(FLOAT_80330a54 *
+                         (float)((double)(*reinterpret_cast<int*>(reinterpret_cast<char*>(this) + 0x4EC) + 0x80000000ULL) - DOUBLE_80330a00)));
+            unsigned int alpha =
+                static_cast<unsigned int>(FLOAT_803309c4 * (float)(iconAlpha * (FLOAT_803309cc + (float)blink)));
+            unsigned char colorBlink[8];
+            __ct__6CColorFUcUcUcUc(colorBlink, 0xFF, 0xFF, 0xFF, static_cast<unsigned char>(alpha & 0xFF));
+            SetColor__8CMenuPcsFR6CColor(MenuPcs, colorBlink);
+            DrawRect__8CMenuPcsFUlfffffffff(
+                MenuPcs, 3, (float)drawX, (float)drawY, FLOAT_80330a48, FLOAT_80330a48, FLOAT_803309c0, FLOAT_80330a58, FLOAT_803309cc,
+                FLOAT_803309cc, 0.0f);
+        } else {
+            int frame = static_cast<int>(System.m_frameCounter);
+            int sign = frame >> 0x1F;
+            unsigned int texSel = (sign * 0x10 | (unsigned int)(((frame >> 1) * 0x10000000 + sign) >> 0x1C)) - sign;
+            if ((int)texSel > 3) {
+                texSel &= 1;
+            }
+            SetTexture__8CMenuPcsFQ28CMenuPcs3TEX(MenuPcs, 0x1D);
+            DrawRect__8CMenuPcsFUlfffffffff(
+                MenuPcs, 3, (float)drawX, (float)drawY, FLOAT_80330a48, FLOAT_80330a48, FLOAT_803309c0, (float)(texSel * 0x30),
+                FLOAT_803309cc, FLOAT_803309cc, 0.0f);
+        }
+    }
+
+    DrawInit__8CMenuPcsFv(MenuPcs);
 }
 
 /*

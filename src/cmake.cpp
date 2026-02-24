6bf3aba089fedba4ad9400df499721e2c65330ad
Zachary Canann <zcanann@Zacharys-Mac-mini.local>
2026-02-24 14:19:06 -0800

    Implement village menu draw/destroy in cmake

--- src/cmake.cpp
+++ src/cmake.cpp
@@ -1,4 +1,39 @@
 #include "ffcc/cmake.h"
+#include "ffcc/p_game.h"
+
+extern "C" void __dl__FPv(void*);
+extern "C" void freeTexture__8CMenuPcsFiiii(CMenuPcs*, int, int, int, int);
+extern "C" void CmakeVillageDraw__8CMenuPcsFv(CMenuPcs*);
+extern "C" void CallWorldParam__8CMenuPcsFiii(CMenuPcs*, int, int, int);
+
+static inline short& MenuS16(CMenuPcs* menu, int offset)
+{
+    return *reinterpret_cast<short*>(reinterpret_cast<unsigned char*>(menu) + offset);
+}
+
+static inline int& MenuS32(CMenuPcs* menu, int offset)
+{
+    return *reinterpret_cast<int*>(reinterpret_cast<unsigned char*>(menu) + offset);
+}
+
+static inline unsigned char& MenuU8(CMenuPcs* menu, int offset)
+{
+    return *reinterpret_cast<unsigned char*>(reinterpret_cast<unsigned char*>(menu) + offset);
+}
+
+static inline void ReleaseRefObject(void* object)
+{
+    if (object == nullptr) {
+        return;
+    }
+
+    int* raw = reinterpret_cast<int*>(object);
+    int refCount = raw[1] - 1;
+    raw[1] = refCount;
+    if (refCount == 0) {
+        reinterpret_cast<void (*)(void*, int)>(*reinterpret_cast<int*>(raw[0] + 8))(object, 1);
+    }
+}
 
 /*
  * --INFO--
@@ -522,12 +557,32 @@ void CMenuPcs::createVillageMenu()
 
 /*
  * --INFO--
- * Address:	TODO
- * Size:	TODO
+ * PAL Address: 0x8016d19c
+ * PAL Size: 192b
+ * EN Address: TODO
+ * EN Size: TODO
+ * JP Address: TODO
+ * JP Size: TODO
  */
 void CMenuPcs::destroyVillageMenu()
 {
-	// TODO
+    if (MenuS16(this, 0x86C) != 0) {
+        if (Game.game.m_gameWork.m_menuStageMode == 0) {
+            void*& font = *reinterpret_cast<void**>(reinterpret_cast<unsigned char*>(this) + 0x108);
+            ReleaseRefObject(font);
+            font = nullptr;
+        }
+
+        freeTexture__8CMenuPcsFiiii(this, 8, 1, 0x60, 9);
+
+        int& villageWork = MenuS32(this, 0x830);
+        if (villageWork != 0) {
+            __dl__FPv(reinterpret_cast<void*>(villageWork));
+            villageWork = 0;
+        }
+
+        MenuS16(this, 0x86C) = 0;
+    }
 }
 
 /*
@@ -542,12 +597,29 @@ void CMenuPcs::calcVillageMenu()
 
 /*
  * --INFO--
- * Address:	TODO
- * Size:	TODO
+ * PAL Address: 0x8016cecc
+ * PAL Size: 140b
+ * EN Address: TODO
+ * EN Size: TODO
+ * JP Address: TODO
+ * JP Size: TODO
  */
 void CMenuPcs::drawVillageMenu()
 {
-	// TODO
+    if (MenuS16(this, 0x86C) != 0) {
+        int villageWork = MenuS32(this, 0x830);
+        CmakeVillageDraw__8CMenuPcsFv(this);
+        if (*reinterpret_cast<short*>(villageWork + 0x2E) != 0) {
+            short& mode = *reinterpret_cast<short*>(villageWork + 0x10);
+            if (mode < 2) {
+                mode = mode + 1;
+            } else {
+                MenuU8(this, 0x16) = 0;
+                CallWorldParam__8CMenuPcsFiii(this, 10, 0, 0);
+            }
+            *reinterpret_cast<short*>(villageWork + 0x22) = 0;
+        }
+    }
 }
 
 /*

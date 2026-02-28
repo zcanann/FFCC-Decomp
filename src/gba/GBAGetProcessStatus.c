8faf2175f1393f29f5d323e812c1cf03b7a4b7f1
asmithbot <asmithbot@proton.me>
2026-02-27 17:27:06 -0800

    gba: improve GBAGetProcessStatus match (#3000)

--- src/gba/GBAGetProcessStatus.c
+++ src/gba/GBAGetProcessStatus.c
@@ -1,40 +1,36 @@
 #include "dolphin/gba/GBAPriv.h"
 
 s32 GBAGetProcessStatus(s32 chan, u8* percentp) {
-    BOOL enabled;           // r26
-    s32 ret;                // r29
-    GBAControl* gba;        // r25
-    GBABootInfo* bootInfo;  // r31
-    u8 percent;             // r30
-    OSTime t;               // r27
+    GBAControl* gba = &__GBA[chan];
+    GBABootInfo* bootInfo = &gba->bootInfo;
+    BOOL enabled = OSDisableInterrupts();
+    s32 ret;
 
-    enabled = OSDisableInterrupts();
-    
-    gba = &__GBA[chan];
-    bootInfo = &__GBA[chan].bootInfo;
+    if (bootInfo->callback != NULL) {
+        u8 percent;
 
-    if (gba->callback != NULL || bootInfo->callback != NULL) {
         ret = GBA_BUSY;
-
-        if (bootInfo->callback != NULL) {
-            percent = (bootInfo->curOffset * 100) / bootInfo->realLength;
-            if (bootInfo->begin != 0) {
-                t = OSGetTime() - bootInfo->begin;
-                if (OSTicksToMilliseconds(t) < 5500) {
-                    percent = (percent * t) / OSMillisecondsToTicks(5500ll);
-                }
-
-                if (percent >= 100) {
-                    percent = 100;
-                }
+        percent = (bootInfo->curOffset * 100) / bootInfo->realLength;
+        if (bootInfo->begin != 0) {
+            OSTime t = OSGetTime() - bootInfo->begin;
+            if (OSTicksToMilliseconds(t) < 5500) {
+                percent = (percent * t) / OSMillisecondsToTicks(5500ll);
             }
 
-            if (percentp != NULL) {
-                *percentp = percent;
+            if (percent >= 100) {
+                percent = 100;
             }
         }
+
+        if (percentp != NULL) {
+            *percentp = percent;
+        }
     } else {
-        ret = GBA_READY;
+        if (gba->callback == NULL) {
+            ret = GBA_READY;
+        } else {
+            ret = GBA_BUSY;
+        }
     }
 
     OSRestoreInterrupts(enabled);

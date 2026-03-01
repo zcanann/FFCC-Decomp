048692402986f83ff1847e8b1a719dba74e4fd5c
Zachary Canann <zcanann@Zacharys-Mac-mini.local>
2026-03-01 02:26:14 -0800

    RedExecute: implement _AdsrStart and fill PAL metadata

--- src/RedSound/RedExecute.cpp
+++ src/RedSound/RedExecute.cpp
@@ -1011,12 +1011,47 @@ void SetVoiceSwitch(RedTrackDATA* track, int voiceSwitch)
 
 /*
  * --INFO--
- * Address:	TODO
- * Size:	TODO
+ * PAL Address: 0x801c4da0
+ * PAL Size: 188b
+ * EN Address: TODO
+ * EN Size: TODO
+ * JP Address: TODO
+ * JP Size: TODO
  */
-void _AdsrStart(RedVoiceDATA*)
+void _AdsrStart(RedVoiceDATA* voice)
 {
-	// TODO
+    int* voiceData = (int*)voice;
+    int envState = 0;
+    u16 envFrames = 0;
+    u8 envTarget = ((u8*)voiceData)[0x58];
+    u8 startLevel = envTarget;
+
+    do {
+        startLevel = envTarget;
+        envFrames = *(u16*)((u8*)voiceData + 0x50 + envState * 2);
+        envTarget = *(((u8*)voiceData + 0x59) + envState);
+        if (envFrames != 0) {
+            break;
+        }
+        envState += 1;
+    } while (envState < 3);
+
+    voiceData[0x17] = envState;
+    voiceData[0x18] = envFrames;
+
+    if (envTarget != 0) {
+        envTarget = ((envTarget + 1) * 0x100 - 1) * 0x1000;
+    }
+
+    if (envFrames == 0) {
+        voiceData[0x2B] = envTarget;
+    } else {
+        if (startLevel != 0) {
+            startLevel = ((startLevel + 1) * 0x100 - 1) * 0x1000;
+        }
+        voiceData[0x2B] = startLevel;
+        voiceData[0x19] = ((int)(envTarget | 0x800) - (int)startLevel) / (int)envFrames;
+    }
 }
 
 /*

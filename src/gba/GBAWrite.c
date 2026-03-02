b1638a0b83dd2d025329e27cfc6e0a907d2da506
Zachary Canann <zacharycanann@gmail.com>
2026-03-02 00:02:14 -0800

    gba: improve matching for GBAWrite unit control access

--- src/gba/GBAWrite.c
+++ src/gba/GBAWrite.c
@@ -1,23 +1,49 @@
 #include "dolphin/gba/GBAPriv.h"
 #include "string.h"
 
+/*
+ * --INFO--
+ * PAL Address: 0x801A88DC
+ * PAL Size: 48b
+ * EN Address: TODO
+ * EN Size: TODO
+ * JP Address: TODO
+ * JP Size: TODO
+ */
 static void WriteProc(s32 chan) {
-    if (__GBA[chan].ret != 0) {
+    GBAControl* gba;
+    gba = &__GBA[chan];
+
+    if (gba->ret != 0) {
         return;
     }
 
-    __GBA[chan].status[0] = __GBA[chan].input[0] & GBA_JSTAT_MASK;
+    gba->status[0] = gba->input[0] & GBA_JSTAT_MASK;
 }
 
+/*
+ * --INFO--
+ * PAL Address: 0x801A890C
+ * PAL Size: 148b
+ * EN Address: TODO
+ * EN Size: TODO
+ * JP Address: TODO
+ * JP Size: TODO
+ */
 s32 GBAWriteAsync(s32 chan, u8* src, u8* status, GBACallback callback) {
-    if (__GBA[chan].callback != NULL) {
+    GBAControl* gba;
+    gba = &__GBA[chan];
+
+    if (gba->callback != NULL) {
         return GBA_BUSY;
     }
-    __GBA[chan].output[0] = 0x15;
-    memcpy(&__GBA[chan].output[1], src, 4);
-    __GBA[chan].ptr = src;
-    __GBA[chan].status = status;
-    __GBA[chan].callback = callback;
+
+    gba->output[0] = 0x15;
+    memcpy(&gba->output[1], src, 4);
+    gba->ptr = src;
+    gba->status = status;
+    gba->callback = callback;
+
     return __GBATransfer(chan, 5, 1, WriteProc);
 }
 

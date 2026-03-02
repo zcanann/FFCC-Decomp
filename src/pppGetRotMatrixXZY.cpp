e49a238c71ab55bd33f3bd63a5975ee54d077a7c
Zachary Canann <zcanann@Zacharys-Mac-mini.local>
2026-03-02 13:06:12 -0800

    pppGetRotMatrixXZY: match angle loads and add function metadata

--- src/pppGetRotMatrixXZY.cpp
+++ src/pppGetRotMatrixXZY.cpp
@@ -7,19 +7,24 @@
 
 /*
  * --INFO--
- * Address:	TODO
- * Size:	TODO
+ * PAL Address: 0x80064948
+ * PAL Size: 120b
+ * EN Address: TODO
+ * EN Size: TODO
+ * JP Address: TODO
+ * JP Size: TODO
  */
 void pppGetRotMatrixXZY(pppFMATRIX& out, pppIVECTOR4* angle)
 {
+	s32* angle32 = (s32*)angle;
 	pppFMATRIX mY;
 	pppFMATRIX mZ;
 	pppFMATRIX zy;
 	pppFMATRIX mX;
 
-	pppGetRotMatrixY(mY, angle->y);
-	pppGetRotMatrixZ(mZ, angle->z);
+	pppGetRotMatrixY(mY, angle32[1]);
+	pppGetRotMatrixZ(mZ, angle32[2]);
 	PSMTXConcat(mZ.value, mY.value, zy.value);
-	pppGetRotMatrixX(mX, angle->x);
+	pppGetRotMatrixX(mX, angle32[0]);
 	PSMTXConcat(mX.value, zy.value, out.value);
 }

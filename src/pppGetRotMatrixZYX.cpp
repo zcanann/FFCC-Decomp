4069991b8494272c3dad8ce883835a3dda9ad5ae
AsclepiusBot <asclepiusbot@proton.me>
2026-03-02 12:57:52 -0800

    main/pppGetRotMatrixZYX: use packed angle words for full match (#3531)
    Co-authored-by: Zachary Canann <zcanann@Zacharys-Mac-mini.local>

--- src/pppGetRotMatrixZYX.cpp
+++ src/pppGetRotMatrixZYX.cpp
@@ -7,19 +7,24 @@
 
 /*
  * --INFO--
- * Address:	TODO
- * Size:	TODO
+ * PAL Address: 0x8005fa88
+ * PAL Size: 120b
+ * EN Address: TODO
+ * EN Size: TODO
+ * JP Address: TODO
+ * JP Size: TODO
  */
 void pppGetRotMatrixZYX(pppFMATRIX& out, pppIVECTOR4* angle)
 {
+    s32* angle32 = (s32*)angle;
     pppFMATRIX mX;
     pppFMATRIX mY;
     pppFMATRIX yx;
     pppFMATRIX mZ;
 
-    pppGetRotMatrixX(mX, angle->x);
-    pppGetRotMatrixY(mY, angle->y);
+    pppGetRotMatrixX(mX, angle32[0]);
+    pppGetRotMatrixY(mY, angle32[1]);
     PSMTXConcat(mY.value, mX.value, yx.value);
-    pppGetRotMatrixZ(mZ, angle->z);
+    pppGetRotMatrixZ(mZ, angle32[2]);
     PSMTXConcat(mZ.value, yx.value, out.value);
 }

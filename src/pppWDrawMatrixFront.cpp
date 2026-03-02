0e98368d2efaf4937e835f95a0ca9731fcaec22e
Zachary Canann <zcanann@Zacharys-Mac-mini.local>
2026-03-02 12:25:39 -0800

    Match pppWDrawMatrixFront using target linkage and offsets

--- src/pppWDrawMatrixFront.cpp
+++ src/pppWDrawMatrixFront.cpp
@@ -2,6 +2,11 @@
 #include "ffcc/partMng.h"
 #include <dolphin/mtx.h>
 
+extern "C" {
+extern unsigned char* lbl_8032ED50;
+extern Mtx ppvCameraMatrix02;
+}
+
 /*
  * --INFO--
  * PAL Address: 0x80090654
@@ -13,23 +18,23 @@
  */
 void pppWDrawMatrixFront(struct _pppPObject* param_1)
 {
-	Vec local_18;
-	
-	PSMTXScaleApply(
-		param_1->m_localMatrix.value,
-		(param_1 + 1)->m_localMatrix.value,
-		(pppMngStPtr->m_scale).x,
-		(pppMngStPtr->m_scale).y,
-		(pppMngStPtr->m_scale).z
-	);
-	
-	local_18.x = (param_1->m_localMatrix).value[0][3];
-	local_18.y = (param_1->m_localMatrix).value[1][3];
-	local_18.z = (param_1->m_localMatrix).value[2][3];
-	
-	PSMTXMultVec(ppvCameraMatrix0, &local_18, &local_18);
-	
-	param_1[1].m_localMatrix.value[0][3] = local_18.x;
-	param_1[1].m_localMatrix.value[1][3] = local_18.y;
-	param_1[1].m_localMatrix.value[2][3] = local_18.z;
+    Vec localPos;
+
+    PSMTXScaleApply(
+        *(Mtx*)((char*)param_1 + 0x10),
+        *(Mtx*)((char*)param_1 + 0x40),
+        *(f32*)(lbl_8032ED50 + 0x28),
+        *(f32*)(lbl_8032ED50 + 0x2C),
+        *(f32*)(lbl_8032ED50 + 0x30)
+    );
+
+    localPos.x = *(float*)((char*)param_1 + 0x1C);
+    localPos.y = *(float*)((char*)param_1 + 0x2C);
+    localPos.z = *(float*)((char*)param_1 + 0x3C);
+
+    PSMTXMultVec(ppvCameraMatrix02, &localPos, &localPos);
+
+    *(float*)((char*)param_1 + 0x4C) = localPos.x;
+    *(float*)((char*)param_1 + 0x5C) = localPos.y;
+    *(float*)((char*)param_1 + 0x6C) = localPos.z;
 }

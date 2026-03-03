ed849fec4de06363ce9da3af140502c899acc525
AsclepiusBot <asclepiusbot@proton.me>
2026-03-02 19:57:10 -0800

    MSL signal: restore critical-region guards in raise (#3591)
    Co-authored-by: Zachary Canann <zcanann@Zacharys-Mac-mini.local>

--- src/MSL_C/PPCEABI/bare/H/signal.c
+++ src/MSL_C/PPCEABI/bare/H/signal.c
@@ -1,6 +1,7 @@
 #include <stddef.h>
 #include "PowerPC_EABI_Support/Msl/MSL_C/MSL_Common/signal.h"
 #include "PowerPC_EABI_Support/Msl/MSL_C/MSL_Common/abort_exit.h"
+#include "PowerPC_EABI_Support/Msl/MSL_C/MSL_Common/critical_regions.h"
 
 __signal_func_ptr signal_funcs[6];
 
@@ -10,10 +11,14 @@ int raise(int sig) {
     if (sig < 1 || sig > 6) {
         return -1;
     }
+
+    __begin_critical_region(4);
     temp_r31 = signal_funcs[sig - 1];
     if ((unsigned long) temp_r31 != 1) {
         signal_funcs[sig - 1] = NULL;
     }
+    __end_critical_region(4);
+
     if ((unsigned long) temp_r31 == 1 || (temp_r31 == NULL && sig == 1)) {
         return 0;
     }

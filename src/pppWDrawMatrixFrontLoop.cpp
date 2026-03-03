874f39c91bebe599711c9e924afa71911c3d4b85
AsclepiusBot <asclepiusbot@proton.me>
2026-03-02 18:32:12 -0800

    pppWDrawMatrixFrontLoop: align globals to match front variant (#3562)
    Co-authored-by: Zachary Canann <zcanann@Zacharys-Mac-mini.local>
    Co-authored-by: Zachary Canann <zacharycanann@gmail.com>

--- src/pppWDrawMatrixFrontLoop.cpp
+++ src/pppWDrawMatrixFrontLoop.cpp
@@ -18,23 +18,23 @@ extern Mtx ppvCameraMatrix02;
  */
 void pppWDrawMatrixFrontLoop(struct _pppPObject* param_1)
 {
-	Vec local_18;
-	
-	PSMTXScaleApply(
-		*(Mtx*)((char*)param_1 + 0x10),
-		*(Mtx*)((char*)param_1 + 0x40),
-		*(f32*)(lbl_8032ED50 + 0x28),
-		*(f32*)(lbl_8032ED50 + 0x2C),
-		*(f32*)(lbl_8032ED50 + 0x30)
-	);
-	
-	local_18.x = *(float*)((char*)param_1 + 0x1c);
-	local_18.y = *(float*)((char*)param_1 + 0x2c);
-	local_18.z = *(float*)((char*)param_1 + 0x3c);
-	
-	PSMTXMultVec(ppvCameraMatrix02, &local_18, &local_18);
-	
-	*(float*)((char*)param_1 + 0x4c) = local_18.x;
-	*(float*)((char*)param_1 + 0x5c) = local_18.y;
-	*(float*)((char*)param_1 + 0x6c) = local_18.z;
+    Vec localPos;
+
+    PSMTXScaleApply(
+        *(Mtx*)((char*)param_1 + 0x10),
+        *(Mtx*)((char*)param_1 + 0x40),
+        *(f32*)(lbl_8032ED50 + 0x28),
+        *(f32*)(lbl_8032ED50 + 0x2C),
+        *(f32*)(lbl_8032ED50 + 0x30)
+    );
+
+    localPos.x = *(float*)((char*)param_1 + 0x1C);
+    localPos.y = *(float*)((char*)param_1 + 0x2C);
+    localPos.z = *(float*)((char*)param_1 + 0x3C);
+
+    PSMTXMultVec(ppvCameraMatrix02, &localPos, &localPos);
+
+    *(float*)((char*)param_1 + 0x4C) = localPos.x;
+    *(float*)((char*)param_1 + 0x5C) = localPos.y;
+    *(float*)((char*)param_1 + 0x6C) = localPos.z;
 }

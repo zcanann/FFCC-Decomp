2428573cc925f39596ce7b76b64ccceaf32de687
Zachary Canann <zcanann@Zacharys-Mac-mini.local>
2026-03-02 17:02:41 -0800

    pppAngle: use third parameter for serialized offset data

--- src/pppAngle.cpp
+++ src/pppAngle.cpp
@@ -12,22 +12,20 @@ void pppAngle(void* dest, void* src, void* param1, void* param2)
     if (lbl_8032ED70 != 0) {
         return;
     }
-    
-    int* srcData = (int*)src;
-    int* destData = (int*)dest;
-    
-    if (srcData[0] != destData[3]) {
+
+    if (*(int*)src != *((int*)dest + 3)) {
         return;
     }
-    
-    int* param2Data = (int*)param2;
-    int* offsetPtr = (int*)param2Data[3];
+
+    (void)param2;
+
+    int* offsetPtr = (int*)((int*)param1)[3];
     int offset = offsetPtr[0];
     int* destPtr = (int*)((char*)dest + offset + 0x80);
     int* srcPtr = (int*)((char*)src + 8);
-    
+
     destPtr[0] += srcPtr[0];
-    destPtr[1] += srcPtr[1]; 
+    destPtr[1] += srcPtr[1];
     destPtr[2] += srcPtr[2];
 }
 

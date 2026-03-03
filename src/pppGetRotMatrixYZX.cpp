17df163ccfa85b741c140a3b2004d445ec4ea035
AsclepiusBot <asclepiusbot@proton.me>
2026-03-02 19:29:07 -0800

    Match pppGetRotMatrixYZX angle access and metadata (#3582)

--- src/pppGetRotMatrixYZX.cpp
+++ src/pppGetRotMatrixYZX.cpp
@@ -7,19 +7,24 @@
 
 /*
  * --INFO--
- * Address:	TODO
- * Size:	TODO
+ * PAL Address: 0x8005f93c
+ * PAL Size: 120b
+ * EN Address: TODO
+ * EN Size: TODO
+ * JP Address: TODO
+ * JP Size: TODO
  */
 void pppGetRotMatrixYZX(pppFMATRIX& out, pppIVECTOR4* angle)
 {
+    s32* angle32 = (s32*)angle;
     pppFMATRIX mX;
     pppFMATRIX mZ;
     pppFMATRIX zx;
     pppFMATRIX mY;
 
-    pppGetRotMatrixX(mX, angle->x);
-    pppGetRotMatrixZ(mZ, angle->z);
+    pppGetRotMatrixX(mX, angle32[0]);
+    pppGetRotMatrixZ(mZ, angle32[2]);
     PSMTXConcat(mZ.value, mX.value, zx.value);
-    pppGetRotMatrixY(mY, angle->y);
+    pppGetRotMatrixY(mY, angle32[1]);
     PSMTXConcat(mY.value, zx.value, out.value);
 }

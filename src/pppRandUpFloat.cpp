0bc43db8043c2dd0b93b9245822ac55fd357d2f9
Zachary Canann <zcanann@Zacharys-Mac-mini.local>
2026-03-02 16:11:19 -0800

    pppRandUpFloat: align extern types and RandF usage

--- src/pppRandUpFloat.cpp
+++ src/pppRandUpFloat.cpp
@@ -2,13 +2,11 @@
 #include "ffcc/math.h"
 #include "types.h"
 
-extern CMath math;
+extern CMath math[];
 extern s32 lbl_8032ED70;
 extern f32 lbl_8032FFF8;
-extern f32 lbl_801EADC8;
-extern "C" {
-f32 RandF__5CMathFv(CMath*);
-}
+extern f32 lbl_801EADC8[];
+extern "C" float RandF__5CMathFv(CMath*);
 
 struct RandUpFloatParam {
     s32 targetId;
@@ -43,10 +41,10 @@ void pppRandUpFloat(void* param1, void* param2, void* param3) {
 
     s32 id = *(s32*)(base + 0xC);
     if (id == 0) {
-        f32 value = RandF__5CMathFv(&math);
+        f32 value = RandF__5CMathFv(math);
 
         if (p2->randomTwice != 0) {
-            value = (value + RandF__5CMathFv(&math)) * lbl_8032FFF8;
+            value = (value + RandF__5CMathFv(math)) * lbl_8032FFF8;
         }
 
         valuePtr = (f32*)(base + *p3->outputOffset + 0x80);
@@ -61,7 +59,7 @@ void pppRandUpFloat(void* param1, void* param2, void* param3) {
     s32 sourceOffset = p2->sourceOffset;
     f32* source;
     if (sourceOffset == -1) {
-        source = &lbl_801EADC8;
+        source = &lbl_801EADC8[0];
     } else {
         source = (f32*)(base + sourceOffset + 0x80);
     }

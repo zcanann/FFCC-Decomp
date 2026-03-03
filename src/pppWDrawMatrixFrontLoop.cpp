4a0324bbaa7d3d28bc1d6238492065f3dd63b534
Zachary Canann <zcanann@Zacharys-Mac-mini.local>
2026-03-02 16:59:46 -0800

    pppWDrawMatrixFrontLoop: align globals to match front variant

--- src/pppWDrawMatrixFrontLoop.cpp
+++ src/pppWDrawMatrixFrontLoop.cpp
@@ -2,6 +2,11 @@
 #include "ffcc/partMng.h"
 #include <dolphin/mtx.h>
 
+extern "C" {
+extern unsigned char* lbl_8032ED50;
+extern Mtx ppvCameraMatrix02;
+}
+
 /*
  * --INFO--
  * PAL Address: 0x800c4f50
@@ -13,23 +18,23 @@
  */
 void pppWDrawMatrixFrontLoop(struct _pppPObject* param_1)
 {
-	Vec local_18;
-	
-	PSMTXScaleApply(
-		*(Mtx*)((char*)param_1 + 0x10),
-		*(Mtx*)((char*)param_1 + 0x40),
-		(pppMngStPtr->m_scale).x,
-		(pppMngStPtr->m_scale).y,
-		(pppMngStPtr->m_scale).z
-	);
-	
-	local_18.x = *(float*)((char*)param_1 + 0x1c);
-	local_18.y = *(float*)((char*)param_1 + 0x2c);
-	local_18.z = *(float*)((char*)param_1 + 0x3c);
-	
-	PSMTXMultVec(ppvCameraMatrix0, &local_18, &local_18);
-	
-	*(float*)((char*)param_1 + 0x4c) = local_18.x;
-	*(float*)((char*)param_1 + 0x5c) = local_18.y;
-	*(float*)((char*)param_1 + 0x6c) = local_18.z;
+    Vec localPos;
+
+    PSMTXScaleApply(
+        *(Mtx*)((char*)param_1 + 0x10),
+        *(Mtx*)((char*)param_1 + 0x40),
+        *(f32*)(lbl_8032ED50 + 0x28),
+        *(f32*)(lbl_8032ED50 + 0x2C),
+        *(f32*)(lbl_8032ED50 + 0x30)
+    );
+
+    localPos.x = *(float*)((char*)param_1 + 0x1C);
+    localPos.y = *(float*)((char*)param_1 + 0x2C);
+    localPos.z = *(float*)((char*)param_1 + 0x3C);
+
+    PSMTXMultVec(ppvCameraMatrix02, &localPos, &localPos);
+
+    *(float*)((char*)param_1 + 0x4C) = localPos.x;
+    *(float*)((char*)param_1 + 0x5C) = localPos.y;
+    *(float*)((char*)param_1 + 0x6C) = localPos.z;
 }

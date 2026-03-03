9118904f8193618697d2f49bc5715a7c11e4d761
AsclepiusBot <asclepiusbot@proton.me>
2026-03-02 18:33:21 -0800

    main/pppRandUpShort: improve type/layout fidelity (#3578)
    Co-authored-by: Zachary Canann <zcanann@Zacharys-Mac-mini.local>

--- src/pppRandUpShort.cpp
+++ src/pppRandUpShort.cpp
@@ -6,7 +6,7 @@ extern CMath math[];
 extern s32 lbl_8032ED70;
 extern f32 lbl_80330038;
 extern f64 lbl_80330040;
-extern s16 lbl_801EADC8;
+extern s16 lbl_801EADC8[];
 extern "C" f32 RandF__5CMathFv(CMath* instance);
 
 struct RandUpShortParam {
@@ -32,20 +32,21 @@ struct RandUpShortCtx {
  */
 extern "C" void pppRandUpShort(void* param1, void* param2, void* param3)
 {
-    if (lbl_8032ED70 != 0) {
-        return;
-    }
-
     u8* base = (u8*)param1;
     RandUpShortParam* in = (RandUpShortParam*)param2;
     RandUpShortCtx* ctx = (RandUpShortCtx*)param3;
+    s16* target;
     f32* valuePtr;
+
+    if (lbl_8032ED70 != 0) {
+        return;
+    }
 
     s32 state = *(s32*)(base + 0xC);
     if (state == 0) {
         f32 value = RandF__5CMathFv(&math[0]);
         if (in->randomTwice != 0) {
-            value = (value + RandF__5CMathFv(&math[0])) * lbl_80330038;
+            value = lbl_80330038 * (value + RandF__5CMathFv(&math[0]));
         }
 
         valuePtr = (f32*)(base + *ctx->outputOffset + 0x80);
@@ -57,9 +58,8 @@ extern "C" void pppRandUpShort(void* param1, void* param2, void* param3)
         valuePtr = (f32*)(base + *ctx->outputOffset + 0x80);
     }
 
-    s16* target;
     if (in->sourceOffset == -1) {
-        target = &lbl_801EADC8;
+        target = lbl_801EADC8;
     } else {
         target = (s16*)(base + in->sourceOffset + 0x80);
     }
@@ -71,9 +71,11 @@ extern "C" void pppRandUpShort(void* param1, void* param2, void* param3)
             u32 lo;
         } parts;
     } cvt;
+    s16 scale = in->scale;
     cvt.parts.hi = 0x43300000;
-    cvt.parts.lo = (u16)in->scale;
+    cvt.parts.lo = (u16)scale;
 
-    s32 delta = (s32)((cvt.d - lbl_80330040) * *valuePtr);
+    f32 value = *valuePtr;
+    s32 delta = (s32)((cvt.d - lbl_80330040) * value);
     *target = (s16)(*target + delta);
 }

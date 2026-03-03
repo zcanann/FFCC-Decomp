3229988cd9d05f296d4e052cb5847048826141e0
Zachary Canann <zcanann@Zacharys-Mac-mini.local>
2026-03-02 18:16:52 -0800

    Align pppMatrixXYZ/XZY ABI and matrix access pattern

--- src/pppMatrixXYZ.cpp
+++ src/pppMatrixXYZ.cpp
@@ -11,43 +11,46 @@
  * JP Address: TODO  
  * JP Size: TODO
  */
-void pppMatrixXYZ(pppFMATRIX& matrix, PPPCREATEPARAM* param)
+void pppMatrixXYZ(void* target, void* unused, void* param)
 {
+    (void)unused;
+
+    f32* matrix = (f32*)target;
     u32* offsets = (u32*)*(void**)((u8*)param + 0xC);
-    pppIVECTOR4* angle = (pppIVECTOR4*)((u8*)&matrix + offsets[1] + 0x80);
-    f32* scale = (f32*)((u8*)&matrix + offsets[2] + 0x80);
-    f32* translation = (f32*)((u8*)&matrix + offsets[0] + 0x80);
+    pppIVECTOR4* angle = (pppIVECTOR4*)((u8*)target + offsets[1] + 0x80);
+    f32* translation = (f32*)((u8*)target + offsets[0] + 0x80);
+    f32* scale = (f32*)((u8*)target + offsets[2] + 0x80);
     Vec tempVec1;
     Vec tempVec2;
     Vec tempVec3;
 
-    pppGetRotMatrixXYZ(matrix, angle);
+    pppGetRotMatrixXYZ(*(pppFMATRIX*)(matrix + 4), angle);
 
-    tempVec1.x = matrix.value[0][0];
-    tempVec1.y = matrix.value[1][0];
-    tempVec1.z = matrix.value[2][0];
+    tempVec1.x = matrix[4];
+    tempVec1.y = matrix[8];
+    tempVec1.z = matrix[12];
     PSVECScale(&tempVec1, &tempVec1, scale[0]);
-    matrix.value[0][0] = tempVec1.x;
-    matrix.value[1][0] = tempVec1.y;
-    matrix.value[2][0] = tempVec1.z;
+    matrix[4] = tempVec1.x;
+    matrix[8] = tempVec1.y;
+    matrix[12] = tempVec1.z;
 
-    tempVec2.x = matrix.value[0][1];
-    tempVec2.y = matrix.value[1][1];
-    tempVec2.z = matrix.value[2][1];
+    tempVec2.x = matrix[5];
+    tempVec2.y = matrix[9];
+    tempVec2.z = matrix[13];
     PSVECScale(&tempVec2, &tempVec2, scale[1]);
-    matrix.value[0][1] = tempVec2.x;
-    matrix.value[1][1] = tempVec2.y;
-    matrix.value[2][1] = tempVec2.z;
+    matrix[5] = tempVec2.x;
+    matrix[9] = tempVec2.y;
+    matrix[13] = tempVec2.z;
 
-    tempVec3.x = matrix.value[0][2];
-    tempVec3.y = matrix.value[1][2];
-    tempVec3.z = matrix.value[2][2];
+    tempVec3.x = matrix[6];
+    tempVec3.y = matrix[10];
+    tempVec3.z = matrix[14];
     PSVECScale(&tempVec3, &tempVec3, scale[2]);
-    matrix.value[0][2] = tempVec3.x;
-    matrix.value[1][2] = tempVec3.y;
-    matrix.value[2][2] = tempVec3.z;
+    matrix[6] = tempVec3.x;
+    matrix[10] = tempVec3.y;
+    matrix[14] = tempVec3.z;
 
-    matrix.value[0][3] = translation[0];
-    matrix.value[1][3] = translation[1];
-    matrix.value[2][3] = translation[2];
+    matrix[7] = translation[0];
+    matrix[11] = translation[1];
+    matrix[15] = translation[2];
 }

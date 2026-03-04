0363ee3a595bf21d6c102ac0b122f130e7ade895
Noelle Harper <noelle21b@proton.me>
2026-03-03 21:27:17 -0800

    Improve pppSRandUpFV match and data typing (#3868)
    Co-authored-by: Zachary Canann <zacharycanann@gmail.com>

--- src/pppSRandUpFV.cpp
+++ src/pppSRandUpFV.cpp
@@ -1,11 +1,12 @@
 #include "ffcc/pppSRandUpFV.h"
 #include "ffcc/math.h"
+#include "dolphin/types.h"
 
 extern CMath math[];
-extern int lbl_8032ED70;
-extern float lbl_803300C0;
-extern float lbl_801EADC8[];
-extern "C" float RandF__5CMathFv(CMath*);
+extern s32 lbl_8032ED70;
+extern f32 lbl_803300C0;
+extern f32 lbl_801EADC8[];
+extern "C" f32 RandF__5CMathFv(CMath*);
 
 /*
  * --INFO--
@@ -38,78 +39,79 @@ void randf(unsigned char param)
  */
 extern "C" void pppSRandUpFV(void* param1, void* param2, void* param3)
 {
-    struct Param {
-        int index;
-        int offset;
-        float x;
-        float y;
-        float z;
-        unsigned char _pad[4];
-        unsigned char blendTwice;
+    struct SRandUpFVParam {
+        s32 index;
+        s32 targetOffset;
+        f32 x;
+        f32 y;
+        f32 z;
+        u8 _pad14[0x18 - 0x14];
+        u8 blendTwice;
     };
-    struct SelectInfo {
-        int _pad0;
-        int _pad1;
-        int _pad2;
-        int* offsetPtr;
+    struct SRandUpFVCtx {
+        u8 _pad0[0xC];
+        s32* outputOffset;
     };
 
-    unsigned char* self = reinterpret_cast<unsigned char*>(param1);
-    Param* cfg = reinterpret_cast<Param*>(param2);
-    float* randVec;
-    int currentIndex;
+    u8* base = (u8*)param1;
+    SRandUpFVParam* in = (SRandUpFVParam*)param2;
+    SRandUpFVCtx* ctx = (SRandUpFVCtx*)param3;
+    f32* randVec;
 
     if (lbl_8032ED70 != 0) {
         return;
     }
 
-    currentIndex = *reinterpret_cast<int*>(self + 0xC);
-    if (currentIndex == 0) {
-        int offset = *reinterpret_cast<SelectInfo*>(param3)->offsetPtr;
-        unsigned char blendTwice = cfg->blendTwice;
-        randVec = reinterpret_cast<float*>(self + offset + 0x80);
+    s32 state = *(s32*)(base + 0xC);
+    if (state == 0) {
+        randVec = (f32*)(base + *ctx->outputOffset + 0x80);
 
-        float value = RandF__5CMathFv(math);
-        if (blendTwice != 0) {
-            value = (value + RandF__5CMathFv(math)) * lbl_803300C0;
+        {
+            u8 flag = in->blendTwice;
+            f32 value = RandF__5CMathFv(math);
+            if (flag != 0) {
+                value = (value + RandF__5CMathFv(math)) * lbl_803300C0;
+            }
+            randVec[0] = value;
         }
-        randVec[0] = value;
 
-        value = RandF__5CMathFv(math);
-        if (blendTwice != 0) {
-            value = (value + RandF__5CMathFv(math)) * lbl_803300C0;
+        {
+            u8 flag = in->blendTwice;
+            f32 value = RandF__5CMathFv(math);
+            if (flag != 0) {
+                value = (value + RandF__5CMathFv(math)) * lbl_803300C0;
+            }
+            randVec[1] = value;
         }
-        randVec[1] = value;
 
-        value = RandF__5CMathFv(math);
-        if (blendTwice != 0) {
-            value = (value + RandF__5CMathFv(math)) * lbl_803300C0;
+        {
+            u8 flag = in->blendTwice;
+            f32 value = RandF__5CMathFv(math);
+            if (flag != 0) {
+                value = (value + RandF__5CMathFv(math)) * lbl_803300C0;
+            }
+            randVec[2] = value;
         }
-        randVec[2] = value;
     } else {
-        if (cfg->index != currentIndex) {
+        if (in->index != state) {
             return;
         }
-        randVec = reinterpret_cast<float*>(self + *reinterpret_cast<SelectInfo*>(param3)->offsetPtr + 0x80);
+        randVec = (f32*)(base + *ctx->outputOffset + 0x80);
     }
 
-    float* target;
-    if (cfg->offset == -1) {
-        target = lbl_801EADC8;
-    } else {
-        target = reinterpret_cast<float*>(self + cfg->offset + 0x80);
-    }
+    s32 targetOffset = in->targetOffset;
+    f32* target = (targetOffset == -1) ? lbl_801EADC8 : (f32*)(base + targetOffset + 0x80);
 
     {
-        float value = cfg->x * randVec[0];
+        f32 value = in->x * randVec[0];
         target[0] = target[0] + value;
     }
     {
-        float value = cfg->y * randVec[1];
+        f32 value = in->y * randVec[1];
         target[1] = target[1] + value;
     }
     {
-        float value = cfg->z * randVec[2];
+        f32 value = in->z * randVec[2];
         target[2] = target[2] + value;
     }
 }

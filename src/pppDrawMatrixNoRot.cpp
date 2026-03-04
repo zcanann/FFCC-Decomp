53f1a11c8b2676d425f755c74b2c081f9ce60cde
asmithbot <asmithbot@proton.me>
2026-03-03 18:37:36 -0800

    main/pppDrawMatrixNoRot: match global scale access in pppDrawMatrixNoRot (#3787)
    Co-authored-by: Zachary Canann <zacharycanann@gmail.com>

--- src/pppDrawMatrixNoRot.cpp
+++ src/pppDrawMatrixNoRot.cpp
@@ -17,15 +17,15 @@ void pppDrawMatrixNoRot(struct _pppPObject* param_1)
     PSMTXScaleApply(
         *(Mtx*)((char*)param_1 + 0x10),
         *(Mtx*)((char*)param_1 + 0x40), 
-        (pppMngStPtr->m_scale).x,
-        (pppMngStPtr->m_scale).y, 
-        (pppMngStPtr->m_scale).z
+        (((_pppMngSt*)lbl_8032ED50)->m_scale).x,
+        (((_pppMngSt*)lbl_8032ED50)->m_scale).y, 
+        (((_pppMngSt*)lbl_8032ED50)->m_scale).z
     );
     *(float*)((char*)param_1 + 0x4c) = 
-        (*(float*)((char*)param_1 + 0x1c)) * (pppMngStPtr->m_scale).x +
+        (*(float*)((char*)param_1 + 0x1c)) * (((_pppMngSt*)lbl_8032ED50)->m_scale).x +
         ppvWorldMatrix[0][3];
     *(float*)((char*)param_1 + 0x5c) =
-        (*(float*)((char*)param_1 + 0x2c)) * (pppMngStPtr->m_scale).y + ppvWorldMatrix[1][3];
+        (*(float*)((char*)param_1 + 0x2c)) * (((_pppMngSt*)lbl_8032ED50)->m_scale).y + ppvWorldMatrix[1][3];
     *(float*)((char*)param_1 + 0x6c) =
-        (*(float*)((char*)param_1 + 0x3c)) * (pppMngStPtr->m_scale).z + ppvWorldMatrix[2][3];
+        (*(float*)((char*)param_1 + 0x3c)) * (((_pppMngSt*)lbl_8032ED50)->m_scale).z + ppvWorldMatrix[2][3];
 }

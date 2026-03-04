5ba7309ff70bd21fd2c6cb49b2a5f9c1006be1ea
AsclepiusBot <asclepiusbot@proton.me>
2026-03-03 21:30:04 -0800

    pppYmCheckBGHeight: align map collision frame logic with typed structures (#3870)
    Co-authored-by: Zachary Canann <zcanann@Zacharys-Mac-mini.local>

--- src/pppYmCheckBGHeight.cpp
+++ src/pppYmCheckBGHeight.cpp
@@ -1,4 +1,5 @@
 #include "ffcc/pppYmCheckBGHeight.h"
+#include "ffcc/map.h"
 #include "ffcc/maphit.h"
 #include "ffcc/pppPart.h"
 
@@ -6,7 +7,6 @@
 
 extern int lbl_8032ED70;
 extern struct _pppMngSt* pppMngStPtr;
-extern unsigned char MapMng[];
 
 // Float constants (addresses from Ghidra)
 extern float lbl_80330ED0;
@@ -14,20 +14,8 @@ extern float lbl_80330ED4;
 extern float lbl_80330ED8;
 extern float lbl_80330EDC;
 
-struct CMapCylinderRaw
-{
-    Vec m_bottom;
-    Vec m_direction;
-    float m_radius;
-    float m_height;
-    Vec m_top;
-    Vec m_direction2;
-    float m_radius2;
-    float m_height2;
-};
-
 extern "C" {
-    int CheckHitCylinderNear__7CMapMngFP12CMapCylinderP3VecUl(void*, CMapCylinder*, Vec*, unsigned int);
+    int CheckHitCylinderNear__7CMapMngFP12CMapCylinderP3VecUl(CMapMng*, CMapCylinder*, Vec*, unsigned int);
     void CalcHitPosition__7CMapObjFP3Vec(void*, Vec*);
     void* pppSetFpMatrix__FP9_pppMngSt(struct _pppMngSt*);
 }
@@ -57,60 +45,55 @@ void pppConstructYmCheckBGHeight(void)
  */
 struct pppYmCheckBGHeight* pppFrameYmCheckBGHeight(struct pppYmCheckBGHeight* pppYmCheckBGHeight, struct UnkC* param_2)
 {
+    _pppMngSt* pppMngSt = pppMngStPtr;
+
     if (lbl_8032ED70 == 0) {
-        _pppMngSt* pppMngSt = pppMngStPtr;
         Vec direction;
-        CMapCylinderRaw cyl;
-        float* cylF = (float*)&cyl;
+        CMapCylinder cyl;
         Vec hitPos;
-        float currentY;
-        float resolvedY;
+        double currentY;
         float baseX;
         float baseZ;
-        float bottomY;
 
         direction.x = lbl_80330ED0;
         direction.y = lbl_80330ED4;
         direction.z = lbl_80330ED0;
 
-        currentY = ((float*)pppMngSt)[0x94 / sizeof(float)];
-        resolvedY = currentY;
-        baseX = ((float*)pppMngSt)[0x84 / sizeof(float)];
-        baseZ = ((float*)pppMngSt)[0xA4 / sizeof(float)];
-        bottomY = currentY + param_2->m_unk0x4;
+        currentY = (double)pppMngSt->m_matrix.value[1][3];
+        baseX = pppMngSt->m_matrix.value[0][3];
+        baseZ = pppMngSt->m_matrix.value[2][3];
 
-        cylF[12] = lbl_80330ED8;
-        cylF[11] = lbl_80330ED8;
-        cylF[10] = lbl_80330ED8;
-        cylF[15] = lbl_80330EDC;
-        cylF[14] = lbl_80330EDC;
-        cylF[13] = lbl_80330EDC;
-        cylF[6] = lbl_80330ED0;
-        cylF[7] = lbl_80330ED4;
-        cylF[8] = lbl_80330ED0;
-        cylF[9] = lbl_80330ED0;
         cyl.m_bottom.x = baseX;
-        cyl.m_bottom.y = bottomY;
+        cyl.m_bottom.y = (float)(currentY + (double)param_2->m_unk0x4);
         cyl.m_bottom.z = baseZ;
+        cyl.m_top.x = lbl_80330ED8;
+        cyl.m_top.y = lbl_80330ED8;
+        cyl.m_top.z = lbl_80330ED8;
+        cyl.m_direction2.x = lbl_80330EDC;
+        cyl.m_direction2.y = lbl_80330EDC;
+        cyl.m_direction2.z = lbl_80330EDC;
+        cyl.m_radius = lbl_80330ED0;
+        cyl.m_height = lbl_80330ED4;
+        cyl.m_direction.x = lbl_80330ED0;
+        cyl.m_direction.y = lbl_80330ED4;
+        cyl.m_direction.z = lbl_80330ED0;
+        cyl.m_radius2 = lbl_80330ED0;
 
-        if (CheckHitCylinderNear__7CMapMngFP12CMapCylinderP3VecUl(MapMng, (CMapCylinder*)&cyl, &direction, 0xffffffff) != 0) {
-            CalcHitPosition__7CMapObjFP3Vec(*(void**)(MapMng + 0x22A78), &hitPos);
-            if (currentY - ((float*)param_2)[3] > hitPos.y) {
-                resolvedY = currentY;
-            } else {
-                resolvedY = hitPos.y + param_2->m_unk0x8;
+        if (CheckHitCylinderNear__7CMapMngFP12CMapCylinderP3VecUl(&MapMng, &cyl, &direction, 0xFFFFFFFF) != 0) {
+            CalcHitPosition__7CMapObjFP3Vec(*(void**)((u8*)&MapMng + 0x22A78), &hitPos);
+            if ((float)(currentY - (double)param_2->m_serializedDataOffsets) <= hitPos.y) {
+                currentY = (double)(hitPos.y + param_2->m_unk0x8);
             }
         }
-        currentY = resolvedY;
 
-        ((float*)pppMngSt)[0x0C / sizeof(float)] = currentY;
-        ((float*)pppMngSt)[0x5C / sizeof(float)] = currentY;
-        ((float*)pppMngSt)[0x6C / sizeof(float)] = currentY;
-        ((float*)pppMngSt)[0x4C / sizeof(float)] = currentY;
+        pppMngSt->m_position.y = (float)currentY;
+        pppMngSt->m_savedPosition.y = (float)currentY;
+        pppMngSt->m_paramVec0.y = (float)currentY;
+        pppMngSt->m_previousPosition.y = (float)currentY;
 
-        ((float*)pppMngStPtr)[0x84 / sizeof(float)] = ((float*)pppMngSt)[0x08 / sizeof(float)];
-        ((float*)pppMngStPtr)[0x94 / sizeof(float)] = ((float*)pppMngSt)[0x0C / sizeof(float)];
-        ((float*)pppMngStPtr)[0xA4 / sizeof(float)] = ((float*)pppMngSt)[0x10 / sizeof(float)];
+        pppMngStPtr->m_matrix.value[0][3] = pppMngSt->m_position.x;
+        pppMngStPtr->m_matrix.value[1][3] = pppMngSt->m_position.y;
+        pppMngStPtr->m_matrix.value[2][3] = pppMngSt->m_position.z;
 
         pppYmCheckBGHeight = (struct pppYmCheckBGHeight*)pppSetFpMatrix__FP9_pppMngSt(pppMngSt);
     }

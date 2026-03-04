f44a9ece1e526c0cdd1cee0ffe37d29ef48bd893
Zachary Canann <zacharycanann@gmail.com>
2026-03-04 13:30:18 -0800

    p_mc: match __sinit_p_mc_cpp table init staging

--- src/p_mc.cpp
+++ src/p_mc.cpp
@@ -38,18 +38,26 @@ struct MenuPcsMcLayout
  */
 extern "C" void __sinit_p_mc_cpp(void)
 {
-    unsigned int* table = (unsigned int*)lbl_80211DAC;
-    lbl_8032EE88 = (unsigned int)lbl_80211F28;
+    unsigned int* table;
+    unsigned int* table0;
+    unsigned int* table1;
+    unsigned int* table2;
 
-    table[1] = lbl_80211D88[0];
-    table[2] = lbl_80211D88[1];
-    table[3] = lbl_80211D88[2];
-    table[4] = lbl_80211D94[0];
-    table[5] = lbl_80211D94[1];
-    table[6] = lbl_80211D94[2];
-    table[7] = lbl_80211DA0[0];
-    table[8] = lbl_80211DA0[1];
-    table[9] = lbl_80211DA0[2];
+    lbl_8032EE88 = (unsigned int)lbl_80211F28;
+    table = (unsigned int*)lbl_80211DAC;
+    table0 = lbl_80211D88;
+    table1 = lbl_80211D94;
+    table2 = lbl_80211DA0;
+
+    table[1] = table0[0];
+    table[2] = table0[1];
+    table[3] = table0[2];
+    table[4] = table1[0];
+    table[5] = table1[1];
+    table[6] = table1[2];
+    table[7] = table2[0];
+    table[8] = table2[1];
+    table[9] = table2[2];
 }
 
 /*

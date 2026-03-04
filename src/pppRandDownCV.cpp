3a444a0d60adc91a5c3f0dd919832229f0aa9ad6
AsclepiusBot <asclepiusbot@proton.me>
2026-03-03 22:28:06 -0800

    Improve pppRandDownCV code/data match (#3892)
    Co-authored-by: Zachary Canann <zcanann@Zacharys-Mac-mini.local>

--- src/pppRandDownCV.cpp
+++ src/pppRandDownCV.cpp
@@ -49,39 +49,36 @@ void pppRandDownCV(void* param1, void* param2, void* param3)
     u8* base = (u8*)param1;
     PppRandDownCVParam2* in = (PppRandDownCVParam2*)param2;
     PppRandDownCVParam3* out = (PppRandDownCVParam3*)param3;
+    u8* target;
     f32* valuePtr;
-    f32 value;
 
     if (lbl_8032ED70 != 0) {
         return;
     }
 
     if (in->field0 == *(s32*)(base + 0xC)) {
-        value = -RandF__5CMathFv(math);
+        f32 value = -RandF__5CMathFv(&math[0]);
         if (in->fieldC != 0) {
-            value = (value - RandF__5CMathFv(math)) * lbl_8032FF28;
+            value = (value - RandF__5CMathFv(&math[0])) * lbl_8032FF28;
         }
+
         valuePtr = (f32*)(base + *out->fieldC + 0x80);
         *valuePtr = value;
+    } else if (in->field0 != *(s32*)(base + 0xC)) {
+        return;
     } else {
-        if (in->field0 != *(s32*)(base + 0xC)) {
-            return;
-        }
         valuePtr = (f32*)(base + *out->fieldC + 0x80);
     }
 
-    u8* target;
-    if (in->field4 == -1) {
-        target = lbl_801EADC8;
-    } else {
-        target = (u8*)(base + in->field4 + 0x80);
-    }
+    target = (in->field4 == -1) ? &lbl_801EADC8[0] : (u8*)(base + in->field4 + 0x80);
 
-    f32 scale = *valuePtr;
-    target[0] = (u8)(target[0] + (s32)((f32)in->field8 * scale));
-    target[1] = (u8)(target[1] + (s32)((f32)in->field9 * scale));
-    target[2] = (u8)(target[2] + (s32)((f32)in->fieldA * scale));
-    target[3] = (u8)(target[3] + (s32)((f32)in->fieldB * scale));
+    {
+        f32 scale = *valuePtr;
+        target[0] = (u8)(target[0] + (s32)((f32)in->field8 * scale));
+        target[1] = (u8)(target[1] + (s32)((f32)in->field9 * scale));
+        target[2] = (u8)(target[2] + (s32)((f32)in->fieldA * scale));
+        target[3] = (u8)(target[3] + (s32)((f32)in->fieldB * scale));
+    }
 }
 
 }

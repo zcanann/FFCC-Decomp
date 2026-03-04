ea6caabe4cd539af2306e1ab6213e38320a68dd9
Zachary Canann <zacharycanann@gmail.com>
2026-03-03 20:12:44 -0800

    KeLns: bind zero/one constants to SDA labels

--- src/pppKeLns.cpp
+++ src/pppKeLns.cpp
@@ -2,6 +2,8 @@
 #include "ffcc/KeLns.h"
 #include "dolphin/types.h"
 
+extern "C" const float lbl_803305E0;
+
 /*
  * --INFO--
  * PAL Address: 0x800957d4
@@ -15,9 +17,9 @@ void pppKeLnsLpCon(void* pObject, void* pPart)
 {
 	u32 offset = *(u32*)*(u32*)((u8*)pPart + 0xC);
 	_KeLnsLp* keLnsLp = (_KeLnsLp*)((u8*)pObject + offset + 0x80);
-	f32 zero = 0.0f;
 
 	KeLnsLp_Init(keLnsLp);
+	f32 zero = lbl_803305E0;
 	*(f32*)((u8*)keLnsLp + 0x8C) = zero;
 	*(f32*)((u8*)keLnsLp + 0x98) = zero;
 }
@@ -35,7 +37,7 @@ void pppKeLnsLpCon2(void* pObject, void* pPart)
 {
 	u32 offset = *(u32*)*(u32*)((u8*)pPart + 0xC);
 	u8* keLnsLp = (u8*)pObject + offset + 0x80;
-	f32 zero = 0.0f;
+	f32 zero = lbl_803305E0;
 
 	*(f32*)(keLnsLp + 0x8C) = zero;
 	*(f32*)(keLnsLp + 0x98) = zero;

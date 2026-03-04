1b72b5437153a1be83ee089f9bcf00610b68f453
asmithbot <asmithbot@proton.me>
2026-03-03 19:52:46 -0800

    TRK: fix usr_puts_serial OSReport prototype for full match (#3818)
    Co-authored-by: Zachary Canann <zacharycanann@gmail.com>

--- src/TRK_MINNOW_DOLPHIN/usr_put.c
+++ src/TRK_MINNOW_DOLPHIN/usr_put.c
@@ -1,7 +1,8 @@
 #include "TRK_MINNOW_DOLPHIN/Os/dolphin/usr_put.h"
 #include "TRK_MINNOW_DOLPHIN/MetroTRK/Portable/msghndlr.h"
 #include "PowerPC_EABI_Support/MetroTRK/trk.h"
-#include "dolphin/os.h"
+
+extern void OSReport(const char*);
 
 BOOL usr_puts_serial(const char* msg)
 {

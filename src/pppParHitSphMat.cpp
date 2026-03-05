8b327527bc7aa2bfc44d2dee74371187122c65e7
BrilliantWasp <brilliantwasp@proton.me>
2026-03-05 11:57:23 -0800

    Improve pppParHitSphMat typing and matrix setup (#4173)
    Co-authored-by: Zachary Canann <zcanann@Zacharys-Mac-mini.local>

--- src/pppParHitSphMat.cpp
+++ src/pppParHitSphMat.cpp
@@ -19,62 +19,59 @@ extern Mtx ppvCameraMatrix02;
  * JP Address: TODO
  * JP Size: TODO
  */
-void pppParHitSphMat(void* param1, void* param2, void* param3)
+void pppParHitSphMat(struct _pppPObject* param_1, int param_2, int param_3)
 {
     Vec local_88;
     Vec local_94;
     Vec local_a0;
-    Mtx local_48;
-    Mtx MStack_78;
+    Mtx sphereMtx;
+    Mtx cameraMtx;
     _GXColor local_a8;
-    u8* data = (u8*)param1;
-    u8* step = (u8*)param2;
     _pppMngSt* pppMngSt = (_pppMngSt*)lbl_8032ED50;
-    f32 radius;
+    float radius;
 
     local_88.z = lbl_80332080;
     local_88.y = lbl_80332080;
     local_88.x = lbl_80332080;
 
-    if (step[0xC] != 0) {
-        s32* offsets = *(s32**)((u8*)param3 + 0xC);
-        Vec* src = (Vec*)(data + offsets[1] + 0x80);
-
+    if (*(u8*)(param_2 + 0xC) != 0) {
+        s32* offsets = *(s32**)(param_3 + 0xC);
+        Vec* src = (Vec*)((u8*)param_1 + offsets[1] + 0x80);
         PSMTXMultVec(pppMngSt->m_matrix.value, src, &local_94);
     } else {
-        local_94.x = pppMngSt->m_matrix.value[0][3];
-        local_94.y = pppMngSt->m_matrix.value[1][3];
-        local_94.z = pppMngSt->m_matrix.value[2][3];
-        s32* offsets = *(s32**)((u8*)param3 + 0xC);
-        Vec* src = (Vec*)(data + offsets[1] + 0x80);
-
-        local_94.x = local_94.x + src->x;
-        local_94.y = local_94.y + src->y;
-        local_94.z = local_94.z + src->z;
+        local_94.x = *(float*)(lbl_8032ED50 + 0x84);
+        local_94.y = *(float*)(lbl_8032ED50 + 0x94);
+        local_94.z = *(float*)(lbl_8032ED50 + 0xA4);
+        s32* offsets = *(s32**)(param_3 + 0xC);
+        Vec* src = (Vec*)((u8*)param_1 + offsets[1] + 0x80);
+        local_94.x += src->x;
+        local_94.y += src->y;
+        local_94.z += src->z;
     }
 
-    if (*(f32*)(step + 4) != lbl_80332080) {
-        PSVECSubtract((Vec*)((u8*)pppMngSt + 8), (Vec*)((u8*)pppMngSt + 0x48), &local_88);
+    if (*(float*)(param_2 + 4) != lbl_80332080) {
+        PSVECSubtract((Vec*)(lbl_8032ED50 + 8), (Vec*)(lbl_8032ED50 + 0x48), &local_88);
     }
 
-    radius = *(f32*)((u8*)pppMngSt + 0x64) * *(f32*)(step + 8);
-    pppHitCylinderSendSystem(pppMngSt, &local_94, &local_88, radius, *(f32*)(step + 4));
+    radius = *(float*)((u8*)pppMngSt + 0x64) * *(float*)(param_2 + 8);
+    pppHitCylinderSendSystem(pppMngSt, &local_94, &local_88, radius, *(float*)(param_2 + 4));
 
     if ((*(u32*)(CFlat + 0x129c) & 0x200000) != 0) {
         local_a8.r = 0xFF;
         local_a8.g = 0xFF;
         local_a8.b = 0xFF;
         local_a8.a = 0xFF;
-        PSMTXIdentity(MStack_78);
-        PSMTXIdentity(local_48);
-        local_48[0][0] = radius;
-        local_48[1][1] = radius;
-        local_48[2][2] = radius;
-        PSMTXConcat(ppvCameraMatrix02, MStack_78, MStack_78);
-        PSMTXMultVec(MStack_78, &local_94, &local_a0);
-        local_48[0][3] = local_a0.x;
-        local_48[1][3] = local_a0.y;
-        local_48[2][3] = local_a0.z;
-        Graphic.DrawSphere(local_48, local_a8);
+        register MtxPtr sphereMtxPtr = sphereMtx;
+        PSMTXIdentity(cameraMtx);
+        PSMTXIdentity(sphereMtxPtr);
+        sphereMtxPtr[0][0] = radius;
+        sphereMtxPtr[1][1] = radius;
+        sphereMtxPtr[2][2] = radius;
+        PSMTXConcat(ppvCameraMatrix02, cameraMtx, cameraMtx);
+        PSMTXMultVec(cameraMtx, &local_94, &local_a0);
+        sphereMtxPtr[0][3] = local_a0.x;
+        sphereMtxPtr[1][3] = local_a0.y;
+        sphereMtxPtr[2][3] = local_a0.z;
+        Graphic.DrawSphere(sphereMtxPtr, local_a8);
     }
 }

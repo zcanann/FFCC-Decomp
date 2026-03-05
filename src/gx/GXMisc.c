3f3917472a368384ec9ca414f8da061070148c1e
Zachary Canann <zacharycanann@gmail.com>
2026-03-05 12:41:51 -0800

    gx: match __GXPEInit in GXMisc

--- src/gx/GXMisc.c
+++ src/gx/GXMisc.c
@@ -9,7 +9,7 @@
 static GXDrawSyncCallback TokenCB;
 static GXDrawDoneCallback DrawDoneCB;
 static u8 DrawDone;
-static OSThreadQueue FinishQueue;
+static OSThreadQueue lbl_8032F33C;
 
 void GXSetMisc(GXMiscToken token, u32 val) {
     switch (token) {
@@ -150,7 +150,7 @@ void GXWaitDrawDone(void) {
 
     enabled = OSDisableInterrupts();
     while (!DrawDone) {
-        OSSleepThread(&FinishQueue);
+        OSSleepThread(&lbl_8032F33C);
     }
     OSRestoreInterrupts(enabled);
 }
@@ -375,16 +375,23 @@ static void GXFinishInterruptHandler(__OSInterrupt interrupt, OSContext* context
         OSClearContext(&exceptionContext);
         OSSetCurrentContext(context);
     }
-    OSWakeupThread(&FinishQueue);
+    OSWakeupThread(&lbl_8032F33C);
 }
 
 void __GXPEInit(void) {
+    u16 reg;
+
     __OSSetInterruptHandler(0x12, GXTokenInterruptHandler);
     __OSSetInterruptHandler(0x13, GXFinishInterruptHandler);
-    OSInitThreadQueue(&FinishQueue);
+    OSInitThreadQueue(&lbl_8032F33C);
     __OSUnmaskInterrupts(0x2000);
     __OSUnmaskInterrupts(0x1000);
-    GX_SET_PE_REG(5, GX_GET_PE_REG(5) & 0xFFF0 | 0xF);
+    reg = GX_GET_PE_REG(5);
+    reg = (reg & ~(1 << 2)) | (1 << 2);
+    reg = (reg & ~(1 << 3)) | (1 << 3);
+    reg = (reg & ~(1 << 0)) | (1 << 0);
+    reg = (reg & ~(1 << 1)) | (1 << 1);
+    GX_SET_PE_REG(5, reg);
 }
 
 u32 GXCompressZ16(u32 z24, GXZFmt16 zfmt) {

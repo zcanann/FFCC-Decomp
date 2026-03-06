547a5dd22d7a59f4d4c3552aaeef1e67b5895f6d
BrilliantWasp <brilliantwasp@proton.me>
2026-03-06 12:35:59 -0800

    main/pppRandCV: use Math global in pppRandCV (#4431)
    Co-authored-by: Zachary Canann <zcanann@Zacharys-Mac-mini.local>

--- src/pppRandCV.cpp
+++ src/pppRandCV.cpp
@@ -2,7 +2,7 @@
 #include "ffcc/math.h"
 #include "dolphin/types.h"
 
-extern CMath math[];
+extern CMath Math;
 extern int gPppCalcDisabled;
 extern f32 lbl_8032FF08;
 extern u8 gPppDefaultValueBuffer[];
@@ -42,9 +42,9 @@ void pppRandCV(void* param1, void* param2, void* param3)
     }
 
     if (params->index == *(s32*)(base + 0xC)) {
-        f32 value = RandF__5CMathFv(&math[0]);
+        f32 value = RandF__5CMathFv(&Math);
         if (params->flag != 0) {
-            value += RandF__5CMathFv(&math[0]);
+            value += RandF__5CMathFv(&Math);
         } else {
             value *= lbl_8032FF08;
         }

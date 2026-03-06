419056ea74c1d53da379e718622a5e9afa77ea99
Zachary Canann <zcanann@Zacharys-Mac-mini.local>
2026-03-06 07:39:55 -0800

    Refine pppPointAp control flow and offset handling

--- src/pppPointAp.cpp
+++ src/pppPointAp.cpp
@@ -48,39 +48,41 @@ void pppPointApCon(_pppPObject* pObject, _pppCtrlTable* ctrlTable)
  */
 void pppPointAp(_pppPObject* pObject, void* step, _pppCtrlTable* ctrlTable)
 {
-    _pppPointApOffsets* data = (_pppPointApOffsets*)ctrlTable->m_serializedDef;
+    u32* offsets = (u32*)ctrlTable->m_serializedDef;
     _pppPointApStep* payload = (_pppPointApStep*)step;
-    Vec* src = (Vec*)((u8*)pObject + data->srcOffset + 0x80);
-    u8* target = (u8*)pObject + data->targetOffset + 0x80;
+    Vec* source = (Vec*)((u8*)pObject + offsets[0] + 0x80);
+    u8* target = (u8*)pObject + offsets[1] + 0x80;
 
-    if (lbl_8032ED70 == 0) {
-        if (target[1] == 0) {
-            u32 objId = payload->m_createProgramIndex;
-            if ((objId + 0x10000) != 0xFFFF) {
-                _pppPDataVal* objData = (_pppPDataVal*)(*(u32*)((u8*)lbl_8032ED50 + 0xD4) + (objId << 4));
-                _pppPObject* obj;
-
-                if (objData == 0) {
-                    obj = 0;
-                } else {
-                    obj = pppCreatePObject((_pppMngSt*)lbl_8032ED50, objData);
-                }
-
-                *(_pppPObject**)((u8*)obj + 4) = pObject;
-
-                Vec* dst = (Vec*)((u8*)obj + payload->m_childDstOffset + 0x80);
-                if (payload->m_useWorldMatrix == 0) {
-                    dst->x = src->x;
-                    dst->y = src->y;
-                    dst->z = src->z;
-                } else {
-                    PSMTXMultVec((MtxPtr)((u8*)lbl_8032ED50 + 0x78), src, dst);
-                }
-
-                target[1] = payload->m_cooldown;
-            }
-        }
-
-        target[1]--;
+    if (lbl_8032ED70 != 0) {
+        return;
     }
+
+    if (target[1] == 0) {
+        u32 objId = payload->m_createProgramIndex;
+        if ((objId + 0x10000) != 0xFFFF) {
+            _pppPDataVal* objData = (_pppPDataVal*)(*(u32*)((u8*)lbl_8032ED50 + 0xD4) + (objId << 4));
+            _pppPObject* obj;
+
+            if (objData == 0) {
+                obj = 0;
+            } else {
+                obj = pppCreatePObject((_pppMngSt*)lbl_8032ED50, objData);
+            }
+
+            *(_pppPObject**)((u8*)obj + 4) = pObject;
+
+            Vec* dst = (Vec*)((u8*)obj + payload->m_childDstOffset + 0x80);
+            if (payload->m_useWorldMatrix == 0) {
+                dst->x = source->x;
+                dst->y = source->y;
+                dst->z = source->z;
+            } else {
+                PSMTXMultVec((MtxPtr)((u8*)lbl_8032ED50 + 0x78), source, dst);
+            }
+
+            target[1] = payload->m_cooldown;
+        }
+    }
+
+    target[1]--;
 }

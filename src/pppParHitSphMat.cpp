cf900b2eab39394e2dd02b07ad9301248c5b025b
Zachary Canann <zacharycanann@gmail.com>
2026-03-05 19:19:50 -0800

    pppParHitSphMat: use DrawSphere C entrypoint for debug path

--- src/pppParHitSphMat.cpp
+++ src/pppParHitSphMat.cpp
@@ -1,14 +1,16 @@
 #include "ffcc/pppParHitSphMat.h"
-#include "ffcc/graphic.h"
 #include "ffcc/partMng.h"
 #include "ffcc/pppPart.h"
 
+#include <dolphin/gx.h>
 #include <dolphin/mtx.h>
 
 extern unsigned char* lbl_8032ED50;
 extern float lbl_80332080;
 extern unsigned char CFlat[];
+extern unsigned char Graphic[];
 extern Mtx ppvCameraMatrix02;
+extern "C" void DrawSphere__8CGraphicFPA4_f8_GXColor(void* graphic, MtxPtr mtx, _GXColor color);
 
 /*
  * --INFO--
@@ -24,11 +26,11 @@ void pppParHitSphMat(struct _pppPObject* param_1, int param_2, int param_3)
     Vec local_88;
     Vec local_94;
     Vec local_a0;
-    _GXColor local_a8;
-    float radius;
     Mtx sphereMtx;
     Mtx cameraMtx;
+    _GXColor local_a8;
     _pppMngSt* pppMngSt = (_pppMngSt*)lbl_8032ED50;
+    float radius;
 
     local_88.z = lbl_80332080;
     local_88.y = lbl_80332080;
@@ -57,22 +59,20 @@ void pppParHitSphMat(struct _pppPObject* param_1, int param_2, int param_3)
     pppHitCylinderSendSystem(pppMngSt, &local_94, &local_88, radius, *(float*)(param_2 + 4));
 
     if ((*(u32*)(CFlat + 0x129c) & 0x200000) != 0) {
-        f32 sphereScale = radius;
         local_a8.r = 0xFF;
         local_a8.g = 0xFF;
         local_a8.b = 0xFF;
         local_a8.a = 0xFF;
-        register MtxPtr sphereMtxPtr = sphereMtx;
         PSMTXIdentity(cameraMtx);
-        PSMTXIdentity(sphereMtxPtr);
-        sphereMtxPtr[0][0] = sphereScale;
-        sphereMtxPtr[1][1] = sphereScale;
-        sphereMtxPtr[2][2] = sphereScale;
+        PSMTXIdentity(sphereMtx);
+        sphereMtx[0][0] = radius;
+        sphereMtx[1][1] = radius;
+        sphereMtx[2][2] = radius;
         PSMTXConcat(ppvCameraMatrix02, cameraMtx, cameraMtx);
         PSMTXMultVec(cameraMtx, &local_94, &local_a0);
-        sphereMtxPtr[0][3] = local_a0.x;
-        sphereMtxPtr[1][3] = local_a0.y;
-        sphereMtxPtr[2][3] = local_a0.z;
-        Graphic.DrawSphere(sphereMtxPtr, local_a8);
+        sphereMtx[0][3] = local_a0.x;
+        sphereMtx[1][3] = local_a0.y;
+        sphereMtx[2][3] = local_a0.z;
+        DrawSphere__8CGraphicFPA4_f8_GXColor(Graphic, sphereMtx, local_a8);
     }
 }

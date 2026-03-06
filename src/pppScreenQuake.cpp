ffec0858b6a50a43d1f658003b9817c921553bed
Zachary Canann <zacharycanann@gmail.com>
2026-03-06 08:18:20 -0800

    Reorder pppScreenQuake function layout

--- src/pppScreenQuake.cpp
+++ src/pppScreenQuake.cpp
@@ -22,52 +22,49 @@ extern "C" void SetQuakeParameter__10CCameraPcsFiissffffffi(
 
 /*
  * --INFO--
- * PAL Address: 0x8013e548
- * PAL Size: 60b
+ * PAL Address: 0x8013e3bc
+ * PAL Size: 4b
  * EN Address: TODO
  * EN Size: TODO
  * JP Address: TODO
  * JP Size: TODO
  */
-void pppConScreenQuake(pppScreenQuake *quake, pppScreenQuakeUnkC *param2)
+void pppRenderScreenQuake(pppScreenQuake*, pppScreenQuakeUnkB*, pppScreenQuakeUnkC*)
 {
-	float val = lbl_80331FC8;
-	float* data = (float*)((char *)&quake->field0_0x0 + 128 + *param2->m_serializedDataOffsets);
-	
-	data[2] = val;
-	data[1] = val;
-	data[0] = val;
-	data[5] = val;
-	data[4] = val;
-	data[3] = val;
-	data[8] = val;
-	data[7] = val;
-	data[6] = val;
 }
 
 /*
  * --INFO--
- * PAL Address: 0x8013e50c
- * PAL Size: 60b
+ * PAL Address: 0x8013e3c0
+ * PAL Size: 248b
  * EN Address: TODO
  * EN Size: TODO
  * JP Address: TODO
  * JP Size: TODO
  */
-void pppCon2ScreenQuake(pppScreenQuake *quake, pppScreenQuakeUnkC *param2)
+void pppFrameScreenQuake(pppScreenQuake *quake, pppScreenQuakeUnkB *param2, pppScreenQuakeUnkC *param3)
 {
-	float val = lbl_80331FC8;
-	float* data = (float*)((char *)&quake->field0_0x0 + 128 + *param2->m_serializedDataOffsets);
-	
-	data[2] = val;
-	data[1] = val;
-	data[0] = val;
-	data[5] = val;
-	data[4] = val;
-	data[3] = val;
-	data[8] = val;
-	data[7] = val;
-	data[6] = val;
+    if (lbl_8032ED70 == 0) {
+        float *value = (float *)((char *)quake + 0x80 + *param3->m_serializedDataOffsets);
+
+        CalcGraphValue((_pppPObject *)&quake->field0_0x0, param2->m_graphId, value[0], value[1], value[2], param2->m_dataValIndex, param2->m_initWOrk, param2->m_stepValue);
+        CalcGraphValue((_pppPObject *)&quake->field0_0x0, param2->m_graphId, value[3], value[4], value[5], param2->m_arg3, param2->m_initWOrk2, param2->m_stepValue2);
+        CalcGraphValue((_pppPObject *)&quake->field0_0x0, param2->m_graphId, value[6], value[7], value[8], param2->m_arg4, param2->m_initWOrk3, param2->m_stepValue3);
+
+        SetQuakeParameter__10CCameraPcsFiissffffffi(
+            &CameraPcs,
+            1,
+            0,
+            0,
+            0,
+            value[0],
+            value[3],
+            value[6],
+            param2->m_quakeParam0,
+            param2->m_quakeParam1,
+            param2->m_quakeParam2,
+            1);
+    }
 }
 
 /*
@@ -82,68 +79,70 @@ void pppCon2ScreenQuake(pppScreenQuake *quake, pppScreenQuakeUnkC *param2)
 #pragma scheduling off
 void pppDesScreenQuake(pppScreenQuake*, pppScreenQuakeUnkC*)
 {
-	float value = lbl_80331FC8;
+    float value = lbl_80331FC8;
 
-	SetQuakeParameter__10CCameraPcsFiissffffffi(
-	    &CameraPcs,
-	    0,
-	    0,
-	    0,
-	    0,
-	    value,
-	    value,
-	    value,
-	    value,
-	    value,
-	    value,
-	    1);
+    SetQuakeParameter__10CCameraPcsFiissffffffi(
+        &CameraPcs,
+        0,
+        0,
+        0,
+        0,
+        value,
+        value,
+        value,
+        value,
+        value,
+        value,
+        1);
 }
 #pragma scheduling reset
 
 /*
  * --INFO--
- * PAL Address: 0x8013e3c0
- * PAL Size: 248b
+ * PAL Address: 0x8013e50c
+ * PAL Size: 60b
  * EN Address: TODO
  * EN Size: TODO
  * JP Address: TODO
  * JP Size: TODO
  */
-void pppFrameScreenQuake(pppScreenQuake *quake, pppScreenQuakeUnkB *param2, pppScreenQuakeUnkC *param3)
+void pppCon2ScreenQuake(pppScreenQuake *quake, pppScreenQuakeUnkC *param2)
 {
-	if (lbl_8032ED70 == 0) {
-		float *value = (float *)((char *)quake + 0x80 + *param3->m_serializedDataOffsets);
+    float val = lbl_80331FC8;
+    float *data = (float *)((char *)&quake->field0_0x0 + 128 + *param2->m_serializedDataOffsets);
 
-		CalcGraphValue((_pppPObject *)&quake->field0_0x0, param2->m_graphId, value[0], value[1], value[2], param2->m_dataValIndex, param2->m_initWOrk, param2->m_stepValue);
-		CalcGraphValue((_pppPObject *)&quake->field0_0x0, param2->m_graphId, value[3], value[4], value[5], param2->m_arg3, param2->m_initWOrk2, param2->m_stepValue2);
-		CalcGraphValue((_pppPObject *)&quake->field0_0x0, param2->m_graphId, value[6], value[7], value[8], param2->m_arg4, param2->m_initWOrk3, param2->m_stepValue3);
-
-		SetQuakeParameter__10CCameraPcsFiissffffffi(
-		    &CameraPcs,
-		    1,
-		    0,
-		    0,
-		    0,
-		    value[0],
-		    value[3],
-		    value[6],
-		    param2->m_quakeParam0,
-		    param2->m_quakeParam1,
-		    param2->m_quakeParam2,
-		    1);
-	}
+    data[2] = val;
+    data[1] = val;
+    data[0] = val;
+    data[5] = val;
+    data[4] = val;
+    data[3] = val;
+    data[8] = val;
+    data[7] = val;
+    data[6] = val;
 }
 
 /*
  * --INFO--
- * PAL Address: 0x8013e3bc
- * PAL Size: 4b
+ * PAL Address: 0x8013e548
+ * PAL Size: 60b
  * EN Address: TODO
  * EN Size: TODO
  * JP Address: TODO
  * JP Size: TODO
  */
-void pppRenderScreenQuake(pppScreenQuake*, pppScreenQuakeUnkB*, pppScreenQuakeUnkC*)
+void pppConScreenQuake(pppScreenQuake *quake, pppScreenQuakeUnkC *param2)
 {
-}
+    float val = lbl_80331FC8;
+    float *data = (float *)((char *)&quake->field0_0x0 + 128 + *param2->m_serializedDataOffsets);
 
+    data[2] = val;
+    data[1] = val;
+    data[0] = val;
+    data[5] = val;
+    data[4] = val;
+    data[3] = val;
+    data[8] = val;
+    data[7] = val;
+    data[6] = val;
+}

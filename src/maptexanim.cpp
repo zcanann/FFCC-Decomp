b178645646c42bb7bd27d98384889c6dffd2ccee
Zachary Canann <zcanann@Zacharys-Mac-mini.local>
2026-03-07 15:04:22 -0800

    maptexanim: rewrite SetMapTexAnim loop structure

--- src/maptexanim.cpp
+++ src/maptexanim.cpp
@@ -326,10 +326,11 @@ void CMapTexAnimSet::Calc()
 void CMapTexAnimSet::SetMapTexAnim(int materialId, int frameStart, int frameEnd, int wrapMode)
 {
     int found = 0;
+    int i = 0;
     int setPtr = reinterpret_cast<int>(this);
     short material = static_cast<short>(materialId);
 
-    for (int i = 0; i < S16At(this, 8); i++) {
+    while (i < S16At(this, 8)) {
         void* animPtr = reinterpret_cast<void*>(*reinterpret_cast<int*>(setPtr + 0xC));
         if (S16At(animPtr, 0x12) == material) {
             int end = frameEnd;
@@ -354,6 +355,7 @@ void CMapTexAnimSet::SetMapTexAnim(int materialId, int frameStart, int frameEnd,
             found = 1;
         }
         setPtr += 4;
+        i += 1;
     }
 
     if ((found == 0) && (static_cast<unsigned int>(System.m_execParam) >= 1)) {

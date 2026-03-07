8e95b1922b8a2b88e5438c21ba5a7000b4d0cc5b
Zachary Canann <zcanann@Zacharys-Mac-mini.local>
2026-03-07 12:46:40 -0800

    memory: match CMemory::SetGroup bit-pack expression

--- src/memory.cpp
+++ src/memory.cpp
@@ -654,7 +654,7 @@ void CMemory::Draw()
 void CMemory::SetGroup(void* ptr, int group)
 {
     unsigned char* header = reinterpret_cast<unsigned char*>(ptr) - 0x3e;
-    *header = (*header & 0x0F) | (static_cast<unsigned char>(group) << 4);
+    *header = static_cast<unsigned char>((*header & 0x0F) | (group << 4));
 }
 
 /*

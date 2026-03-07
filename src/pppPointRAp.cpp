97905842793a82a2fd1655d67e440ed7b87008a0
Zachary Canann <zacharycanann@gmail.com>
2026-03-06 18:41:44 -0800

    Improve pppPointRAp trig/offset expression ordering

--- src/pppPointRAp.cpp
+++ src/pppPointRAp.cpp
@@ -77,13 +77,14 @@ void pppPointRAp(_pppPObject* pObject, void* step, _pppCtrlTable* ctrlTable)
         float* trig = gPppTrigTable;
         s32 angleA = (s32)(gPppPointRApRandomAngleRange * RandF__5CMathFv(&Math) - gPppPointRApRandomAngleBias);
         float scaleA = payload->m_radius;
-        float yOff = scaleA * *(float*)((u8*)trig + ((angleA + 0x4000) & 0xFFFC));
-        float planarOff = scaleA * *(float*)((u8*)trig + (angleA & 0xFFFC));
-        s32 angleB = (s32)(gPppPointRApSpinScale * (gPppPointRApRandomAngleRange * RandF__5CMathFv(&Math)));
-        Vec* dstPos = (Vec*)((u8*)obj + payload->m_childPosOffset + 0x80);
-        Vec* dstVel = (Vec*)((u8*)obj + payload->m_childVelocityOffset + 0x80);
+        float planarOff = scaleA * *(float*)((u8*)trig + ((angleA + 0x4000) & 0xFFFC));
+        float yOff = scaleA * *(float*)((u8*)trig + (angleA & 0xFFFC));
+        float spinAngle = gPppPointRApRandomAngleRange * RandF__5CMathFv(&Math);
+        s32 angleB = (s32)(spinAngle * gPppPointRApSpinScale);
         float xOff = planarOff * *(float*)((u8*)trig + (angleB & 0xFFFC));
         float zOff = planarOff * *(float*)((u8*)trig + ((angleB + 0x4000) & 0xFFFC));
+        Vec* dstPos = (Vec*)((u8*)obj + payload->m_childPosOffset + 0x80);
+        Vec* dstVel = (Vec*)((u8*)obj + payload->m_childVelocityOffset + 0x80);
 
         dstPos->x = srcPos->x + xOff;
         dstPos->y = srcPos->y + yOff;
@@ -98,5 +99,4 @@ void pppPointRAp(_pppPObject* pObject, void* step, _pppCtrlTable* ctrlTable)
 
     state[1]--;
 }
-
 

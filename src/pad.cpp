521da7d17a2a7d04ec1257ae85e3a824e52b1dc1
Zachary Canann <zcanann@Zacharys-Mac-mini.local>
2026-03-07 02:24:43 -0800

    pad: use direct CMemory symbol calls in Init/Quit

--- src/pad.cpp
+++ src/pad.cpp
@@ -1,6 +1,5 @@
 #include "ffcc/pad.h"
 
-#include "ffcc/memory.h"
 #include "ffcc/system.h"
 #include "ffcc/joybus.h"
 #include "ffcc/p_minigame.h"
@@ -16,7 +15,16 @@
 
 CPad Pad;
 
+class CMemory
+{
+public:
+	class CStage;
+};
+
 void* operator new[](unsigned long, CMemory::CStage*, char*, int);
+extern "C" unsigned char Memory[];
+extern "C" void* CreateStage__7CMemoryFUlPci(void*, unsigned long, const char*, int);
+extern "C" void DestroyStage__7CMemoryFPQ27CMemory6CStage(void*, void*);
 extern "C" char __vt__8CManager[];
 extern "C" char PTR_PTR_DAT_801e8864[];
 extern "C" unsigned char BYTE_ARRAY_8024430c[];
@@ -82,9 +90,9 @@ void CPad::Init()
 	_1c0_4_ = 0xFFFFFFFF;
 	_1c8_4_ = 1;
 
-	if (System.IsGdev() != 0)
+	if (System.IsGdev())
 	{
-		_1ac_4_ = Memory.CreateStage(0x800000, (char*)"pad.cpp", 1);
+		_1ac_4_ = CreateStage__7CMemoryFUlPci(Memory, 0x800000, "pad.cpp", 1);
 		if (_1ac_4_ != 0)
 		{
 			_1b0_4_ = new (reinterpret_cast<CMemory::CStage*>(_1ac_4_), (char*)"pad.cpp", 0x54) unsigned char[0x69780C];
@@ -131,7 +139,7 @@ void CPad::Quit()
 	CMemory::CStage* stage = *reinterpret_cast<CMemory::CStage**>(base + 0x1AC);
 	if (stage != nullptr)
 	{
-		Memory.DestroyStage(stage);
+		DestroyStage__7CMemoryFPQ27CMemory6CStage(Memory, stage);
 	}
 }
 

69f95ac56ad00c2cb70a0d5c39b02f4f7e5bf9d0
Zachary Canann <zcanann@Zacharys-Mac-mini.local>
2026-03-06 22:31:59 -0800

    MSL_C: improve __copy_longs_aligned mem_funcs match

--- src/MSL_C/PPCEABI/bare/H/mem_funcs.c
+++ src/MSL_C/PPCEABI/bare/H/mem_funcs.c
@@ -9,103 +9,123 @@
 void __copy_longs_aligned(void* dst, const void* src, size_t n)
 {
 	unsigned long i;
+	unsigned long v;
+	const unsigned char* src8;
+	unsigned char* dst8;
+	const unsigned long* src32;
+	unsigned long* dst32;
 
 	i = (-(unsigned long)dst) & 3;
-
-	cps = ((unsigned char*)src) - 1;
-	cpd = ((unsigned char*)dst) - 1;
+	src8 = ((const unsigned char*)src) - 1;
+	dst8 = ((unsigned char*)dst) - 1;
 
 	if (i) {
 		n -= i;
-
 		do
-			deref_auto_inc(cpd) = deref_auto_inc(cps);
+			*++dst8 = *++src8;
 		while (--i);
 	}
 
-	lps = ((unsigned long*)(cps + 1)) - 1;
-	lpd = ((unsigned long*)(cpd + 1)) - 1;
+	src32 = ((const unsigned long*)(src8 + 1)) - 1;
+	dst32 = ((unsigned long*)(dst8 + 1)) - 1;
 
 	i = n >> 5;
-
 	if (i)
 		do {
-			deref_auto_inc(lpd) = deref_auto_inc(lps);
-			deref_auto_inc(lpd) = deref_auto_inc(lps);
-			deref_auto_inc(lpd) = deref_auto_inc(lps);
-			deref_auto_inc(lpd) = deref_auto_inc(lps);
-			deref_auto_inc(lpd) = deref_auto_inc(lps);
-			deref_auto_inc(lpd) = deref_auto_inc(lps);
-			deref_auto_inc(lpd) = deref_auto_inc(lps);
-			deref_auto_inc(lpd) = deref_auto_inc(lps);
+			v = *++src32;
+			*++dst32 = v;
+			v = *++src32;
+			*++dst32 = v;
+			v = *++src32;
+			*++dst32 = v;
+			v = *++src32;
+			*++dst32 = v;
+			v = *++src32;
+			*++dst32 = v;
+			v = *++src32;
+			*++dst32 = v;
+			v = *++src32;
+			*++dst32 = v;
+			v = *++src32;
+			*++dst32 = v;
 		} while (--i);
 
 	i = (n & 31) >> 2;
-
 	if (i)
-		do
-			deref_auto_inc(lpd) = deref_auto_inc(lps);
-		while (--i);
+		do {
+			v = *++src32;
+			*++dst32 = v;
+		} while (--i);
 
-	cps = ((unsigned char*)(lps + 1)) - 1;
-	cpd = ((unsigned char*)(lpd + 1)) - 1;
+	src8 = ((const unsigned char*)(src32 + 1)) - 1;
+	dst8 = ((unsigned char*)(dst32 + 1)) - 1;
 
 	n &= 3;
-
 	if (n)
 		do
-			deref_auto_inc(cpd) = deref_auto_inc(cps);
+			*++dst8 = *++src8;
 		while (--n);
-
-	return;
 }
 
 void __copy_longs_rev_aligned(void* dst, const void* src, size_t n)
 {
 	unsigned long i;
+	unsigned long v;
+	const unsigned char* src8;
+	unsigned char* dst8;
+	const unsigned long* src32;
+	unsigned long* dst32;
 
-	cps = ((unsigned char*)src) + n;
-	cpd = ((unsigned char*)dst) + n;
+	src8 = ((const unsigned char*)src) + n;
+	dst8 = ((unsigned char*)dst) + n;
 
-	i = ((unsigned long)cpd) & 3;
-
+	i = ((unsigned long)dst8) & 3;
 	if (i) {
 		n -= i;
-
 		do
-			*--cpd = *--cps;
+			*--dst8 = *--src8;
 		while (--i);
 	}
 
-	i = n >> 5;
+	src32 = (const unsigned long*)src8;
+	dst32 = (unsigned long*)dst8;
 
+	i = n >> 5;
 	if (i)
 		do {
-			*--lpd = *--lps;
-			*--lpd = *--lps;
-			*--lpd = *--lps;
-			*--lpd = *--lps;
-			*--lpd = *--lps;
-			*--lpd = *--lps;
-			*--lpd = *--lps;
-			*--lpd = *--lps;
+			v = *--src32;
+			*--dst32 = v;
+			v = *--src32;
+			*--dst32 = v;
+			v = *--src32;
+			*--dst32 = v;
+			v = *--src32;
+			*--dst32 = v;
+			v = *--src32;
+			*--dst32 = v;
+			v = *--src32;
+			*--dst32 = v;
+			v = *--src32;
+			*--dst32 = v;
+			v = *--src32;
+			*--dst32 = v;
 		} while (--i);
 
 	i = (n & 31) >> 2;
-
 	if (i)
-		do
-			*--lpd = *--lps;
-		while (--i);
+		do {
+			v = *--src32;
+			*--dst32 = v;
+		} while (--i);
+
+	src8 = (const unsigned char*)src32;
+	dst8 = (unsigned char*)dst32;
 
 	n &= 3;
-
 	if (n)
 		do
-			*--cpd = *--cps;
+			*--dst8 = *--src8;
 		while (--n);
-
-	return;
 }
 
 void __copy_longs_unaligned(void* dst, const void* src, size_t n)

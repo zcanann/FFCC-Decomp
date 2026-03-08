361202889982b2a1e1af6f5cc7f14981a03deae9
Zachary Canann <zacharycanann@gmail.com>
2026-03-07 21:00:42 -0800

    pppSRandHCV: refine color offset/update flow

--- src/pppSRandHCV.cpp
+++ src/pppSRandHCV.cpp
@@ -95,17 +95,15 @@ void pppSRandHCV(void* data1, void* data2, void* data3)
 		target = (float*)(base + *out->fieldC + 0x80);
 	}
 
-	targetColor = (in->field4 == -1) ? &gPppDefaultValueBuffer[0] : (s16*)(base + in->field4 + 0x80);
+	s32 colorOffset = in->field4;
+	if (colorOffset == -1) {
+		targetColor = gPppDefaultValueBuffer;
+	} else {
+		targetColor = (s16*)(base + colorOffset + 0x80);
+	}
 
-	s16 baseValue = in->field8;
-	targetColor[0] = (s16)(targetColor[0] + (s8)((float)baseValue * target[0] - (float)baseValue));
-
-	baseValue = in->fieldA;
-	targetColor[1] = (s16)(targetColor[1] + (s8)((float)baseValue * target[1] - (float)baseValue));
-
-	baseValue = in->fieldC;
-	targetColor[2] = (s16)(targetColor[2] + (s8)((float)baseValue * target[2] - (float)baseValue));
-
-	baseValue = in->fieldE;
-	targetColor[3] = (s16)(targetColor[3] + (s8)((float)baseValue * target[3] - (float)baseValue));
+	targetColor[0] = (s16)(targetColor[0] + (s8)((float)in->field8 * target[0] - (float)in->field8));
+	targetColor[1] = (s16)(targetColor[1] + (s8)((float)in->fieldA * target[1] - (float)in->fieldA));
+	targetColor[2] = (s16)(targetColor[2] + (s8)((float)in->fieldC * target[2] - (float)in->fieldC));
+	targetColor[3] = (s16)(targetColor[3] + (s8)((float)in->fieldE * target[3] - (float)in->fieldE));
 }

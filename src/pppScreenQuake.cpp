83d3219386accd2855f2fa27689db7859efa732b
Zachary Canann <zcanann@Zacharys-Mac-mini.local>
2026-03-07 18:12:04 -0800

    main/pppScreenQuake: use local CameraPcs extern for better matching

--- src/pppScreenQuake.cpp
+++ src/pppScreenQuake.cpp
@@ -1,9 +1,11 @@
 #include "ffcc/pppScreenQuake.h"
 #include "ffcc/partMng.h"
-#include "ffcc/p_camera.h"
 #include "ffcc/pppYmEnv.h"
 #include "ffcc/symbols_shared.h"
 
+extern struct {
+    u8 m_pad[0x4C8];
+} CameraPcs;
 
 extern "C" void SetQuakeParameter__10CCameraPcsFiissffffffi(
     void*,

7dc0e272a7e430a78d1bc4bc91ba54f6d5c20d14
Zachary Canann <zacharycanann@gmail.com>
2026-03-07 21:55:37 -0800

    pppLerpPos: improve pppFrameLerpPos match via loop/local ordering

--- src/pppLerpPos.cpp
+++ src/pppLerpPos.cpp
@@ -39,27 +39,28 @@ char s_pppLerpPos_cpp_801dd418[] = "pppLerpPos.cpp";
  */
 void pppFrameLerpPos(struct pppLerpPos* pppLerpPos, struct pppLerpPosUnkB* param_2, struct pppLerpPosUnkC* param_3)
 {
-    _pppMngSt* pppMngSt = pppMngStPtr;
+    _pppMngSt* pppMngSt;
     s32 iVar1;
     s32 iVar2;
     s32 iVar4;
     s32 iVar5;
     f32* pfVar6;
     s32 iVar7;
-    Vec local_2c;
     Vec local_38;
+    Vec local_2c;
     u32 count;
     Vec** historyPtr;
 
     if (gPppCalcDisabled == 0) {
+        pppMngSt = pppMngStPtr;
         iVar2 = *param_3->m_serializedDataOffsets;
         historyPtr = (Vec**)((u8*)pppLerpPos + 0x80 + iVar2);
         if (*historyPtr == 0) {
             *historyPtr = (Vec*)pppMemAlloc__FUlPQ27CMemory6CStagePci(
                 (u32)(u8)param_2->m_dataValIndex * 0xc, pppEnvStPtr->m_stagePtr, s_pppLerpPos_cpp_801dd418,
                 0x37);
-            iVar5 = 0;
-            for (iVar7 = 0; iVar7 < (s32)(u8)param_2->m_dataValIndex; iVar7 = iVar7 + 1) {
+
+            for (iVar7 = 0, iVar5 = 0; iVar7 < (s32)(u8)param_2->m_dataValIndex; iVar7 = iVar7 + 1) {
                 iVar4 = iVar5 + 4;
                 iVar1 = iVar5 + 8;
                 *(f32*)((u8*)*historyPtr + iVar5) = pppMngStPtr->m_matrix.value[0][3];
@@ -68,29 +69,30 @@ void pppFrameLerpPos(struct pppLerpPos* pppLerpPos, struct pppLerpPosUnkB* param
                 *(f32*)((u8*)*historyPtr + iVar1) = pppMngStPtr->m_matrix.value[2][3];
             }
         } else {
-            local_2c.z = FLOAT_80331bf8;
-            local_2c.y = FLOAT_80331bf8;
             local_2c.x = FLOAT_80331bf8;
+            local_2c.y = FLOAT_80331bf8;
+            local_2c.z = FLOAT_80331bf8;
 
-            iVar5 = (u8)param_2->m_dataValIndex - 1;
-            iVar7 = iVar5 * 0xc;
-            for (; 0 < iVar5; iVar5 = iVar5 - 1) {
-                pfVar6 = (f32*)((u8*)*historyPtr + iVar7 - 0xc);
+            iVar7 = (u8)param_2->m_dataValIndex - 1;
+            iVar5 = iVar7 * 0xc;
+            while (0 < iVar7) {
+                pfVar6 = (f32*)((u8*)*historyPtr + iVar5 - 0xc);
                 local_38.x = pfVar6[0];
                 local_38.y = pfVar6[1];
                 local_38.z = pfVar6[2];
-                pppCopyVector__FR3Vec3Vec((void*)((u8*)*historyPtr + iVar7), &local_38);
-                iVar7 = iVar7 - 0xc;
+                pppCopyVector__FR3Vec3Vec((void*)((u8*)*historyPtr + iVar5), &local_38);
+                iVar5 = iVar5 - 0xc;
+                iVar7 = iVar7 - 1;
             }
 
             (*historyPtr)->x = pppMngStPtr->m_matrix.value[0][3];
             *(f32*)((u8*)*historyPtr + 4) = pppMngStPtr->m_matrix.value[1][3];
             *(f32*)((u8*)*historyPtr + 8) = pppMngStPtr->m_matrix.value[2][3];
 
-            iVar7 = 0;
-            for (iVar5 = 0; count = (u32)(u8)param_2->m_dataValIndex, iVar5 < (s32)count; iVar5 = iVar5 + 1) {
-                PSVECAdd((Vec*)((u8*)*historyPtr + iVar7), &local_2c, &local_2c);
-                iVar7 = iVar7 + 0xc;
+            iVar5 = 0;
+            for (iVar7 = 0; count = (u32)(u8)param_2->m_dataValIndex, iVar7 < (s32)count; iVar7 = iVar7 + 1) {
+                PSVECAdd((Vec*)((u8*)*historyPtr + iVar5), &local_2c, &local_2c);
+                iVar5 = iVar5 + 0xc;
             }
 
             PSVECScale(&local_2c, &local_2c, FLOAT_80331bfc / (f32)count);
@@ -137,5 +139,3 @@ void pppConstructLerpPos(struct pppLerpPos* pppLerpPos, struct pppLerpPosUnkC* p
     Vec** work = (Vec**)((u8*)pppLerpPos + 0x80 + dataOffset);
     *work = 0;
 }
-
-

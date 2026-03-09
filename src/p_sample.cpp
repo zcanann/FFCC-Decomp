6cb5a2ecaf4e7d92bb00b3ff802e495d95521a65
Zachary Canann <zcanann@Zacharys-Mac-mini.local>
2026-03-08 18:13:21 -0700

    Improve __sinit_p_sample_cpp descriptor load pattern

--- src/p_sample.cpp
+++ src/p_sample.cpp
@@ -121,26 +121,49 @@ void CSamplePcs::func1()
  */
 extern "C" void __sinit_p_sample_cpp(void)
 {
-	unsigned int* srcA = m_table_desc0__10CSamplePcs;
-	unsigned int* srcB = m_table_desc1__10CSamplePcs;
-	unsigned int* srcC = m_table_desc2__10CSamplePcs;
-	unsigned int* srcD = m_table_desc3__10CSamplePcs;
+	unsigned int* src = m_table_desc0__10CSamplePcs;
 	unsigned int* table;
+	unsigned int d0;
+	unsigned int d1;
+	unsigned int d2;
+	unsigned int d3;
+	unsigned int d4;
+	unsigned int d5;
+	unsigned int d6;
+	unsigned int d7;
+	unsigned int d8;
+	unsigned int d9;
+	unsigned int d10;
+	unsigned int d11;
 
 	*reinterpret_cast<void**>(&SamplePcs) = __vt__10CSamplePcs;
 	table = (unsigned int*)m_table__10CSamplePcs;
-	table[1] = srcA[0];
-	table[2] = srcA[1];
-	table[3] = srcA[2];
-	table[4] = srcB[0];
-	table[5] = srcB[1];
-	table[6] = srcB[2];
-	table[7] = srcC[0];
-	table[8] = srcC[1];
-	table[9] = srcC[2];
-	table[12] = srcD[0];
-	table[13] = srcD[1];
-	table[14] = srcD[2];
+
+	d0 = src[0];
+	d1 = src[1];
+	d2 = src[2];
+	d3 = src[3];
+	d4 = src[4];
+	d5 = src[5];
+	d6 = src[6];
+	d7 = src[7];
+	d8 = src[8];
+	d9 = src[9];
+	d10 = src[10];
+	d11 = src[11];
+
+	table[1] = d0;
+	table[2] = d1;
+	table[3] = d2;
+	table[4] = d3;
+	table[5] = d4;
+	table[6] = d5;
+	table[7] = d6;
+	table[8] = d7;
+	table[9] = d8;
+	table[12] = d9;
+	table[13] = d10;
+	table[14] = d11;
 }
 
 /*

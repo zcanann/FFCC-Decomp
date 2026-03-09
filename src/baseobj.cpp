0b86293108909b2155ff11ca9884fb65daf75945
BrilliantWasp <brilliantwasp@proton.me>
2026-03-08 18:07:29 -0700

    baseobj: match onTalk/onPush SystemCall linkage (#5270)
    Co-authored-by: Zachary Canann <zcanann@Zacharys-Mac-mini.local>

--- src/baseobj.cpp
+++ src/baseobj.cpp
@@ -1,6 +1,9 @@
 #include "ffcc/baseobj.h"
 #include "ffcc/cflat_runtime2.h"
 
+extern "C" void SystemCall__12CFlatRuntimeFPQ212CFlatRuntime7CObjectiiiPQ212CFlatRuntime6CStackPQ212CFlatRuntime6CStack(
+    CFlatRuntime*, CFlatRuntime::CObject*, int, int, int, CFlatRuntime::CStack*, CFlatRuntime::CStack*);
+
 void CGBaseObj::onCreate()
 {
 }
@@ -19,28 +22,38 @@ void CGBaseObj::onFrame()
 
 /*
  * --INFO--
- * Address:	TODO
- * Size:	TODO
+ * PAL Address: 0x8010b15c
+ * PAL Size: 76b
+ * EN Address: TODO
+ * EN Size: TODO
+ * JP Address: TODO
+ * JP Size: TODO
  */
 void CGBaseObj::onTalk(CGBaseObj* other, int param_3)
 {
 	CFlatRuntime::CStack stack[2];
 	stack[0].m_word = (u32)other->m_particleId;
 	stack[1].m_word = (u32)param_3;
-	reinterpret_cast<CFlatRuntime*>(&gCFlatRuntime2)->SystemCall(this, 2, 6, 2, stack, 0);
+	SystemCall__12CFlatRuntimeFPQ212CFlatRuntime7CObjectiiiPQ212CFlatRuntime6CStackPQ212CFlatRuntime6CStack(
+	    reinterpret_cast<CFlatRuntime*>(CFlat), this, 2, 6, 2, stack, 0);
 }
 
 /*
  * --INFO--
- * Address:	TODO
- * Size:	TODO
+ * PAL Address: 0x8010b1a8
+ * PAL Size: 76b
+ * EN Address: TODO
+ * EN Size: TODO
+ * JP Address: TODO
+ * JP Size: TODO
  */
 void CGBaseObj::onPush(CGBaseObj* other, int param_3)
 {
 	CFlatRuntime::CStack stack[2];
 	stack[0].m_word = (u32)other->m_particleId;
 	stack[1].m_word = (u32)param_3;
-	reinterpret_cast<CFlatRuntime*>(&gCFlatRuntime2)->SystemCall(this, 2, 4, 2, stack, 0);
+	SystemCall__12CFlatRuntimeFPQ212CFlatRuntime7CObjectiiiPQ212CFlatRuntime6CStackPQ212CFlatRuntime6CStack(
+	    reinterpret_cast<CFlatRuntime*>(CFlat), this, 2, 4, 2, stack, 0);
 }
 
 /*

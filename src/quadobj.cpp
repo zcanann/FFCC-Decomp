2062b752825b45dc884473c84019a965a1930166
Zachary Canann <zacharycanann@gmail.com>
2026-03-08 22:47:13 -0700

    Improve isInner control flow matching in quadobj

--- src/quadobj.cpp
+++ src/quadobj.cpp
@@ -94,35 +94,30 @@ int CGQuadObj::GetCID()
  */
 bool CGQuadObj::isInner(Vec* vec)
 {
-    u32 count = (u32)m_vertexCount;
-
-    if ((count != 0) && (m_bboxMinX <= vec->x) && (m_bboxMinZ <= vec->z) && (vec->x <= m_bboxMaxX) &&
-        (vec->z <= m_bboxMaxZ)) {
-        if ((m_yBase <= vec->y) && (vec->y <= m_yBase + m_yHeight)) {
-            u32 i = 0;
-            u32 remaining = count;
-            QuadVertex* vert = m_vertices;
-
-            while (remaining != 0) {
-                float z0 = vert->z;
-                float x0 = vert->x;
-                int next = (i + 1) - ((int)(i + 1) / (int)count) * count;
-                float cross =
-                    (m_vertices[next].x - x0) * (vec->z - z0) - (m_vertices[next].z - z0) * (vec->x - x0);
-                if (cross < EPS) {
-                    break;
-                }
-                vert++;
-                i++;
-                remaining--;
-            }
-            if (i == count) {
-                return true;
-            }
-        }
+    int count = m_vertexCount;
+    if (count == 0) {
+        return false;
+    }
+    if ((m_bboxMinX > vec->x) || (m_bboxMinZ > vec->z) || (m_bboxMaxX < vec->x) || (m_bboxMaxZ < vec->z)) {
+        return false;
+    }
+    if ((m_yBase > vec->y) || (m_yBase + m_yHeight < vec->y)) {
+        return false;
     }
 
-    return false;
+    int i = 0;
+    QuadVertex* vert = m_vertices;
+    for (; i < count; i++) {
+        float z0 = vert->z;
+        float x0 = vert->x;
+        int next = (i + 1) - ((i + 1) / count) * count;
+        float cross = (m_vertices[next].x - x0) * (vec->z - z0) - (m_vertices[next].z - z0) * (vec->x - x0);
+        if (cross < EPS) {
+            break;
+        }
+        vert++;
+    }
+    return i == count;
 }
 
 /*

bea521dc84129d0d0ab984a2e9f783eb81bded6e
asmithbot <asmithbot@proton.me>
2026-03-08 21:32:15 -0700

    pppSRandDownHCV: type params and improve match to 98.72% (#5304)

--- src/pppSRandDownHCV.cpp
+++ src/pppSRandDownHCV.cpp
@@ -7,6 +7,21 @@
 extern s16 gPppDefaultValueBuffer[];
 extern "C" float RandF__5CMathFv(CMath* instance);
 
+struct PppSRandDownHCVParam2 {
+    s32 field0;
+    s32 field4;
+    s16 field8;
+    s16 fieldA;
+    s16 fieldC;
+    s16 fieldE;
+    u8 field10;
+};
+
+struct PppSRandDownHCVParam3 {
+    u8 field0[0xC];
+    s32* fieldC;
+};
+
 /*
  * --INFO--
  * PAL Address: 800639ec
@@ -18,19 +33,21 @@ extern "C" float RandF__5CMathFv(CMath* instance);
  */
 void pppSRandDownHCV(void* param1, void* param2, void* param3)
 {
+	u8* base = (u8*)param1;
+	PppSRandDownHCVParam2* in = (PppSRandDownHCVParam2*)param2;
+	PppSRandDownHCVParam3* out = (PppSRandDownHCVParam3*)param3;
+
 	if (gPppCalcDisabled != 0) {
 		return;
 	}
 
 	float* target;
 
-	if (*(int*)param2 == *((int*)param1 + 3)) {
-		int** base_ptr = (int**)((char*)param3 + 0xc);
-		int offset = **base_ptr;
-		target = (float*)((char*)param1 + offset + 0x80);
+	if (in->field0 == *(s32*)(base + 0xC)) {
+		target = (float*)(base + *out->fieldC + 0x80);
 
 		{
-			u8 flag = *((u8*)param2 + 0x10);
+			u8 flag = in->field10;
 			float value = -RandF__5CMathFv(&Math);
 			if (flag != 0) {
 				float random = RandF__5CMathFv(&Math);
@@ -42,7 +59,7 @@ void pppSRandDownHCV(void* param1, void* param2, void* param3)
 		}
 
 		{
-			u8 flag = *((u8*)param2 + 0x10);
+			u8 flag = in->field10;
 			float value = -RandF__5CMathFv(&Math);
 			if (flag != 0) {
 				float random = RandF__5CMathFv(&Math);
@@ -54,7 +71,7 @@ void pppSRandDownHCV(void* param1, void* param2, void* param3)
 		}
 
 		{
-			u8 flag = *((u8*)param2 + 0x10);
+			u8 flag = in->field10;
 			float value = -RandF__5CMathFv(&Math);
 			if (flag != 0) {
 				float random = RandF__5CMathFv(&Math);
@@ -66,7 +83,7 @@ void pppSRandDownHCV(void* param1, void* param2, void* param3)
 		}
 
 		{
-			u8 flag = *((u8*)param2 + 0x10);
+			u8 flag = in->field10;
 			float value = -RandF__5CMathFv(&Math);
 			if (flag != 0) {
 				float random = RandF__5CMathFv(&Math);
@@ -77,40 +94,19 @@ void pppSRandDownHCV(void* param1, void* param2, void* param3)
 			target[3] = value;
 		}
 	} else {
-		int** base_ptr = (int**)((char*)param3 + 0xc);
-		int offset = **base_ptr;
-		target = (float*)((char*)param1 + offset + 0x80);
+		target = (float*)(base + *out->fieldC + 0x80);
 	}
 
 	s16* target_colors;
-	int color_offset = *((int*)param2 + 1);
+	s32 color_offset = in->field4;
 	if (color_offset == -1) {
 		target_colors = gPppDefaultValueBuffer;
 	} else {
-		target_colors = (s16*)((char*)param1 + color_offset + 0x80);
+		target_colors = (s16*)(base + color_offset + 0x80);
 	}
 
-	{
-		s16 base = *(s16*)((char*)param2 + 0x8);
-		s8 delta = (s8)(base * target[0]);
-		target_colors[0] = (s16)(target_colors[0] + delta);
-	}
-
-	{
-		s16 base = *(s16*)((char*)param2 + 0xa);
-		s8 delta = (s8)(base * target[1]);
-		target_colors[1] = (s16)(target_colors[1] + delta);
-	}
-
-	{
-		s16 base = *(s16*)((char*)param2 + 0xc);
-		s8 delta = (s8)(base * target[2]);
-		target_colors[2] = (s16)(target_colors[2] + delta);
-	}
-
-	{
-		s16 base = *(s16*)((char*)param2 + 0xe);
-		s8 delta = (s8)(base * target[3]);
-		target_colors[3] = (s16)(target_colors[3] + delta);
-	}
+	target_colors[0] += (s8)((f32)in->field8 * target[0]);
+	target_colors[1] += (s8)((f32)in->fieldA * target[1]);
+	target_colors[2] += (s8)((f32)in->fieldC * target[2]);
+	target_colors[3] += (s8)((f32)in->fieldE * target[3]);
 }

d298fa35f8346fe5ba69e005548217995f9d9a42
Zachary Canann <zacharycanann@gmail.com>
2026-03-09 13:03:43 -0700

    p_sample: improve __sinit_p_sample_cpp table init match

--- src/p_sample.cpp
+++ src/p_sample.cpp
@@ -121,12 +121,8 @@ void CSamplePcs::func1()
  */
 extern "C" void __sinit_p_sample_cpp(void)
 {
-	struct DescBlock {
-		unsigned int d[12];
-	};
-	DescBlock* src;
+	unsigned int* src;
 	unsigned int* table;
-	unsigned int d0;
 	unsigned int d1;
 	unsigned int d2;
 	unsigned int d3;
@@ -138,26 +134,25 @@ extern "C" void __sinit_p_sample_cpp(void)
 	unsigned int d9;
 	unsigned int d10;
 	unsigned int d11;
+	unsigned int d0;
 
 	*reinterpret_cast<void**>(&SamplePcs) = __vt__10CSamplePcs;
-	src = reinterpret_cast<DescBlock*>(m_table_desc0__10CSamplePcs);
+	src = m_table_desc0__10CSamplePcs;
 	table = reinterpret_cast<unsigned int*>(m_table__10CSamplePcs);
-
-	d0 = src->d[0];
-	d1 = src->d[1];
-	d2 = src->d[2];
-	d3 = src->d[3];
-	d4 = src->d[4];
-	d5 = src->d[5];
-	d6 = src->d[6];
-	d7 = src->d[7];
-	d8 = src->d[8];
-	d9 = src->d[9];
-	d10 = src->d[10];
-	d11 = src->d[11];
-
-	table[1] = d0;
+	d1 = src[1];
+	d2 = src[2];
+	d3 = src[3];
+	d4 = src[4];
+	d5 = src[5];
+	d6 = src[6];
+	d7 = src[7];
+	d8 = src[8];
+	d9 = src[9];
+	d10 = src[10];
+	d11 = src[11];
+	d0 = src[0];
 	table[2] = d1;
+	table[1] = d0;
 	table[3] = d2;
 	table[4] = d3;
 	table[5] = d4;

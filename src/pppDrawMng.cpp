9468e5961bc3326a4718a5f5fadb482cf95c768e
Zachary Canann <zcanann@Zacharys-Mac-mini.local>
2026-03-09 01:36:40 -0700

    pppDrawMng: use CGamePcs map id field in ClearOt

--- src/pppDrawMng.cpp
+++ src/pppDrawMng.cpp
@@ -174,9 +174,8 @@ void pppDrawMng::ClearOt()
 {
 	int count = sizeof(m_primitiveRefs) / sizeof(pppDrawPrimitive*);
 	pppDrawPrimitive** primitiveRef = m_primitiveRefs;
-	CGame* game = (CGame*)&Game;
 
-	if (game->m_currentMapId == 0x21)
+	if (Game.game.m_currentMapId == 0x21)
 	{
 		m_depthScale = kPppDrawDepthScaleNear;
 	}

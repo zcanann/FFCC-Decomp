14b3d93474fb35c3af7b8d7dc9a6b7b14a09d4c8
AsclepiusBot <asclepiusbot@proton.me>
2026-03-08 21:57:42 -0700

    game: tighten count comparison in art name helpers (#5320)

--- src/game.cpp
+++ src/game.cpp
@@ -1435,7 +1435,7 @@ CGPartyObj* CGame::GetPartyObj(int index)
 char* CGame::MakeArtItemName(char* out, int itemIndex, int count)
 {
     char** itemTable = reinterpret_cast<CFlatDataView*>(&m_cFlatDataArr[1])->m_tabl[0].m_strings;
-    if (count >= 2) {
+    if (count > 1) {
         sprintf(out, s_numNameFmt, count, itemTable[itemIndex * 5 + 3]);
         return out;
     }
@@ -1519,7 +1519,7 @@ char* CGame::MakeNumItemName(char* out, int itemIndex, int count)
 char* CGame::MakeArtMonName(char* out, int monIndex, int count)
 {
     char** monTable = reinterpret_cast<CFlatDataView*>(&m_cFlatDataArr[1])->m_tabl[1].m_strings;
-    if (count >= 2) {
+    if (count > 1) {
         sprintf(out, s_numNameFmt, count, monTable[monIndex * 5 + 3]);
         return out;
     }

bc9d3bdbe00124145f8b7408b8e0ac47e73ef871
Zachary Canann <zcanann@Zacharys-Mac-mini.local>
2026-03-09 03:54:20 -0700

    gx/GXAttr: improve __GXSetVAT bitmask codegen

--- src/gx/GXAttr.c
+++ src/gx/GXAttr.c
@@ -578,9 +578,11 @@ void GXSetVtxAttrFmtv(GXVtxFmt vtxfmt, const GXVtxAttrFmtList* list) {
 void __GXSetVAT(void) {
     s32 i;
     u8 b;
+    u32 bit;
 
     for (b = 0, i = 0; b < 8; b++, i++) {
-        if ((__GXData->dirtyVAT & (u8)(1 << b)) != 0) {
+        bit = 1 << (u8)b;
+        if ((__GXData->dirtyVAT & bit) != 0) {
             GX_WRITE_SOME_REG4(8, b | 0x70, __GXData->vatA[i], i - 12);
             GX_WRITE_SOME_REG4(8, b | 0x80, __GXData->vatB[i], i - 12);
             GX_WRITE_SOME_REG4(8, b | 0x90, __GXData->vatC[i], i - 12);

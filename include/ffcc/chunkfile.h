c0f5a486e80bfd87192ef1ae02756198dc266636
Zachary Canann <zcanann@Zacharys-Mac-mini.local>
2026-03-09 20:36:30 -0700

    chunkfile: fix scope stack layout to 20 entries

--- include/ffcc/chunkfile.h
+++ include/ffcc/chunkfile.h
@@ -48,7 +48,7 @@ private:
     unsigned char *m_headerPtr;  // 0x10
     unsigned char *m_cursor;     // 0x14
     int m_stackDepth;            // 0x18
-    ChunkScope m_chunkScopes[1]; // 0x1C (Possibly larger?)
+    ChunkScope m_chunkScopes[20]; // 0x1C
 };
 
 #endif // _FFCC_CHUNKFILE_H_

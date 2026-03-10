ade709f8de4497bf412b9102d9bec467b607291f
Noelle Harper <noelle21b@proton.me>
2026-03-10 08:12:51 -0700

    TRK: type support request file-access calls with proper prototypes (#5657)

--- include/TRK_MINNOW_DOLPHIN/MetroTRK/Portable/support.h
+++ include/TRK_MINNOW_DOLPHIN/MetroTRK/Portable/support.h
@@ -2,6 +2,7 @@
 #define METROTRK_PORTABLE_SUPPORT_H
 
 #include "PowerPC_EABI_Support/MetroTRK/trk.h"
+#include "stddef.h"
 
 #ifdef __cplusplus
 extern "C" {
@@ -10,6 +11,13 @@ extern "C" {
 typedef struct TRKBuffer TRKBuffer;
 
 DSError TRKRequestSend(TRKBuffer* msgBuf, int* bufferId, u32 p1, u32 p2, int p3);
+DSError TRKSuppAccessFile(u32 file_handle, u8* data, size_t* count,
+                          DSIOResult* io_result, BOOL needReply, BOOL read);
+DSError HandleOpenFileSupportRequest(const char* path, u8 replyError, u32* param_3,
+                                     DSIOResult* ioResult);
+DSError HandleCloseFileSupportRequest(int replyError, DSIOResult* ioResult);
+DSError HandlePositionFileSupportRequest(DSReplyError replyErr, u32* param_2,
+                                         u8 param_3, DSIOResult* ioResult);
 
 #ifdef __cplusplus
 }

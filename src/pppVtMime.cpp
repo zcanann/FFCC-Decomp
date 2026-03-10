27df49e8316f8c515b3b05c1ae6ac5fed4ddb11c
asmithbot <asmithbot@proton.me>
2026-03-09 20:45:03 -0700

    pppVtMime: align pppDrawVtMime loop control flow (#5600)
    Co-authored-by: Zachary Canann <zacharycanann@gmail.com>

--- src/pppVtMime.cpp
+++ src/pppVtMime.cpp
@@ -147,24 +147,33 @@ void pppDrawVtMime(_pppPObject* object, void* step, _pppCtrlTable* ctrl)
 
     float* outputVerts = (float*)*memPtr;
     if (vertCount != 0) {
-        int remaining = vertCount;
-        int pairCount = remaining >> 1;
-        while (pairCount != 0) {
-            outputVerts[0] = vert1Pos[0] + state->value * (vert2Pos[0] - vert1Pos[0]);
-            outputVerts[1] = vert1Pos[1] + state->value * (vert2Pos[1] - vert1Pos[1]);
-            outputVerts[2] = vert1Pos[2] + state->value * (vert2Pos[2] - vert1Pos[2]);
-            outputVerts[3] = vert1Pos[3] + state->value * (vert2Pos[3] - vert1Pos[3]);
-            outputVerts[4] = vert1Pos[4] + state->value * (vert2Pos[4] - vert1Pos[4]);
-            outputVerts[5] = vert1Pos[5] + state->value * (vert2Pos[5] - vert1Pos[5]);
-            vert1Pos += 6;
-            vert2Pos += 6;
-            outputVerts += 6;
-            pairCount--;
+        int pairCount = (unsigned int)vertCount >> 1;
+        if (pairCount != 0) {
+            do {
+                outputVerts[0] = vert1Pos[0] + state->value * (vert2Pos[0] - vert1Pos[0]);
+                outputVerts[1] = vert1Pos[1] + state->value * (vert2Pos[1] - vert1Pos[1]);
+                outputVerts[2] = vert1Pos[2] + state->value * (vert2Pos[2] - vert1Pos[2]);
+                outputVerts[3] = vert1Pos[3] + state->value * (vert2Pos[3] - vert1Pos[3]);
+                outputVerts[4] = vert1Pos[4] + state->value * (vert2Pos[4] - vert1Pos[4]);
+                outputVerts[5] = vert1Pos[5] + state->value * (vert2Pos[5] - vert1Pos[5]);
+                vert1Pos += 6;
+                vert2Pos += 6;
+                outputVerts += 6;
+                pairCount--;
+            } while (pairCount != 0);
         }
-        if ((remaining & 1) != 0) {
-            outputVerts[0] = vert1Pos[0] + state->value * (vert2Pos[0] - vert1Pos[0]);
-            outputVerts[1] = vert1Pos[1] + state->value * (vert2Pos[1] - vert1Pos[1]);
-            outputVerts[2] = vert1Pos[2] + state->value * (vert2Pos[2] - vert1Pos[2]);
+
+        pairCount = vertCount & 1;
+        if (pairCount != 0) {
+            do {
+                outputVerts[0] = vert1Pos[0] + state->value * (vert2Pos[0] - vert1Pos[0]);
+                outputVerts[1] = vert1Pos[1] + state->value * (vert2Pos[1] - vert1Pos[1]);
+                outputVerts[2] = vert1Pos[2] + state->value * (vert2Pos[2] - vert1Pos[2]);
+                vert1Pos += 3;
+                vert2Pos += 3;
+                outputVerts += 3;
+                pairCount--;
+            } while (pairCount != 0);
         }
     }
 
@@ -199,4 +208,3 @@ void pppVtMime(_pppPObject* object, void* step, _pppCtrlTable* ctrl)
         state->accel += data->addZ;
     }
 }
-

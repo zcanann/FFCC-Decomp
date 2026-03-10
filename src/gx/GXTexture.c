470e977e2168cb15e3a27f07a1b5076e16914c2e
Zachary Canann <zcanann@Zacharys-Mac-mini.local>
2026-03-10 04:08:20 -0700

    Adjust GXTexture callback view for GXLoadTlut

--- src/gx/GXTexture.c
+++ src/gx/GXTexture.c
@@ -41,6 +41,12 @@ typedef struct __GXTlutRegionInt_struct {
     __GXTlutObjInt tlutObj;
 } __GXTlutRegionInt;
 
+typedef struct GXDataCallbacksView {
+    u8 _pad[0x410];
+    GXTexRegionCallback texRegionCallback;
+    GXTlutRegionCallback tlutRegionCallback;
+} GXDataCallbacksView;
+
 u8 GXTexMode0Ids[8] = { 0x80, 0x81, 0x82, 0x83, 0xA0, 0xA1, 0xA2, 0xA3 };
 u8 GXTexMode1Ids[8] = { 0x84, 0x85, 0x86, 0x87, 0xA4, 0xA5, 0xA6, 0xA7 };
 u8 GXTexImage0Ids[8] = { 0x88, 0x89, 0x8A, 0x8B, 0xA8, 0xA9, 0xAA, 0xAB };
@@ -738,11 +744,12 @@ void GXLoadTlut(GXTlutObj* tlut_obj, u32 tlut_name) {
     __GXTlutRegionInt* r;
     u32 tlut_offset;
     __GXTlutObjInt* t = (__GXTlutObjInt*)tlut_obj;
+    GXDataCallbacksView* callbacks = (GXDataCallbacksView*)__GXData;
 
     ASSERTMSGLINE(1432, tlut_obj, "TLut Object Pointer is null");
     CHECK_GXBEGIN(1434, "GXLoadTlut");
-    ASSERTMSGLINEV(1435, __GXData->tlutRegionCallback, "%s: Tex/Tlut Region Callback not set", "GXLoadTlut");
-    r = (__GXTlutRegionInt*)__GXData->tlutRegionCallback(tlut_name);
+    ASSERTMSGLINEV(1435, callbacks->tlutRegionCallback, "%s: Tex/Tlut Region Callback not set", "GXLoadTlut");
+    r = (__GXTlutRegionInt*)callbacks->tlutRegionCallback(tlut_name);
     ASSERTMSGLINEV(1437, r, "%s: Tex/Tlut Region Callback returns NULL", "GXLoadTlut");
 
     __GXFlushTextureState();

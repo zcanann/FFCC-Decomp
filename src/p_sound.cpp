d2dafcf2f1f06827f798d196b623d0ab688dbe66
Zachary Canann <zcanann@Zacharys-Mac-mini.local>
2026-03-11 03:48:05 -0700

    p_sound: tighten __sinit_p_sound_cpp descriptor load order

--- src/p_sound.cpp
+++ src/p_sound.cpp
@@ -28,10 +28,7 @@ unsigned int m_table__9CSoundPcs[0x15C / sizeof(unsigned int)] = {
  */
 extern "C" void __sinit_p_sound_cpp(void)
 {
-	struct DescBlock {
-		unsigned int d[12];
-	};
-	DescBlock* src;
+	unsigned int* src;
 	unsigned int* table;
 	unsigned int d0;
 	unsigned int d1;
@@ -47,24 +44,24 @@ extern "C" void __sinit_p_sound_cpp(void)
 	unsigned int d11;
 
 	*reinterpret_cast<void**>(&SoundPcs) = __vt__9CSoundPcs;
-	src = reinterpret_cast<DescBlock*>(m_table_desc0__9CSoundPcs);
+	src = m_table_desc0__9CSoundPcs;
 	table = reinterpret_cast<unsigned int*>(m_table__9CSoundPcs);
 
-	d1 = src->d[1];
-	d2 = src->d[2];
-	d3 = src->d[3];
-	d4 = src->d[4];
-	d5 = src->d[5];
-	d6 = src->d[6];
-	d7 = src->d[7];
-	d8 = src->d[8];
-	d9 = src->d[9];
-	d10 = src->d[10];
-	d11 = src->d[11];
-	d0 = src->d[0];
+	d2 = src[2];
+	d3 = src[3];
+	d4 = src[4];
+	d5 = src[5];
+	d6 = src[6];
+	d7 = src[7];
+	d8 = src[8];
+	d9 = src[9];
+	d10 = src[10];
+	d11 = src[11];
+	d0 = src[0];
+	d1 = src[1];
 
-	table[2] = d1;
 	table[1] = d0;
+	table[2] = d1;
 	table[3] = d2;
 	table[4] = d3;
 	table[5] = d4;

302cbccce6b48125ad8dfbab37725ade8c5d6cc6
Zachary Canann <zacharycanann@gmail.com>
2026-03-11 01:19:31 -0700

    Improve pppSRandCV data match

--- src/pppSRandCV.cpp
+++ src/pppSRandCV.cpp
@@ -93,15 +93,29 @@ void pppSRandCV(void* param1, void* param2, void* param3)
     }
 
     s32 color_offset = in->sourceOffset;
-    u8* target_colors;
-    if (color_offset == -1) {
-        target_colors = &gPppDefaultValueBuffer[0];
-    } else {
-        target_colors = base + color_offset + 0x80;
+    u8* target_colors = (color_offset == -1) ? gPppDefaultValueBuffer : (base + color_offset + 0x80);
+
+    {
+        s8 deltaBase = in->delta[0];
+        s32 delta = (s32)((float)deltaBase * target[0] - (float)deltaBase);
+        target_colors[0] = (u8)(target_colors[0] + delta);
     }
 
-    target_colors[0] += (s8)((float)in->delta[0] * target[0] - (float)in->delta[0]);
-    target_colors[1] += (s8)((float)in->delta[1] * target[1] - (float)in->delta[1]);
-    target_colors[2] += (s8)((float)in->delta[2] * target[2] - (float)in->delta[2]);
-    target_colors[3] += (s8)((float)in->delta[3] * target[3] - (float)in->delta[3]);
+    {
+        s8 deltaBase = in->delta[1];
+        s32 delta = (s32)((float)deltaBase * target[1] - (float)deltaBase);
+        target_colors[1] = (u8)(target_colors[1] + delta);
+    }
+
+    {
+        s8 deltaBase = in->delta[2];
+        s32 delta = (s32)((float)deltaBase * target[2] - (float)deltaBase);
+        target_colors[2] = (u8)(target_colors[2] + delta);
+    }
+
+    {
+        s8 deltaBase = in->delta[3];
+        s32 delta = (s32)((float)deltaBase * target[3] - (float)deltaBase);
+        target_colors[3] = (u8)(target_colors[3] + delta);
+    }
 }

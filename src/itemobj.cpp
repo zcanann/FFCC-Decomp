73f50dace04043e2b4f8a981c31a40ca917d76a5
Zachary Canann <zcanann@Zacharys-Mac-mini.local>
2026-03-12 11:15:24 -0700

    itemobj: improve ItemJump matching with typed object access

--- src/itemobj.cpp
+++ src/itemobj.cpp
@@ -1122,54 +1122,54 @@ void CGItemObj::DrawOmoideName(CFont* font)
  */
 void CGItemObj::ItemJump(int state, float jump)
 {
-	for (unsigned char* itemObj = (unsigned char*)FindGItemObjFirst__13CFlatRuntime2Fv(CFlat); itemObj != 0;
-	     itemObj = (unsigned char*)FindGItemObjNext__13CFlatRuntime2FP9CGItemObj(CFlat, itemObj)) {
-		if ((*(unsigned int*)(itemObj + 0x60) & 0x10) != 0) {
-			continue;
-		}
+	for (CGItemObj* itemObj = static_cast<CGItemObj*>(FindGItemObjFirst__13CFlatRuntime2Fv(CFlat)); itemObj != 0;
+	     itemObj = static_cast<CGItemObj*>(FindGItemObjNext__13CFlatRuntime2FP9CGItemObj(CFlat, itemObj))) {
+		CGObject* obj = reinterpret_cast<CGObject*>(itemObj);
+		if ((obj->m_displayFlags & 0x10) == 0) {
+			float local_78;
+			float local_74;
+			float local_70;
+			float local_6c;
+			float local_68;
+			float local_64;
+			float local_60;
+			float local_5c;
+			float local_58;
+			float local_48;
+			float local_44;
+			float local_40;
+			float local_3c;
+			float local_38;
+			float local_34;
+			float local_30;
+			float local_2c;
+			float local_28;
+			float local_24;
+			unsigned int mapMask = *reinterpret_cast<unsigned int*>(&obj->m_moveVec.x);
 
-		float local_78;
-		float local_74;
-		float local_70;
-		float local_6c;
-		float local_68;
-		float local_64;
-		float local_60;
-		float local_5c;
-		float local_58;
-		float local_48;
-		float local_44;
-		float local_40;
-		float local_3c;
-		float local_38;
-		float local_34;
-		float local_30;
-		float local_2c;
-		float local_28;
-		float local_24;
-
-		local_6c = *(float*)(itemObj + 0x68);
-		local_64 = *(float*)(itemObj + 0x70);
-		local_68 = *(float*)(itemObj + 0x6c) + FLOAT_80331b1c;
-		local_78 = FLOAT_80331b20;
-		local_70 = FLOAT_80331b20;
-		local_74 = FLOAT_80331b24;
-		local_30 = FLOAT_80331b28;
-		local_34 = FLOAT_80331b28;
-		local_38 = FLOAT_80331b28;
-		local_24 = FLOAT_80331b2c;
-		local_28 = FLOAT_80331b2c;
-		local_2c = FLOAT_80331b2c;
-		local_48 = FLOAT_80331b20;
-		local_44 = FLOAT_80331b24;
-		local_40 = FLOAT_80331b20;
-		local_3c = FLOAT_80331b20;
-		local_60 = local_6c;
-		local_5c = local_68;
-		local_58 = local_64;
-		if (CheckHitCylinderNear__7CMapMngFP12CMapCylinderP3VecUl(&MapMng, &local_60, &local_78, *(unsigned int*)(itemObj + 0x1c4)) != 0 &&
-		    reinterpret_cast<unsigned char*>(gMapHitFace)[0x47] == state) {
-			*(float*)(itemObj + 0x108) = *(float*)(itemObj + 0x108) + jump;
+			local_6c = obj->m_worldPosition.x;
+			local_64 = obj->m_worldPosition.z;
+			local_68 = obj->m_worldPosition.y + FLOAT_80331b1c;
+			local_78 = FLOAT_80331b20;
+			local_70 = FLOAT_80331b20;
+			local_74 = FLOAT_80331b24;
+			local_30 = FLOAT_80331b28;
+			local_34 = FLOAT_80331b28;
+			local_38 = FLOAT_80331b28;
+			local_24 = FLOAT_80331b2c;
+			local_28 = FLOAT_80331b2c;
+			local_2c = FLOAT_80331b2c;
+			local_48 = FLOAT_80331b20;
+			local_44 = FLOAT_80331b24;
+			local_40 = FLOAT_80331b20;
+			local_3c = FLOAT_80331b20;
+			local_60 = local_6c;
+			local_5c = local_68;
+			local_58 = local_64;
+			if (CheckHitCylinderNear__7CMapMngFP12CMapCylinderP3VecUl(&MapMng, &local_60, &local_78, mapMask) != 0 &&
+			    reinterpret_cast<unsigned char*>(gMapHitFace)[0x47] == state) {
+				obj->m_groundHitOffset.y = obj->m_groundHitOffset.y + jump;
+			}
 		}
 	}
 }

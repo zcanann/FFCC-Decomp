aa23daacd83eb366dc40a7d883a4f822441fd577
asmithbot <asmithbot@proton.me>
2026-03-11 22:14:48 -0700

    pppSRandCV: improve color buffer selection match (#6029)
    Co-authored-by: Zachary Canann <zacharycanann@gmail.com>

--- src/pppSRandCV.cpp
+++ src/pppSRandCV.cpp
@@ -93,26 +93,15 @@ void pppSRandCV(void* param1, void* param2, void* param3)
         target = (f32*)(base + *ctx->outputOffset + 0x80);
     }
 
-    s32 color_offset = in->sourceOffset;
-    u8* target_colors = (color_offset == -1) ? gPppDefaultValueBuffer : (base + color_offset + 0x80);
-
-    {
-        s8 baseValue = in->delta[0];
-        target_colors[0] = (u8)(target_colors[0] + (s8)((f32)baseValue * target[0] - (f32)baseValue));
+    u8* target_colors;
+    if (in->sourceOffset == -1) {
+        target_colors = &gPppDefaultValueBuffer[0];
+    } else {
+        target_colors = base + in->sourceOffset + 0x80;
     }
 
-    {
-        s8 baseValue = in->delta[1];
-        target_colors[1] = (u8)(target_colors[1] + (s8)((f32)baseValue * target[1] - (f32)baseValue));
-    }
-
-    {
-        s8 baseValue = in->delta[2];
-        target_colors[2] = (u8)(target_colors[2] + (s8)((f32)baseValue * target[2] - (f32)baseValue));
-    }
-
-    {
-        s8 baseValue = in->delta[3];
-        target_colors[3] = (u8)(target_colors[3] + (s8)((f32)baseValue * target[3] - (f32)baseValue));
-    }
+    target_colors[0] += (s8)((f32)in->delta[0] * target[0] - (f32)in->delta[0]);
+    target_colors[1] += (s8)((f32)in->delta[1] * target[1] - (f32)in->delta[1]);
+    target_colors[2] += (s8)((f32)in->delta[2] * target[2] - (f32)in->delta[2]);
+    target_colors[3] += (s8)((f32)in->delta[3] * target[3] - (f32)in->delta[3]);
 }

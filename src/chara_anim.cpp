54dad8a0d64b023c215f07e86bf94c95d60582a7
Zachary Canann <zcanann@Zacharys-Mac-mini.local>
2026-03-13 07:50:29 -0700

    Refine CChara::CAnim constructor init ordering

--- src/chara_anim.cpp
+++ src/chara_anim.cpp
@@ -164,29 +164,30 @@ void i2f_5(float*, unsigned short*)
  */
 CChara::CAnim::CAnim()
 {
-	int zero = 0;
-	int one = 1;
 	CAnimFields& anim = Anim(this);
+	unsigned char flags;
 
 	__ct__4CRefFv(this);
 	anim.m_vtable = __vt__Q26CChara5CAnim;
-	anim.m_nodeCount = zero;
-	anim.m_frameCount = zero;
+	anim.m_nodeCount = 0;
+	anim.m_frameCount = 0;
 	anim.m_nodes = 0;
 	anim.m_bank = 0;
-	*reinterpret_cast<unsigned char*>(&anim.m_flags) =
-	    static_cast<unsigned char>(__rlwimi(*reinterpret_cast<unsigned char*>(&anim.m_flags), one, 7, 24, 24));
-	*reinterpret_cast<unsigned char*>(&anim.m_flags) =
-	    static_cast<unsigned char>(__rlwimi(*reinterpret_cast<unsigned char*>(&anim.m_flags), zero, 6, 25, 25));
+	flags = *reinterpret_cast<unsigned char*>(&anim.m_flags);
+	flags = static_cast<unsigned char>(__rlwimi(flags, 1, 7, 24, 24));
+	*reinterpret_cast<unsigned char*>(&anim.m_flags) = flags;
+	flags = *reinterpret_cast<unsigned char*>(&anim.m_flags);
+	flags = static_cast<unsigned char>(__rlwimi(flags, 0, 6, 25, 25));
+	*reinterpret_cast<unsigned char*>(&anim.m_flags) = flags;
 	anim.m_quantizeX = 5;
 	anim.m_quantizeY = 0xB;
 	anim.m_quantizeZ = 10;
-	anim.m_interp = zero;
-	anim.m_interpOffset = zero;
+	anim.m_interp = 0;
+	anim.m_interpOffset = 0;
+	anim.m_bankAddress = 0;
+	anim.m_bankSize = 0;
 	anim.m_stage = 0;
-	anim.m_bankSize = zero;
-	anim.m_bankAddress = zero;
-	anim.m_lastFrame = zero;
+	anim.m_lastFrame = 0;
 }
 
 /*

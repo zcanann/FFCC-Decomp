db2d51c6e9bd2eaa53eb7e8ea46a868da430e50a
Zachary Canann <zcanann@Zacharys-Mac-mini.local>
2026-03-13 21:39:52 -0700

    Revert "mtx: improve C_MTXLightOrtho expression matching (#4005)"
    This reverts commit bc3be0422de978a644153b0e187447a5bf8199fa.

--- src/mtx/mtx.c
+++ src/mtx/mtx.c
@@ -1351,20 +1351,24 @@ void C_MTXLightPerspective(Mtx m, f32 fovY, f32 aspect, float scaleS, float scal
 void C_MTXLightOrtho(Mtx m, f32 t, f32 b, f32 l, f32 r, float scaleS, float scaleT, float transS, float transT)
 {
     f32 tmp;
+    f32 add;
+    f32 proj;
 
     tmp = 1.0f / (r - l);
     m[0][0] = (2.0f * tmp) * scaleS;
     m[0][1] = 0.0f;
     m[0][2] = 0.0f;
-    scaleS *= tmp;
-    m[0][3] = scaleS * (-(r + l)) + transS;
+    add = r + l;
+    proj = (-add * tmp) * scaleS;
+    m[0][3] = transS + proj;
 
     tmp = 1.0f / (t - b);
     m[1][0] = 0.0f;
     m[1][1] = (2.0f * tmp) * scaleT;
     m[1][2] = 0.0f;
-    scaleT *= tmp;
-    m[1][3] = scaleT * (-(t + b)) + transT;
+    add = t + b;
+    proj = (-add * tmp) * scaleT;
+    m[1][3] = transT + proj;
 
     m[2][0] = 0.0f;
     m[2][1] = 0.0f;

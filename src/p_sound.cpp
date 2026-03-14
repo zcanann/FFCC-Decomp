4f964d1186eae2f3948bbad0bcfcfbbec09b0075
Zachary Canann <zacharycanann@gmail.com>
2026-03-14 04:58:41 -0700

    p_sound: simplify static init descriptor copy

--- src/p_sound.cpp
+++ src/p_sound.cpp
@@ -9,10 +9,12 @@ extern "C" void destroy__9CSoundPcsFv(CSoundPcs*);
 extern "C" void calc__9CSoundPcsFv(CSoundPcs*);
 extern "C" void draw__9CSoundPcsFv(CSoundPcs*);
 char s_CSoundPcs_801DB4E8[] = "CSoundPcs";
-unsigned int m_table_desc0__9CSoundPcs[3] = {0, 0xFFFFFFFF, reinterpret_cast<unsigned int>(create__9CSoundPcsFv)};
-unsigned int m_table_desc1__9CSoundPcs[3] = {0, 0xFFFFFFFF, reinterpret_cast<unsigned int>(destroy__9CSoundPcsFv)};
-unsigned int m_table_desc2__9CSoundPcs[3] = {0, 0xFFFFFFFF, reinterpret_cast<unsigned int>(calc__9CSoundPcsFv)};
-unsigned int m_table_desc3__9CSoundPcs[3] = {0, 0xFFFFFFFF, reinterpret_cast<unsigned int>(draw__9CSoundPcsFv)};
+unsigned int m_table_desc0__9CSoundPcs[12] = {
+    0, 0xFFFFFFFF, reinterpret_cast<unsigned int>(create__9CSoundPcsFv),
+    0, 0xFFFFFFFF, reinterpret_cast<unsigned int>(destroy__9CSoundPcsFv),
+    0, 0xFFFFFFFF, reinterpret_cast<unsigned int>(calc__9CSoundPcsFv),
+    0, 0xFFFFFFFF, reinterpret_cast<unsigned int>(draw__9CSoundPcsFv),
+};
 unsigned int m_table__9CSoundPcs[0x15C / sizeof(unsigned int)] = {
     reinterpret_cast<unsigned int>(s_CSoundPcs_801DB4E8), 0, 0, 0, 0, 0, 0, 0, 0, 0, 0x25, 0, 0, 0, 0, 0x44, 1
 };
@@ -28,49 +30,34 @@ unsigned int m_table__9CSoundPcs[0x15C / sizeof(unsigned int)] = {
  */
 extern "C" void __sinit_p_sound_cpp(void)
 {
-	volatile void** soundPcs = reinterpret_cast<volatile void**>(&SoundPcs);
-	volatile unsigned int* src = m_table_desc0__9CSoundPcs;
-	volatile unsigned int* table = m_table__9CSoundPcs;
-	unsigned int d1;
-	unsigned int d2;
-	unsigned int d3;
-	unsigned int d4;
-	unsigned int d5;
-	unsigned int d6;
-	unsigned int d7;
-	unsigned int d8;
-	unsigned int d9;
-	unsigned int d10;
-	unsigned int d11;
-	unsigned int d0;
+    unsigned int* src = m_table_desc0__9CSoundPcs;
+    unsigned int* table = m_table__9CSoundPcs;
+    unsigned int d0 = src[0];
+    unsigned int d1 = src[1];
+    unsigned int d2 = src[2];
+    unsigned int d3 = src[3];
+    unsigned int d4 = src[4];
+    unsigned int d5 = src[5];
+    unsigned int d6 = src[6];
+    unsigned int d7 = src[7];
+    unsigned int d8 = src[8];
+    unsigned int d9 = src[9];
+    unsigned int d10 = src[10];
+    unsigned int d11 = src[11];
 
-	*soundPcs = __vt__9CSoundPcs;
-
-	d1 = src[1];
-	d2 = src[2];
-	d3 = src[3];
-	d4 = src[4];
-	d5 = src[5];
-	d6 = src[6];
-	d7 = src[7];
-	d8 = src[8];
-	d9 = src[9];
-	d10 = src[10];
-	d11 = src[11];
-	d0 = src[0];
-
-	table[2] = d1;
-	table[1] = d0;
-	table[3] = d2;
-	table[4] = d3;
-	table[5] = d4;
-	table[6] = d5;
-	table[7] = d6;
-	table[8] = d7;
-	table[9] = d8;
-	table[12] = d9;
-	table[13] = d10;
-	table[14] = d11;
+    *(void**)&SoundPcs = __vt__9CSoundPcs;
+    table[1] = d0;
+    table[2] = d1;
+    table[3] = d2;
+    table[4] = d3;
+    table[5] = d4;
+    table[6] = d5;
+    table[7] = d6;
+    table[8] = d7;
+    table[9] = d8;
+    table[12] = d9;
+    table[13] = d10;
+    table[14] = d11;
 }
 
 /*

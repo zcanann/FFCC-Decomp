f7a2cf2789558e00695d0344371772bcc152aff0
Zachary Canann <zcanann@Zacharys-Mac-mini.local>
2026-03-14 05:48:45 -0700

    p_mc: add .ctors init reference for __sinit

--- src/p_mc.cpp
+++ src/p_mc.cpp
@@ -11,6 +11,8 @@ extern "C" int Format__6McCtrlFi(McCtrl* mcCtrl, int slot);
 extern "C" int Rand__5CMathFUl(CMath* instance, unsigned long max);
 extern "C" void CallWorldParam__8CMenuPcsFiii(void* menu, int mode, int param, int unused);
 extern "C" void __sinit_p_mc_cpp(void);
+__declspec(section ".ctors")
+void* const __init_p_mc_reference = __sinit_p_mc_cpp;
 
 class McCtrl
 {

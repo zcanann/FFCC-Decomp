377b557209cd5ad1133bee9bb923f834e4624cce
Noelle Harper <noelle21b@proton.me>
2026-03-19 09:27:15 -0700

    Fix global destructor chain dtors layout (#6639)
    Co-authored-by: Zachary Canann <zacharycanann@gmail.com>

--- src/Runtime.PPCEABI.H/global_destructor_chain.c
+++ src/Runtime.PPCEABI.H/global_destructor_chain.c
@@ -4,15 +4,6 @@
 /* 80451988-80451990 000E88 0004+04 2/2 0/0 0/0 .sbss            __global_destructor_chain */
 DestructorChain* __global_destructor_chain;
 
-/* 80361C24-80361C3C 35C564 0018+00 0/0 116/116 0/0 .text            __register_global_object */
-void* __register_global_object(void* object, void* destructor, void* regmem) {
-    ((DestructorChain*)regmem)->next = __global_destructor_chain;
-    ((DestructorChain*)regmem)->destructor = destructor;
-    ((DestructorChain*)regmem)->object = object;
-    __global_destructor_chain = (DestructorChain*)regmem;
-    return object;
-}
-
 /* 80361BDC-80361C24 35C51C 0048+00 0/0 2/1 0/0 .text            __destroy_global_chain */
 void __destroy_global_chain(void) {
     DestructorChain* iter;
@@ -22,8 +13,15 @@ void __destroy_global_chain(void) {
     }
 }
 
+/* 80361C24-80361C3C 35C564 0018+00 0/0 116/116 0/0 .text            __register_global_object */
+void* __register_global_object(void* object, void* destructor, void* regmem) {
+    ((DestructorChain*)regmem)->next = __global_destructor_chain;
+    ((DestructorChain*)regmem)->destructor = destructor;
+    ((DestructorChain*)regmem)->object = object;
+    __global_destructor_chain = (DestructorChain*)regmem;
+    return object;
+}
+
 /* clang-format off */
-#pragma section ".dtors$10"
-__declspec(section ".dtors$10")
-static void* const __destroy_global_chain_reference = __destroy_global_chain;
+__declspec(section ".dtors") extern void* const __destroy_global_chain_reference = __destroy_global_chain;
 /* clang-format on */

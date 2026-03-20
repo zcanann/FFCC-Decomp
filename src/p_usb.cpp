8a91f2de19c182005fc4e65c038553036001a2a9
Zachary Canann <zcanann@Zacharys-Mac-mini.local>
2026-03-20 05:17:58 -0700

    Move p_usb table init into CUSBPcs constructor

--- src/p_usb.cpp
+++ src/p_usb.cpp
@@ -36,6 +36,17 @@ extern "C" void* __vt__7CUSBPcs[];
 CUSBPcs::CUSBPcs()
     : CProcess()
 {
+    u32* table = m_table__7CUSBPcs;
+
+    table[1] = m_table_desc0__7CUSBPcs[0];
+    table[2] = m_table_desc0__7CUSBPcs[1];
+    table[3] = m_table_desc0__7CUSBPcs[2];
+    table[4] = m_table_desc1__7CUSBPcs[0];
+    table[5] = m_table_desc1__7CUSBPcs[1];
+    table[6] = m_table_desc1__7CUSBPcs[2];
+    table[7] = m_table_desc2__7CUSBPcs[0];
+    table[8] = m_table_desc2__7CUSBPcs[1];
+    table[9] = m_table_desc2__7CUSBPcs[2];
 }
 
 /*
@@ -266,24 +277,3 @@ int CUSBPcs::SendDataCode(int code, void* src, int elemSize, int elemCount)
  * JP Address: TODO
  * JP Size: TODO
  */
-extern "C" void __sinit_p_usb_cpp()
-{
-    // NOTE: This __sinit is compiler-generated. To match, move the vtable setup and
-    // m_table_desc copying into the class constructor, then delete this function.
-    // The compiler will auto-generate __sinit from the global object declaration.
-
-    *reinterpret_cast<void**>(&USBPcs) = __vt__8CManager;
-    *reinterpret_cast<void**>(&USBPcs) = __vt_CProcess;
-    *reinterpret_cast<void**>(&USBPcs) = __vt__7CUSBPcs;
-
-    u32* table = &m_table__7CUSBPcs[1];
-    table[0] = m_table_desc0__7CUSBPcs[0];
-    table[1] = m_table_desc0__7CUSBPcs[1];
-    table[2] = m_table_desc0__7CUSBPcs[2];
-    table[3] = m_table_desc1__7CUSBPcs[0];
-    table[4] = m_table_desc1__7CUSBPcs[1];
-    table[5] = m_table_desc1__7CUSBPcs[2];
-    table[6] = m_table_desc2__7CUSBPcs[0];
-    table[7] = m_table_desc2__7CUSBPcs[1];
-    table[8] = m_table_desc2__7CUSBPcs[2];
-}

ce954fd58d9ff8b19f8d56e6bae774b913b6c07d
AsclepiusBot <asclepiusbot@proton.me>
2026-03-20 11:47:42 -0700

    Fix C string header linkage for C++ callers (#6828)
    Co-authored-by: Zachary Canann <zcanann@Zacharys-Mac-mini.local>

--- include/string.h
+++ include/string.h
@@ -3,6 +3,10 @@
 
 typedef unsigned long size_t;
 
+#ifdef __cplusplus
+extern "C" {
+#endif
+
 void* memcpy(void* dst, const void* src, size_t n);
 void* memset(void* dst, int val, size_t n);
 
@@ -10,11 +14,16 @@ char* strrchr(const char* str, int c);
 char* strchr(const char* str, int c);
 int strncmp(const char* str1, const char* str2, size_t n);
 int strcmp(const char* str1, const char* str2);
-char* strcat(char* dst, const char* src, size_t n);
 char* strncat(char* dst, const char* src, size_t n);
 char* strncpy(char* dst, const char* src, size_t n);
 char* strcpy(char* dst, const char* src);
 size_t strlen(const char* str);
 char* strtok(char* str, const char* delim);
+
+#ifdef __cplusplus
+}
+#endif
+
+char* strcat(char* dst, const char* src, size_t n);
 
 #endif

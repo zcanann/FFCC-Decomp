ad6c8b9389009dc3e5c874c7e737fa4311b1714a
BrilliantWasp <brilliantwasp@proton.me>
2026-03-20 00:08:01 -0700

    Recover tmp arti menu members (#6759)
    Co-authored-by: Zachary Canann <zcanann@Zacharys-Mac-mini.local>

--- include/ffcc/menu_tmparti.h
+++ include/ffcc/menu_tmparti.h
@@ -1,6 +1,8 @@
 #ifndef _FFCC_MENU_TMPARTI_H_
 #define _FFCC_MENU_TMPARTI_H_
 
+class CFont;
+
 class CMenuPcs
 {
 public:
@@ -11,6 +13,13 @@ public:
     unsigned int TmpArtiClose();
     void TmpArtiDraw();
     void TmpArtiCtrlCur();
+
+    char pad_0000[0x108];
+    CFont* m_tmpArtiFont;
+    char pad_010C[0x720];
+    short* m_tmpArtiState;
+    char pad_0830[0x20];
+    short* m_tmpArtiList;
 };
 
 #endif // _FFCC_MENU_TMPARTI_H_

7f4c5ac4f475a8a8d36d30cdfae1ce48bcb74459
Zachary Canann <zcanann@Zacharys-Mac-mini.local>
2026-03-21 00:15:37 -0700

    Simplify pppLight target selection

--- src/pppLight.cpp
+++ src/pppLight.cpp
@@ -250,16 +250,14 @@ void pppLight(void* param1, void* param2, void* param3)
 				Add__9CLightPcsFPQ29CLightPcs6CLight(&LightPcs, &light);
 			} else {
 				unsigned char* obj;
-				u32 targetIndex;
 
 				light.m_type = 1;
-				targetIndex = (u32)step->targetIndex;
-				if (targetIndex == (u32)-1) {
+				if (step->targetIndex == -1) {
 					obj = gPppDefaultValueBuffer;
 				} else {
 					pppLightTarget* targetTable =
 						(pppLightTarget*)((PppLightMngProgramInfo*)pppMngStPtr)->programInfoTable;
-					obj = targetTable[targetIndex].obj;
+					obj = targetTable[step->targetIndex].obj;
 				}
 
 				light.m_targetPosition.x = *(f32*)(obj + 0x1c);

bc7d549caa12ec442b9d6a71606b2b9c182971f4
BrilliantWasp <brilliantwasp@proton.me>
2026-03-26 08:54:53 -0700

    gx: simplify GXSetScissorBoxOffset packing (#7077)
    Co-authored-by: Zachary Canann <zcanann@Zacharys-Mac-mini.local>
    Co-authored-by: Zachary Canann <zacharycanann@gmail.com>

--- src/gx/GXTransform.c
+++ src/gx/GXTransform.c
@@ -637,7 +637,6 @@ void GXGetScissor(u32* left, u32* top, u32* wd, u32* ht) {
  * JP Size: TODO
  */
 void GXSetScissorBoxOffset(s32 x_off, s32 y_off) {
-    u32 x;
     u32 reg;
 
     CHECK_GXBEGIN(1119, "GXSetScissorBoxOffset");
@@ -645,11 +644,11 @@ void GXSetScissorBoxOffset(s32 x_off, s32 y_off) {
     ASSERTMSGLINE(1122, (u32)(x_off + 342) < 2048, "GXSetScissorBoxOffset: Invalid X offset");
     ASSERTMSGLINE(1124, (u32)(y_off + 342) < 2048, "GXSetScissorBoxOffset: Invalid Y offset");
 
-    x = y_off + 0x156;
-    reg = x_off + 0x156U;
-    reg = (reg >> 1) & 0xFFF003FF;
-    x = (x << 9) & 0xFFFFFC00U;
-    reg = (reg | x) & 0x00FFFFFF;
+    x_off += 0x156;
+    y_off += 0x156;
+    x_off = ((u32)x_off >> 1) & 0xFFF003FF;
+    y_off = (y_off << 9) & 0xFFFFFC00;
+    reg = ((u32)x_off | (u32)y_off) & 0x00FFFFFF;
     reg |= 0x59000000;
     GX_WRITE_RAS_REG(reg);
     __GXData->bpSentNot = 0;

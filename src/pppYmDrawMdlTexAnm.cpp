0fc626636415b56d27564e50cb691ce3ad3ac123
Zachary Canann <zacharycanann@gmail.com>
2026-03-26 21:36:13 -0700

    Tighten pppConstructYmDrawMdlTexAnm UV loop

--- src/pppYmDrawMdlTexAnm.cpp
+++ src/pppYmDrawMdlTexAnm.cpp
@@ -96,7 +96,8 @@ void pppConstructYmDrawMdlTexAnm(_pppPObjLink* object, _pppCtrlTable* ctrl)
     work->m_perV = work->m_perU;
 
     if (uvLayout != NULL) {
-        for (uvByteOffset = 0, i = 0; i < (s32)(u16)uvLayout->m_uvCount; i++, uvByteOffset += 4) {
+        uvByteOffset = 0;
+        for (i = 0; i < (s32)(u16)uvLayout->m_uvCount; i++) {
             if (work->m_perU < (f32)*(s16*)((u8*)uvLayout->m_uvPairs + uvByteOffset)) {
                 work->m_perU = (f32)*(s16*)((u8*)uvLayout->m_uvPairs + uvByteOffset);
             }
@@ -104,6 +105,8 @@ void pppConstructYmDrawMdlTexAnm(_pppPObjLink* object, _pppCtrlTable* ctrl)
             if (work->m_perV < (f32)*(s16*)((u8*)uvLayout->m_uvPairs + uvByteOffset + 2)) {
                 work->m_perV = (f32)*(s16*)((u8*)uvLayout->m_uvPairs + uvByteOffset + 2);
             }
+
+            uvByteOffset += 4;
         }
 
         OSReport(s_PerU___0_2f_PerV___0_2f_801d9c38, work->m_perU, work->m_perV);

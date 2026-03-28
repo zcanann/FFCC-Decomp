8be20c4d5b017c1d853af57d6abfa2c1ec42c3ef
Zachary Canann <zacharycanann@gmail.com>
2026-03-27 23:49:41 -0700

    p_tina: reuse rodata base in createLoad

--- src/p_tina.cpp
+++ src/p_tina.cpp
@@ -475,7 +475,9 @@ void CPartPcs::create()
  */
 void CPartPcs::createLoad()
 {
-    CPartMngState* state = reinterpret_cast<CPartMngState*>(&PartMng);
+    CPartMngState* state = GetPartMngState();
+    char* stringBase = s_p_tina_rodata_801d7ee0;
+    char* chobitPath = stringBase + 0x24C;
 
     state->m_partAMemBase = 0;
     state->m_partAMemCursor = 0;
@@ -484,14 +486,14 @@ void CPartPcs::createLoad()
     state->m_asyncHandleCount = 0;
     state->m_partLoadMode = 0;
 
-    pppLoadPtx__8CPartMngFPCciiPvi(&PartMng, s_dvd_tina_chobit_801d812c, 1, 1, 0, 0);
-    pppLoadPmd__8CPartMngFPCc(&PartMng, s_dvd_tina_chobit_801d812c);
-    pppLoadPan__8CPartMngFPCc(&PartMng, s_dvd_tina_chobit_801d812c);
-    pppLoadPdt__8CPartMngFPCciiPvi(&PartMng, s_dvd_tina_chobit_0_801d813c, 1, 1, 0, 0);
-    pppLoadPdt__8CPartMngFPCciiPvi(&PartMng, s_dvd_tina_chobit_1_801d8150, 2, 1, 0, 0);
-    pppLoadPdt__8CPartMngFPCciiPvi(&PartMng, s_dvd_tina_chobit_2_801d8164, 3, 1, 0, 0);
-    pppLoadPdt__8CPartMngFPCciiPvi(&PartMng, s_dvd_tina_chobit_3_801d8178, 4, 1, 0, 0);
-    pppLoadPdt__8CPartMngFPCciiPvi(&PartMng, s_dvd_tina_chobit_4_801d818c, 5, 1, 0, 0);
+    pppLoadPtx__8CPartMngFPCciiPvi(&PartMng, chobitPath, 1, 1, 0, 0);
+    pppLoadPmd__8CPartMngFPCc(&PartMng, chobitPath);
+    pppLoadPan__8CPartMngFPCc(&PartMng, chobitPath);
+    pppLoadPdt__8CPartMngFPCciiPvi(&PartMng, stringBase + 0x25C, 1, 1, 0, 0);
+    pppLoadPdt__8CPartMngFPCciiPvi(&PartMng, stringBase + 0x270, 2, 1, 0, 0);
+    pppLoadPdt__8CPartMngFPCciiPvi(&PartMng, stringBase + 0x284, 3, 1, 0, 0);
+    pppLoadPdt__8CPartMngFPCciiPvi(&PartMng, stringBase + 0x298, 4, 1, 0, 0);
+    pppLoadPdt__8CPartMngFPCciiPvi(&PartMng, stringBase + 0x2AC, 5, 1, 0, 0);
     AmemSetLock__13CAmemCacheSetFv(&ppvAmemCacheSet);
 }
 

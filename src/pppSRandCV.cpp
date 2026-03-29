f7edbac88b744dafb66cf476bb191721bdd2d43c
Zachary Canann <zcanann@Zacharys-Mac-mini.local>
2026-03-29 16:55:17 -0700

    Improve pppSRandCV color updates

--- src/pppSRandCV.cpp
+++ src/pppSRandCV.cpp
@@ -96,28 +96,22 @@ void pppSRandCV(void* param1, void* param2, void* param3)
         target = (float*)(base + *ctx->outputOffset + 0x80);
     }
 
-    s32 color_offset = in->sourceOffset;
-    u8* target_colors;
-    if (color_offset == -1) {
-        target_colors = gPppDefaultValueBuffer;
-    } else {
-        target_colors = base + color_offset + 0x80;
-    }
+    u8* target_colors = (in->sourceOffset == -1) ? &gPppDefaultValueBuffer[0] : (base + in->sourceOffset + 0x80);
 
     {
-        u8 color = target_colors[0];
-        target_colors[0] = color + (s8)((f32)in->delta[0] * target[0] - (f32)in->delta[0]);
+        s8 baseValue = in->delta[0];
+        target_colors[0] = (u8)(target_colors[0] + (s8)((f32)baseValue * target[0] - (f32)baseValue));
     }
     {
-        u8 color = target_colors[1];
-        target_colors[1] = color + (s8)((f32)in->delta[1] * target[1] - (f32)in->delta[1]);
+        s8 baseValue = in->delta[1];
+        target_colors[1] = (u8)(target_colors[1] + (s8)((f32)baseValue * target[1] - (f32)baseValue));
     }
     {
-        u8 color = target_colors[2];
-        target_colors[2] = color + (s8)((f32)in->delta[2] * target[2] - (f32)in->delta[2]);
+        s8 baseValue = in->delta[2];
+        target_colors[2] = (u8)(target_colors[2] + (s8)((f32)baseValue * target[2] - (f32)baseValue));
     }
     {
-        u8 color = target_colors[3];
-        target_colors[3] = color + (s8)((f32)in->delta[3] * target[3] - (f32)in->delta[3]);
+        s8 baseValue = in->delta[3];
+        target_colors[3] = (u8)(target_colors[3] + (s8)((f32)baseValue * target[3] - (f32)baseValue));
     }
 }

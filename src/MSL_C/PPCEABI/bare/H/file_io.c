89b3a0a2397f309015bf33596c4abe7e837b05e9
Zachary Canann <zacharycanann@gmail.com>
2026-03-29 20:17:09 -0700

    MSL file_io: improve __get_file_modes byte handling

--- src/MSL_C/PPCEABI/bare/H/file_io.c
+++ src/MSL_C/PPCEABI/bare/H/file_io.c
@@ -106,17 +106,18 @@ int fflush(FILE* file) {
 
 int __get_file_modes(const char* mode, file_modes* modes)
 {
-	int mode_str;
-	int next_mode;
+	const unsigned char* mode_ptr = (const unsigned char*)mode + 2;
+	unsigned int mode_str;
+	unsigned char next_mode;
 	unsigned char open_mode;
 	unsigned char io_mode = 0;
 
 	modes->file_kind = __disk_file;
+	mode_str = *(const unsigned char*)mode;
 #ifndef __NO_WIDE_CHAR
 	modes->file_orientation = UNORIENTED;
 #endif
 	modes->binary_io = 0;
-	mode_str = mode[0];
 	
 	switch (mode_str)
 	{
@@ -136,13 +137,13 @@ int __get_file_modes(const char* mode, file_modes* modes)
 			return(0);
 	}
 	
-	next_mode = mode[1];
+	next_mode = ((const unsigned char*)mode)[1];
 	modes->open_mode = open_mode;
 	
 	switch (next_mode)
 	{
 		case 'b':
-			next_mode = mode[2];
+			next_mode = *mode_ptr;
 			modes->binary_io = 1;
 			
 			if (next_mode == '+')
@@ -151,7 +152,7 @@ int __get_file_modes(const char* mode, file_modes* modes)
 			break;
 			
 		case '+':
-			next_mode = mode[2];
+			next_mode = *mode_ptr;
 			mode_str = (mode_str << 8) | '+';
 			
 			if (next_mode == 'b')

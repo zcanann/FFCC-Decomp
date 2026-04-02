5d1850fe2e42af1dc8aff16d28040858222cdaa2
Zachary Canann <zcanann@Zacharys-Mac-mini.local>
2026-04-02 03:20:06 -0700

    Refine pppRenderBlurChara data pointer setup

--- src/pppBlurChara.cpp
+++ src/pppBlurChara.cpp
@@ -363,9 +363,11 @@ struct BlurCharaTexData {
  */
 void pppRenderBlurChara(pppBlurChara* blurChara, pppBlurCharaUnkB* param_2, pppBlurCharaUnkC* param_3)
 {
-    int textureBase = 0;
     int texDataOffset = param_3->m_serializedDataOffsets[2];
     int colorDataOffset = param_3->m_serializedDataOffsets[1];
+    BlurCharaColorData* colorData = reinterpret_cast<BlurCharaColorData*>((u8*)blurChara + 0x80 + colorDataOffset);
+    BlurCharaTexData* texData = reinterpret_cast<BlurCharaTexData*>((u8*)blurChara + 0x80 + texDataOffset);
+    int textureBase = 0;
     int textureIndex;
     int objPosBase;
     _GXTexObj smallBackTex;
@@ -387,8 +389,6 @@ void pppRenderBlurChara(pppBlurChara* blurChara, pppBlurCharaUnkB* param_2, pppB
     float projZ;
     Vec quadA;
     Vec quadB;
-    BlurCharaTexData* texData = reinterpret_cast<BlurCharaTexData*>((u8*)blurChara + 0x80 + texDataOffset);
-    BlurCharaColorData* colorData = reinterpret_cast<BlurCharaColorData*>((u8*)blurChara + 0x80 + colorDataOffset);
 
     if (param_2->m_textureMode == 1) {
         textureIndex = 0;

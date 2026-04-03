a8d5946cdb8c67d8b3d14f3616b0b4c53f91ab4d
Zachary Canann <zcanann@Zacharys-Mac-mini.local>
2026-04-03 16:18:35 -0700

    Improve pppRenderLocationTitle2 temp zero init

--- src/LocationTitle2.cpp
+++ src/LocationTitle2.cpp
@@ -25,9 +25,9 @@ extern float FLOAT_80330f4c;
 extern double DOUBLE_80330f58;
 extern char DAT_80330f50[];
 
-static int GetGraphFrameFromId(u32 graphId)
+static int GetGraphFrameFromId(int graphId)
 {
-    return (int)graphId / 0x1000;
+    return graphId / 0x1000;
 }
 
 struct LocationTitle2Work {
@@ -360,9 +360,7 @@ extern "C" void pppRenderLocationTitle2(struct pppLocationTitle2* locationTitle,
         Vec transformedPos;
 
         if ((int)particle->m_frame >= graphFrame) {
-            transformedPos.x = 0.0f;
-            transformedPos.y = 0.0f;
-            transformedPos.z = 0.0f;
+            transformedPos.x = transformedPos.y = transformedPos.z = 0.0f;
             PSMTXIdentity(model);
             model[0][0] = pppMngStPtr->m_scale.x * locationTitle->m_localMatrix.value[0][0];
             model[1][1] = pppMngStPtr->m_scale.y * locationTitle->m_localMatrix.value[1][1];

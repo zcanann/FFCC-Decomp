ce615f57e6cdf5a0b701b7c2ee7029735da91343
Zachary Canann <zacharycanann@gmail.com>
2026-04-08 16:15:36 -0700

    gecko except (#8123)
    * gecko except
    
    * gekko

--- include/PowerPC_EABI_Support/Runtime/exception.h
+++ include/PowerPC_EABI_Support/Runtime/exception.h
@@ -1,6 +1,10 @@
 #ifndef _EXCEPTION
 #define _EXCEPTION
 
+#ifdef __cplusplus
+
+namespace std {
+
 typedef void (*unexpected_handler)();
 unexpected_handler set_unexpected(unexpected_handler handler);
 void unexpected();
@@ -9,5 +13,15 @@ typedef void (*terminate_handler)();
 terminate_handler set_terminate(terminate_handler handler);
 void terminate();
 
+} // namespace std
+
+using std::terminate;
+using std::terminate_handler;
+using std::set_terminate;
+using std::unexpected;
+using std::unexpected_handler;
+using std::set_unexpected;
+
+#endif // __cplusplus
 
 #endif

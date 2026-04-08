5d34b998b51dfd549f638cf281d81b4c3c895636
Zachary Canann <zacharycanann@gmail.com>
2026-04-08 04:28:35 -0700

    quadobj: improve CGQuadObj::onDraw match

--- src/quadobj.cpp
+++ src/quadobj.cpp
@@ -39,29 +39,27 @@ void CGQuadObj::onDestroy()
 void CGQuadObj::onDraw()
 {
     if (m_vertexCount != 0 && (*(u32*)(CFlat + 0x129C) & 0x10000) != 0) {
-        GXSetChanMatColor(GX_COLOR0A0, CColor(0xff, 0xff, 0xff, 0xff).color);
+        CColor color(0xff, 0xff, 0xff, 0xff);
+        GXSetChanMatColor(GX_COLOR0A0, color.color);
         GXLoadPosMtxImm(*reinterpret_cast<Mtx*>(reinterpret_cast<unsigned char*>(&CameraPcs) + 0x4), GX_PNMTX0);
         GXBegin(GX_LINES, GX_VTXFMT0, ((u32)m_vertexCount << 1) + ((u32)m_vertexCount << 2));
 
-        CGQuadObj* self = this;
         int i = 0;
+        CGQuadObj* self = this;
 
         while (i < (int)(u32)m_vertexCount) {
             int next = i + 1;
-            QuadVertex* vertex = self->m_vertices;
+            i = i + 1;
 
-            i++;
-
-            GXPosition3f32(vertex->x, m_yBase, vertex->z);
-            next = next - (next / (int)(u32)m_vertexCount) * (u32)m_vertexCount;
-            GXPosition3f32(m_vertices[next].x, m_yBase, m_vertices[next].z);
-            GXPosition3f32(vertex->x, m_yBase + m_yHeight, vertex->z);
-            next = next - (next / (int)(u32)m_vertexCount) * (u32)m_vertexCount;
-            GXPosition3f32(m_vertices[next].x, m_yBase + m_yHeight, m_vertices[next].z);
-            GXPosition3f32(vertex->x, m_yBase, vertex->z);
-
+            GXPosition3f32(self->m_vertices[0].x, m_yBase, self->m_vertices[0].z);
+            int wrapped = next - (next / (int)(u32)m_vertexCount) * (u32)m_vertexCount;
+            GXPosition3f32(m_vertices[wrapped].x, m_yBase, m_vertices[wrapped].z);
+            GXPosition3f32(self->m_vertices[0].x, m_yBase + m_yHeight, self->m_vertices[0].z);
+            wrapped = next - (next / (int)(u32)m_vertexCount) * (u32)m_vertexCount;
+            GXPosition3f32(m_vertices[wrapped].x, m_yBase + m_yHeight, m_vertices[wrapped].z);
+            GXPosition3f32(self->m_vertices[0].x, m_yBase, self->m_vertices[0].z);
+            GXPosition3f32(self->m_vertices[0].x, m_yBase + m_yHeight, self->m_vertices[0].z);
             self = reinterpret_cast<CGQuadObj*>(reinterpret_cast<unsigned char*>(self) + sizeof(QuadVertex));
-            GXPosition3f32(vertex->x, m_yBase + m_yHeight, vertex->z);
         }
     }
 }

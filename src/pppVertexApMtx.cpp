3ab135147b7cb84dbce6bf351be2b58352dffe17
Zachary Canann <zcanann@Zacharys-Mac-mini.local>
2026-04-10 00:01:52 -0700

    Improve pppVertexApMtx mode dispatch match

--- src/pppVertexApMtx.cpp
+++ src/pppVertexApMtx.cpp
@@ -101,8 +101,7 @@ void pppVertexApMtx(_pppPObject* parent, PVertexApMtx* dataRaw, void* ctrlRaw)
 
 		count = data->spawnCount;
 
-		switch (data->mode) {
-		case 0:
+		if (data->mode == 0) {
 			while (count-- != 0) {
 				if (state->index >= entry->maxValue) {
 					state->index = 0;
@@ -151,8 +150,7 @@ void pppVertexApMtx(_pppPObject* parent, PVertexApMtx* dataRaw, void* ctrlRaw)
 					}
 				}
 			}
-			break;
-		case 1:
+		} else if (data->mode == 1) {
 			while (count-- != 0) {
 				f32 randValue = Math.RandF();
 				f32 maxValue = (f32)entry->maxValue;
@@ -198,7 +196,6 @@ void pppVertexApMtx(_pppPObject* parent, PVertexApMtx* dataRaw, void* ctrlRaw)
 					}
 				}
 			}
-			break;
 		}
 		state->countdown = data->spawnDelay;
 	}

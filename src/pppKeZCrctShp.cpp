8d12c28e36cbc30a078c180f21096cd8945872d9
Zachary Canann <zcanann@Zacharys-Mac-mini.local>
2026-04-10 11:59:35 -0700

    Improve pppKeZCrctShpDraw matching

--- src/pppKeZCrctShp.cpp
+++ src/pppKeZCrctShp.cpp
@@ -18,6 +18,12 @@ struct pppKeZCrctShpObject {
  */
 void pppKeZCrctShpDraw(_pppPObject* object, pppKeZCrctShpStep* stepData, _pppCtrlTable* ctrlTable)
 {
+    float scaledPosX;
+    float scaledPosY;
+    float scaledPosZ;
+    float offsetPosX;
+    float offsetPosY;
+    float offsetPosZ;
     Vec rowX;
     Vec rowY;
     Vec rowZ;
@@ -37,30 +43,39 @@ void pppKeZCrctShpDraw(_pppPObject* object, pppKeZCrctShpStep* stepData, _pppCtr
     pppScaleVector(scaledY, rowY, pppMngStPtr->m_scale.y);
     pppScaleVector(scaledZ, rowZ, pppMngStPtr->m_scale.z);
 
-    zeroVec.x = 0.0f;
-    zeroVec.y = 0.0f;
     zeroVec.z = 0.0f;
+    zeroVec.y = 0.0f;
+    zeroVec.x = 0.0f;
     pppSetRowVector(transformMatrix, scaledX, scaledY, scaledZ, zeroVec);
 
     pppCopyVector(transformedPos, rowPos);
-    transformedPos.x *= stepData->m_positionScale.x;
-    transformedPos.y *= stepData->m_positionScale.y;
-    transformedPos.z *= stepData->m_positionScale.z;
+    scaledPosX = transformedPos.x * stepData->m_positionScale.x;
+    scaledPosY = transformedPos.y * stepData->m_positionScale.y;
+    scaledPosZ = transformedPos.z * stepData->m_positionScale.z;
 
     mode = stepData->m_mode;
+    transformedPos.x = scaledPosX;
+    transformedPos.y = scaledPosY;
+    transformedPos.z = scaledPosZ;
 
     if (mode == 0) {
-        transformedPos.x += stepData->m_offset.x;
-        transformedPos.y += stepData->m_offset.y;
-        transformedPos.z += stepData->m_offset.z;
+        offsetPosX = scaledPosX + stepData->m_offset.x;
+        offsetPosY = scaledPosY + stepData->m_offset.y;
+        offsetPosZ = scaledPosZ + stepData->m_offset.z;
+        transformedPos.x = offsetPosX;
+        transformedPos.y = offsetPosY;
+        transformedPos.z = offsetPosZ;
         pppApplyMatrix(transformedPos, *(pppFMATRIX*)&ppvWorldMatrix, transformedPos);
     } else if (mode == 1) {
         pppApplyMatrix(zeroVec, *(pppFMATRIX*)&ppvWorldMatrix, transformedPos);
-    } else if (mode == 2) {
+    } else if (mode < 3) {
         pppApplyMatrix(zeroVec, pppMngStPtr->m_matrix, transformedPos);
-        zeroVec.x += stepData->m_offset.x * pppMngStPtr->m_scale.x;
-        zeroVec.y += stepData->m_offset.y * pppMngStPtr->m_scale.y;
-        zeroVec.z += stepData->m_offset.z * pppMngStPtr->m_scale.z;
-        pppApplyMatrix(zeroVec, *(pppFMATRIX*)&ppvCameraMatrix02, zeroVec);
+        offsetPosX = stepData->m_offset.x * pppMngStPtr->m_scale.x + zeroVec.x;
+        offsetPosY = stepData->m_offset.y * pppMngStPtr->m_scale.y + zeroVec.y;
+        offsetPosZ = stepData->m_offset.z * pppMngStPtr->m_scale.z + zeroVec.z;
+        zeroVec.x = offsetPosX;
+        zeroVec.y = offsetPosY;
+        zeroVec.z = offsetPosZ;
+        pppApplyMatrix(zeroVec, *(pppFMATRIX*)&ppvCameraMatrix0, zeroVec);
     }
 }

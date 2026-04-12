7adcdba5e734612ee326e8b9f63fa39b0bae2d21
Zachary Canann <zcanann@Zacharys-Mac-mini.local>
2026-04-11 21:45:58 -0700

    Improve RedStream empty stream search match

--- src/RedSound/RedStream.cpp
+++ src/RedSound/RedStream.cpp
@@ -30,19 +30,15 @@ int SearchSeEmptyTrack__Fiii(int, int, int);
  */
 unsigned int _SearchEmptyStreamData()
 {
-	register RedStreamDATA* streamData;
+	unsigned int streamData = (unsigned int)DAT_8032f438;
 
-	streamData = (RedStreamDATA*)DAT_8032f438;
-	do {
-		if (*(int*)((int)streamData + 0x10c) == 0) {
-			break;
+	while (streamData < (unsigned int)DAT_8032f438 + 0x4c0) {
+		if (*(int*)(streamData + 0x10c) == 0) {
+			return streamData;
 		}
-		streamData = (RedStreamDATA*)((int)streamData + 0x130);
-	} while ((unsigned int)streamData < (unsigned int)DAT_8032f438 + 0x4c0);
-
-	if ((unsigned int)streamData < (unsigned int)DAT_8032f438 + 0x4c0) {
-		return (unsigned int)streamData;
+		streamData += 0x130;
 	}
+
 	return 0;
 }
 

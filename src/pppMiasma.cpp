00036b4e48232b6a30174ac042e825348de280b4
asmithbot <asmithbot@proton.me>
2026-04-12 22:46:13 -0700

    Align pppMiasma object layout (#8574)
    Co-authored-by: Zachary Canann <zacharycanann@gmail.com>

--- src/pppMiasma.cpp
+++ src/pppMiasma.cpp
@@ -66,22 +66,99 @@ struct PppMiasmaRenderStep {
 
 /*
  * --INFO--
- * Address:	TODO
- * Size:	TODO
+ * PAL Address: 0x80109930
+ * PAL Size: 368b
+ * EN Address: TODO
+ * EN Size: TODO
+ * JP Address: TODO
+ * JP Size: TODO
  */
-void CalcSphereRadius(Vec*, unsigned short)
+void pppFrameMiasma(pppMiasma* pppMiasma, pppMiasmaFrameStep* param_2, pppMiasmaCtrl* param_3)
 {
-	// TODO
+    s16* work;
+
+    if (gPppCalcDisabled != 0) {
+        return;
+    }
+
+    work = (s16*)((u8*)pppMiasma + 0x80 + param_3->m_serializedDataOffsets[2]);
+    work[4] = work[4] + work[8];
+    work[0] = work[0] + work[4];
+    work[5] = work[5] + work[9];
+    work[1] = work[1] + work[5];
+    work[6] = work[6] + work[10];
+    work[2] = work[2] + work[6];
+    work[7] = work[7] + work[11];
+    work[3] = work[3] + work[7];
+
+    if (pppMiasma->m_graphId != param_2->m_graphId) {
+        return;
+    }
+
+    work[0] = work[0] + param_2->m_addPosX;
+    work[1] = work[1] + param_2->m_addPosY;
+    work[2] = work[2] + param_2->m_addPosZ;
+    work[3] = work[3] + param_2->m_addPosW;
+    work[4] = work[4] + param_2->m_addVelX;
+    work[5] = work[5] + param_2->m_addVelY;
+    work[6] = work[6] + param_2->m_addVelZ;
+    work[7] = work[7] + param_2->m_addVelW;
+    work[8] = work[8] + param_2->m_addAccX;
+    work[9] = work[9] + param_2->m_addAccY;
+    work[10] = work[10] + param_2->m_addAccZ;
+    work[11] = work[11] + param_2->m_addAccW;
 }
 
 /*
  * --INFO--
- * Address:	TODO
- * Size:	TODO
+ * PAL Address: 0x80109aa0
+ * PAL Size: 4b
+ * EN Address: TODO
+ * EN Size: TODO
+ * JP Address: TODO
+ * JP Size: TODO
  */
-void CreateScaleMatrix(_pppPObject*, float)
+void pppDestructMiasma(pppMiasma*, pppMiasmaCtrl*)
 {
-	// TODO
+    return;
+}
+
+/*
+ * --INFO--
+ * PAL Address: 0x80109aa4
+ * PAL Size: 100b
+ * EN Address: TODO
+ * EN Size: TODO
+ * JP Address: TODO
+ * JP Size: TODO
+ */
+void pppConstruct2Miasma(pppMiasma* pppMiasma, pppMiasmaCtrl* param_2)
+{
+    u8* work;
+
+    work = (u8*)pppMiasma + 0x80 + param_2->m_serializedDataOffsets[2];
+    memset(work, 0, 8);
+    memset(work + 8, 0, 8);
+    memset(work + 0x10, 0, 8);
+}
+
+/*
+ * --INFO--
+ * PAL Address: 0x80109b08
+ * PAL Size: 100b
+ * EN Address: TODO
+ * EN Size: TODO
+ * JP Address: TODO
+ * JP Size: TODO
+ */
+void pppConstructMiasma(pppMiasma* pppMiasma, pppMiasmaCtrl* param_2)
+{
+    u8* work;
+
+    work = (u8*)pppMiasma + 0x80 + param_2->m_serializedDataOffsets[2];
+    memset(work, 0, 8);
+    memset(work + 8, 0, 8);
+    memset(work + 0x10, 0, 8);
 }
 
 /*
@@ -544,111 +621,4 @@ void pppRenderMiasma(pppMiasma* pppMiasma, void* param_2, pppMiasmaCtrl* param_3
     _GXSetTevSwapMode__F13_GXTevStageID13_GXTevSwapSel13_GXTevSwapSel(0, 0, 0);
     Graphic.SetViewport();
     gUtil.InitConstantRegister();
-}
-
-/*
- * --INFO--
- * PAL Address: 0x80109b08
- * PAL Size: 100b
- * EN Address: TODO
- * EN Size: TODO
- * JP Address: TODO
- * JP Size: TODO
- */
-void pppConstructMiasma(pppMiasma* pppMiasma, pppMiasmaCtrl* param_2)
-{
-    u8* work;
-
-    work = (u8*)pppMiasma + 0x80 + param_2->m_serializedDataOffsets[2];
-    memset(work, 0, 8);
-    memset(work + 8, 0, 8);
-    memset(work + 0x10, 0, 8);
-}
-
-/*
- * --INFO--
- * PAL Address: 0x80109aa4
- * PAL Size: 100b
- * EN Address: TODO
- * EN Size: TODO
- * JP Address: TODO
- * JP Size: TODO
- */
-void pppConstruct2Miasma(pppMiasma* pppMiasma, pppMiasmaCtrl* param_2)
-{
-    u8* work;
-
-    work = (u8*)pppMiasma + 0x80 + param_2->m_serializedDataOffsets[2];
-    memset(work, 0, 8);
-    memset(work + 8, 0, 8);
-    memset(work + 0x10, 0, 8);
-}
-
-/*
- * --INFO--
- * PAL Address: 0x80109aa0
- * PAL Size: 4b
- * EN Address: TODO
- * EN Size: TODO
- * JP Address: TODO
- * JP Size: TODO
- */
-void pppDestructMiasma(pppMiasma*, pppMiasmaCtrl*)
-{
-	return;
-}
-
-/*
- * --INFO--
- * PAL Address: 0x80109930
- * PAL Size: 368b
- * EN Address: TODO
- * EN Size: TODO
- * JP Address: TODO
- * JP Size: TODO
- */
-void pppFrameMiasma(pppMiasma* pppMiasma, pppMiasmaFrameStep* param_2, pppMiasmaCtrl* param_3)
-{
-    s16* work;
-
-    if (gPppCalcDisabled != 0) {
-        return;
-    }
-
-    work = (s16*)((u8*)pppMiasma + 0x80 + param_3->m_serializedDataOffsets[2]);
-    work[4] = work[4] + work[8];
-    work[0] = work[0] + work[4];
-    work[5] = work[5] + work[9];
-    work[1] = work[1] + work[5];
-    work[6] = work[6] + work[10];
-    work[2] = work[2] + work[6];
-    work[7] = work[7] + work[11];
-    work[3] = work[3] + work[7];
-
-    if (pppMiasma->m_graphId != param_2->m_graphId) {
-        return;
-    }
-
-    work[0] = work[0] + param_2->m_addPosX;
-    work[1] = work[1] + param_2->m_addPosY;
-    work[2] = work[2] + param_2->m_addPosZ;
-    work[3] = work[3] + param_2->m_addPosW;
-    work[4] = work[4] + param_2->m_addVelX;
-    work[5] = work[5] + param_2->m_addVelY;
-    work[6] = work[6] + param_2->m_addVelZ;
-    work[7] = work[7] + param_2->m_addVelW;
-    work[8] = work[8] + param_2->m_addAccX;
-    work[9] = work[9] + param_2->m_addAccY;
-    work[10] = work[10] + param_2->m_addAccZ;
-    work[11] = work[11] + param_2->m_addAccW;
-}
-
-/*
- * --INFO--
- * Address:	TODO
- * Size:	TODO
- */
-void GXSetTexCoordGen(void)
-{
-	// TODO
 }

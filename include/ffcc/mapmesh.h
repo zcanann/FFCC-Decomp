6f4aa3913a11be631bef0e84d9e9a82e89904df3
AsclepiusBot <asclepiusbot@proton.me>
2026-04-13 10:21:08 -0700

    Define the real CMapMesh layout (#8655)
    * Correct CMapMesh layout
    
    * Define CMapMesh members
    
    ---------
    
    Co-authored-by: Zachary Canann <zcanann@Zacharys-Mac-mini.local>

--- include/ffcc/mapmesh.h
+++ include/ffcc/mapmesh.h
@@ -16,7 +16,7 @@ class CMapMesh
 public:
     CMapMesh();
     ~CMapMesh();
-    
+
     void Destroy();
     void Ptr2Off();
     void Off2Ptr();
@@ -33,7 +33,26 @@ public:
     void pppCacheRefCnt0UpModelTexture(CMaterialSet*, CAmemCacheSet*);
     void pppCacheDumpModelTexture(CMaterialSet*, CAmemCacheSet*);
 
-    char placeholder[0x40];
+    unsigned short m_vertexCount;      // 0x00
+    unsigned short m_normalCount;      // 0x02
+    unsigned short m_nbtCount;         // 0x04
+    unsigned short m_uvCount;          // 0x06
+    unsigned short m_colorCount;       // 0x08
+    unsigned short m_displayListCount; // 0x0A
+    float m_bboxMinX;                  // 0x0C
+    float m_bboxMinY;                  // 0x10
+    float m_bboxMinZ;                  // 0x14
+    float m_bboxMaxX;                  // 0x18
+    float m_bboxMaxY;                  // 0x1C
+    float m_bboxMaxZ;                  // 0x20
+    void* m_meshData;                  // 0x24
+    void* m_displayListData;           // 0x28
+    void* m_vertices;                  // 0x2C
+    void* m_normals;                   // 0x30
+    void* m_nbt;                       // 0x34
+    void* m_uvPairs;                   // 0x38
+    void* m_colors;                    // 0x3C
+    void* m_drawEntries;               // 0x40
 };
 
 #endif // _FFCC_MAPMESH_H_

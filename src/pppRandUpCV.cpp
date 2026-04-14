112bb7c31b8b7d49dd49d6ef9a8bbb9c14c759e6
Zachary Canann <zacharycanann@gmail.com>
2026-04-13 20:50:45 -0700

    Improve pppRandUpCV match

--- src/pppRandUpCV.cpp
+++ src/pppRandUpCV.cpp
@@ -63,19 +63,23 @@ void pppRandUpCV(void* param1, void* param2, void* param3)
         f32 scale = *valuePtr;
         {
             s8 baseValue = in->delta[0];
-            target[0] = (u8)(target[0] + (s32)((f32)baseValue * scale));
+            f32 scaledValue = (f32)baseValue;
+            target[0] = (u8)(target[0] + (s32)(scaledValue * scale));
         }
         {
             s8 baseValue = in->delta[1];
-            target[1] = (u8)(target[1] + (s32)((f32)baseValue * scale));
+            f32 scaledValue = (f32)baseValue;
+            target[1] = (u8)(target[1] + (s32)(scaledValue * scale));
         }
         {
             s8 baseValue = in->delta[2];
-            target[2] = (u8)(target[2] + (s32)((f32)baseValue * scale));
+            f32 scaledValue = (f32)baseValue;
+            target[2] = (u8)(target[2] + (s32)(scaledValue * scale));
         }
         {
             s8 baseValue = in->delta[3];
-            target[3] = (u8)(target[3] + (s32)((f32)baseValue * scale));
+            f32 scaledValue = (f32)baseValue;
+            target[3] = (u8)(target[3] + (s32)(scaledValue * scale));
         }
     }
 }

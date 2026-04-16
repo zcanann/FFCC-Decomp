d812598ece709b918d5055303ea929969d40be42
asmithbot <asmithbot@proton.me>
2026-04-16 13:42:17 -0700

    Match RedDriver _SetMusicData wrapper (#8865)
    Co-authored-by: Zachary Canann <zacharycanann@gmail.com>

--- src/RedSound/RedDriver.cpp
+++ src/RedSound/RedDriver.cpp
@@ -231,7 +231,7 @@ void _SetReverbDepth(int* param_1)
  * Address:	TODO
  * Size:	TODO
  */
-#pragma optimization_level 4
+#pragma optimization_level 0
 void _SetMusicData(int* param_1)
 {
     SetMusicData__9CRedEntryFP12RedMusicHEAD(&DAT_8032e154, (RedMusicHEAD*)*param_1);

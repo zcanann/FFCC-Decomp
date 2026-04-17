37059a5df3e91245ff102f84b13e6ea632e56a82
asmithbot <asmithbot@proton.me>
2026-04-16 23:58:38 -0700

    Align pppShape function order (#8935)
    Co-authored-by: Zachary Canann <zacharycanann@gmail.com>

--- src/pppShape.cpp
+++ src/pppShape.cpp
@@ -38,183 +38,72 @@ public:
 
 /*
  * --INFO--
- * PAL Address: 0x80065c6c
- * PAL Size: 64b
+ * PAL Address: 0x80065678
+ * PAL Size: 100b
  * EN Address: TODO
  * EN Size: TODO
  * JP Address: TODO
  * JP Size: TODO
  */
-void* pppShapeSt::GetTexture(long* animData, CMaterialSet* materialSet, int& textureIndex)
+void pppCalcFrameShape(long* animData, short& currentFrame, short& drawFrame, short& frameTime,
+                       short deltaTime)
 {
-    unsigned char* shape = (unsigned char*)animData + *(short*)((int)animData + 0x10) + 8;
+    char* frameData = (char*)animData + (currentFrame << 3) + 0x10;
 
-    textureIndex = shape[2];
-    CMaterial* material = (*reinterpret_cast<CPtrArray<CMaterial*>*>((char*)materialSet + 8))[shape[2]];
-    return *(void**)((char*)material + 0x3C);
+    drawFrame = currentFrame;
+    frameTime += deltaTime;
+    if (frameTime < *(short*)(frameData + 2)) {
+        return;
+    }
+    frameTime -= *(short*)(frameData + 2);
+    currentFrame += 1;
+    if (currentFrame < *(short*)((int)animData + 6)) {
+        return;
+    }
+    currentFrame = 0;
+    frameTime = 0;
 }
 
 /*
  * --INFO--
- * PAL Address: 0x80065b74
- * PAL Size: 248b
+ * PAL Address: 0x800656dc
+ * PAL Size: 184b
  * EN Address: TODO
  * EN Size: TODO
  * JP Address: TODO
  * JP Size: TODO
  */
-void pppDrawShp(long* animData, short frameIndex, CMaterialSet* materialSet, unsigned char blendMode)
+void pppGetShapeUV(long* animData, short frameIndex, Vec2d& minUv, Vec2d& maxUv, int shapeIndex)
 {
-    int iVar1;
-    int iVar2;
+    int shapeBase = *(short*)((int)animData + frameIndex * 8 + 0x10);
+    int shapeEntry = *(int*)((int)animData + shapeBase + 0xc + shapeIndex * 8);
+    float* minUvF = (float*)&minUv;
+    float* maxUvF = (float*)&maxUv;
+    const float uvScale = FLOAT_80330108;
 
-    int shapePtr = (int)animData;
-    shapePtr = shapePtr + *(short*)(shapePtr + frameIndex * 8 + 0x10);
-
-    *(int*)(MaterialManRaw() + 296) = *(int*)(MaterialManRaw() + 284);
-    *(int*)(MaterialManRaw() + 300) = *(int*)(MaterialManRaw() + 288);
-    *(int*)(MaterialManRaw() + 304) = *(int*)(MaterialManRaw() + 292);
-    *(int*)(MaterialManRaw() + 64) = *(int*)(MaterialManRaw() + 72);
-
-    SetMaterialPart__12CMaterialManFP12CMaterialSetii(&MaterialMan, materialSet,
-                                                      *(unsigned char*)(shapePtr + 10), 0);
-
-    GXClearVtxDesc();
-    GXSetVtxDesc((GXAttr)9, GX_DIRECT);
-    GXSetVtxDesc((GXAttr)11, GX_DIRECT);
-    GXSetVtxDesc((GXAttr)13, GX_DIRECT);
-
-    iVar2 = shapePtr;
-    for (iVar1 = 0; iVar1 < *(short*)(shapePtr + 2); iVar1 = iVar1 + 1) {
-        if (blendMode == 0xFF) {
-            pppSetBlendMode(*(unsigned char*)(iVar2 + 8));
-        }
-        GXCallDisplayList(*(void**)(iVar2 + 0xc), 0x60);
-        iVar2 = iVar2 + 8;
-    }
+    minUvF[0] = (float)*(short*)(shapeEntry + 0x13) * uvScale;
+    minUvF[1] = (float)*(short*)(shapeEntry + 0x15) * uvScale;
+    maxUvF[0] = (float)*(short*)(shapeEntry + 0x3b) * uvScale;
+    maxUvF[1] = (float)*(short*)(shapeEntry + 0x3d) * uvScale;
 }
 
 /*
  * --INFO--
- * PAL Address: 0x80065a94
- * PAL Size: 224b
+ * PAL Address: 0x80065794
+ * PAL Size: 120b
  * EN Address: TODO
  * EN Size: TODO
  * JP Address: TODO
  * JP Size: TODO
  */
-void pppDrawShp(tagOAN3_SHAPE* shape, CMaterialSet* materialSet, unsigned char blendMode)
+void pppGetShapePos(long* animData, short frameIndex, Vec& minPos, Vec& maxPos, int shapeIndex)
 {
-    int shapePtr;
-    int shapeCount;
+    int shapeBase = *(short*)((int)animData + frameIndex * 8 + 0x10);
+    u8* shapeEntry = (u8*)*(int*)((int)animData + shapeBase + 0xc + shapeIndex * 8);
+    u8* minSrc = shapeEntry + 3;
 
-    *(int*)(MaterialManRaw() + 296) = *(int*)(MaterialManRaw() + 284);
-    *(int*)(MaterialManRaw() + 300) = *(int*)(MaterialManRaw() + 288);
-    *(int*)(MaterialManRaw() + 304) = *(int*)(MaterialManRaw() + 292);
-    *(int*)(MaterialManRaw() + 64) = *(int*)(MaterialManRaw() + 72);
-
-    SetMaterialPart__12CMaterialManFP12CMaterialSetii(&MaterialMan, materialSet,
-                                                      *(unsigned char*)((int)shape + 10), 0);
-
-    GXClearVtxDesc();
-    GXSetVtxDesc((GXAttr)9, GX_DIRECT);
-    GXSetVtxDesc((GXAttr)11, GX_DIRECT);
-    GXSetVtxDesc((GXAttr)13, GX_DIRECT);
-
-    shapePtr = (int)shape;
-    for (shapeCount = 0; shapeCount < *(short*)((int)shape + 2); shapeCount = shapeCount + 1) {
-        if (blendMode == 0xFF) {
-            pppSetBlendMode(*(unsigned char*)(shapePtr + 8));
-        }
-        GXCallDisplayList(*(void**)(shapePtr + 0xc), 0x60);
-        shapePtr = shapePtr + 8;
-    }
-}
-
-/*
- * --INFO--
- * PAL Address: 0x800659ec
- * PAL Size: 168b
- * EN Address: TODO
- * EN Size: TODO
- * JP Address: TODO
- * JP Size: TODO
- */
-void pppSetShapeMaterial(pppShapeSt* shapeSt, CMaterialSet* materialSet, char** textureNames)
-{
-    int shapeIndex;
-    int shapeEntry;
-    char* currentFrame;
-    char* animData;
-    int frameIndex;
-    int shapeBase;
-
-    animData = (char*)shapeSt->m_animData;
-    currentFrame = animData;
-    for (frameIndex = 0; frameIndex < *(short*)((int)animData + 6); frameIndex = frameIndex + 1) {
-        shapeBase = (int)animData + (int)*(short*)((int)currentFrame + 0x10);
-        shapeEntry = shapeBase;
-        for (shapeIndex = 0; shapeIndex < *(short*)(shapeBase + 2); shapeIndex = shapeIndex + 1) {
-            *(unsigned char*)(shapeEntry + 10) =
-                FindTexName__12CMaterialSetFPcPl(materialSet, textureNames[*(unsigned char*)(shapeEntry + 10)], 0);
-            *(int*)(shapeEntry + 0xc) = (int)shapeSt->m_displayListData + *(int*)(shapeEntry + 0xc);
-            shapeEntry = shapeEntry + 8;
-        }
-        currentFrame += 8;
-    }
-}
-
-/*
- * --INFO--
- * PAL Address: 0x800658fc
- * PAL Size: 240b
- * EN Address: TODO
- * EN Size: TODO
- * JP Address: TODO
- * JP Size: TODO
- */
-void pppCacheLoadShapeTexture(pppShapeSt* shapeSt, CMaterialSet* materialSet)
-{
-    short shapeOffset;
-    char* currentFrame;
-    int frameIndex;
-    char* animData;
-    unsigned char* texturePtr;
-    unsigned int textureIndex;
-    unsigned char textureUsed[0x100];
-
-    animData = (char*)shapeSt->m_animData;
-    memset(textureUsed, 0, sizeof(textureUsed));
-
-    currentFrame = animData;
-    for (frameIndex = 0; frameIndex < *(short*)((int)animData + 6); frameIndex = frameIndex + 1) {
-        int shapeIndex;
-        int shapeStep;
-        char* shapeBase;
-        unsigned char* shapeEntry;
-
-        shapeOffset = *(short*)((int)currentFrame + 0x10);
-        shapeBase = animData + shapeOffset;
-        shapeIndex = 0;
-        shapeStep = 0;
-        while (shapeIndex < *(short*)(shapeBase + 2)) {
-            shapeEntry = (unsigned char*)(shapeBase + 8 + shapeStep);
-            shapeIndex += 1;
-            shapeStep += 8;
-            textureUsed[shapeEntry[2]] = 1;
-        }
-        currentFrame += 8;
-    }
-
-    texturePtr = textureUsed;
-    textureIndex = 0;
-    do {
-        if (*texturePtr != 0) {
-            CacheLoadTexture__12CMaterialSetFiP13CAmemCacheSet(materialSet, textureIndex, &ppvAmemCacheSet);
-        }
-        textureIndex++;
-        texturePtr++;
-    } while (textureIndex < 0x100);
+    memcpy(&minPos, minSrc, 0xc);
+    memcpy(&maxPos, minSrc + 0x28, 0xc);
 }
 
 /*
@@ -272,70 +161,181 @@ void pppCacheDumpShapeTexture(pppShapeSt* shapeSt, CMaterialSet* materialSet)
 
 /*
  * --INFO--
- * PAL Address: 0x80065794
- * PAL Size: 120b
+ * PAL Address: 0x800658fc
+ * PAL Size: 240b
  * EN Address: TODO
  * EN Size: TODO
  * JP Address: TODO
  * JP Size: TODO
  */
-void pppGetShapePos(long* animData, short frameIndex, Vec& minPos, Vec& maxPos, int shapeIndex)
+void pppCacheLoadShapeTexture(pppShapeSt* shapeSt, CMaterialSet* materialSet)
 {
-    int shapeBase = *(short*)((int)animData + frameIndex * 8 + 0x10);
-    u8* shapeEntry = (u8*)*(int*)((int)animData + shapeBase + 0xc + shapeIndex * 8);
-    u8* minSrc = shapeEntry + 3;
+    short shapeOffset;
+    char* currentFrame;
+    int frameIndex;
+    char* animData;
+    unsigned char* texturePtr;
+    unsigned int textureIndex;
+    unsigned char textureUsed[0x100];
 
-    memcpy(&minPos, minSrc, 0xc);
-    memcpy(&maxPos, minSrc + 0x28, 0xc);
+    animData = (char*)shapeSt->m_animData;
+    memset(textureUsed, 0, sizeof(textureUsed));
+
+    currentFrame = animData;
+    for (frameIndex = 0; frameIndex < *(short*)((int)animData + 6); frameIndex = frameIndex + 1) {
+        int shapeIndex;
+        int shapeStep;
+        char* shapeBase;
+        unsigned char* shapeEntry;
+
+        shapeOffset = *(short*)((int)currentFrame + 0x10);
+        shapeBase = animData + shapeOffset;
+        shapeIndex = 0;
+        shapeStep = 0;
+        while (shapeIndex < *(short*)(shapeBase + 2)) {
+            shapeEntry = (unsigned char*)(shapeBase + 8 + shapeStep);
+            shapeIndex += 1;
+            shapeStep += 8;
+            textureUsed[shapeEntry[2]] = 1;
+        }
+        currentFrame += 8;
+    }
+
+    texturePtr = textureUsed;
+    textureIndex = 0;
+    do {
+        if (*texturePtr != 0) {
+            CacheLoadTexture__12CMaterialSetFiP13CAmemCacheSet(materialSet, textureIndex, &ppvAmemCacheSet);
+        }
+        textureIndex++;
+        texturePtr++;
+    } while (textureIndex < 0x100);
 }
 
 /*
  * --INFO--
- * PAL Address: 0x800656dc
- * PAL Size: 184b
+ * PAL Address: 0x800659ec
+ * PAL Size: 168b
  * EN Address: TODO
  * EN Size: TODO
  * JP Address: TODO
  * JP Size: TODO
  */
-void pppGetShapeUV(long* animData, short frameIndex, Vec2d& minUv, Vec2d& maxUv, int shapeIndex)
+void pppSetShapeMaterial(pppShapeSt* shapeSt, CMaterialSet* materialSet, char** textureNames)
 {
-    int shapeBase = *(short*)((int)animData + frameIndex * 8 + 0x10);
-    int shapeEntry = *(int*)((int)animData + shapeBase + 0xc + shapeIndex * 8);
-    float* minUvF = (float*)&minUv;
-    float* maxUvF = (float*)&maxUv;
-    const float uvScale = FLOAT_80330108;
+    int shapeIndex;
+    int shapeEntry;
+    char* currentFrame;
+    char* animData;
+    int frameIndex;
+    int shapeBase;
 
-    minUvF[0] = (float)*(short*)(shapeEntry + 0x13) * uvScale;
-    minUvF[1] = (float)*(short*)(shapeEntry + 0x15) * uvScale;
-    maxUvF[0] = (float)*(short*)(shapeEntry + 0x3b) * uvScale;
-    maxUvF[1] = (float)*(short*)(shapeEntry + 0x3d) * uvScale;
+    animData = (char*)shapeSt->m_animData;
+    currentFrame = animData;
+    for (frameIndex = 0; frameIndex < *(short*)((int)animData + 6); frameIndex = frameIndex + 1) {
+        shapeBase = (int)animData + (int)*(short*)((int)currentFrame + 0x10);
+        shapeEntry = shapeBase;
+        for (shapeIndex = 0; shapeIndex < *(short*)(shapeBase + 2); shapeIndex = shapeIndex + 1) {
+            *(unsigned char*)(shapeEntry + 10) =
+                FindTexName__12CMaterialSetFPcPl(materialSet, textureNames[*(unsigned char*)(shapeEntry + 10)], 0);
+            *(int*)(shapeEntry + 0xc) = (int)shapeSt->m_displayListData + *(int*)(shapeEntry + 0xc);
+            shapeEntry = shapeEntry + 8;
+        }
+        currentFrame += 8;
+    }
 }
 
 /*
  * --INFO--
- * PAL Address: 0x80065678
- * PAL Size: 100b
+ * PAL Address: 0x80065a94
+ * PAL Size: 224b
  * EN Address: TODO
  * EN Size: TODO
  * JP Address: TODO
  * JP Size: TODO
  */
-void pppCalcFrameShape(long* animData, short& currentFrame, short& drawFrame, short& frameTime,
-                       short deltaTime)
+void pppDrawShp(tagOAN3_SHAPE* shape, CMaterialSet* materialSet, unsigned char blendMode)
 {
-    char* frameData = (char*)animData + (currentFrame << 3) + 0x10;
+    int shapePtr;
+    int shapeCount;
 
-    drawFrame = currentFrame;
-    frameTime += deltaTime;
-    if (frameTime < *(short*)(frameData + 2)) {
-        return;
+    *(int*)(MaterialManRaw() + 296) = *(int*)(MaterialManRaw() + 284);
+    *(int*)(MaterialManRaw() + 300) = *(int*)(MaterialManRaw() + 288);
+    *(int*)(MaterialManRaw() + 304) = *(int*)(MaterialManRaw() + 292);
+    *(int*)(MaterialManRaw() + 64) = *(int*)(MaterialManRaw() + 72);
+
+    SetMaterialPart__12CMaterialManFP12CMaterialSetii(&MaterialMan, materialSet,
+                                                      *(unsigned char*)((int)shape + 10), 0);
+
+    GXClearVtxDesc();
+    GXSetVtxDesc((GXAttr)9, GX_DIRECT);
+    GXSetVtxDesc((GXAttr)11, GX_DIRECT);
+    GXSetVtxDesc((GXAttr)13, GX_DIRECT);
+
+    shapePtr = (int)shape;
+    for (shapeCount = 0; shapeCount < *(short*)((int)shape + 2); shapeCount = shapeCount + 1) {
+        if (blendMode == 0xFF) {
+            pppSetBlendMode(*(unsigned char*)(shapePtr + 8));
+        }
+        GXCallDisplayList(*(void**)(shapePtr + 0xc), 0x60);
+        shapePtr = shapePtr + 8;
     }
-    frameTime -= *(short*)(frameData + 2);
-    currentFrame += 1;
-    if (currentFrame < *(short*)((int)animData + 6)) {
-        return;
+}
+
+/*
+ * --INFO--
+ * PAL Address: 0x80065b74
+ * PAL Size: 248b
+ * EN Address: TODO
+ * EN Size: TODO
+ * JP Address: TODO
+ * JP Size: TODO
+ */
+void pppDrawShp(long* animData, short frameIndex, CMaterialSet* materialSet, unsigned char blendMode)
+{
+    int iVar1;
+    int iVar2;
+
+    int shapePtr = (int)animData;
+    shapePtr = shapePtr + *(short*)(shapePtr + frameIndex * 8 + 0x10);
+
+    *(int*)(MaterialManRaw() + 296) = *(int*)(MaterialManRaw() + 284);
+    *(int*)(MaterialManRaw() + 300) = *(int*)(MaterialManRaw() + 288);
+    *(int*)(MaterialManRaw() + 304) = *(int*)(MaterialManRaw() + 292);
+    *(int*)(MaterialManRaw() + 64) = *(int*)(MaterialManRaw() + 72);
+
+    SetMaterialPart__12CMaterialManFP12CMaterialSetii(&MaterialMan, materialSet,
+                                                      *(unsigned char*)(shapePtr + 10), 0);
+
+    GXClearVtxDesc();
+    GXSetVtxDesc((GXAttr)9, GX_DIRECT);
+    GXSetVtxDesc((GXAttr)11, GX_DIRECT);
+    GXSetVtxDesc((GXAttr)13, GX_DIRECT);
+
+    iVar2 = shapePtr;
+    for (iVar1 = 0; iVar1 < *(short*)(shapePtr + 2); iVar1 = iVar1 + 1) {
+        if (blendMode == 0xFF) {
+            pppSetBlendMode(*(unsigned char*)(iVar2 + 8));
+        }
+        GXCallDisplayList(*(void**)(iVar2 + 0xc), 0x60);
+        iVar2 = iVar2 + 8;
     }
-    currentFrame = 0;
-    frameTime = 0;
+}
+
+/*
+ * --INFO--
+ * PAL Address: 0x80065c6c
+ * PAL Size: 64b
+ * EN Address: TODO
+ * EN Size: TODO
+ * JP Address: TODO
+ * JP Size: TODO
+ */
+void* pppShapeSt::GetTexture(long* animData, CMaterialSet* materialSet, int& textureIndex)
+{
+    unsigned char* shape = (unsigned char*)animData + *(short*)((int)animData + 0x10) + 8;
+
+    textureIndex = shape[2];
+    CMaterial* material = (*reinterpret_cast<CPtrArray<CMaterial*>*>((char*)materialSet + 8))[shape[2]];
+    return *(void**)((char*)material + 0x3C);
 }

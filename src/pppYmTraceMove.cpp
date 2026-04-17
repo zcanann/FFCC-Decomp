002f9aefee0128ccadcc5140ca0ce962c410e4f7
asmithbot <asmithbot@proton.me>
2026-04-17 00:51:30 -0700

    Match pppFrameYmTraceMove quaternion setup (#8940)
    Co-authored-by: Zachary Canann <zacharycanann@gmail.com>

--- src/pppYmTraceMove.cpp
+++ src/pppYmTraceMove.cpp
@@ -53,8 +53,8 @@ void pppFrameYmTraceMove(pppYmTraceMove* pppYmTraceMove, pppYmTraceMoveUnkB* par
 	Vec local_8c;
 	Vec local_ec;
 	Vec local_f8;
-	Quaternion local_70;
 	Quaternion local_60;
+	Quaternion local_70;
 	Quaternion local_80;
 
 	work->m_velocity = work->m_velocity + work->m_acceleration;
@@ -90,14 +90,14 @@ void pppFrameYmTraceMove(pppYmTraceMove* pppYmTraceMove, pppYmTraceMoveUnkB* par
 		pppNormalize(local_2c, local_2c);
 	}
 
-	local_70.x = local_2c.x;
-	local_70.y = local_2c.y;
-	local_70.z = local_2c.z;
-	local_70.w = kPppYmTraceMoveOne;
 	local_60.x = local_20.x;
 	local_60.y = local_20.y;
 	local_60.z = local_20.z;
-	local_60.w = local_70.w;
+	local_60.w = kPppYmTraceMoveOne;
+	local_70.x = local_2c.x;
+	local_70.y = local_2c.y;
+	local_70.z = local_2c.z;
+	local_70.w = local_60.w;
 	C_QUATLerp(&local_70, &local_60, &local_80, param_2->m_dataValIndex);
 	PSQUATNormalize(&local_80, &local_80);
 

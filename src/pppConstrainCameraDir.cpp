2efb0ac0cfc6428dc7ae728fd8569bd9de1dbb36
AsclepiusBot <asclepiusbot@proton.me>
2026-04-16 18:53:37 -0700

    Use recovered camera fields in pppConstrainCameraDir (#8901)
    Co-authored-by: Zachary Canann <zcanann@Zacharys-Mac-mini.local>

--- src/pppConstrainCameraDir.cpp
+++ src/pppConstrainCameraDir.cpp
@@ -4,14 +4,6 @@
 #include "ffcc/pppYmEnv.h"
 #include <dolphin/mtx.h>
 
-static inline float CameraPosX() { return *reinterpret_cast<float*>(reinterpret_cast<unsigned char*>(&CameraPcs) + 0xE0); }
-static inline float CameraPosY() { return *reinterpret_cast<float*>(reinterpret_cast<unsigned char*>(&CameraPcs) + 0xE4); }
-static inline float CameraPosZ() { return *reinterpret_cast<float*>(reinterpret_cast<unsigned char*>(&CameraPcs) + 0xE8); }
-static inline float CameraDirX() { return *reinterpret_cast<float*>(reinterpret_cast<unsigned char*>(&CameraPcs) + 0xEC); }
-static inline float CameraDirY() { return *reinterpret_cast<float*>(reinterpret_cast<unsigned char*>(&CameraPcs) + 0xF0); }
-static inline float CameraDirZ() { return *reinterpret_cast<float*>(reinterpret_cast<unsigned char*>(&CameraPcs) + 0xF4); }
-static inline float CameraDistance() { return *reinterpret_cast<float*>(reinterpret_cast<unsigned char*>(&CameraPcs) + 0xFC); }
-static inline MtxPtr CameraMatrix() { return reinterpret_cast<MtxPtr>(reinterpret_cast<unsigned char*>(&CameraPcs) + 0x4); }
 extern "C" void pppSetFpMatrix__FP9_pppMngSt(_pppMngSt*);
 
 /*
@@ -83,44 +75,44 @@ void pppFrameConstrainCameraDir(pppConstrainCameraDir* pppConstrainCameraDir, pp
 
         CalcGraphValue(&pppConstrainCameraDir->m_pppPObject, param_2->m_graphId, value[0], value[1], value[2],
                        param_2->m_dataValIndex, param_2->m_initWOrk, param_2->m_stepValue);
-        
+
         if ((gPppInConstructor != 1) && ((flags[1] != 0 || flags[0] != 0))) {
-            float cameraDirX = CameraDirX();
-            float cameraDirY = CameraDirY();
-            float cameraDirZ = CameraDirZ();
-            
-            Mtx MStack_b8;
-            PSMTXCopy(CameraMatrix(), MStack_b8);
-            
-            float cameraPosX = CameraPosX();
-            float cameraPosY = CameraPosY();
-            float cameraPosZ = CameraPosZ();
-            float scale = ((CameraDistance() - 25.0f) / 25.0f) + 1.0f;
+            float cameraDirX = CameraPcs._236_4_;
+            float cameraDirY = CameraPcs._240_4_;
+            float cameraDirZ = CameraPcs._244_4_;
+
+            Mtx cameraMtx;
+            PSMTXCopy(CameraPcs.m_cameraMatrix, cameraMtx);
+
+            float cameraPosX = CameraPcs._224_4_;
+            float cameraPosY = CameraPcs._228_4_;
+            float cameraPosZ = CameraPcs._232_4_;
+            float scale = ((CameraPcs._252_4_ - 25.0f) / 25.0f) + 1.0f;
 
             PSMTXIdentity(pppMngStPtr->m_matrix.value);
 
             pppMngSt->m_scale.x = 1.3333f * scale;
             pppMngSt->m_scale.y = scale;
             pppMngSt->m_scale.z = 1.0f;
-            
-            Mtx MStack_e8;
-            PSMTXScale(MStack_e8, pppMngSt->m_scale.x, pppMngSt->m_scale.y, pppMngSt->m_scale.z);
-            
+
+            Mtx scaleMtx;
+            PSMTXScale(scaleMtx, pppMngSt->m_scale.x, pppMngSt->m_scale.y, pppMngSt->m_scale.z);
+
             if (flags[1] != 0) {
-                PSMTXInverse(MStack_b8, pppMngStPtr->m_matrix.value);
+                PSMTXInverse(cameraMtx, pppMngStPtr->m_matrix.value);
             }
-            
-            PSMTXConcat(MStack_e8, pppMngStPtr->m_matrix.value, pppMngStPtr->m_matrix.value);
-            
+
+            PSMTXConcat(scaleMtx, pppMngStPtr->m_matrix.value, pppMngStPtr->m_matrix.value);
+
             if (flags[0] != 0) {
-                float resultZ = cameraDirZ * value[0] + cameraPosZ;
-                float resultY = cameraDirY * value[0] + cameraPosY;
-                float resultX = cameraDirX * value[0] + cameraPosX;
+                float resultZ = cameraDirZ * *value + cameraPosZ;
+                float resultY = cameraDirY * *value + cameraPosY;
+                float resultX = cameraDirX * *value + cameraPosX;
                 pppMngStPtr->m_matrix.value[0][3] = resultX;
                 pppMngStPtr->m_matrix.value[1][3] = resultY;
                 pppMngStPtr->m_matrix.value[2][3] = resultZ;
             }
-            
+
             pppSetFpMatrix__FP9_pppMngSt(pppMngSt);
         }
     }

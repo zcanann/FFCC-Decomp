e9f081328fa7dc9bfbc91e08ed21b3ab6dafbbb3
asmithbot <asmithbot@proton.me>
2026-04-19 09:56:03 -0700

    Match p_sound static init layout (#9183)
    Co-authored-by: Zachary Canann <zacharycanann@gmail.com>

--- include/ffcc/p_sound.h
+++ include/ffcc/p_sound.h
@@ -14,24 +14,24 @@ class CSoundPcs : public CProcess
 public:
     CSoundPcs()
     {
-        unsigned int* table = m_table__9CSoundPcs;
+        unsigned int* table = &m_table__9CSoundPcs[1];
         const unsigned int* desc0 = m_table_desc0__9CSoundPcs;
         const unsigned int* desc1 = m_table_desc1__9CSoundPcs;
         const unsigned int* desc2 = m_table_desc2__9CSoundPcs;
         const unsigned int* desc3 = m_table_desc3__9CSoundPcs;
 
-        table[1] = desc0[0];
-        table[2] = desc0[1];
-        table[3] = desc0[2];
-        table[4] = desc1[0];
-        table[5] = desc1[1];
-        table[6] = desc1[2];
-        table[7] = desc2[0];
-        table[8] = desc2[1];
-        table[9] = desc2[2];
-        table[12] = desc3[0];
-        table[13] = desc3[1];
-        table[14] = desc3[2];
+        table[0] = desc0[0];
+        table[1] = desc0[1];
+        table[2] = desc0[2];
+        table[3] = desc1[0];
+        table[4] = desc1[1];
+        table[5] = desc1[2];
+        table[6] = desc2[0];
+        table[7] = desc2[1];
+        table[8] = desc2[2];
+        table[11] = desc3[0];
+        table[12] = desc3[1];
+        table[13] = desc3[2];
     }
 
     void draw();

700f2a97b980f3ff21691d994dad217523e1b3bc
AsclepiusBot <asclepiusbot@proton.me>
2026-04-19 08:56:25 -0700

    Refine pppMiasma object layout (#9172)
    Co-authored-by: Zachary Canann <zcanann@Zacharys-Mac-mini.local>

--- include/ffcc/pppMiasma.h
+++ include/ffcc/pppMiasma.h
@@ -1,12 +1,15 @@
 #ifndef _PPP_MIASMA_H_
 #define _PPP_MIASMA_H_
 
-#include <dolphin/types.h>
+#include "ffcc/partMng.h"
 
 typedef struct {
     u8 m_pad_0x0[0xc];
     s32 m_graphId;
-    u8 m_pad_0x10[0x70];
+    pppFMATRIX m_localMatrix;
+    pppFMATRIX m_drawMatrix;
+    Vec* m_meshPoints;
+    u8 m_pad_0x74[0xc];
 } pppMiasma;
 
 typedef struct {
@@ -27,6 +30,16 @@ typedef struct {
 } pppMiasmaFrameStep;
 
 typedef struct {
+    s32 m_graphId;
+    s32 m_dataValIndex;
+    u16 m_initWOrk;
+    u8 m_pad_0x0a[2];
+    float m_stepValue;
+    u8 m_arg3;
+    u8 m_payload[0x1f];
+} pppMiasmaRenderStep;
+
+typedef struct {
     u8 m_pad_0x0[0xc];
     s32* m_serializedDataOffsets;
 } pppMiasmaCtrl;
@@ -35,7 +48,7 @@ typedef struct {
 extern "C" {
 #endif
 
-void pppRenderMiasma(pppMiasma*, void*, pppMiasmaCtrl*);
+void pppRenderMiasma(pppMiasma*, pppMiasmaRenderStep*, pppMiasmaCtrl*);
 void pppConstructMiasma(pppMiasma*, pppMiasmaCtrl*);
 void pppConstruct2Miasma(pppMiasma*, pppMiasmaCtrl*);
 void pppDestructMiasma(pppMiasma*, pppMiasmaCtrl*);

a0c7be0329f065f4edc912906e9231ed62ffc3af
Zachary Canann <zcanann@Zacharys-Mac-mini.local>
2026-04-18 17:52:46 -0700

    Match p_game static init

--- src/p_game.cpp
+++ src/p_game.cpp
@@ -8,6 +8,42 @@ unsigned int m_table__8CGamePcs[0x15C / sizeof(unsigned int)] = {
 };
 
 CGamePcs GamePcs;
+extern "C" void* __vt__8CGamePcs[];
+
+extern "C" void __sinit_p_game_cpp(void)
+{
+    *reinterpret_cast<void**>(&GamePcs) = __vt__8CGamePcs;
+
+    unsigned int* dst = reinterpret_cast<unsigned int*>(m_table__8CGamePcs);
+
+    dst[1] = m_table_desc0__8CGamePcs[0];
+    dst[2] = m_table_desc0__8CGamePcs[1];
+    dst[3] = m_table_desc0__8CGamePcs[2];
+    dst[4] = m_table_desc1__8CGamePcs[0];
+    dst[5] = m_table_desc1__8CGamePcs[1];
+    dst[6] = m_table_desc1__8CGamePcs[2];
+    dst[7] = m_table_desc2__8CGamePcs[0];
+    dst[8] = m_table_desc2__8CGamePcs[1];
+    dst[9] = m_table_desc2__8CGamePcs[2];
+    dst[12] = m_table_desc3__8CGamePcs[0];
+    dst[13] = m_table_desc3__8CGamePcs[1];
+    dst[14] = m_table_desc3__8CGamePcs[2];
+    dst[17] = m_table_desc4__8CGamePcs[0];
+    dst[18] = m_table_desc4__8CGamePcs[1];
+    dst[19] = m_table_desc4__8CGamePcs[2];
+    dst[22] = m_table_desc5__8CGamePcs[0];
+    dst[23] = m_table_desc5__8CGamePcs[1];
+    dst[24] = m_table_desc5__8CGamePcs[2];
+    dst[27] = m_table_desc6__8CGamePcs[0];
+    dst[28] = m_table_desc6__8CGamePcs[1];
+    dst[29] = m_table_desc6__8CGamePcs[2];
+    dst[32] = m_table_desc7__8CGamePcs[0];
+    dst[33] = m_table_desc7__8CGamePcs[1];
+    dst[34] = m_table_desc7__8CGamePcs[2];
+    dst[37] = m_table_desc8__8CGamePcs[0];
+    dst[38] = m_table_desc8__8CGamePcs[1];
+    dst[39] = m_table_desc8__8CGamePcs[2];
+}
 
 /*
  * --INFO--

52bf26924ed5f4da5c8d561cf4598e9709adc7b3
Zachary Canann <zacharycanann@gmail.com>
2026-04-20 05:10:21 -0700

    Match p_system static init

--- src/p_system.cpp
+++ src/p_system.cpp
@@ -2,19 +2,13 @@
 #include "ffcc/pad.h"
 #include "ffcc/p_dbgmenu.h"
 
-extern "C" void create__10CSystemPcsFv(CSystemPcs*);
-extern "C" void destroy__10CSystemPcsFv(CSystemPcs*);
-extern "C" void calc__10CSystemPcsFv(CSystemPcs*);
-
-CSystemPcs SystemPcs;
-char s_CSystemPcs_801D7C48[] = "CSystemPcs";
-unsigned int m_table_desc0__10CSystemPcs[3] = {0, 0xFFFFFFFF, reinterpret_cast<unsigned int>(create__10CSystemPcsFv)};
-unsigned int m_table_desc1__10CSystemPcs[3] = {0, 0xFFFFFFFF, reinterpret_cast<unsigned int>(destroy__10CSystemPcsFv)};
-unsigned int m_table_desc2__10CSystemPcs[3] = {0, 0xFFFFFFFF, reinterpret_cast<unsigned int>(calc__10CSystemPcsFv)};
+const char s_CSystemPcs_801D7C48[] = "CSystemPcs";
 
 unsigned int m_table__10CSystemPcs[0x15C / sizeof(unsigned int)] = {
-    reinterpret_cast<unsigned int>(s_CSystemPcs_801D7C48), 0, 0, 0, 0, 0, 0, 0, 0, 0, 0x16, 0x8
+    reinterpret_cast<unsigned int>(const_cast<char*>(s_CSystemPcs_801D7C48)), 0, 0, 0, 0, 0, 0, 0, 0, 0, 0x16, 0x8
 };
+
+CSystemPcs SystemPcs;
 
 /*
  * --INFO--

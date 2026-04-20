bc295377f71bc8447281bf52f40559ce31b35126
Zachary Canann <zcanann@Zacharys-Mac-mini.local>
2026-04-20 11:33:28 -0700

    Restore pppRandDownCV randchar helper

--- include/ffcc/pppRandDownCV.h
+++ include/ffcc/pppRandDownCV.h
@@ -1,10 +1,16 @@
 #ifndef _PPP_RANDDOWNCV_H_
 #define _PPP_RANDDOWNCV_H_
 
+#ifdef __cplusplus
+char randchar(char, float);
+
 extern "C" {
+#endif
 
 void pppRandDownCV(void*, void*, void*);
 
+#ifdef __cplusplus
 }
+#endif
 
 #endif // _PPP_RANDDOWNCV_H_

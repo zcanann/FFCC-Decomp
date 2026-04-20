af6814fd8b1550e6b73d1ce62b0dc7db205160bc
Zachary Canann <zcanann@Zacharys-Mac-mini.local>
2026-04-20 11:06:52 -0700

    Improve pppPointRAp temporary ordering

--- src/pppPointRAp.cpp
+++ src/pppPointRAp.cpp
@@ -79,10 +79,10 @@ void pppPointRAp(_pppPObject* pObject, void* step, _pppCtrlTable* ctrlTable)
         float spinRand = Math.RandF();
         float spinAngle = gPppPointRApRandomAngleRange * spinRand;
         s32 angleB = (s32)(gPppPointRApSpinScale * spinAngle);
-        float xOff = planarOff * *(float*)((u8*)trig + (angleB & 0xFFFC));
         float zOff = planarOff * *(float*)((u8*)trig + ((angleB + 0x4000) & 0xFFFC));
-        Vec* dstPos = (Vec*)((u8*)obj + payload->m_childPosOffset + 0x80);
+        float xOff = planarOff * *(float*)((u8*)trig + (angleB & 0xFFFC));
         Vec* dstVel = (Vec*)((u8*)obj + payload->m_childVelocityOffset + 0x80);
+        Vec* dstPos = (Vec*)((u8*)obj + payload->m_childPosOffset + 0x80);
 
         dstPos->x = srcPos->x + xOff;
         dstPos->y = srcPos->y + yOff;

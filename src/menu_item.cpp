edd795f66daadede019e9543b94b3a14b9992940
Zachary Canann <zcanann@Zacharys-Mac-mini.local>
2026-04-21 13:29:28 -0700

    Improve menu item control mode typing

--- src/menu_item.cpp
+++ src/menu_item.cpp
@@ -421,23 +421,23 @@ bool CMenuPcs::ItemOpen()
  */
 int CMenuPcs::ItemCtrl()
 {
+    short sVar1;
     int iVar2;
     int iVar3;
-    int iVar4;
 
     iVar3 = 0;
     *(s16*)((int)itemMenuState + 0x32) = *(s16*)((int)itemMenuState + 0x30);
     iVar2 = (int)itemMenuState;
-    iVar4 = *(s16*)(iVar2 + 0x30);
+    sVar1 = *(s16*)(iVar2 + 0x30);
 
-    if ((iVar4 == 0) || ((iVar4 != 0) && (*(s16*)(iVar2 + 0x12) == 1))) {
+    if ((sVar1 == 0) || ((sVar1 != 0) && (*(s16*)(iVar2 + 0x12) == 1))) {
         iVar3 = ItemCtrlCur();
-    } else if ((iVar4 == 1) && (*(s16*)(iVar2 + 0x12) == 0)) {
+    } else if ((sVar1 == 1) && (*(s16*)(iVar2 + 0x12) == 0)) {
         if (*(s16*)((int)singWindowInfo + 10) == 1) {
             iVar3 = 0;
             *(s16*)(iVar2 + 0x12) = *(s16*)(iVar2 + 0x12) + 1;
         }
-    } else if (((iVar4 == 1) && (*(s16*)(iVar2 + 0x12) == 2)) &&
+    } else if (((sVar1 == 1) && (*(s16*)(iVar2 + 0x12) == 2)) &&
                (*(s16*)((int)singWindowInfo + 10) == 3)) {
         iVar3 = 0;
         *(s16*)(iVar2 + 0x12) = 0;

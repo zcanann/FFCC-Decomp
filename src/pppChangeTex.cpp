e79e20647b93a47446c4773e5bf9473c92b28540
AsclepiusBot <asclepiusbot@proton.me>
2026-04-21 13:06:02 -0700

    Improve pppChangeTex material setup (#9615)
    Co-authored-by: Zachary Canann <zcanann@Zacharys-Mac-mini.local>

--- src/pppChangeTex.cpp
+++ src/pppChangeTex.cpp
@@ -109,30 +109,29 @@ extern "C" void ChangeTex_DrawMeshDLCallback__FPQ26CChara6CModelPvPviiPA4_f2(CCh
 
 	if (*(u8*)((char*)param_3 + 0x14) == 0) {
 		int drawTevBits = 0xACE0F;
-		int fullWord = -1;
-		u8 fullByte = 0xFF;
 		int zero = 0;
-		int fullTevBits;
+		int allOnes = -1;
+		int tevScale = 0x1E;
 
-		*(int*)(MaterialManRaw() + 0x128) = zero;
-		fullTevBits = drawTevBits | 0x1000;
 		*(int*)(MaterialManRaw() + 0x48) = drawTevBits;
-		*(int*)(MaterialManRaw() + 0x12c) = 0x1E;
+		int fullTevBits = 0xADE0F;
+		*(int*)(MaterialManRaw() + 0x128) = zero;
+		*(int*)(MaterialManRaw() + 0x12c) = tevScale;
 		*(int*)(MaterialManRaw() + 0x130) = zero;
-		*(int*)(MaterialManRaw() + 0x44) = fullWord;
-		*(char*)(MaterialManRaw() + 0x4c) = fullByte;
+		*(int*)(MaterialManRaw() + 0x44) = allOnes;
+		*(char*)(MaterialManRaw() + 0x4c) = allOnes;
 		*(int*)(MaterialManRaw() + 0x11c) = zero;
-		*(int*)(MaterialManRaw() + 0x120) = 0x1E;
+		*(int*)(MaterialManRaw() + 0x120) = tevScale;
 		*(int*)(MaterialManRaw() + 0x124) = zero;
-		*(char*)(MaterialManRaw() + 0x205) = fullByte;
-		*(char*)(MaterialManRaw() + 0x206) = fullByte;
+		*(char*)(MaterialManRaw() + 0x205) = allOnes;
+		*(char*)(MaterialManRaw() + 0x206) = allOnes;
 		*(int*)(MaterialManRaw() + 0x58) = zero;
 		*(int*)(MaterialManRaw() + 0x5c) = zero;
 		*(char*)(MaterialManRaw() + 0x208) = zero;
 		*(int*)(MaterialManRaw() + 0x48) = fullTevBits;
 		*(int*)(MaterialManRaw() + 0xd0) = *(int*)((char*)param_2 + 0x1C) + 0x28;
 		*(int*)(MaterialManRaw() + 0x128) = zero;
-		*(int*)(MaterialManRaw() + 0x12c) = 0x1E;
+		*(int*)(MaterialManRaw() + 0x12c) = tevScale;
 		*(int*)(MaterialManRaw() + 0x130) = zero;
 		*(int*)(MaterialManRaw() + 0x40) = fullTevBits;
 	}
@@ -159,8 +158,9 @@ extern "C" void ChangeTex_AfterDrawMeshCallback__FPQ26CChara6CModelPvPviPA4_f2(C
 	int dlArrayBase;
 	int dlOffset;
 	int drawTevBits;
-	int fullWord;
-	u8 fullByte;
+	int fullTevBits;
+	int allOnes;
+	int tevScale;
 	void* meshColorArrays;
 	void* meshColorArray;
 	ChangeTexMeshData* meshData;
@@ -178,31 +178,32 @@ extern "C" void ChangeTex_AfterDrawMeshCallback__FPQ26CChara6CModelPvPviPA4_f2(C
 				GXSetArray((GXAttr)0xb, meshColorArray, 4);
 				*(int*)(MaterialManRaw() + 0xd0) = dlOffset + 0x28;
 				drawTevBits = 0xACE0F;
-				fullWord = drawTevBits | 0x1000;
-				fullByte = 0xFF;
+				fullTevBits = 0xADE0F;
+				allOnes = -1;
+				tevScale = 0x1e;
 				displayListIdx = meshData->m_displayListCount - 1;
 				dlOffset = displayListIdx * 4;
 				while (displayListIdx >= 0) {
 					dlArrayBase = *(int*)(meshIdx * 4 + *(int*)((char*)param_2 + 0x10));
 					*(int*)(MaterialManRaw() + 0x48) = drawTevBits;
 					*(int*)(MaterialManRaw() + 0x128) = 0;
-					*(int*)(MaterialManRaw() + 0x12c) = 0x1e;
+					*(int*)(MaterialManRaw() + 0x12c) = tevScale;
 					*(int*)(MaterialManRaw() + 0x130) = 0;
-					*(int*)(MaterialManRaw() + 0x44) = -1;
-					*(char*)(MaterialManRaw() + 0x4c) = fullByte;
+					*(int*)(MaterialManRaw() + 0x44) = allOnes;
+					*(char*)(MaterialManRaw() + 0x4c) = allOnes;
 					*(int*)(MaterialManRaw() + 0x11c) = 0;
-					*(int*)(MaterialManRaw() + 0x120) = 0x1e;
+					*(int*)(MaterialManRaw() + 0x120) = tevScale;
 					*(int*)(MaterialManRaw() + 0x124) = 0;
-					*(char*)(MaterialManRaw() + 0x205) = fullByte;
-					*(char*)(MaterialManRaw() + 0x206) = fullByte;
+					*(char*)(MaterialManRaw() + 0x205) = allOnes;
+					*(char*)(MaterialManRaw() + 0x206) = allOnes;
 					*(int*)(MaterialManRaw() + 0x58) = 0;
 					*(int*)(MaterialManRaw() + 0x5c) = 0;
 					*(char*)(MaterialManRaw() + 0x208) = 0;
-					*(int*)(MaterialManRaw() + 0x48) = fullWord;
+					*(int*)(MaterialManRaw() + 0x48) = fullTevBits;
 					*(int*)(MaterialManRaw() + 0x128) = 0;
-					*(int*)(MaterialManRaw() + 0x12c) = 0x1e;
+					*(int*)(MaterialManRaw() + 0x12c) = tevScale;
 					*(int*)(MaterialManRaw() + 0x130) = 0;
-					*(int*)(MaterialManRaw() + 0x40) = fullWord;
+					*(int*)(MaterialManRaw() + 0x40) = fullTevBits;
 					SetMaterial__12CMaterialManFP12CMaterialSetii11_GXTevScale(
 					    &MaterialMan, *(void**)(*(int*)((char*)model + 0xA4) + 0x24), displayList->m_material, 0, 0);
 					displayListPtr = *(int**)(dlArrayBase + dlOffset);

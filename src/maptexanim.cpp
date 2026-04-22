2691180d9b504608616f9cda505f8a997fc60597
Noelle Harper <noelle21b@proton.me>
2026-04-22 08:39:01 -0700

    Improve map texture animation setter match (#9841)
    Co-authored-by: Zachary Canann <zacharycanann@gmail.com>

--- src/maptexanim.cpp
+++ src/maptexanim.cpp
@@ -294,14 +294,14 @@ void CMapTexAnimSet::Calc()
 void CMapTexAnimSet::SetMapTexAnim(int materialId, int frameStart, int frameEnd, int wrapMode)
 {
     int found = 0;
-    int i = 0;
     int setPtr = reinterpret_cast<int>(this);
+    short targetMaterialId = static_cast<short>(materialId);
 
-    while (i < m_count) {
+    for (int i = 0; i < m_count; i++) {
         void* animPtr = reinterpret_cast<void*>(*reinterpret_cast<int*>(setPtr + 0xC));
-        if (static_cast<short>(materialId) == S16At(animPtr, 0x12)) {
-            int end = frameEnd;
+        if (S16At(animPtr, 0x12) == targetMaterialId) {
             if (U8At(animPtr, 0x15) != 0) {
+                int end = frameEnd;
                 S32At(animPtr, 0x30) = frameStart;
                 S32At(animPtr, 0x2C) = frameStart;
                 if (frameEnd > S32At(animPtr, 0x38)) {
@@ -311,6 +311,7 @@ void CMapTexAnimSet::SetMapTexAnim(int materialId, int frameStart, int frameEnd,
                 U8At(animPtr, 0x27) = static_cast<unsigned char>(wrapMode);
                 U8At(animPtr, 0x28) = 1;
             } else {
+                int end = frameEnd;
                 S16At(animPtr, 0xE) = static_cast<short>(frameStart);
                 F32At(animPtr, 0x1C) = static_cast<float>(static_cast<short>(frameStart));
                 if (frameEnd > S16At(animPtr, 0xC)) {
@@ -322,7 +323,6 @@ void CMapTexAnimSet::SetMapTexAnim(int materialId, int frameStart, int frameEnd,
             found = 1;
         }
         setPtr += 4;
-        i += 1;
     }
 
     if ((found == 0) && (static_cast<unsigned int>(System.m_execParam) >= 1)) {

fd50d6d46d798c6798ab1ef3b93d68052ed85bbe
Zachary Canann <zcanann@Zacharys-Mac-mini.local>
2026-04-21 23:06:16 -0700

    Move SoundPcs table init into constructor

--- include/ffcc/p_sound.h
+++ include/ffcc/p_sound.h
@@ -4,11 +4,31 @@
 #include "ffcc/system.h"
 
 extern unsigned int m_table__9CSoundPcs[];
+extern unsigned int m_table_desc0__9CSoundPcs[];
+extern unsigned int m_table_desc1__9CSoundPcs[];
+extern unsigned int m_table_desc2__9CSoundPcs[];
+extern unsigned int m_table_desc3__9CSoundPcs[];
 
 class CSoundPcs : public CProcess
 {
 public:
-    CSoundPcs() {}
+    CSoundPcs()
+    {
+        unsigned int* table = &m_table__9CSoundPcs[1];
+
+        table[0] = m_table_desc0__9CSoundPcs[0];
+        table[1] = m_table_desc0__9CSoundPcs[1];
+        table[2] = m_table_desc0__9CSoundPcs[2];
+        table[3] = m_table_desc1__9CSoundPcs[0];
+        table[4] = m_table_desc1__9CSoundPcs[1];
+        table[5] = m_table_desc1__9CSoundPcs[2];
+        table[6] = m_table_desc2__9CSoundPcs[0];
+        table[7] = m_table_desc2__9CSoundPcs[1];
+        table[8] = m_table_desc2__9CSoundPcs[2];
+        table[11] = m_table_desc3__9CSoundPcs[0];
+        table[12] = m_table_desc3__9CSoundPcs[1];
+        table[13] = m_table_desc3__9CSoundPcs[2];
+    }
 
     void draw();
     void calc();

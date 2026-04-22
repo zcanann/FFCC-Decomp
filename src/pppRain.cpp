a0aac9f94fa922931ab9e20d41d968ac05756666
Zachary Canann <zcanann@Zacharys-Mac-mini.local>
2026-04-22 16:39:07 -0700

    Improve pppFrameRain constant placement

--- src/pppRain.cpp
+++ src/pppRain.cpp
@@ -7,13 +7,14 @@
 extern "C" {
 extern const float kPppRainTexCoordBase;
 extern const float FLOAT_8033101c;
-extern const float FLOAT_80331020;
 extern int gPppCalcDisabled;
 extern unsigned char gPppInConstructor;
 }
 #include "ffcc/util.h"
 #include "dolphin/gx.h"
 static const char s_pppRain_cpp_801DB610[] = "pppRain.cpp";
+
+extern const float FLOAT_80331020 = 3.0518509e-05f;
 
 extern "C" {
 int rand(void);
@@ -333,4 +334,3 @@ void pppConstructRain(struct pppRain* pppRain, struct RAIN_DATA* param_2)
 
 extern const float kPppRainTexCoordBase = 0.0f;
 extern const float FLOAT_8033101c = 1.0f;
-extern const float FLOAT_80331020 = 3.0518509e-05f;

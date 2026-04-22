0cd13cb2361ae15346e95e5227551f8e88615906
Zachary Canann <zcanann@Zacharys-Mac-mini.local>
2026-04-21 21:38:05 -0700

    Match p_mc static initializer

--- include/ffcc/p_mc.h
+++ include/ffcc/p_mc.h
@@ -18,16 +18,17 @@ public:
         static unsigned int desc0[] = {0, 0xFFFFFFFF, reinterpret_cast<unsigned int>(create__6CMcPcsFv)};
         static unsigned int desc1[] = {0, 0xFFFFFFFF, reinterpret_cast<unsigned int>(destroy__6CMcPcsFv)};
         static unsigned int desc2[] = {0, 0xFFFFFFFF, reinterpret_cast<unsigned int>(calc__6CMcPcsFv)};
+        unsigned int* table = &m_table__6CMcPcs[1];
 
-        m_table__6CMcPcs[1] = desc0[0];
-        m_table__6CMcPcs[2] = desc0[1];
-        m_table__6CMcPcs[3] = desc0[2];
-        m_table__6CMcPcs[4] = desc1[0];
-        m_table__6CMcPcs[5] = desc1[1];
-        m_table__6CMcPcs[6] = desc1[2];
-        m_table__6CMcPcs[7] = desc2[0];
-        m_table__6CMcPcs[8] = desc2[1];
-        m_table__6CMcPcs[9] = desc2[2];
+        table[0] = desc0[0];
+        table[1] = desc0[1];
+        table[2] = desc0[2];
+        table[3] = desc1[0];
+        table[4] = desc1[1];
+        table[5] = desc1[2];
+        table[6] = desc2[0];
+        table[7] = desc2[1];
+        table[8] = desc2[2];
     }
 
     void Init();

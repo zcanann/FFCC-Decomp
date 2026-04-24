e94b0ef5b3bc29bd87f7f0c7764a5b77ad617933
Zachary Canann <zacharycanann@gmail.com>
2026-04-24 10:37:47 -0700

    Inline unused pppRandIV helper

--- src/pppRandIV.cpp
+++ src/pppRandIV.cpp
@@ -21,6 +21,11 @@ struct PppRandIVParam3 {
     s32* fieldC;
 };
 
+inline int randint(int value, float scale)
+{
+    return (int)((float)value * scale - (float)value);
+}
+
 /*
  * --INFO--
  * PAL Address: 0x800622d4
@@ -65,18 +70,4 @@ void pppRandIV(void* param1, void* param2, void* param3)
     target[0] += (s32)((f32)in->field8 * scale - (f32)in->field8);
     target[1] += (s32)((f32)in->fieldC * scale - (f32)in->fieldC);
     target[2] += (s32)((f32)in->field10 * scale - (f32)in->field10);
-}
-
-/*
- * --INFO--
- * PAL Address: UNUSED
- * PAL Size: 72b
- * EN Address: TODO
- * EN Size: TODO
- * JP Address: TODO
- * JP Size: TODO
- */
-static int randint(int value, float scale)
-{
-    return (int)(((float)value * scale) - (float)value);
 }

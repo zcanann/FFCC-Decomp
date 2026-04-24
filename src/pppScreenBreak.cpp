96bcba77c07f8ba5d94acb67d465775836250327
Zachary Canann <zacharycanann@gmail.com>
2026-04-23 23:34:13 -0700

    Improve pppCon2ScreenBreak zero constant reuse

--- src/pppScreenBreak.cpp
+++ src/pppScreenBreak.cpp
@@ -331,7 +331,7 @@ void pppCon2ScreenBreak(PScreenBreak* pppScreenBreak, pppScreenBreakUnkC* param_
     s32 dataOffset = param_2->m_serializedDataOffsets[2];
     float* value = (float*)((u8*)pppScreenBreak + dataOffset + 0x80);
     float f = FLOAT_80331cc4;
-    value[2] = FLOAT_80331cc4;
+    value[2] = f;
     value[1] = f;
     *value = f;
 }

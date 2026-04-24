8f8197cc1770d52d5ee3b051a328d626a3efc00a
asmithbot <asmithbot@proton.me>
2026-04-24 09:11:31 -0700

    Inline CSoundPcs constructor in p_sound (#10206)
    Co-authored-by: Zachary Canann <zacharycanann@gmail.com>

--- src/p_sound.cpp
+++ src/p_sound.cpp
@@ -34,24 +34,6 @@ unsigned int m_table__9CSoundPcs[0x15C / sizeof(unsigned int)] = {
 
 CSoundPcs SoundPcs;
 
-CSoundPcs::CSoundPcs()
-{
-    unsigned int* table = &m_table__9CSoundPcs[1];
-
-    table[0] = m_table_desc0__9CSoundPcs[0];
-    table[1] = m_table_desc0__9CSoundPcs[1];
-    table[2] = m_table_desc0__9CSoundPcs[2];
-    table[3] = m_table_desc1__9CSoundPcs[0];
-    table[4] = m_table_desc1__9CSoundPcs[1];
-    table[5] = m_table_desc1__9CSoundPcs[2];
-    table[6] = m_table_desc2__9CSoundPcs[0];
-    table[7] = m_table_desc2__9CSoundPcs[1];
-    table[8] = m_table_desc2__9CSoundPcs[2];
-    table[11] = m_table_desc3__9CSoundPcs[0];
-    table[12] = m_table_desc3__9CSoundPcs[1];
-    table[13] = m_table_desc3__9CSoundPcs[2];
-}
-
 /*
  * --INFO--
  * PAL Address: 0x800d8710

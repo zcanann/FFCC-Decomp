93296ea03cc846ea2cd989d45a0790823600722a
Zachary Canann <zacharycanann@gmail.com>
2026-04-24 20:27:26 -0700

    pppPObjPoint: improve vecPtr codegen

--- src/pppPObjPoint.cpp
+++ src/pppPObjPoint.cpp
@@ -27,23 +27,17 @@ void pppPObjPoint(PppPointData* pointData, PppObjData* objData, PppContainer* co
         return;
     }
 
-    s32 objOffset = *(s32*)container->ptrData;
-    PppPointObj* objPtr = (PppPointObj*)((u8*)pointData + objOffset + 0x80);
+    PppPointObj* objPtr = (PppPointObj*)((u8*)pointData + *(s32*)container->ptrData + 0x80);
 
     if (objData->id == pointData->id) {
-        u8* vecPtr;
-
         if ((objData->field_4 + 0x10000) == 0xFFFF) {
-            vecPtr = (u8*)gPppDefaultValueBuffer;
+            objPtr->vecPtr = gPppDefaultValueBuffer;
         } else {
             PObjPointEntry* table = *(PObjPointEntry**)((u8*)pppMngStPtr + 0xD4);
             u8* data = (u8*)objData->data;
-            u32 vecOffset = table[objData->field_4].vecOffset;
-            vecPtr = data + 0x80;
-            vecPtr += vecOffset;
-        }
 
-        objPtr->vecPtr = vecPtr;
+            objPtr->vecPtr = data + 0x80 + table[objData->field_4].vecOffset;
+        }
     }
 
     objPtr->x = ((f32*)objPtr->vecPtr)[0];

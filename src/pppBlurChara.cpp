8123cda62c8797c2fe61323acf17e5a2c2a2f475
Zachary Canann <zcanann@Zacharys-Mac-mini.local>
2026-04-26 20:05:48 -0700

    Define pppBlurChara sdata2 constant

--- src/pppBlurChara.cpp
+++ src/pppBlurChara.cpp
@@ -61,6 +61,7 @@ extern float FLOAT_80331048;
 extern float FLOAT_8033104c;
 extern float FLOAT_80331050;
 extern float FLOAT_80331054;
+extern const double DOUBLE_80330FE8 = 3.0;
 
 static inline unsigned char* MaterialManRaw() { return reinterpret_cast<unsigned char*>(&MaterialMan); }
 

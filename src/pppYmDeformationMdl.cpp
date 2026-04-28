55915c937b99310f9f85106c82bf3a2a62c0c906
BrilliantWasp <brilliantwasp@proton.me>
2026-04-28 09:15:07 -0700

    Name PPP sdata2 constants (#10871)
    * Define pppYmDeformationMdl double bias
    
    * Name pppYmTracer double constants
    
    * Name pppCorona sdata2 constants
    
    ---------
    
    Co-authored-by: Zachary Canann <zcanann@Zacharys-Mac-mini.local>

--- src/pppYmDeformationMdl.cpp
+++ src/pppYmDeformationMdl.cpp
@@ -341,4 +341,5 @@ void pppConstructYmDeformationMdl(pppYmDeformationMdl* pppYmDeformationMdl_, str
     *(float*)(puVar2 + 8) = fVar1;
 }
 
+extern const double DOUBLE_80330DB0 = 4503601774854144.0;
 extern const double kPppYmSharedDoubleBias = 4503601774854144.0;

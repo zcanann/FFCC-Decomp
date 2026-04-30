6d13d2b33a84792427cffd5b85f3c4bb6ac23ea5
Zachary Canann <zcanann@Zacharys-Mac-mini.local>
2026-04-30 13:51:38 -0700

    Define process table name strings

--- src/p_system.cpp
+++ src/p_system.cpp
@@ -5,7 +5,7 @@
 extern "C" void create__10CSystemPcsFv(CSystemPcs*);
 extern "C" void destroy__10CSystemPcsFv(CSystemPcs*);
 extern "C" void calc__10CSystemPcsFv(CSystemPcs*);
-extern const char s_CSystemPcs_801D7C48[];
+extern const char s_CSystemPcs_801D7C48[] = "CSystemPcs";
 
 CSystemPcs SystemPcs;
 unsigned int m_table__10CSystemPcs[0x15C / sizeof(unsigned int)] = {

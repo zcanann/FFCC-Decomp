340dd254cd7b0025f20005e909c05ae889d4f353
Zachary Canann <zcanann@Zacharys-Mac-mini.local>
2026-04-30 09:50:51 -0700

    Match textureman string rodata

--- src/textureman.cpp
+++ src/textureman.cpp
@@ -69,11 +69,11 @@ extern "C" CTexture* __vc__21CPtrArray_P8CTexture_FUl(void*, unsigned long);
 extern "C" void SetAt__21CPtrArray_P8CTexture_FUlP8CTexture(void*, unsigned long, CTexture*);
 extern "C" void ReleaseAndRemoveAll__21CPtrArray_P8CTexture_Fv(void*);
 
+static const char s_textureman_cpp_801D7974[] = "textureman.cpp";
+static const char s_Error_width_pctd_height_pctd_801D7984[] = "Error width=%d height=%d\n";
+static const char s_CTexture_texture_801D79A0[] = "CTexture.texture";
 static const char s_ptrarray_grow_error_801D79D8[] = "CPtrArray grow error";
 static const char s_collection_ptrarray_h_801D79F4[] = "collection_ptrarray.h";
-static const char s_textureman_cpp_801D7974[] = "textureman.cpp";
-static const char s_Error_width_pctd_height_pctd_801D7984[] = "Error width %d, height %d\n";
-static const char s_CTexture_texture_801D79A0[] = "CTexture.texture";
 
 namespace {
 static inline unsigned char* Ptr(void* p, unsigned int offset)

1d1cdfb8c88f85c1ac3deac559e61342e8efd5ed
Zachary Canann <zcanann@Zacharys-Mac-mini.local>
2026-04-30 13:34:02 -0700

    Improve pppConformBGNormal flag check

--- src/pppConformBGNormal.cpp
+++ src/pppConformBGNormal.cpp
@@ -43,6 +43,17 @@ struct CMapCylinderRaw {
     Vec m_direction2;
 };
 
+struct WeaponNodeFlagBits {
+    signed char m_prg : 1;
+    signed char m_unk40 : 1;
+    signed char m_unk20 : 1;
+    signed char m_unk10 : 1;
+    signed char m_control3 : 1;
+    signed char m_unk04 : 1;
+    signed char m_unk02 : 1;
+    signed char m_unk01 : 1;
+};
+
 /*
  * --INFO--
  * PAL Address: 0x801091d4
@@ -217,7 +228,7 @@ void pppFrameConformBGNormal(struct pppConformBGNormal* pppConformBGNormal, stru
                     pppMngStPtr->m_matrix.value[0][3] = owner->m_worldPosition.x;
                     pppMngStPtr->m_matrix.value[1][3] = owner->m_worldPosition.y;
                     pppMngStPtr->m_matrix.value[2][3] = owner->m_worldPosition.z;
-                } else if (((s8)((s32)((u32)(*(u8*)&owner->m_weaponNodeFlags & 2) << 30) >> 31) != 0) &&
+                } else if (((WeaponNodeFlagBits*)&owner->m_weaponNodeFlags)->m_unk01 &&
                     (owner->m_attachOwner != NULL)) {
                     ownerY = owner->m_attachOwner->m_worldPosition.y;
                     ownerZ = owner->m_worldPosition.z;

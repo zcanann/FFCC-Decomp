13d331b3294f2ef9c4aeabca7ce6d44c1f047de7
Zachary Canann <zcanann@Zacharys-Mac-mini.local>
2026-04-30 09:10:46 -0700

    Fix material CPtrArray constructor build

--- src/materialman.cpp
+++ src/materialman.cpp
@@ -55,7 +55,6 @@ extern "C" void* __vt__9CMaterial[];
 extern "C" void* __vt__8CManager[];
 extern "C" void* __vt__12CMaterialMan[];
 extern "C" void* __vt__12CMaterialSet[];
-extern "C" void* __vt__8CPtrArrayIP9CMaterial[];
 extern "C" void __ct__22CPtrArray_P9CMaterial_Fv(void*);
 extern "C" void __dt__22CPtrArray_P9CMaterial_Fv(void*, int);
 extern float FLOAT_8032faf0;
@@ -107,7 +106,6 @@ private:
 template <>
 CPtrArray<CMaterial*>::CPtrArray()
 {
-    m_vtable = __vt__8CPtrArrayIP9CMaterial;
     m_size = 0;
     m_numItems = 0;
     m_defaultSize = 0x10;

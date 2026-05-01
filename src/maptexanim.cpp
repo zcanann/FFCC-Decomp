e2f1a6a9a42a0b0ae42015c7c8df7b44d8b572ea
Zachary Canann <zcanann@Zacharys-Mac-mini.local>
2026-05-01 08:11:49 -0700

    Improve map texture slot access

--- src/maptexanim.cpp
+++ src/maptexanim.cpp
@@ -89,7 +89,7 @@ static inline void ReplaceRef(void** slot, void* ref)
 
 static inline void SetMaterialTextureSlot(void* material, unsigned long slotIndex, void* texture)
 {
-    void** slot = reinterpret_cast<void**>(Ptr(material, 0x3C + (slotIndex * 4)));
+    void** slot = reinterpret_cast<void**>(Ptr(material, 0x3C) + (slotIndex * 4));
     ReplaceRef(slot, texture);
 
     unsigned short& numTexture = *reinterpret_cast<unsigned short*>(Ptr(material, 0x18));

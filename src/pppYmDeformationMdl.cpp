1e48218cb02b97c10ef347f23697926310e2f236
Zachary Canann <zcanann@Zacharys-Mac-mini.local>
2026-04-30 17:42:07 -0700

    Match pppYmDeformationMdl sdata2 constants

--- src/pppYmDeformationMdl.cpp
+++ src/pppYmDeformationMdl.cpp
@@ -52,7 +52,12 @@ extern float FLOAT_80330DA0;
 extern float FLOAT_80330DA4;
 extern float FLOAT_80330DA8;
 extern const float FLOAT_80330dac = 0.0f;
-extern const double DOUBLE_80330DB0 = 4503601774854144.0;
+struct DoubleWords {
+    u32 u[2];
+};
+
+extern const DoubleWords DOUBLE_80330DB0 = {{0x43300000, 0x80000000}};
+extern const DoubleWords kPppYmSharedDoubleBias = {{0x43300000, 0x80000000}};
 
 static inline Mtx& CameraMatrix()
 {

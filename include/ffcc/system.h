81d44baa68474cc81c417cb7914884039e78a5a5
Zachary Canann <zacharycanann@gmail.com>
2026-05-01 12:19:24 -0700

    Add explicit CProcess init and quit stubs

--- include/ffcc/system.h
+++ include/ffcc/system.h
@@ -15,6 +15,8 @@ class CProcess : public CManager
 {
 public:
     CProcess() {}
+    virtual void Init();
+    virtual void Quit();
     virtual int GetTable(unsigned long) = 0;
     virtual void onScriptChanging(char*);
     virtual void onScriptChanged(char*, int);

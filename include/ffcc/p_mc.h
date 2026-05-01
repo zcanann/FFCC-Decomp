20a08a2672d2cb8247aecb9c8006b1103f5dd03d
Zachary Canann <zacharycanann@gmail.com>
2026-05-01 09:08:59 -0700

    Simplify CMcPcs table init

--- include/ffcc/p_mc.h
+++ include/ffcc/p_mc.h
@@ -15,20 +15,17 @@ class CMcPcs : public CProcess
 public:
     CMcPcs()
     {
-        static unsigned int desc0[] = {0, 0xFFFFFFFF, reinterpret_cast<unsigned int>(create__6CMcPcsFv)};
-        static unsigned int desc1[] = {0, 0xFFFFFFFF, reinterpret_cast<unsigned int>(destroy__6CMcPcsFv)};
-        static unsigned int desc2[] = {0, 0xFFFFFFFF, reinterpret_cast<unsigned int>(calc__6CMcPcsFv)};
         unsigned int* table = &m_table__6CMcPcs[1];
 
-        table[0] = desc0[0];
-        table[1] = desc0[1];
-        table[2] = desc0[2];
-        table[3] = desc1[0];
-        table[4] = desc1[1];
-        table[5] = desc1[2];
-        table[6] = desc2[0];
-        table[7] = desc2[1];
-        table[8] = desc2[2];
+        table[0] = 0;
+        table[1] = 0xFFFFFFFF;
+        table[2] = reinterpret_cast<unsigned int>(create__6CMcPcsFv);
+        table[3] = 0;
+        table[4] = 0xFFFFFFFF;
+        table[5] = reinterpret_cast<unsigned int>(destroy__6CMcPcsFv);
+        table[6] = 0;
+        table[7] = 0xFFFFFFFF;
+        table[8] = reinterpret_cast<unsigned int>(calc__6CMcPcsFv);
     }
 
     void Init();

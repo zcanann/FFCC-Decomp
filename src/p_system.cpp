fa6ed9d589aee8550ba66ecd3f9acf411975bca9
Zachary Canann <zacharycanann@gmail.com>
2026-05-01 01:49:37 -0700

    Match process base type names

--- src/p_system.cpp
+++ src/p_system.cpp
@@ -6,6 +6,8 @@ extern "C" void create__10CSystemPcsFv(CSystemPcs*);
 extern "C" void destroy__10CSystemPcsFv(CSystemPcs*);
 extern "C" void calc__10CSystemPcsFv(CSystemPcs*);
 extern const char s_CSystemPcs_801D7C48[] = "CSystemPcs";
+extern const char s_CManager_801D7C54[] = "CManager";
+extern const char s_CProcess_801D7C60[] = "CProcess";
 
 CSystemPcs SystemPcs;
 unsigned int m_table__10CSystemPcs[0x15C / sizeof(unsigned int)] = {

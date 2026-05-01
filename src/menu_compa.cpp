704d06582f29c036609436dcd80164e1f4da7fc3
Zachary Canann <zacharycanann@gmail.com>
2026-04-30 17:51:34 -0700

    Improve CompaCtrl reset codegen

--- src/menu_compa.cpp
+++ src/menu_compa.cpp
@@ -278,7 +278,6 @@ void CMenuPcs::CompaCtrl()
 	short hold;
 	bool doReset = false;
 	CompaMenuState* compaState = this->compaMenuState;
-	CompaOpenAnimList* compaList = reinterpret_cast<CompaOpenAnimList*>(this->compaList);
 
 	if ((Pad._452_4_ != 0) || (Pad._448_4_ != -1)) {
 		activeInput = true;
@@ -323,22 +322,22 @@ void CMenuPcs::CompaCtrl()
 	}
 
 	if (doReset) {
-		compaList->entries[0].startFrame = 2;
-		compaList->entries[0].duration = 5;
-		compaList->entries[1].startFrame = 2;
-		compaList->entries[1].duration = 5;
-		compaList->entries[2].startFrame = 2;
-		compaList->entries[2].duration = 5;
-		compaList->entries[3].startFrame = 7;
-		compaList->entries[3].duration = 5;
-		compaList->entries[4].startFrame = 7;
-		compaList->entries[4].duration = 5;
-		compaList->entries[5].flags = 2;
-		compaList->entries[5].startFrame = 7;
-		compaList->entries[5].duration = 5;
+		reinterpret_cast<CompaOpenAnimList*>(this->compaList)->entries[0].startFrame = 2;
+		reinterpret_cast<CompaOpenAnimList*>(this->compaList)->entries[0].duration = 5;
+		reinterpret_cast<CompaOpenAnimList*>(this->compaList)->entries[1].startFrame = 2;
+		reinterpret_cast<CompaOpenAnimList*>(this->compaList)->entries[1].duration = 5;
+		reinterpret_cast<CompaOpenAnimList*>(this->compaList)->entries[2].startFrame = 2;
+		reinterpret_cast<CompaOpenAnimList*>(this->compaList)->entries[2].duration = 5;
+		reinterpret_cast<CompaOpenAnimList*>(this->compaList)->entries[3].startFrame = 7;
+		reinterpret_cast<CompaOpenAnimList*>(this->compaList)->entries[3].duration = 5;
+		reinterpret_cast<CompaOpenAnimList*>(this->compaList)->entries[4].startFrame = 7;
+		reinterpret_cast<CompaOpenAnimList*>(this->compaList)->entries[4].duration = 5;
+		reinterpret_cast<CompaOpenAnimList*>(this->compaList)->entries[5].flags = 2;
+		reinterpret_cast<CompaOpenAnimList*>(this->compaList)->entries[5].startFrame = 7;
+		reinterpret_cast<CompaOpenAnimList*>(this->compaList)->entries[5].duration = 5;
 
-		unsigned int entryCount = static_cast<unsigned short>(compaList->count);
-		CompaOpenAnim* entry = compaList->entries;
+		unsigned int entryCount = *this->compaList;
+		CompaOpenAnim* entry = reinterpret_cast<CompaOpenAnimList*>(this->compaList)->entries;
 		while (entryCount != 0) {
 			entry->frame = 0;
 			entry->alpha = 1.0f;

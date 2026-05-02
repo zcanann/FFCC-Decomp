f6632477482828e8bd4db269e79588031d8f66a1
Zachary Canann <zcanann@Zacharys-Mac-mini.local>
2026-05-02 01:52:52 -0700

    Fix pppYmTracer2 color globals linkage

--- src/pppYmTracer2.cpp
+++ src/pppYmTracer2.cpp
@@ -72,8 +72,8 @@ union PackedColor {
     u8 bytes[4];
 };
 
-static PackedColor g_pppYmTracer2_1;
-static PackedColor g_pppYmTracer2_2;
+extern PackedColor g_pppYmTracer2_1;
+extern PackedColor g_pppYmTracer2_2;
 
 static inline void copyPolygonData(TRACE_POLYGON* dst, TRACE_POLYGON* src)
 {

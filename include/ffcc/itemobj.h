5aeed738078bc9687edf58c35ce3c2a0c3c521b2
Zachary Canann <zacharycanann@gmail.com>
2026-05-03 00:31:21 -0700

    Correct CGItemObj inheritance and layout

--- include/ffcc/itemobj.h
+++ include/ffcc/itemobj.h
@@ -2,16 +2,18 @@
 #define _FFCC_ITEMOBJ_H_
 
 #include "ffcc/cflat_runtime.h"
+#include "ffcc/prgobj.h"
+
+#include <dolphin/types.h>
 
 class CGObject;
 class CGPartyObj;
-class CGPrgObj;
 class CFlatRuntime;
 class CFont;
 struct Vec;
 class PPPIFPARAM;
 
-class CGItemObj
+class CGItemObj : public CGPrgObj
 {
 public:
 	struct CCFS
@@ -42,6 +44,21 @@ public:
 	static void DeleteAllFieldItem();
 	static void DispAllFieldItem(int);
 	int GetCID();
+
+	CGObject* m_owner;             // 0x550
+	int m_carryFrame;              // 0x554
+	int m_scriptArg;               // 0x558
+	int m_particleSlot;            // 0x55C
+	u16 m_createFlags;             // 0x560
+	u16 unk_0x562;                 // 0x562
+	void* m_pendingModelHandle;    // 0x564
+	float m_savedBodyRadius;       // 0x568
+	int m_itemJumpCountdown;       // 0x56C
+	int m_memoryCapsuleNameIndex;  // 0x570
+	u32 m_pendingAnimFlags;        // 0x574
+	char* m_pendingAnimName;       // 0x578
 };
+
+typedef int CGItemObj_size_mismatch[(sizeof(CGItemObj) == 0x57C) ? 1 : -1];
 
 #endif // _FFCC_ITEMOBJ_H_

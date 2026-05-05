feae0bf2268a9e2bf85bcbd8abdd94462576e828
Zachary Canann <zcanann@Zacharys-Mac-mini.local>
2026-05-04 17:10:34 -0700

    Clean up pppYmDeformationMdl sdata2 constant

--- src/pppYmDeformationMdl.cpp
+++ src/pppYmDeformationMdl.cpp
@@ -52,7 +52,6 @@ extern float FLOAT_80330DA0;
 extern float FLOAT_80330DA4;
 extern float FLOAT_80330DA8;
 extern const float FLOAT_80330dac = 0.0f;
-extern const double DOUBLE_80330DB0 = 4503601774854144.0;
 extern const double kPppYmSharedDoubleBias = 4503601774854144.0;
 
 static inline Mtx& CameraMatrix()

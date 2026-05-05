de6642c69898916e6c6c7bca86f82fc7df1967e1
Zachary Canann <zacharycanann@gmail.com>
2026-05-05 07:18:37 -0700

    Improve CUtil spline position matching

--- src/util.cpp
+++ src/util.cpp
@@ -1265,9 +1265,9 @@ void CUtil::GetSplinePos(Vec& out, Vec p0, Vec p1, Vec p2, Vec p3, float t, floa
 	hermite[2] = t - ((kUtilHermiteCoeff2 * t2) - t3);
 	hermite[3] = t3 - t2;
 
-	out.x = (hermite[1] * p2.x) + (hermite[0] * p1.x) + (hermite[2] * tan0.x) + (hermite[3] * tan1.x);
-	out.y = (hermite[1] * p2.y) + (hermite[0] * p1.y) + (hermite[2] * tan0.y) + (hermite[3] * tan1.y);
-	out.z = (hermite[1] * p2.z) + (hermite[0] * p1.z) + (hermite[2] * tan0.z) + (hermite[3] * tan1.z);
+	out.x = (hermite[3] * tan1.x) + (hermite[2] * tan0.x) + (hermite[0] * p1.x) + (hermite[1] * p2.x);
+	out.y = (hermite[3] * tan1.y) + (hermite[2] * tan0.y) + (hermite[0] * p1.y) + (hermite[1] * p2.y);
+	out.z = (hermite[3] * tan1.z) + (hermite[2] * tan0.z) + (hermite[0] * p1.z) + (hermite[1] * p2.z);
 }
 
 /*

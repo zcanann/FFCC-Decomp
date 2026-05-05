b84c33afaf4411783a073f08121182d0c585c41d
Zachary Canann <zacharycanann@gmail.com>
2026-05-05 15:38:11 -0700

    Use unsigned Mana2 step type in destructor

--- src/pppMana2.cpp
+++ src/pppMana2.cpp
@@ -422,7 +422,7 @@ void pppDestructMana2(pppMana2* pppMana2, pppMana2UnkC* param_2)
     meshEntry = *(s32*)(model + 0xAC);
     step = work[0x1C];
     for (i = 0; i < *(u32*)(*(s32*)(model + 0xA4) + 0xC); i++) {
-        char stepType = *(char*)(step + 0x1C);
+        u8 stepType = *(u8*)(step + 0x1C);
         s32 shape = *(s32*)(meshEntry + 8);
 
         if (stepType == 1) {

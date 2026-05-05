ef0b77715c05c617ffa3dc53984064c2c0d670a3
Zachary Canann <zcanann@Zacharys-Mac-mini.local>
2026-05-05 07:09:42 -0700

    Improve CUSBPcs SendDataCode match

--- src/p_usb.cpp
+++ src/p_usb.cpp
@@ -57,12 +57,12 @@ static inline unsigned int Swap32(unsigned int x)
 int CUSBPcs::SendDataCode(int code, void* src, int elemSize, int elemCount)
 {
     unsigned int count;
+    int result;
     unsigned int* ptr;
     int connected;
     unsigned int* dstBuffer;
     CMemory::CStage* stage;
     unsigned int value;
-    int result;
 
     count = elemSize * elemCount;
     value = (count + 0x5F) & ~0x1F;

d87692dfc72edb81549c68167a2a3e5ecbd6879b
Zachary Canann <zcanann@Zacharys-Mac-mini.local>
2026-05-06 18:16:59 -0700

    Use boss ghost counters for party damage

--- src/partyobj.cpp
+++ src/partyobj.cpp
@@ -91,6 +91,15 @@ struct GhostPartyWork {
 
 static GhostPartyWork sGhostPartyWork;
 
+struct BossGhostPartyCounters {
+	unsigned char _pad0[0x24];
+	int thresholdA;
+	int thresholdB;
+	int thresholdC;
+};
+
+extern "C" BossGhostPartyCounters m_boss__8CGMonObj_field108_0x6c;
+
 struct PartyObjFlags {
 	unsigned char commandActive : 1;
 	unsigned char reserved : 7;
@@ -148,13 +157,13 @@ static inline int& CharaGhostValue(int offset)
 static inline void UpdateGhostPartyDamageCounters(CGPrgObj* attacker)
 {
 	if (((static_cast<unsigned short>(attacker->GetCID()) & 0xAD) == 0xAD) && Game.m_gameWork.m_menuStageMode != 0) {
-		sGhostPartyWork.thresholdA++;
-		sGhostPartyWork.thresholdB++;
-		sGhostPartyWork.thresholdC++;
+		m_boss__8CGMonObj_field108_0x6c.thresholdA++;
+		m_boss__8CGMonObj_field108_0x6c.thresholdB++;
+		m_boss__8CGMonObj_field108_0x6c.thresholdC++;
 		Printf__7CSystemFPce(&System, lbl_801DCB1C,
-		    sGhostPartyWork.thresholdA, CharaGhostValue(0x2048),
-		    sGhostPartyWork.thresholdB, CharaGhostValue(0x204C),
-		    sGhostPartyWork.thresholdC, CharaGhostValue(0x2050));
+		    m_boss__8CGMonObj_field108_0x6c.thresholdA, CharaGhostValue(0x2048),
+		    m_boss__8CGMonObj_field108_0x6c.thresholdB, CharaGhostValue(0x204C),
+		    m_boss__8CGMonObj_field108_0x6c.thresholdC, CharaGhostValue(0x2050));
 	}
 }
 

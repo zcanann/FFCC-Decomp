97a7a43c7f4e6373b000e52dd7fd351780cf67ff
Zachary Canann <zcanann@Zacharys-Mac-mini.local>
2026-05-07 03:24:36 -0700

    Match StopAndFreeAllSe

--- src/sound.cpp
+++ src/sound.cpp
@@ -1517,23 +1517,24 @@ void CSound::FreeWave(int waveId)
  */
 void CSound::StopAndFreeAllSe(int clearMode)
 {
-    CSoundLayout& sound = SoundData(this);
+    u8* self = reinterpret_cast<u8*>(this);
+    CSoundLayout& sound = *reinterpret_cast<CSoundLayout*>(self);
     if (clearMode != 0) {
-        SeStop__9CRedSoundFi(RedSound(this), -1);
-        ClearSeSepData__9CRedSoundFi(RedSound(this), -1);
-        ClearWaveData__9CRedSoundFi(RedSound(this), -3);
+        SeStop__9CRedSoundFi(reinterpret_cast<CRedSound*>(self + 8), -1);
+        ClearSeSepData__9CRedSoundFi(reinterpret_cast<CRedSound*>(self + 8), -1);
+        ClearWaveData__9CRedSoundFi(reinterpret_cast<CRedSound*>(self + 8), -3);
     } else {
-        SeStopMG__9CRedSoundFiiii(RedSound(this),
+        SeStopMG__9CRedSoundFiiii(reinterpret_cast<CRedSound*>(self + 8),
                                   sound.m_noFreeSeGroups[0],
                                   sound.m_noFreeSeGroups[1],
                                   sound.m_noFreeSeGroups[2],
                                   sound.m_noFreeSeGroups[3]);
-        ClearSeSepDataMG__9CRedSoundFiiii(RedSound(this),
+        ClearSeSepDataMG__9CRedSoundFiiii(reinterpret_cast<CRedSound*>(self + 8),
                                           sound.m_noFreeSeGroups[0],
                                           sound.m_noFreeSeGroups[1],
                                           sound.m_noFreeSeGroups[2],
                                           sound.m_noFreeSeGroups[3]);
-        ClearWaveDataM__9CRedSoundFiiii(RedSound(this),
+        ClearWaveDataM__9CRedSoundFiiii(reinterpret_cast<CRedSound*>(self + 8),
                                         sound.m_noFreeWaves[0],
                                         sound.m_noFreeWaves[1],
                                         sound.m_noFreeWaves[2],

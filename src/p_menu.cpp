cf9d9150f16823a7a481d782c0d753f2c63b1b6a
Zachary Canann <zcanann@Zacharys-Mac-mini.local>
2026-05-06 18:02:07 -0700

    Improve p_menu loadFont TLUT loop

--- src/p_menu.cpp
+++ src/p_menu.cpp
@@ -592,15 +592,9 @@ void CMenuPcs::loadFont(int type, char* path, int slot, int tlutMode)
         MenuFontTlutPalette* palette = &sMenuFontTlutPaletteTable[tlutMode * 0x1C];
 
         for (int colorIndex = 0; colorIndex < 0x10; colorIndex++) {
-            float blend = 0.0f;
-            float blendInv = 0.0f;
-
-            if (colorIndex >= 8) {
-                blend = LoadFloat(FLOAT_80330808) - static_cast<float>(colorIndex - 8) * LoadFloat(FLOAT_8033086C);
-                blendInv = LoadFloat(FLOAT_80330808) - blend;
-            }
-
             for (int tlutIndex = 0; tlutIndex < 0x1C; tlutIndex++) {
+                float blend = 0.0f;
+                float blendInv = 0.0f;
                 _GXColor color = {
                     static_cast<u8>(0xFF - sMenuFontShadeTable[colorIndex]),
                     static_cast<u8>(0xFF - sMenuFontShadeTable[colorIndex]),
@@ -613,6 +607,8 @@ void CMenuPcs::loadFont(int type, char* path, int slot, int tlutMode)
                     color.g = palette[tlutIndex].highlight.g;
                     color.b = palette[tlutIndex].highlight.b;
                 } else {
+                    blend = LoadFloat(FLOAT_80330808) - static_cast<float>(colorIndex - 8) * LoadFloat(FLOAT_8033086C);
+                    blendInv = LoadFloat(FLOAT_80330808) - blend;
                     color.r = static_cast<u8>(static_cast<float>(palette[tlutIndex].shadow.r) * blendInv +
                                               static_cast<float>(palette[tlutIndex].highlight.r) * blend);
                     color.g = static_cast<u8>(static_cast<float>(palette[tlutIndex].shadow.g) * blendInv +

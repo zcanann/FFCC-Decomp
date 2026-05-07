41ef6a67df9823c49d33451a8ff4d23c623e7af8
Zachary Canann <zcanann@Zacharys-Mac-mini.local>
2026-05-06 21:48:45 -0700

    Define menu artifact sdata2 constants

--- src/menu_tmparti.cpp
+++ src/menu_tmparti.cpp
@@ -25,19 +25,16 @@ extern "C" void SetPosY__5CFontFf(float, CFont*);
 extern "C" void Draw__5CFontFPc(CFont*, const char*);
 
 
-extern float FLOAT_80332f2c;
-extern float FLOAT_80332F28;
-extern float FLOAT_80332f30;
-extern float FLOAT_80332F34;
-extern float FLOAT_80332F38;
-extern double DOUBLE_80332f20;
-extern double DOUBLE_80332f40;
-extern double DOUBLE_80332f48;
-extern double DOUBLE_80332f50;
-extern double DOUBLE_80332f58;
-
-extern const double DOUBLE_80333418 = 0.0;
-extern const double DOUBLE_80333420 = 216.0;
+extern const double DOUBLE_80332f20 = 0.5;
+extern const float FLOAT_80332F28 = 255.0f;
+extern const float FLOAT_80332f2c = 0.0f;
+extern const float FLOAT_80332f30 = 1.0f;
+extern const float FLOAT_80332F34 = 0.9f;
+extern const float FLOAT_80332F38 = 4.0f;
+extern const double DOUBLE_80332f40 = 4503601774854144.0;
+extern const double DOUBLE_80332f48 = 1.0;
+extern const double DOUBLE_80332f50 = 0.0;
+extern const double DOUBLE_80332f58 = 216.0;
 
 static inline double TmpArtiIntToDouble(int value)
 {

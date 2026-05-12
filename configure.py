f06284d02db6a9e3a08fa778839826173c296aea
Zachary Canann <zacharycanann@gmail.com>
2026-05-12 12:06:17 -0700

    Enable RTTI for usb

--- configure.py
+++ configure.py
@@ -755,7 +755,7 @@ config.libs = [
                 ],
             ),
             Object(NonMatching, "THPSimple.cpp"),
-            Object(NonMatching, "usb.cpp"),
+            Object(NonMatching, "usb.cpp", extra_cflags=["-RTTI on"]),
             Object(Matching, "USBStreamData.cpp"),
             Object(NonMatching, "util.cpp", extra_cflags=["-RTTI on"]),
             Object(Matching, "vector.cpp", mw_version="GC/2.6"),

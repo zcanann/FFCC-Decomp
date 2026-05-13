707458d5d47b10ac2adb58a314eb727c3edd2b98
Zachary Canann <zacharycanann@gmail.com>
2026-05-13 15:17:50 -0700

    Match pppFrameYmTracer2

--- src/pppYmTracer2.cpp
+++ src/pppYmTracer2.cpp
@@ -244,22 +244,18 @@ void pppFrameYmTracer2(pppYmTracer2* pppYmTracer2, pppYmTracer2UnkB* param_2, pp
     work = (TracerWork*)(pppYmTracer2->m_serializedData + *param_3->m_serializedDataOffsets);
     colorData = pppYmTracer2->m_serializedData + param_3->m_serializedDataOffsets[1];
 
-    if (param_2->m_initWOrk == 0xffffffff) {
-        pfVar6 = gPppDefaultValueBuffer;
-    } else {
-        pfVar6 = reinterpret_cast<float*>(
-            reinterpret_cast<TracerMngRaw*>(pppMngStPtr)->dataValues[param_2->m_initWOrk].workBase + 0x80 +
-            param_2->m_stepValue);
-    }
+    pfVar6 = (param_2->m_initWOrk == 0xffffffff)
+                 ? gPppDefaultValueBuffer
+                 : reinterpret_cast<float*>(
+                       reinterpret_cast<TracerMngRaw*>(pppMngStPtr)->dataValues[param_2->m_initWOrk].workBase + 0x80 +
+                       param_2->m_stepValue);
     work->initWork = pfVar6;
 
-    if (param_2->m_arg3 == 0xffffffff) {
-        pfVar6 = gPppDefaultValueBuffer;
-    } else {
-        pfVar6 = reinterpret_cast<float*>(
-            reinterpret_cast<TracerMngRaw*>(pppMngStPtr)->dataValues[param_2->m_arg3].workBase + 0x80 +
-            param_2->m_tracer.m_arg3WorkOffset);
-    }
+    pfVar6 = (param_2->m_arg3 == 0xffffffff)
+                 ? gPppDefaultValueBuffer
+                 : reinterpret_cast<float*>(
+                       reinterpret_cast<TracerMngRaw*>(pppMngStPtr)->dataValues[param_2->m_arg3].workBase + 0x80 +
+                       param_2->m_tracer.m_arg3WorkOffset);
     work->arg3Work = pfVar6;
 
     if (work->entries == nullptr) {

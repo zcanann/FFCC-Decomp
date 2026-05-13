c01cd5ef2b6af8c23e95516247ef6825e9946248
Zachary Canann <zacharycanann@gmail.com>
2026-05-12 17:42:40 -0700

    Improve MaterialEditor data layout

--- src/p_MaterialEditor.cpp
+++ src/p_MaterialEditor.cpp
@@ -42,7 +42,7 @@ unsigned int m_table__18CMaterialEditorPcs[0x15C / sizeof(unsigned int)] = {
     reinterpret_cast<unsigned int>(const_cast<char*>(s_CMaterialEditorPcs_VIEWER_801D7D18)), 0, 0, 0, 0, 0, 0, 0, 0, 0, 0x20, 0, 0, 0, 0, 0x41, 1
 };
 unsigned int lbl_801EA624[3] = {reinterpret_cast<unsigned int>(lbl_8032E648), 0, 0};
-unsigned int lbl_801EA630 = reinterpret_cast<unsigned int>(lbl_8032E648);
+unsigned int lbl_801EA630[5] = {reinterpret_cast<unsigned int>(lbl_8032E648), 0, 0, 0, 0};
 CMaterialEditorPcs MaterialEditorPcs;
 u8 lbl_8026D338[0xC];
 

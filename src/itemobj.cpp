fc2ef683057c1f8966b3a5a0c8d9c51e3c74dd04
Zachary Canann <zacharycanann@gmail.com>
2026-05-14 11:36:24 -0700

    Improve CGItemObj DeleteOld flow

--- src/itemobj.cpp
+++ src/itemobj.cpp
@@ -782,11 +782,7 @@ int CGItemObj::DeleteOld(int deleteMask, int maxDeleteCount, CFlatRuntime::CObje
 {
 	int deletedCount = 0;
 
-	while (true) {
-		if (maxDeleteCount <= deletedCount) {
-			return deletedCount;
-		}
-
+	while (deletedCount < maxDeleteCount) {
 		unsigned char* bestItemObj = 0;
 		int bestScriptObjectPos = 0x00989680;
 
@@ -802,12 +798,14 @@ int CGItemObj::DeleteOld(int deleteMask, int maxDeleteCount, CFlatRuntime::CObje
 			}
 		}
 
-		if (bestItemObj != 0) {
-			deleteObject__12CFlatRuntimeFPQ212CFlatRuntime7CObject(CFlat, bestItemObj);
-		} else {
+		if (bestItemObj == 0) {
+			if (static_cast<unsigned int>(System.m_execParam) >= 3) {
+				Printf__7CSystemFPce(&System, const_cast<char*>(DAT_801dced4));
+			}
 			break;
 		}
 
+		deleteObject__12CFlatRuntimeFPQ212CFlatRuntime7CObject(CFlat, bestItemObj);
 		deletedCount++;
 	}
 

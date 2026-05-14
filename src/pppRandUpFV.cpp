d0fed8f83248aaa61b0278c3f6bb297ae2ce3df8
Zachary Canann <zacharycanann@gmail.com>
2026-05-14 00:09:37 -0700

    Improve RandFV first lane matching

--- src/pppRandUpFV.cpp
+++ src/pppRandUpFV.cpp
@@ -54,10 +54,10 @@ void pppRandUpFV(_pppPObject* basePtr, RandUpFVParams* in, _pppCtrlTable* ctrl)
     }
 
     f32* target = (in->sourceOffset == -1) ? (f32*)gPppDefaultValueBuffer : (f32*)((u8*)basePtr + in->sourceOffset + 0x80);
-    f32 delta = randf(in->blend[0], *valuePtr);
+    f32 delta = in->blend[0] * *valuePtr;
     f32 value = *valuePtr;
 
-    target[0] = delta + target[0];
+    target[0] = target[0] + delta;
     target[1] += randf(in->blend[1], value);
     target[2] += randf(in->blend[2], value);
 }

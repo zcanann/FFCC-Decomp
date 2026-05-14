7e264f4c7eada548e67d7cdcd38dd57a70b3bf41
Zachary Canann <zcanann@Zacharys-Mac-mini.local>
2026-05-14 00:19:55 -0700

    Match CMemory allocation helper

--- src/memory.cpp
+++ src/memory.cpp
@@ -843,7 +843,7 @@ void CMemory::DestroyStage(CMemory::CStage* stage)
  */
 void* CMemory::_Alloc(unsigned long size, CMemory::CStage* stage, char* source, int line, int noError)
 {
-    return stage->alloc(size, source == (char*)nullptr ? DAT_8032f7d4 : source, line, noError);
+    return stage->alloc(size, source != (char*)nullptr ? source : DAT_8032f7d4, line, noError);
 }
 
 /*

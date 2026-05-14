84d706a301ab2fd9ae732a5446859993176c020e
Zachary Canann <zcanann@Zacharys-Mac-mini.local>
2026-05-14 01:40:40 -0700

    Match item object deletion loop

--- src/itemobj.cpp
+++ src/itemobj.cpp
@@ -804,7 +804,7 @@ int CGItemObj::DeleteOld(int deleteMask, int maxDeleteCount, CFlatRuntime::CObje
 			if ((unsigned int)System.m_execParam >= 3U) {
 				Printf__7CSystemFPce(&System, const_cast<char*>(DAT_801dced4));
 			}
-			return deletedCount;
+			break;
 		}
 
 		deletedCount++;

4dfcb6911045201e2ce9935e02e0dcb2eceb090b
Zachary Canann <zcanann@Zacharys-Mac-mini.local>
2026-05-14 14:44:46 -0700

    Improve DrawEquipMark matching

--- src/singmenu.cpp
+++ src/singmenu.cpp
@@ -2539,8 +2539,14 @@ void CMenuPcs::DrawEquipMark(int x, int y, float alpha)
     _GXSetBlendMode__F12_GXBlendMode14_GXBlendFactor14_GXBlendFactor10_GXLogicOp(1, 4, 5, 1);
     SetAttrFmt__8CMenuPcsFQ28CMenuPcs3FMT(&MenuPcs, 0);
 
-    _GXColor color = {0xFF, 0xFF, 0xFF, static_cast<u8>(static_cast<int>(FLOAT_80332940 * alpha))};
-    GXSetChanMatColor(GX_COLOR0A0, color);
+    union {
+        u32 word;
+        _GXColor color;
+    } color;
+    color.word = 0xFFFFFF00;
+    color.color.a = static_cast<u8>(static_cast<int>(FLOAT_80332940 * alpha));
+    GXSetChanMatColor(GX_COLOR0A0, color.color);
+    SetTexture__8CMenuPcsFQ28CMenuPcs3TEX(&MenuPcs, 0x2C);
 
     union {
         double value;
@@ -2552,7 +2558,6 @@ void CMenuPcs::DrawEquipMark(int x, int y, float alpha)
     yPos.words[0] = 0x43300000;
     yPos.words[1] = static_cast<u32>(y) ^ 0x80000000U;
 
-    SetTexture__8CMenuPcsFQ28CMenuPcs3TEX(&MenuPcs, 0x2C);
     DrawRect__8CMenuPcsFUlfffffffff(
         &MenuPcs, 0, static_cast<float>(xPos.value - DOUBLE_80332938),
         static_cast<float>(yPos.value - DOUBLE_80332938), FLOAT_80332948, FLOAT_80332948, FLOAT_8033294c, FLOAT_8033294c,

04f59973f20478dd9619120759f14cfc4426a0ce
Zachary Canann <zcanann@Zacharys-Mac-mini.local>
2026-05-14 19:54:38 -0700

    Use equip list base directly in EquipInit1

--- src/menu_equip.cpp
+++ src/menu_equip.cpp
@@ -1078,16 +1078,14 @@ void CMenuPcs::EquipInit1()
 	short* psVar13;
 	int iVar14;
 	unsigned int uVar15;
-	int* workPtr;
 
-	workPtr = (int*)GetEquipListBase(this);
 	fVar5 = FLOAT_80332f14;
 	fVar4 = FLOAT_80332f10;
 	fVar3 = FLOAT_80332ee0;
 	fVar2 = FLOAT_80332eb8;
 	sVar7 = 0;
-	iVar8 = (int)**(short**)workPtr;
-	psVar10 = *(short**)workPtr + iVar8 * 0x20 + 4;
+	iVar8 = (int)*GetEquipList(this);
+	psVar10 = GetEquipList(this) + iVar8 * 0x20 + 4;
 	psVar10[0xe] = 0;
 	psVar10[0xf] = 0x2e;
 	*psVar10 = 0xb8;
@@ -1105,7 +1103,7 @@ void CMenuPcs::EquipInit1()
 	psVar10[0x14] = 0;
 	psVar10[0x15] = 5;
 
-	puVar12 = (short*)(*workPtr + (iVar8 + 1) * 0x40 + 8);
+	puVar12 = (short*)(GetEquipListBase(this) + (iVar8 + 1) * 0x40 + 8);
 	*(int*)(puVar12 + 0xe) = 0x2f;
 	*puVar12 = 0xa0;
 	puVar12[1] = 0xe;
@@ -1117,7 +1115,7 @@ void CMenuPcs::EquipInit1()
 	*(int*)(puVar12 + 0x12) = 0;
 	*(int*)(puVar12 + 0x14) = 5;
 
-	puVar12 = (short*)(*workPtr + (iVar8 + 2) * 0x40 + 8);
+	puVar12 = (short*)(GetEquipListBase(this) + (iVar8 + 2) * 0x40 + 8);
 	*(int*)(puVar12 + 0xe) = 0x2f;
 	puVar12[2] = 0x30;
 	puVar12[3] = 0x30;
@@ -1129,7 +1127,7 @@ void CMenuPcs::EquipInit1()
 	*(int*)(puVar12 + 0x12) = 0;
 	*(int*)(puVar12 + 0x14) = 5;
 
-	puVar12 = (short*)(*workPtr + (iVar8 + 3) * 0x40 + 8);
+	puVar12 = (short*)(GetEquipListBase(this) + (iVar8 + 3) * 0x40 + 8);
 	*(int*)(puVar12 + 0x16) = 2;
 	*(int*)(puVar12 + 0xe) = 0x2e;
 	*puVar12 = 0xa0;
@@ -1141,10 +1139,10 @@ void CMenuPcs::EquipInit1()
 	*(int*)(puVar12 + 0x12) = 0;
 	*(int*)(puVar12 + 0x14) = 5;
 
-	psVar10 = *(short**)workPtr + **(short**)workPtr * 0x20 + 4;
+	psVar10 = GetEquipList(this) + *GetEquipList(this) * 0x20 + 4;
 	iVar8 = 4;
 	do {
-		psVar13 = (short*)(*workPtr + iVar11 + 8);
+		psVar13 = (short*)(GetEquipListBase(this) + iVar11 + 8);
 		psVar13[0x16] = 0;
 		psVar13[0x17] = 2;
 		psVar13[0xe] = 0;
@@ -1164,7 +1162,7 @@ void CMenuPcs::EquipInit1()
 
 		iVar14 = iVar11 + 0x48;
 		iVar11 = iVar11 + 0x80;
-		psVar13 = (short*)(*workPtr + iVar14);
+		psVar13 = (short*)(GetEquipListBase(this) + iVar14);
 		psVar13[0x16] = 0;
 		psVar13[0x17] = 2;
 		psVar13[0xe] = 0;
@@ -1184,8 +1182,8 @@ void CMenuPcs::EquipInit1()
 		iVar8 = iVar8 - 1;
 	} while (iVar8 != 0);
 
-	*(short*)(*workPtr + 2) = (short)iVar9;
-	psVar10 = *(short**)workPtr;
+	*(short*)(GetEquipListBase(this) + 2) = (short)iVar9;
+	psVar10 = GetEquipList(this);
 	uVar6 = (unsigned int)((int)psVar10[1] - (int)*psVar10);
 	psVar10 = psVar10 + *psVar10 * 0x20 + 4;
 	if (0 < (int)uVar6) {

4c3b6401a45c3a9d7ce6c54c7f23de32b96e659c
Zachary Canann <zcanann@Zacharys-Mac-mini.local>
2026-05-16 16:48:30 -0700

    Improve menu compa animation matching

--- src/menu_compa.cpp
+++ src/menu_compa.cpp
@@ -305,8 +305,8 @@ bool CMenuPcs::CompaClose()
 {
     int finishedCount;
     int count;
-    int frame;
     CompaOpenAnim* entry;
+    int frame;
 
     finishedCount = 0;
     this->compaMenuState->frame = this->compaMenuState->frame + 1;
@@ -422,23 +422,23 @@ activeHold:
 	}
 
 	if (doReset != 0) {
-		CompaOpenAnim* entries = this->compaList->entries;
-		entries[0].startFrame = 2;
-		entries[0].duration = 5;
-		entries[1].startFrame = 2;
-		entries[1].duration = 5;
-		entries[2].startFrame = 2;
-		entries[2].duration = 5;
-		entries[3].startFrame = 7;
-		entries[3].duration = 5;
-		entries[4].startFrame = 7;
-		entries[4].duration = 5;
-		entries[5].flags = 2;
-		entries[5].startFrame = 7;
-		entries[5].duration = 5;
+		CompaOpenAnimList* compaList = this->compaList;
+		compaList->entries[0].startFrame = 2;
+		compaList->entries[0].duration = 5;
+		compaList->entries[1].startFrame = 2;
+		compaList->entries[1].duration = 5;
+		compaList->entries[2].startFrame = 2;
+		compaList->entries[2].duration = 5;
+		compaList->entries[3].startFrame = 7;
+		compaList->entries[3].duration = 5;
+		compaList->entries[4].startFrame = 7;
+		compaList->entries[4].duration = 5;
+		compaList->entries[5].flags = 2;
+		compaList->entries[5].startFrame = 7;
+		compaList->entries[5].duration = 5;
 
-		unsigned int entryCount = this->compaList->count;
-		CompaOpenAnim* entry = entries;
+		unsigned int entryCount = compaList->count;
+		CompaOpenAnim* entry = compaList->entries;
 		while (entryCount != 0) {
 			entry->frame = 0;
 			entry->alpha = 1.0f;
@@ -461,8 +461,8 @@ bool CMenuPcs::CompaOpen()
 {
     int finishedCount;
     int count;
-    int frame;
     CompaOpenAnim* entry;
+    int frame;
 
     if (this->compaMenuState->initialized == '\0') {
         CompaInit();

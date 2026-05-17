f9aa4c2cb607bc8b70f23c6cbc5081200c7a1311
Zachary Canann <zcanann@Zacharys-Mac-mini.local>
2026-05-17 16:29:43 -0700

    Use normal CFont construction in font manager

--- src/fontman.cpp
+++ src/fontman.cpp
@@ -12,11 +12,9 @@ unsigned char g_tFont22[0x10D40] = {
 #include <dolphin/mtx.h>
 
 extern "C" void __dt__8CFontManFv(void*);
-extern "C" void __ct__4CRefFv(void*);
 extern "C" void* _Alloc__7CMemoryFUlPQ27CMemory6CStagePcii(CMemory*, unsigned long, CMemory::CStage*, char*, int, int);
 extern "C" void _GXSetBlendMode__F12_GXBlendMode14_GXBlendFactor14_GXBlendFactor10_GXLogicOp(int, int, int, int);
 extern "C" void _GXSetAlphaCompare__F10_GXCompareUc10_GXAlphaOp10_GXCompareUc(int, int, int, int, int);
-extern "C" void* __vt__5CFont[];
 extern "C" const float FLOAT_803306B8;
 extern "C" const float FLOAT_803306C8;
 extern "C" const float FLOAT_803306D8;
@@ -863,37 +861,7 @@ void CFontMan::Init()
 	CMemory::CStage* stage = Memory.CreateStage(0x8000, const_cast<char*>(s_CFontMan), 0);
 	m_stage = stage;
 
-	CFont* font = reinterpret_cast<CFont*>(
-	    _Alloc__7CMemoryFUlPQ27CMemory6CStagePcii(
-	        &Memory,
-	        sizeof(CFont),
-	        FontMan.m_stage,
-	        const_cast<char*>(s_fontman_cpp),
-	        0x3D,
-	        0));
-
-	if (font != 0) {
-		__ct__4CRefFv(font);
-		*reinterpret_cast<void**>(font) = __vt__5CFont;
-		font->m_glyphData = 0;
-		font->texturePtr = 0;
-		font->margin = FLOAT_803306B8;
-		font->posZ = FLOAT_803306B8;
-		font->posY = FLOAT_803306B8;
-		font->posX = FLOAT_803306B8;
-		CFontRenderFlagBits& bits = GetRenderFlagBits(font->renderFlags);
-		bits.shadow = 0;
-		font->scaleY = FLOAT_803306C8;
-		font->scaleX = FLOAT_803306C8;
-		bits.snapPosition = 0;
-		font->m_color.r = 0xFF;
-		font->m_color.g = 0xFF;
-		font->m_color.b = 0xFF;
-		font->m_color.a = 0xFF;
-		bits.zCompare = 0;
-		bits.zUpdate = 0;
-		font->m_usesEmbeddedData = 0;
-	}
+	CFont* font = new (FontMan.m_stage, const_cast<char*>(s_fontman_cpp), 0x3D) CFont;
 
 	m_font = font;
 	m_font->Create(0, 0);

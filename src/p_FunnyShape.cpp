6df1d3a3bf872dba84dd23d8448ef93596657dcf
Zachary Canann <zcanann@Zacharys-Mac-mini.local>
2026-05-18 02:33:04 -0700

    Model FunnyShape pointer array vtables

--- src/p_FunnyShape.cpp
+++ src/p_FunnyShape.cpp
@@ -25,7 +25,8 @@ template <class T>
 class CPtrArray
 {
 public:
-    void** vtable;
+    virtual ~CPtrArray();
+
     unsigned long size;
     unsigned long numItems;
     unsigned long defaultSize;
@@ -34,16 +35,10 @@ public:
     int growCapacity;
 
     CPtrArray();
-    ~CPtrArray();
 
     void RemoveAll();
     void DeleteAndRemoveAll();
 };
-
-template <class T>
-inline void SetPtrArrayDtorVtable(CPtrArray<T>*)
-{
-}
 
 extern "C" void __dl__FPv(void* ptr);
 extern "C" void __dla__FPv(void* ptr);
@@ -58,8 +53,6 @@ extern "C" CFunnyShape* __dt__11CFunnyShapeFv(CFunnyShape*, short);
 extern "C" void __dt__14CFunnyShapePcsFv(void*);
 extern "C" void* __vt__8CManager[];
 extern "C" void* __vt__8CProcess[];
-extern "C" void* gVtable_CPtrArray_OSFSTexture[];
-extern "C" void* gVtable_CPtrArray_GXTexObj[];
 extern "C" void* __vt__14CFunnyShapePcs[];
 extern "C" const char lbl_801D7DD0[] = "CFunnyShapePcs(VIEWER)";
 extern "C" const Vec s_funnyEye = {0.0f, 0.0f, 4.0f};
@@ -98,7 +91,6 @@ static inline CFunnyShape* FunnyShape(CFunnyShapePcs* self)
 template <class T>
 CPtrArray<T>::CPtrArray()
 {
-    vtable = 0;
     numItems = 0;
     size = 0;
     defaultSize = 0x10;
@@ -110,20 +102,7 @@ CPtrArray<T>::CPtrArray()
 template <class T>
 CPtrArray<T>::~CPtrArray()
 {
-    SetPtrArrayDtorVtable(this);
     RemoveAll();
-}
-
-template <>
-inline void SetPtrArrayDtorVtable(CPtrArray<_GXTexObj*>* ptrArray)
-{
-    ptrArray->vtable = gVtable_CPtrArray_GXTexObj;
-}
-
-template <>
-inline void SetPtrArrayDtorVtable(CPtrArray<OSFS_TEXTURE_ST*>* ptrArray)
-{
-    ptrArray->vtable = gVtable_CPtrArray_OSFSTexture;
 }
 
 /*
@@ -485,7 +464,6 @@ CFunnyShapePcs::~CFunnyShapePcs()
 template <>
 CPtrArray<_GXTexObj*>::CPtrArray()
 {
-    vtable = gVtable_CPtrArray_GXTexObj;
     numItems = 0;
     size = 0;
     defaultSize = 0x10;
@@ -497,7 +475,6 @@ CPtrArray<_GXTexObj*>::CPtrArray()
 template <>
 CPtrArray<OSFS_TEXTURE_ST*>::CPtrArray()
 {
-    vtable = gVtable_CPtrArray_OSFSTexture;
     numItems = 0;
     size = 0;
     defaultSize = 0x10;
@@ -509,14 +486,12 @@ CPtrArray<OSFS_TEXTURE_ST*>::CPtrArray()
 template <>
 CPtrArray<OSFS_TEXTURE_ST*>::~CPtrArray()
 {
-    SetPtrArrayDtorVtable(this);
     RemoveAll();
 }
 
 template <>
 CPtrArray<_GXTexObj*>::~CPtrArray()
 {
-    SetPtrArrayDtorVtable(this);
     RemoveAll();
 }
 

57822056270cb9ee0430ca2377dd33a6501a0cb6
Zachary Canann <zcanann@Zacharys-Mac-mini.local>
2026-05-18 09:39:18 -0700

    Improve YmMiasma frame angle calculation

--- src/pppYmMiasma.cpp
+++ src/pppYmMiasma.cpp
@@ -272,6 +272,7 @@ void pppFrameYmMiasma(pppYmMiasma* pppYmMiasma_, YmMiasmaFrameStep* step, pppYmM
         s32 angleIdx;
         float impulseX;
         float impulseZ;
+        float angleScale;
 
         work->m_emitTimer = 0;
         work->m_speedDecay = step->m_unk18;
@@ -284,7 +285,10 @@ void pppFrameYmMiasma(pppYmMiasma* pppYmMiasma_, YmMiasmaFrameStep* step, pppYmM
         }
 
         angleDelta += step->m_baseAngle;
-        angleIdx = (s32)((((float)angleDelta * FLOAT_80330640) * FLOAT_80330650) / FLOAT_80330654);
+        angleScale = FLOAT_80330640 * (float)angleDelta;
+        angleScale = FLOAT_80330650 * angleScale;
+        angleScale = angleScale / FLOAT_80330654;
+        angleIdx = (s32)angleScale;
         impulseX = *(float*)((u8*)gPppTrigTable + ((angleIdx + 0x4000) & 0xfffc));
         impulseZ = *(float*)((u8*)gPppTrigTable + (angleIdx & 0xfffc));
         zero = FLOAT_80330644;

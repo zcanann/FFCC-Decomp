fba6e402e293040d26f4a8a06b2e800e38b92ae1
Zachary Canann <zcanann@Zacharys-Mac-mini.local>
2026-05-19 11:43:22 -0700

    Match CNode ref data constructor

--- src/chara.cpp
+++ src/chara.cpp
@@ -2569,14 +2569,15 @@ void CChara::CNode::CalcBind(CChara::CModel* model)
 CChara::CNode::CRefData::CRefData()
 {
 	u8* raw = reinterpret_cast<u8*>(this);
-	u16 invalidIndex = 0xFF;
+	s8* signedRaw = reinterpret_cast<s8*>(this);
+	s32 invalidIndex = -1;
 
-	raw[0x8D] = invalidIndex;
+	signedRaw[0x8D] = invalidIndex;
 	raw[0x8E] = 0;
 	raw[0x8A] = 0;
 	memset(raw + 0x6A, 0, 0x20);
 	*reinterpret_cast<float*>(raw + 0x60) = FLOAT_803301b0;
-	raw[0x90] = invalidIndex;
+	signedRaw[0x90] = invalidIndex;
 }
 
 /*

95e6d4eed2451688cc73b8168be8180bf91bf5fb
Zachary Canann <zcanann@Zacharys-Mac-mini.local>
2026-05-20 05:27:08 -0700

    Improve p_FunnyShape storage linkage

--- src/pppCrystal.cpp
+++ src/pppCrystal.cpp
@@ -80,7 +80,7 @@ static const CrystalTexMtx s_crystalTexMtxBase = {
 
 static const CrystalIndTexMtx s_crystalIndTexMtxBase = {{{0.0f, 0.0f, 0.0f}, {0.0f, 0.0f, 0.0f}}};
 
-static const char s_pppCrystalCpp[] = "pppCrystal.cpp";
+extern const char s_pppCrystalCpp[] = "pppCrystal.cpp";
 
 static inline int CrystalFpClassify(float value)
 {

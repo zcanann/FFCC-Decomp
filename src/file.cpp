c724a2ae02a160652ead4ba01cad5e8791b1eb54
Zachary Canann <zcanann@Zacharys-Mac-mini.local>
2026-05-21 04:13:35 -0700

    Match CFile Init handle pool setup

--- src/file.cpp
+++ src/file.cpp
@@ -11,6 +11,7 @@
 #include "ffcc/util.h"
 
 #include <PowerPC_EABI_Support/Runtime/New.h>
+#include <PowerPC_EABI_Support/Runtime/MWCPlusLib.h>
 #include <dolphin/gx.h>
 #include <dolphin/os/OSCache.h>
 #include <dolphin/vi.h>
@@ -747,8 +748,8 @@ void CFile::Init()
     m_fatalDiskErrorFlag = 0;
     m_isDiskError = 0;
     m_readBuffer = new ((CMemory::CStage*)m_allocStage, const_cast<char*>(s_fileCpp), 0x2b) unsigned char[0x100000];
-    m_handlePoolHead.m_currentOffset =
-        (u32)new ((CMemory::CStage*)m_allocStage, const_cast<char*>(s_fileCpp), 0x2e) CHandle[0x80];
+    void* handlePool = new ((CMemory::CStage*)m_allocStage, const_cast<char*>(s_fileCpp), 0x2e) unsigned char[0x5610];
+    m_handlePoolHead.m_currentOffset = (u32)__construct_new_array(handlePool, 0, 0, sizeof(CHandle), 0x80);
     m_fileHandle.m_next = &m_fileHandle;
     m_fileHandle.m_previous = &m_fileHandle;
     m_fileHandle.m_priority = PRI_SENTINEL;

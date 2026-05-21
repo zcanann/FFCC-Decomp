489b4428cc1e480e4a9ee8c140eff6e19b4fc3e2
Zachary Canann <zcanann@Zacharys-Mac-mini.local>
2026-05-20 18:44:34 -0700

    Improve p_light bump light cleanup

--- src/p_light.cpp
+++ src/p_light.cpp
@@ -212,29 +212,31 @@ void CLightPcs::create()
  */
 void CLightPcs::destroy()
 {
-    for (u32 i = 0; i < 8; i++) {
-        CBumpLight& light = m_bumpLights[8 + i];
-        if (light.m_textureData != 0) {
-            bool hasTexture = light.m_textureData != 0;
+    CBumpLight* light = &m_bumpLights[8];
+
+    for (u32 i = 0; i < 8; i++, light++) {
+        if (light->m_textureData != 0) {
+            bool hasTexture = light->m_textureData != 0;
             if (hasTexture) {
-                delete[] light.m_textureData;
-                light.m_textureData = 0;
+                Memory.Free(light->m_textureData);
+                light->m_textureData = 0;
             }
-            light.m_hasTexture = 0;
-            light.m_useViewSpace = 0;
+            light->m_hasTexture = 0;
+            light->m_useViewSpace = 0;
         }
     }
 
-    for (u32 i = 0; i < 8; i++) {
-        CBumpLight& light = m_bumpLights[i];
-        if (light.m_textureData != 0) {
-            bool hasTexture = light.m_textureData != 0;
+    light = m_bumpLights;
+
+    for (u32 i = 0; i < 8; i++, light++) {
+        if (light->m_textureData != 0) {
+            bool hasTexture = light->m_textureData != 0;
             if (hasTexture) {
-                delete[] light.m_textureData;
-                light.m_textureData = 0;
+                Memory.Free(light->m_textureData);
+                light->m_textureData = 0;
             }
-            light.m_hasTexture = 0;
-            light.m_useViewSpace = 0;
+            light->m_hasTexture = 0;
+            light->m_useViewSpace = 0;
         }
     }
 }
@@ -250,18 +252,18 @@ void CLightPcs::destroy()
  */
 void CLightPcs::DestroyBumpLightAll(CLightPcs::TARGET target)
 {
-    CBumpLight* light = &m_bumpLights[static_cast<int>(target) * 8];
+    CBumpLight* light = m_bumpLights + (static_cast<int>(target) * 8);
 
-    for (u32 i = 0; i < 8; i++) {
-        if (light[i].m_textureData != 0) {
-            bool hasTexture = light[i].m_textureData != 0;
+    for (u32 i = 0; i < 8; i++, light++) {
+        if (light->m_textureData != 0) {
+            bool hasTexture = light->m_textureData != 0;
             if (hasTexture) {
-                delete[] light[i].m_textureData;
-                light[i].m_textureData = 0;
+                Memory.Free(light->m_textureData);
+                light->m_textureData = 0;
             }
 
-            light[i].m_hasTexture = 0;
-            light[i].m_useViewSpace = 0;
+            light->m_hasTexture = 0;
+            light->m_useViewSpace = 0;
         }
     }
 }

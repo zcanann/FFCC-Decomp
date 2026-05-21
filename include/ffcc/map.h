99bafdfc4a91e33a22dcfff2bb0ba2b29978b993
Zachary Canann <zcanann@Zacharys-Mac-mini.local>
2026-05-21 03:09:25 -0700

    Inline CMapKeyFrame lifecycle

--- include/ffcc/map.h
+++ include/ffcc/map.h
@@ -25,8 +25,33 @@ void GXSetTexCoordGen();
 class CMapKeyFrame
 {
 public:
-    CMapKeyFrame();
-    ~CMapKeyFrame();
+    CMapKeyFrame()
+    {
+        m_junTable = 0;
+        m_keyFrame = 0;
+        m_keyValue = 0;
+        m_splineTable = 0;
+    }
+
+    ~CMapKeyFrame()
+    {
+        if (m_junTable != 0) {
+            delete[] m_junTable;
+            m_junTable = 0;
+        }
+        if (m_keyFrame != 0) {
+            delete[] m_keyFrame;
+            m_keyFrame = 0;
+        }
+        if (m_keyValue != 0) {
+            delete[] m_keyValue;
+            m_keyValue = 0;
+        }
+        if (m_splineTable != 0) {
+            delete[] m_splineTable;
+            m_splineTable = 0;
+        }
+    }
 
     float Get();
     int Get(int&, int&, float&);

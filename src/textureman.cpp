0067af8e5a599afa2a30b3d39cc1c0eb74b62a25
AsclepiusBot <asclepiusbot@proton.me>
2026-05-20 17:26:20 -0700

    Fix local data ownership in pad and textureman (#13597)
    Co-authored-by: Zachary Canann <zcanann@Zacharys-Mac-mini.local>

--- src/textureman.cpp
+++ src/textureman.cpp
@@ -63,8 +63,9 @@ extern "C" void __ct__21CPtrArray_P8CTexture_Fv(void*);
 static const char s_textureman_cpp_801D7974[] = "textureman.cpp";
 static const char s_Error_width_pctd_height_pctd_801D7984[] = "Error width=%d height=%d\n";
 static const char s_CTexture_texture_801D79A0[] = "CTexture.texture";
-extern const char s_ptrarray_grow_error_801D79D8[];
-extern const char s_collection_ptrarray_h_801D79F4[];
+static const char s_ptrarray_grow_error_801D79D8[] =
+    "\x83\x6F\x83\x62\x83\x74\x83\x40\x90\xAC\x92\xB7\x82\xAA\x95\x73\x8B\x96\x89\xC2\x82\xC5\x82\xB7\x81\x42\x0A";
+static const char s_collection_ptrarray_h_801D79F4[] = "collection_ptrarray.h";
 extern const char s_CRef_8032FAE8[];
 extern const float FLOAT_8032faf0;
 extern const float FLOAT_8032faf4;

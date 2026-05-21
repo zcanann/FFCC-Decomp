57a903f2c87e657cde9b0adef21a7f2efc1b4e23
Zachary Canann <zcanann@Zacharys-Mac-mini.local>
2026-05-21 02:46:22 -0700

    Fix pppYmDrawMdlTexAnm rodata claim

--- src/pppYmDrawMdlTexAnm.cpp
+++ src/pppYmDrawMdlTexAnm.cpp
@@ -34,11 +34,11 @@ extern "C" {
 extern f32 FLOAT_80330548;
 extern f32 FLOAT_8033054c;
 extern const char s_PerU___0_2f_PerV___0_2f_801d9c38[] = "PerU: %0.2f PerV: %0.2f\n";
-extern const char DAT_801d9c54[0x14] = {
+extern const char DAT_801d9c54[0x10] = {
     (char)0x83, (char)0x52, (char)0x83, (char)0x93, (char)0x83,
     (char)0x58, (char)0x83, (char)0x67, (char)0x83, (char)0x89,
     (char)0x83, (char)0x4E, (char)0x83, (char)0x5E, '\n',
-    '\0',       '\0',       '\0',       '\0',       '\0',
+    '\0',
 };
 }
 

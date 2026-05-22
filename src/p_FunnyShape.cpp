b82a810fc89ea1530c5c63f9697ebd64c4447cf5
Zachary Canann <zacharycanann@gmail.com>
2026-05-21 22:52:32 -0700

    Revert "Use raw storage for FunnyShapePcs (#13792)"
    This reverts commit 3ff125f17f30bfb9bb7370ff6a819c148492e7ac.

--- src/p_FunnyShape.cpp
+++ src/p_FunnyShape.cpp
@@ -1,4 +1,3 @@
-#define FFCC_DEFINE_FUNNYSHAPEPCS_STORAGE
 #include "ffcc/p_FunnyShape.h"
 #include "ffcc/FunnyShape.h"
 #include "ffcc/USBStreamData.h"
@@ -457,7 +456,7 @@ unsigned int lbl_801EA910[5] = {
     0,
 };
 u8 ARRAY_8026D728[0xC];
-u8 FunnyShapePcs[sizeof(CFunnyShapePcs)];
+CFunnyShapePcs FunnyShapePcs;
 
 /*
  * --INFO--

04c3c1c5ae57140086cbbc2729be4850cb3bcfff
Zachary Canann <zcanann@Zacharys-Mac-mini.local>
2026-05-24 03:44:33 -0700

    Match CMapHit ReadOtmHit

--- src/maphit.cpp
+++ src/maphit.cpp
@@ -710,10 +710,10 @@ int CMapHit::ReadOtmHit(CChunkFile& chunkFile)
                 new (MapMng.m_stage, const_cast<char*>(s_maphit_cpp), 0x159)
                     CMapHitFace[m_faceCount];
 
-            const float offsetScale = kMapHitVertexOffsetScale;
-            const float radiusScale = kMapHitRadiusScale;
-            const float zero = kMapHitZero;
-            const double radiusBase = kMapHitRadiusBase;
+            const float& offsetScale = kMapHitVertexOffsetScale;
+            const float& radiusScale = kMapHitRadiusScale;
+            const float& zero = kMapHitZero;
+            const double& radiusBase = kMapHitRadiusBase;
 
             for (int faceIdx = 0; faceIdx < m_faceCount; faceIdx++) {
                 chunkFile.Align(4);

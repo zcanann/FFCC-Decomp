be22db99ade50143396123cc38ff15de32a85bbe
AsclepiusBot <asclepiusbot@proton.me>
2026-05-24 08:20:43 -0700

    Improve bump light map codegen (#13911)
    Co-authored-by: Zachary Canann <zcanann@Zacharys-Mac-mini.local>

--- src/p_light.cpp
+++ src/p_light.cpp
@@ -814,6 +814,7 @@ void CLightPcs::CBumpLight::MakeLightMap()
     _GXColor chanMat;
     _GXColor chanAmb2;
     _GXColor chanMat2;
+    _GXColor lightColor = kBumpLightMapColor;
 
     unsigned char u0 = m_bumpShade[0];
     unsigned char u1 = m_bumpShade[1];
@@ -898,7 +899,6 @@ void CLightPcs::CBumpLight::MakeLightMap()
     }
 
     int copySize = GXGetTexBufferSize(0x40, 0x40, 3, 0, 0);
-    _GXColor lightColor = kBumpLightMapColor;
     double dScale = (double)FLOAT_8032fc40;
     double dHalf = (double)FLOAT_8032fc1c;
     static float tParam[4] = {48.0f, 128.0f, 256.0f, 512.0f};

98f76ffbcb2469be806a3f7eb09282ff9aa9b3ab
AsclepiusBot <asclepiusbot@proton.me>
2026-05-24 14:01:00 -0700

    Define pppCharaBreak constants (#14012)
    Co-authored-by: Zachary Canann <zcanann@Zacharys-Mac-mini.local>

--- src/pppCharaBreak.cpp
+++ src/pppCharaBreak.cpp
@@ -18,22 +18,22 @@
 #include <PowerPC_EABI_Support/Msl/MSL_C/MSL_Common/stdlib.h>
 #include "ffcc/ppp_linkage.h"
 
+extern Vec kPppCharaBreakUpVector = {0.0f, 1.0f, 0.0f};
+extern int kCharaBreakInitialVertexFlag0 = 0;
+extern int kCharaBreakInitialVertexFlag1 = 0;
+extern int kCharaBreakInitialVertexFlag2 = 0;
 extern "C" const char s_pppCharaBreak_cpp[] = "pppCharaBreak.cpp";
-extern float FLOAT_80332048;
-extern float FLOAT_8033204c;
-extern float FLOAT_80332050;
-extern float FLOAT_80332058;
-extern float FLOAT_8033205c;
-extern float FLOAT_80332060;
-extern float FLOAT_80332064;
-extern float FLOAT_80332078;
-extern double DOUBLE_80332068;
-extern double DOUBLE_80332070;
-extern const char sPppCharaBreakObjMeshName[4];
-extern int kCharaBreakInitialVertexFlag0;
-extern int kCharaBreakInitialVertexFlag1;
-extern int kCharaBreakInitialVertexFlag2;
-extern Vec kPppCharaBreakUpVector;
+extern const float FLOAT_80332048 = 0.0f;
+extern const float FLOAT_8033204c = 1.0f;
+extern const float FLOAT_80332050 = -10000.0f;
+extern const char sPppCharaBreakObjMeshName[4] = "obj";
+extern const float FLOAT_80332058 = 0.333333313f;
+extern const float FLOAT_8033205c = 0.017453292f;
+extern const float FLOAT_80332060 = 180.0f;
+extern const float FLOAT_80332064 = 0.8f;
+extern const double DOUBLE_80332068 = 4503601774854144.0;
+extern const double DOUBLE_80332070 = 4503599627370496.0;
+extern const float FLOAT_80332078 = -1.0f;
 
 static inline Mtx& CameraMatrix()
 {

841f5f263f2f221145be31902653d9a3a727ecb6
BrilliantWasp <brilliantwasp@proton.me>
2026-05-25 08:39:42 -0700

    Use shared menu constants in menu list (#14092)
    Co-authored-by: Zachary Canann <zcanann@Zacharys-Mac-mini.local>

--- src/menu_lst.cpp
+++ src/menu_lst.cpp
@@ -8,21 +8,21 @@
 #include "ffcc/system.h"
 #include <string.h>
 
-static const float FLOAT_803333D0 = 0.0f;
-static const float FLOAT_803333D4 = 255.0f;
-static const double DOUBLE_803333D8 = 20.0;
-static const float FLOAT_803333E0 = 40.0f;
-static const double DOUBLE_803333E8 = 0.5;
-static const float FLOAT_803333F0 = 1.0f;
-static const float FLOAT_803333F4 = 4.0f;
-static const float FLOAT_803333F8 = 320.0f;
-static const float FLOAT_803333FC = 0.5f;
-static const float FLOAT_80333400 = 352.0f;
-static const float FLOAT_80333404 = 3.0f;
-static const double DOUBLE_80333408 = 4503601774854144.0;
-static const double DOUBLE_80333410 = 1.0;
-static const double DOUBLE_80333418 = 0.0;
-static const double DOUBLE_80333420 = 216.0;
+extern const float FLOAT_803333D0;
+extern const float FLOAT_803333D4;
+extern const double DOUBLE_803333D8;
+extern const float FLOAT_803333E0;
+extern const double DOUBLE_803333E8;
+extern const float FLOAT_803333F0;
+extern const float FLOAT_803333F4;
+extern const float FLOAT_803333F8;
+extern const float FLOAT_803333FC;
+extern const float FLOAT_80333400;
+extern const float FLOAT_80333404;
+extern const double DOUBLE_80333408;
+extern const double DOUBLE_80333410;
+extern const double DOUBLE_80333418;
+extern const double DOUBLE_80333420;
 
 STATIC_ASSERT(offsetof(CMenuPcs, listFont) == 0x108);
 STATIC_ASSERT(offsetof(CMenuPcs, helpFont) == 0xF8);

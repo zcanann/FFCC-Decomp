3caedba737abed704cae83ea44f2e351f3a739c4
Zachary Canann <zacharycanann@gmail.com>
2026-05-25 09:40:50 -0700

    Use default RTTI setting for pad

--- configure.py
+++ configure.py
@@ -577,7 +577,7 @@ config.libs = [
             Object(NonMatching, "p_system.cpp", extra_cflags=["-sdata 8", "-str reuse,readonly"]),
             Object(NonMatching, "p_tina.cpp", extra_cflags=["-RTTI on", "-sdata 8", "-str reuse,pool,readonly"]),
             Object(NonMatching, "p_usb.cpp", extra_cflags=["-sdata 8"]),
-            Object(NonMatching, "pad.cpp", extra_cflags=["-RTTI on"]),
+            Object(NonMatching, "pad.cpp"),
             Object(NonMatching, "partMng.cpp"),
             Object(NonMatching, "partyobj.cpp"),
             Object(Matching, "pppAccele.cpp"),

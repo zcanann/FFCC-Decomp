fa4ffa6aa77816c00868ae45ae2a852346eed4eb
AsclepiusBot <asclepiusbot@proton.me>
2026-05-25 16:29:32 -0700

    Improve deformation model literal matching (#14193)
    Co-authored-by: Zachary Canann <zcanann@Zacharys-Mac-mini.local>

--- src/pppYmDeformationMdl.cpp
+++ src/pppYmDeformationMdl.cpp
@@ -50,11 +50,6 @@ static inline _pppEnvStYmDeformationMdl* DeformationMdlEnv()
     return reinterpret_cast<_pppEnvStYmDeformationMdl*>(ppvEnv);
 }
 
-extern const float kYmDeformationMdlBackTextureWidth;
-extern const float kYmDeformationMdlBackTextureHeight;
-extern const float kYmDeformationMdlTexMtxOffset;
-extern const float kYmDeformationMdlTexMtxDepth;
-extern const float kYmDeformationMdlDegToRad;
 extern const float kYmDeformationMdlZero = 0.0f;
 
 static inline Mtx& CameraMatrix()
@@ -169,11 +164,11 @@ void pppRenderYmDeformationMdl(pppYmDeformationMdl* pppYmDeformationMdl, pppYmDe
         texMtx[0][2] = screenMtx[0][2];
         texMtx[1][2] = screenMtx[1][2];
         texMtx[2][2] = screenMtx[2][2];
-        texMtx[0][0] = texMtx[0][0] * (kYmDeformationMdlBackTextureWidth / (float)width);
-        texMtx[1][1] = texMtx[1][1] * -(kYmDeformationMdlBackTextureHeight / (float)height);
-        texMtx[0][2] = kYmDeformationMdlTexMtxOffset;
-        texMtx[1][2] = kYmDeformationMdlTexMtxOffset;
-        texMtx[2][2] = kYmDeformationMdlTexMtxDepth;
+        texMtx[0][0] = texMtx[0][0] * (320.0f / (float)width);
+        texMtx[1][1] = texMtx[1][1] * -(224.0f / (float)height);
+        texMtx[0][2] = -0.5f;
+        texMtx[1][2] = -0.5f;
+        texMtx[2][2] = -1.0f;
         PSMTXConcat(texMtx, modelObject->m_modelMatrix.value, texMtx);
         GXLoadTexMtxImm(texMtx, 0x1E, GX_MTX3x4);
         GXSetTexCoordGen2(GX_TEXCOORD0, GX_TG_MTX3x4, GX_TG_POS, 0x1E, GX_FALSE, GX_PTIDENTITY);
@@ -187,7 +182,7 @@ void pppRenderYmDeformationMdl(pppYmDeformationMdl* pppYmDeformationMdl, pppYmDe
             state->m_angle = 1;
         }
 
-        PSMTXRotRad(rotMtx, 'z', kYmDeformationMdlDegToRad * (float)state->m_angle);
+        PSMTXRotRad(rotMtx, 'z', 0.017453292f * (float)state->m_angle);
         indMtx[0][0] = rotMtx[0][0] * state->m_scale;
         indMtx[0][1] = rotMtx[0][1] * state->m_scale;
         indMtx[0][2] = DeformationMdlZero();

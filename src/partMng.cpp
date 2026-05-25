f876a15594690d4bbcf15f51c87661727f48d3aa
Zachary Canann <zcanann@Zacharys-Mac-mini.local>
2026-05-24 17:37:21 -0700

    Improve part draw heap check

--- src/partMng.cpp
+++ src/partMng.cpp
@@ -972,10 +972,11 @@ void CPartMng::setProcSpeed(ProcSpdSt*, int)
 void CPartMng::drawEnd()
 {
     gPppHeapUseRateWords[0] = pppHeapCheckLeak__FPQ27CMemory6CStage2(pppEnvStPtr->m_stagePtr);
-    if ((gPppHeapUseRateWords[2] == 0)
-        || ((gPppHeapUseRateWords[2] = gPppHeapUseRateWords[2] - 1), gPppHeapUseRateWords[1] < gPppHeapUseRateWords[0])) {
-        gPppHeapUseRateWords[2] = *(int*)((char*)this + 0x16C) << 1;
+    int heapCheckCount = gPppHeapUseRateWords[2];
+    gPppHeapUseRateWords[2] = heapCheckCount - 1;
+    if ((heapCheckCount == 0) || (gPppHeapUseRateWords[1] < gPppHeapUseRateWords[0])) {
         gPppHeapUseRateWords[1] = gPppHeapUseRateWords[0];
+        gPppHeapUseRateWords[2] = *(int*)((char*)this + 0x16C) << 1;
     }
 }
 

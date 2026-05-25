5086eb1c855893ce40b7728b7336d07ab769e3a3
Zachary Canann <zcanann@Zacharys-Mac-mini.local>
2026-05-25 12:30:19 -0700

    Recover FunnyShapePcs member layout

--- src/p_FunnyShape.cpp
+++ src/p_FunnyShape.cpp
@@ -74,7 +74,7 @@ static inline CUSBStreamData* UsbStream(CFunnyShapePcs* self)
 
 static inline CFunnyShape* FunnyShape(CFunnyShapePcs* self)
 {
-    return reinterpret_cast<CFunnyShape*>(self->m_funnyShapeStorage);
+    return &self->m_funnyShape;
 }
 
 static inline CPtrArray<OSFS_TEXTURE_ST*>* TextureHeaders(CFunnyShapePcs* self)
@@ -190,7 +190,7 @@ void CFunnyShapePcs::calcViewer()
         UsbStream(this)->SetUSBStreamDataDone();
     }
 
-    if (m_textureCount == 0 || m_anm.anmData == 0) {
+    if (m_funnyShape.m_textureCount == 0 || m_funnyShape.m_anm.anmData == 0) {
         return;
     }
 
@@ -395,7 +395,6 @@ void CPtrArray<OSFS_TEXTURE_ST*>::RemoveAll()
 inline CFunnyShapePcs::CFunnyShapePcs()
 {
     new (UsbStream(this)) CUSBStreamData;
-    new (FunnyShape(this)) CFunnyShape;
     new (TextureHeaders(this)) CPtrArray<OSFS_TEXTURE_ST*>;
     new (TextureObjects(this)) CPtrArray<_GXTexObj*>;
 }
@@ -442,7 +441,6 @@ CFunnyShapePcs::~CFunnyShapePcs()
 {
     TextureObjects(this)->CPtrArray<_GXTexObj*>::~CPtrArray();
     TextureHeaders(this)->CPtrArray<OSFS_TEXTURE_ST*>::~CPtrArray();
-    FunnyShape(this)->~CFunnyShape();
     UsbStream(this)->~CUSBStreamData();
 }
 

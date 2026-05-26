8e3a2ce99c14de56fb4eeef2bd274c31d0636ceb
AsclepiusBot <asclepiusbot@proton.me>
2026-05-25 22:24:23 -0700

    Add pppYmDeformationShp sdata2 constants (#14257)
    Co-authored-by: Zachary Canann <zcanann@Zacharys-Mac-mini.local>

--- src/pppYmDeformationShp.cpp
+++ src/pppYmDeformationShp.cpp
@@ -643,3 +643,8 @@ void pppConstructYmDeformationShp(pppYmDeformationShp* pppYmDeformationShp_, ppp
 	state->m_values[3] = value;
 	state->m_values[2] = value;
 }
+
+extern const float FLOAT_80330634 = 0.0f;
+extern const float FLOAT_80330638 = 0.0f;
+extern const float FLOAT_8033063c = 0.0f;
+extern const float FLOAT_80330640 = 0.01745329238474369f;

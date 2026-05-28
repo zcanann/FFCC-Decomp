05a9392234fc4207296908772e0f09d47a020fd4
Zachary Canann <zcanann@Zacharys-Mac-mini.local>
2026-05-28 06:33:57 -0700

    Use linked YmTracer2 color seeds

--- src/pppYmTracer2.cpp
+++ src/pppYmTracer2.cpp
@@ -51,8 +51,8 @@ union PackedColor {
     u8 bytes[4];
 };
 
-PackedColor g_pppYmTracer2_1;
-PackedColor g_pppYmTracer2_2;
+extern "C" PackedColor g_pppYmTracer2_1;
+extern "C" PackedColor g_pppYmTracer2_2;
 
 static inline void copyPolygonData(TRACE_POLYGON* dst, TRACE_POLYGON* src)
 {

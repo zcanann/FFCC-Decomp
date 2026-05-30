049d7123c1ac3e864d04f4fa0a68e132929574ad
Zachary Canann <zacharycanann@gmail.com>
2026-05-29 23:23:06 -0700

    Restore wm_menu frame record draw loop

--- src/wm_menu.cpp
+++ src/wm_menu.cpp
@@ -9554,14 +9554,18 @@ LAB_draw:
 	GXSetChanMatColor(static_cast<GXChannelID>(4), frameColor);
 	SetTexture(static_cast<CMenuPcs::TEX>(0x1E));
 	unsigned char* const frame = reinterpret_cast<unsigned char*>(reinterpret_cast<unsigned int*>(bytes + 0x820)[0]);
-	unsigned char* const frameEntry = frame + 0x20;
-	DrawRect(0xFFFFFFFF, static_cast<float>(*reinterpret_cast<short*>(frameEntry)),
-	         static_cast<float>(*reinterpret_cast<short*>(frameEntry + 2)),
-	         static_cast<float>(*reinterpret_cast<short*>(frameEntry + 4)),
-	         static_cast<float>(*reinterpret_cast<short*>(frameEntry + 6)),
-	         *reinterpret_cast<float*>(frameEntry + 8), *reinterpret_cast<float*>(frameEntry + 0x0C),
-	         FLOAT_803313e8, FLOAT_803313e8,
-	         static_cast<float>(*reinterpret_cast<unsigned int*>(frameEntry + 0x18)));
+	for (int i = 0, offset = 0; i < 2; i++, offset += 0x1C) {
+		if (((1 << i) & 2) != 0) {
+			unsigned char* const frameEntry = frame + offset + 4;
+			DrawRect(0xFFFFFFFF, static_cast<float>(*reinterpret_cast<short*>(frameEntry)),
+			         static_cast<float>(*reinterpret_cast<short*>(frameEntry + 2)),
+			         static_cast<float>(*reinterpret_cast<short*>(frameEntry + 4)),
+			         static_cast<float>(*reinterpret_cast<short*>(frameEntry + 6)),
+			         *reinterpret_cast<float*>(frameEntry + 8), *reinterpret_cast<float*>(frameEntry + 0x0C),
+			         FLOAT_803313e8, FLOAT_803313e8,
+			         static_cast<float>(*reinterpret_cast<unsigned int*>(frameEntry + 0x18)));
+		}
+	}
 
 	SetAttrFmt(static_cast<CMenuPcs::FMT>(0));
 	GXColor white = {0xFF, 0xFF, 0xFF, 0xFF};

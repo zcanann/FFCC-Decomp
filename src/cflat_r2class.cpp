63ad8e3514b13d7b5254dd62dedcd8597f36fb8c
Zachary Canann <zacharycanann@gmail.com>
2026-05-30 15:56:23 -0700

    Store cflat look accumulators directly

--- src/cflat_r2class.cpp
+++ src/cflat_r2class.cpp
@@ -1459,12 +1459,8 @@ int CFlatRuntime2::onClassSystemFunc(CFlatRuntime::CObject* object, int, int com
 			outResult = 0;
 			break;
 		case -0x90:
-			{
-				float yaw = static_cast<float>(localBase[0]);
-				float pitch = static_cast<float>(localBase[1]);
-				engineObject->m_lookAtAccumYaw = yaw;
-				engineObject->m_lookAtAccumPitch = pitch;
-			}
+			engineObject->m_lookAtAccumYaw = static_cast<float>(localBase[0]);
+			engineObject->m_lookAtAccumPitch = static_cast<float>(localBase[1]);
 			PushValue(this, object, 0);
 			outResult = 0;
 			break;

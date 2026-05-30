8c2f66f100719a0421b8e8cded58fc3ced573820
Zachary Canann <zcanann@Zacharys-Mac-mini.local>
2026-05-30 15:38:07 -0700

    Define pppYmMana local constants

--- src/pppYmMana.cpp
+++ src/pppYmMana.cpp
@@ -18,11 +18,11 @@
 
 
 
-extern float FLOAT_80330e48;
-extern float FLOAT_80330e4c;
-extern float FLOAT_80330e58;
-extern float FLOAT_80330e5c;
+extern const float FLOAT_80330e48 = -1.5707964f;
+extern const float FLOAT_80330e4c = 0.0f;
 extern const char s_ymManaRuin2Name[] = "ruin_2";
+extern const float FLOAT_80330e58 = 1.0f;
+extern const float FLOAT_80330e5c = 0.5f;
 
 extern const float FLOAT_80330e60 = 2.0f;
 extern const float FLOAT_80330e64 = 0.015625f;

540ff2a99c5c0af202ed46bacda52242112bacde
Zachary Canann <zacharycanann@gmail.com>
2026-05-30 10:42:10 -0700

    Use signed cflat r2class flag indexing

--- src/cflat_r2class.cpp
+++ src/cflat_r2class.cpp
@@ -432,9 +432,9 @@ CFlatRuntime::CVal* CFlatRuntime2::onClassSystemVal(CFlatRuntime::CObject* objec
 				}
 			} else if (systemVal <= -400) {
 				if (systemVal <= -1000 && systemVal >= -0xBE7) {
-					const unsigned int bit = static_cast<unsigned int>(systemVal + 0xBE7);
-					const u8 byteValue = *(classData + (bit >> 3) + 0x8A4);
-					const unsigned int mask = 1U << (bit & 7);
+					const int bit = systemVal + 0xBE7;
+					const u8 byteValue = *(classData + (bit / 8) + 0x8A4);
+					const unsigned int mask = 1U << (bit % 8);
 					value = static_cast<unsigned int>((byteValue & mask) != 0);
 				} else if (systemVal <= -500 && systemVal >= -0x2F3) {
 					value = LoadS16(classData, (systemVal + 0x2F3) * 2 + 0x9A4);

009a9c1fadc9bbe7ca25070ca74a5fe5f6a694c2
Zachary Canann <zcanann@Zacharys-Mac-mini.local>
2026-05-30 14:13:40 -0700

    Promote chara model data fields

--- include/ffcc/chara.h
+++ include/ffcc/chara.h
@@ -19,8 +19,13 @@ struct Vec;
 
 struct CCharaModelData
 {
-    u8 _pad0[0x24];
+    u8 _pad0[0xC];
+    u32 m_meshCount;
+    u8 _pad10[0x14];
     CMaterialSet* m_materialSet;
+    u8 _pad28[0xC];
+    u32 m_posQuant;
+    u32 m_normQuant;
 };
 
 void D3DXMatrixMultiplyRotate(float (*)[4], float (*)[4], float (*)[4]);

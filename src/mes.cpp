b4cfd6cae2a4889e21d6b27027ff2b624ef64152
Zachary Canann <zacharycanann@gmail.com>
2026-05-29 19:42:37 -0700

    Use direct CMes Calc flag loop

--- src/mes.cpp
+++ src/mes.cpp
@@ -809,13 +809,8 @@ void CMes::Calc()
 
 	unsigned char* flagEntry =
 	    (unsigned char*)((char*)this + *(int*)((char*)this + 0x3C10) * 6 + 0x3C14);
-	while (true)
+	while ((int)maxAdvance > *(int*)((char*)this + 0x3C10))
 	{
-		if ((int)maxAdvance <= *(int*)((char*)this + 0x3C10))
-		{
-			break;
-		}
-
 		int type = *flagEntry;
 		if (type != 3)
 		{

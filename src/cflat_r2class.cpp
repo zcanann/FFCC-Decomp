de67d599ba7a55a03e9637ef9b6498ca5e493fe9
Zachary Canann <zacharycanann@gmail.com>
2026-05-30 11:19:16 -0700

    Inline cflat r2class direct class halfword setter

--- src/cflat_r2class.cpp
+++ src/cflat_r2class.cpp
@@ -290,7 +290,17 @@ void CFlatRuntime2::onSetClassSystemVal(int systemVal, CFlatRuntime::CObject* ob
 						StoreU16(stack, classData, (systemVal + 0x199) * 2 + 0x3B8, setMode);
 					}
 				} else {
-					StoreU16(stack, classData, 0x3C8, setMode);
+					unsigned short* value = reinterpret_cast<unsigned short*>(classData + 0x3C8);
+					stack[-1].m_word = *value;
+					if (setMode == 0) {
+						*value = stack->m_word;
+					} else if (setMode < 0) {
+						if (setMode > -2) {
+							*value = *value - stack->m_word;
+						}
+					} else if (setMode < 2) {
+						*value = *value + stack->m_word;
+					}
 				}
 			} else if (systemVal < -0x95 && systemVal > -0x176) {
 				u8* const itemTable = *reinterpret_cast<u8**>(classData + 0x24);

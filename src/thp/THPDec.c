2df6ba2171b2318b3b96559d5570e0cadd700ab0
Zachary Canann <zacharycanann@gmail.com>
2026-05-31 13:01:16 -0700

    Define THP version data in source

--- src/thp/THPDec.c
+++ src/thp/THPDec.c
@@ -4,7 +4,7 @@
 #if DEBUG
 static const char s___THPVersion[] = "<< Dolphin SDK - THP\tdebug build: Apr  5 2004 04:18:14 (0x2301) >>";
 #else
-extern const char s___THPVersion[];
+static char s___THPVersion[] = "<< Dolphin SDK - THP\trelease build: Aug 27 2002 20:42:01 >>";
 #endif
 const char* __THPVersion = s___THPVersion;
 

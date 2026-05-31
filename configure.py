daf623ac10f22aae2ac178f2555cf687614c1d61
Zachary Canann <zacharycanann@gmail.com>
2026-05-31 11:54:37 -0700

    Use generated p_chara RTTI descriptors

--- configure.py
+++ configure.py
@@ -563,7 +563,7 @@ config.libs = [
             Object(NonMatching, "monobj_boss.cpp"),
             Object(Matching, "monobj_table.cpp", extra_cflags=["-pooldata off"]),
             Object(NonMatching, "p_camera.cpp", extra_cflags=["-RTTI on", "-sdata 8"]),
-            Object(NonMatching, "p_chara.cpp", extra_cflags=["-sdata 8"]),
+            Object(NonMatching, "p_chara.cpp", extra_cflags=["-RTTI on", "-sdata 8", "-str reuse,pool,readonly"]),
             Object(NonMatching, "p_chara_viewer.cpp", extra_cflags=["-sdata 8"]),
             Object(NonMatching, "p_dbgmenu.cpp", extra_cflags=["-RTTI on", "-sdata 8", "-inline auto,deferred"]),
             Object(NonMatching, "p_FunnyShape.cpp", extra_cflags=["-RTTI on", "-sdata 8"]),

ea78c87cb19527fb887f1af641e624ee4fa7b825
Zachary Canann <zacharycanann@gmail.com>
2026-05-31 11:54:01 -0700

    Claim generated font RTTI boundary

--- configure.py
+++ configure.py
@@ -514,7 +514,7 @@ config.libs = [
             Object(Matching, "color.cpp"),
             Object(NonMatching, "file.cpp", extra_cflags=["-RTTI on"]),
             Object(Matching, "strcase.c"),
-            Object(NonMatching, "fontman.cpp", extra_cflags=["-RTTI on"]),
+            Object(NonMatching, "fontman.cpp", extra_cflags=["-RTTI on", "-sdata 8", "-str reuse,pool,readonly"]),
             Object(NonMatching, "FS_USB_Process.cpp"),
             Object(NonMatching, "FunnyShape.cpp"),
             Object(NonMatching, "game.cpp"),

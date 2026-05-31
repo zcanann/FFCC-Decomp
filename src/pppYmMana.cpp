e22cd729761064f9c28372e66f6f6236ac170862
Zachary Canann <zacharycanann@gmail.com>
2026-05-31 00:05:15 -0700

    Match YmMana shadow mesh callback

--- src/pppYmMana.cpp
+++ src/pppYmMana.cpp
@@ -248,28 +248,30 @@ void Mana_BeforeDrawShadowLockEnvCallback(CChara::CModel*, void*, void*, int)
  */
 void Chara_DrawShadowMeshDLCallback(CChara::CModel* model, void* work, void* vYmMana, int meshIndex, int dlIndex, float (*) [4])
 {
+    u8* workBytes = (u8*)work;
+    u8* vYmManaBytes = (u8*)vYmMana;
     s32 meshData = *(s32*)((u8*)model + 0xAC);
-    const u8 alpha = *(u8*)((u8*)vYmMana + 0x3B);
-
+    u8 alpha = vYmManaBytes[0x3B];
     if (alpha != 0) {
-        *(u8*)((u8*)work + 0xFC) = 0xFF;
-        *(u8*)((u8*)work + 0xFD) = 0xFF;
-        *(u8*)((u8*)work + 0xFE) = 0xFF;
-        *(u8*)((u8*)work + 0xFF) = alpha;
-    } else {
-        *(u8*)((u8*)work + 0xFC) = 0xFF;
-        *(u8*)((u8*)work + 0xFD) = 0xFF;
-        *(u8*)((u8*)work + 0xFE) = 0xFF;
-        *(u8*)((u8*)work + 0xFF) = 0xFF;
+        workBytes[0xFC] = 0xFF;
+        workBytes[0xFD] = 0xFF;
+        workBytes[0xFE] = 0xFF;
+        workBytes[0xFF] = vYmManaBytes[0x3B];
+    } else if (alpha == 0) {
+        workBytes[0xFC] = 0xFF;
+        workBytes[0xFD] = 0xFF;
+        workBytes[0xFE] = 0xFF;
+        workBytes[0xFF] = 0xFF;
     }
 
-    u8* shadowColor = (u8*)work + 0xFC;
-    DCFlushRange(shadowColor, 4);
-    GXSetArray((GXAttr)0xB, shadowColor, 4);
+    DCFlushRange(workBytes + 0xFC, 4);
+    GXSetArray((GXAttr)0xB, workBytes + 0xFC, 4);
 
-    s32 meshOffset = meshIndex * 0x14 + 8;
-    s32 mesh = *(s32*)(meshData + meshOffset);
-    u32* dl = (u32*)(*(s32*)(mesh + 0x50) + dlIndex * 0xC);
+    meshData += meshIndex * 0x14;
+    s32 mesh = *(s32*)(meshData + 8);
+    meshData = *(s32*)(mesh + 0x50);
+    meshData += dlIndex * 0xC;
+    u32* dl = (u32*)meshData;
     MaterialMan.SetMaterial(reinterpret_cast<CMaterialSet*>(*(void**)(*(s32*)((u8*)model + 0xA4) + 0x24)),
                             *(u16*)((u8*)dl + 8), 0, (_GXTevScale)0);
     GXCallDisplayList((void*)dl[1], dl[0]);

b883ecf3902bde43ae7cbea863500e8065803a54
Zachary Canann <zacharycanann@gmail.com>
2026-05-30 20:51:58 -0700

    Match volume row fill threshold

--- src/MenuUtil.cpp
+++ src/MenuUtil.cpp
@@ -1210,7 +1210,7 @@ void CMenuPcs::DrawOptionMenu()
 			float barX = FLOAT_80333610 + static_cast<float>(x);
 			gUtil.RenderTextureQuad(barX, FLOAT_80333614, FLOAT_80333624, FLOAT_80333624, meterTexture, &uv0,
 			                        &uv1, &color, GX_BL_SRCALPHA, GX_BL_INVSRCALPHA);
-			if ((m_optionMenuState != 2) && (i < volume)) {
+			if ((m_optionMenuState != 2) && (i + 1 <= volume)) {
 				gUtil.RenderTextureQuad(barX, FLOAT_80333614, FLOAT_80333624, FLOAT_80333624, meterTexture, &uv0,
 				                        &uv1, &color, GX_BL_ONE, GX_BL_ONE);
 			}
@@ -1316,7 +1316,7 @@ void CMenuPcs::DrawOptionMenu()
 			float barX = FLOAT_80333610 + static_cast<float>(x);
 			gUtil.RenderTextureQuad(barX, FLOAT_80333614, FLOAT_80333624, FLOAT_80333624, meterTexture, &uv0,
 			                        &uv1, &color, GX_BL_SRCALPHA, GX_BL_INVSRCALPHA);
-			if ((m_optionMenuState != 2) && (i < volume)) {
+			if ((m_optionMenuState != 2) && (i + 1 <= volume)) {
 				gUtil.RenderTextureQuad(barX, FLOAT_80333614, FLOAT_80333624, FLOAT_80333624, meterTexture, &uv0,
 				                        &uv1, &color, GX_BL_ONE, GX_BL_ONE);
 			}

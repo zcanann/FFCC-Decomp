49a19a4e70ec6b9e24b468fe28c8c8b66092da00
BrilliantWasp <brilliantwasp@proton.me>
2026-06-01 10:15:15 -0700

    Improve laser and YmMiasma matching (#14717)
    * Improve laser trail count typing
    
    * Match YmMiasma construct2 constant loads
    
    ---------
    
    Co-authored-by: Zachary Canann <zcanann@Zacharys-Mac-mini.local>

--- src/pppYmLaser.cpp
+++ src/pppYmLaser.cpp
@@ -86,7 +86,7 @@ extern "C" void pppRenderYmLaser(pppYmLaser* laser, pppYmLaserUnkB* step, _pppCt
 	int colorOffset = serializedDataOffsets[1];
 	pppYmLaserColorData* colorData = (pppYmLaserColorData*)((u8*)laser + 0x80 + colorOffset);
 	s32 dataValIndex = step->m_dataValIndex;
-	s32 count;
+	u32 count;
 	s32 i;
 	s32 alphaStep;
 	char alphaMax;

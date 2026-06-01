e4e89ffe41cdfdd72b12739d2bdfe02d79824f19
asmithbot <asmithbot@proton.me>
2026-05-31 20:45:03 -0700

    Use polygon fields in CharaBreak creation (#14600)
    Co-authored-by: Zachary Canann <zacharycanann@gmail.com>

--- src/pppCharaBreak.cpp
+++ src/pppCharaBreak.cpp
@@ -855,19 +855,16 @@ void CreatePolygon(POLYGON_DATA* polygonData, void* displayList, unsigned long, 
 
                     gUtil.ConvI2FVector(posFloat, posQuantized, ModelData(model)->m_posQuant);
                     PSMTXMultVec(meshMtx, &posFloat, &posFloat);
-                    gUtil.ConvF2IVector(*reinterpret_cast<S16Vec*>((u8*)polygonData + (outVertex * 6) + 0x10), posFloat,
+                    gUtil.ConvF2IVector(polygonData->m_pos[outVertex], posFloat,
                         ModelData(model)->m_posQuant);
                 } else {
                     S16Vec* sourcePos = workPositions + posIndex;
-                    s32 positionOffset = outVertex * 6;
-                    *(s16*)((u8*)polygonData + positionOffset + 0x10) = sourcePos->x;
-                    *(s16*)((u8*)polygonData + positionOffset + 0x12) = sourcePos->y;
-                    *(s16*)((u8*)polygonData + positionOffset + 0x14) = sourcePos->z;
+                    polygonData->m_pos[outVertex] = *sourcePos;
                 }
 
-                *(u16*)((u8*)polygonData + (outVertex * 2) + 0x22) = posIndex;
-                *(u16*)((u8*)polygonData + (outVertex * 2) + 0x2E) = texIndex;
-                *(u16*)((u8*)polygonData + (outVertex * 2) + 0x28) = nrmIndex;
+                polygonData->m_posIndices[outVertex] = posIndex;
+                polygonData->m_texIndices[outVertex] = texIndex;
+                polygonData->m_nrmIndices[outVertex] = nrmIndex;
                 outVertex++;
                 stripRestart = previousRestart;
 

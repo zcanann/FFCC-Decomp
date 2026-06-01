822cd39b22da84a3f1b7c36cc771af7aa217f695
Zachary Canann <zcanann@Zacharys-Mac-mini.local>
2026-06-01 10:08:27 -0700

    Improve laser trail count typing

--- src/pppLaser.cpp
+++ src/pppLaser.cpp
@@ -374,7 +374,7 @@ extern "C" void pppRenderLaser(struct pppLaser *pppLaser, struct pppLaserUnkB *p
     int colorOffset = serializedDataOffsets[1];
     LaserColorData* colorData = (LaserColorData*)((u8*)pppLaser + 0x80 + colorOffset);
     s32 dataValIndex = step->m_dataValIndex;
-    s32 count;
+    u32 count;
     s32 i;
     s32 alphaStep;
     char alphaMax;

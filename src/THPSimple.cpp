c58dd80b3704298acb077960d45999bb722296c3
Zachary Canann <zacharycanann@gmail.com>
2026-06-01 18:53:01 -0700

    Match THPSimple default volume load

--- src/THPSimple.cpp
+++ src/THPSimple.cpp
@@ -967,8 +967,9 @@ s32 THPSimpleOpen(const char* path)
     SimpleControl.isBufferSet = 0;
     SimpleControl.isLooping = 0;
     SimpleControl.isOpen = 1;
-    SimpleControl.curVolume = kTHPSimpleDefaultVolume;
-    SimpleControl.targetVolume = kTHPSimpleDefaultVolume;
+    const float& defaultVolume = kTHPSimpleDefaultVolume;
+    SimpleControl.curVolume = defaultVolume;
+    SimpleControl.targetVolume = defaultVolume;
     SimpleControl.rampCount = 0;
 
     return 1;

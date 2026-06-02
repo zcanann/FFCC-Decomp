02ef1aa547a570124d0311260f5bd6b6d4893754
Zachary Canann <zcanann@Zacharys-Mac-mini.local>
2026-06-02 05:57:16 -0700

    Improve pppFrameLaser cylinder layout

--- src/pppLaser.cpp
+++ src/pppLaser.cpp
@@ -71,7 +71,17 @@ struct LaserColorData {
     pppCVECTOR m_color;
 };
 
+struct LaserMapCylinderRaw {
+    Vec m_bottom;
+    Vec m_top;
+    Vec m_axis;
+    f32 m_radius;
+    Vec m_boundsMin;
+    Vec m_boundsMax;
+};
+
 STATIC_ASSERT(offsetof(struct pppLaser, m_workArea) == 0x80);
+STATIC_ASSERT(sizeof(LaserMapCylinderRaw) == sizeof(CMapCylinder));
 
 /*
  * --INFO--
@@ -194,7 +204,7 @@ extern "C" void pppFrameLaser(struct pppLaser *pppLaser, struct pppLaserUnkB *pa
     Vec localA;
     Mtx tempMtx;
     Mtx charaMtx;
-    CMapCylinder cyl;
+    LaserMapCylinderRaw cyl;
 
     int emptyHistory;
     int fillIndex;
@@ -275,7 +285,7 @@ extern "C" void pppFrameLaser(struct pppLaser *pppLaser, struct pppLaserUnkB *pa
         cyl.m_axis = localA;
         cyl.m_radius = LaserConst(kPppLaserZero);
 
-        int check = MapMng.CheckHitCylinderNear(&cyl, &localA, 0xffffffff);
+        int check = MapMng.CheckHitCylinderNear(reinterpret_cast<CMapCylinder*>(&cyl), &localA, 0xffffffff);
         int hit = 0;
         if (check != 0) {
             hit = 1;

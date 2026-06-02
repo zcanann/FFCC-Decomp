24038cac657466520b78db986d05ba1eebc39404
asmithbot <asmithbot@proton.me>
2026-06-02 10:06:54 -0700

    Match ME USB texture wrap check order (#15035)
    Co-authored-by: Zachary Canann <zacharycanann@gmail.com>

--- src/ME_USB_process.cpp
+++ src/ME_USB_process.cpp
@@ -415,7 +415,7 @@ void CMaterialEditorPcs::SetUSBData()
             }
             heightFactor >>= 1;
         }
-        if ((widthFactor != 1) || (heightFactor != 1)) {
+        if ((heightFactor != 1) || (widthFactor != 1)) {
             isPowerOfTwo = 0;
         }
 

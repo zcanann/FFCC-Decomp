a39351198f88af61f0f80fd898a62e83a401a9a6
asmithbot <asmithbot@proton.me>
2026-06-02 10:36:56 -0700

    Use loop-calc guard for chara break update (#15043)
    Co-authored-by: Zachary Canann <zacharycanann@gmail.com>

--- src/pppCharaBreak.cpp
+++ src/pppCharaBreak.cpp
@@ -364,7 +364,7 @@ void pppFrameCharaBreak(pppCharaBreak* charaBreak, CharaBreakUnkB* step, _pppCtr
         }
     }
 
-    if (gPppInConstructor == 0) {
+    if (ppvIsLoopCalc == 0) {
         UpdatePolygonData((PCharaBreak*)stepData, (VCharaBreak*)work, model);
     }
     return;

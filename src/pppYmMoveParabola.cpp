c24fcea96dcfa3ccbeea04aa92e71866a81bb9c8
Zachary Canann <zcanann@Zacharys-Mac-mini.local>
2026-06-01 20:46:55 -0700

    Fix PPP small data ownership

--- src/pppYmMoveParabola.cpp
+++ src/pppYmMoveParabola.cpp
@@ -125,9 +125,9 @@ extern "C" void pppConstructYmMoveParabola(struct pppYmMoveParabola* basePtr, st
     }
 }
 
-extern const float FLOAT_80330e38 = 0.0f;
-extern const float FLOAT_80330e3c = 1.0f;
-extern const float FLOAT_80330e40 = 0.0f;
-extern const float FLOAT_80330e44 = 1.0f;
+extern const float gPppParMoveMatrixZero = 0.0f;
+extern const float gPppParMoveMatrixOne = 1.0f;
+extern const float kPppYmTraceMoveZero = 0.0f;
+extern const float kPppYmTraceMoveOne = 1.0f;
 extern const float FLOAT_80330e48 = -1.5707964f;
 extern const float FLOAT_80330e4c = 0.0f;

42c2428ea0dc2f73449b68a937cebae4673c83c9
Zachary Canann <zcanann@Zacharys-Mac-mini.local>
2026-06-02 19:55:19 -0700

    Claim p_graphic debug rodata

--- src/p_graphic.cpp
+++ src/p_graphic.cpp
@@ -61,10 +61,10 @@ extern "C" float FLOAT_8032FBC0;
 extern "C" float FLOAT_8032fbfc;
 extern "C" float FLOAT_8032fc00;
 
-extern const char s_p_graphic_cpp[];
-extern const char s_graphic_order_debug_fmt[];
-extern const char s_graphic_move_debug_fmt[];
-extern const char s_graphic_pad_input_fmt[];
+extern const char s_graphic_order_debug_fmt[] = "%s(%d) %.3f%%";
+extern const char s_graphic_move_debug_fmt[] = " MOVE=%.1f%% BG=%.1f%% OBJ=%.1f%% UP=%.1f%% HIT=%.1f%% SCR=%.1f%%";
+extern const char s_graphic_pad_input_fmt[] = "%c%c%c%c%c%c%c%c%c%c";
+extern const char s_p_graphic_cpp[] = "p_graphic.cpp";
 const char s_debug_pad_port_fmt[] = "%dP";
 const char s_debug_frame_fmt[] = "%d";
 extern "C" const GXColor s_debug_bar_color;

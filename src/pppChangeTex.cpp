5bf1fc96714265b2dc1b5f971fe6d87ea975fe7b
Zachary Canann <zacharycanann@gmail.com>
2026-06-04 15:01:51 -0700

    Fix ChangeTex reflection TEV bits

--- src/pppChangeTex.cpp
+++ src/pppChangeTex.cpp
@@ -421,8 +421,9 @@ static void ChangeTex_DrawMeshDLCallback(CChara::CModel* model, void* param_2, v
 	CTexture* texture = work->m_texture;
 
 	if (step->m_changeTex.m_mode == 0) {
+		unsigned int drawTevBits = 0xACE0F;
 		MaterialMan.SetChangeTexReflectionState(
-		    &texture->m_texObj, 0xADE0F, 0xADE0F);
+		    &texture->m_texObj, drawTevBits, drawTevBits | 0x1000);
 	}
 
 	MaterialMan.SetMaterial(model->m_data->m_materialSet, displayList->m_material, 0, (_GXTevScale)0);

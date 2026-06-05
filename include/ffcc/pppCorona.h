f19e5d85305b49bf238b22f12883debd57a7b022
Zachary Canann <zacharycanann@gmail.com>
2026-06-04 22:02:57 -0700

    Expose Corona packet layout

--- include/ffcc/pppCorona.h
+++ include/ffcc/pppCorona.h
@@ -1,9 +1,28 @@
 #ifndef _FFCC_PPPCORONA_H_
 #define _FFCC_PPPCORONA_H_
 
+#include <dolphin/types.h>
+
 struct _pppPObject;
 struct _pppCtrlTable;
-struct CoronaParam;
+
+struct CoronaParam {
+    s32 m_graphId;
+    s32 m_dataValIndex;
+    u8 m_colorR;
+    u8 m_colorG;
+    u8 m_colorB;
+    s16 m_shapeStep;
+    float m_distMin;
+    float m_distMax;
+    float m_distRange;
+    float m_addX;
+    float m_addY;
+    float m_addZ;
+    u8 m_blendMode;
+    u8 m_drawB;
+    u8 m_drawA;
+};
 
 #ifdef __cplusplus
 extern "C" {

96a9283c078c924b11e4b264f81f00055f0b1a54
Zachary Canann <zacharycanann@gmail.com>
2026-06-04 21:16:13 -0700

    Type move control inputs

--- include/ffcc/pppMove.h
+++ include/ffcc/pppMove.h
@@ -1,9 +1,18 @@
 #ifndef _PPP_MOVE_H_
 #define _PPP_MOVE_H_
 
+#include <dolphin/types.h>
+
 struct _pppPObject;
 struct _pppCtrlTable;
-struct PppMoveInput;
+
+struct PppMoveInput {
+    int m_graphId;
+    int m_padding;
+    f32 x;
+    f32 y;
+    f32 z;
+};
 
 #ifdef __cplusplus
 extern "C" {

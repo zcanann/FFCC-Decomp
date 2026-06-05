d03e18ce03cae1c38b5c98bfa23e7ad40d94afe3
Zachary Canann <zacharycanann@gmail.com>
2026-06-04 21:20:17 -0700

    Expose callback distance step data

--- include/ffcc/pppCallBackDistance.h
+++ include/ffcc/pppCallBackDistance.h
@@ -1,9 +1,16 @@
 #ifndef _FFCC_PPPCALLBACKDISTANCE_H_
 #define _FFCC_PPPCALLBACKDISTANCE_H_
 
+#include <dolphin/types.h>
+
 struct _pppPObject;
 struct _pppCtrlTable;
-struct pppCallBackDistanceUnkB;
+
+struct pppCallBackDistanceStep {
+    u32 m_unk0;
+    f32 m_dataValIndex;
+    s16 m_initWOrk;
+};
 
 #ifdef __cplusplus
 extern "C" {
@@ -11,7 +18,7 @@ extern "C" {
 
 void pppConstructCallBackDistance(_pppPObject* object, _pppCtrlTable* ctrlTable);
 void pppDestructCallBackDistance(_pppPObject* object, _pppCtrlTable* ctrlTable);
-void pppFrameCallBackDistance(_pppPObject* object, pppCallBackDistanceUnkB* step, _pppCtrlTable* ctrlTable);
+void pppFrameCallBackDistance(_pppPObject* object, pppCallBackDistanceStep* step, _pppCtrlTable* ctrlTable);
 
 #ifdef __cplusplus
 }

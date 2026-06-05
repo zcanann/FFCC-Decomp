7ac5e88d08e63daa0d42c4fa6100eef3933e1212
Zachary Canann <zacharycanann@gmail.com>
2026-06-04 21:09:52 -0700

    Expose Ym move step layouts

--- include/ffcc/pppYmTraceMove.h
+++ include/ffcc/pppYmTraceMove.h
@@ -1,9 +1,19 @@
 #ifndef _FFCC_PPP_YMTRACEMOVE_H_
 #define _FFCC_PPP_YMTRACEMOVE_H_
 
+#include <dolphin/types.h>
+
 struct _pppCtrlTable;
 struct _pppPObject;
-struct pppYmTraceMoveStep;
+
+struct pppYmTraceMoveStep {
+    s32 m_graphId;
+    f32 m_dataValIndex;
+    f32 m_initWOrk;
+    f32 m_stepValue;
+    f32 m_arg3;
+    f32 m_payload;
+};
 
 typedef _pppPObject pppYmTraceMove;
 

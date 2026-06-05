ac31e95929b5144d4a04657edcbc563a46fd34fd
BrilliantWasp <brilliantwasp@proton.me>
2026-06-05 00:24:15 -0700

    Improve CGame SetNextScript loop (#15587)
    Co-authored-by: Zachary Canann <zcanann@Zacharys-Mac-mini.local>

--- src/cflat_r2system.cpp
+++ src/cflat_r2system.cpp
@@ -1571,17 +1571,19 @@ void CCameraPcs::SetZRotate(float zRotate)
  */
 void CGame::SetNextScript(CGame::CNextScript* nextScript)
 {
-    unsigned int* dst = (unsigned int*)((char*)&m_nextScript - 4);
+    int count = 0x20;
+    unsigned int* dst = (unsigned int*)&m_nextScript;
     unsigned int* src = (unsigned int*)((char*)nextScript - 4);
 
-    for (int i = 0; i < 0x20; i++) {
+    do {
         unsigned int a = src[1];
         src += 2;
         unsigned int b = src[0];
         dst[1] = a;
         dst += 2;
         dst[0] = b;
-    }
+        count--;
+    } while (count != 0);
 
     m_newGameFlag = 1;
 }

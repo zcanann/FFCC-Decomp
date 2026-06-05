d55ff58661d79005d3a65e427ffe85a0cea7e0d7
Zachary Canann <zacharycanann@gmail.com>
2026-06-04 21:11:04 -0700

    Expose miasma step layouts

--- include/ffcc/pppYmMiasma.h
+++ include/ffcc/pppYmMiasma.h
@@ -1,16 +1,74 @@
 #ifndef _FFCC_PPP_YMMIASMA_H_
 #define _FFCC_PPP_YMMIASMA_H_
 
+#include <dolphin/types.h>
+
 struct _pppCtrlTable;
 struct _pppPObject;
 
 typedef _pppPObject pppYmMiasma;
 
 struct pppYmMiasmaUnkB;
-struct YmMiasmaRenderStep;
-struct YmMiasmaFrameStep;
 struct VYmMiasma;
-struct PYmMiasma;
+
+struct PYmMiasma {
+    s32 m_graphId;
+    s32 m_dataValIndex;
+    s32 m_shapeFrameStep;
+    u16 m_particleCount;
+    u16 m_pad0E;
+    f32 m_baseSpeed;
+    f32 m_speedVariance;
+    f32 m_unk18;
+    f32 m_minDistance;
+    f32 m_gravity;
+    u8 m_colorStartR;
+    u8 m_colorStartG;
+    u8 m_colorStartB;
+    u8 m_colorStartA;
+    s16 m_colorEndR;
+    s16 m_colorEndG;
+    s16 m_colorEndB;
+    s16 m_colorEndA;
+    s16 m_colorStepFrames;
+    s16 m_pad32;
+    f32 m_initialSpeedDecay;
+    f32 m_speedDecay;
+    f32 m_radiusJitter;
+    f32 m_spawnHeightJitter;
+    f32 m_heightJitter;
+    u8 m_lifeBase;
+    u8 m_lifeRange;
+    u8 m_pad4A[2];
+    f32 m_minSpeed;
+    s16 m_fadeFrames;
+    s16 m_colorDecayFrames;
+};
+
+struct YmMiasmaRenderStep {
+    s32 m_graphId;
+    s32 m_dataValIndex;
+    u8 m_pad8[4];
+    u16 m_particleCount;
+    u8 m_pad0E[0x24];
+    u8 m_blendMode;
+    u8 m_pad33[0x41];
+    u8 m_drawEnvA;
+    u8 m_drawEnvB;
+};
+
+struct YmMiasmaFrameStep : PYmMiasma {
+    u8 m_pad54[4];
+    f32 m_radiusDelta;
+    f32 m_radiusVelocity;
+    f32 m_radiusAcceleration;
+    u8 m_emitInterval;
+    u8 m_pad65;
+    s16 m_baseAngle;
+    s16 m_angleRange;
+    u8 m_pad6A[2];
+    f32 m_speedDecayStep;
+};
 
 #ifdef __cplusplus
 extern "C" {

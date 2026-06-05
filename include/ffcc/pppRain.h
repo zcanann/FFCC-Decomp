e1ebd7f20d87eee163a241a6ddcff36e0b6b9f42
Zachary Canann <zacharycanann@gmail.com>
2026-06-04 22:03:36 -0700

    Expose Rain packet layout

--- include/ffcc/pppRain.h
+++ include/ffcc/pppRain.h
@@ -1,13 +1,46 @@
 #ifndef _PPP_RAIN_H_
 #define _PPP_RAIN_H_
 
+#include <dolphin/types.h>
+
 struct _pppCtrlTable;
 struct _pppPObject;
 
 typedef _pppPObject pppRain;
 typedef _pppCtrlTable RAIN_DATA;
 
-struct PRain;
+struct PRain {
+    s32 m_graphId;
+    u16 m_dataValIndex;
+    u8 _pad0[2];
+    f32 m_initWOrk;
+    f32 m_stepValue;
+    f32 m_arg3;
+    f32 m_unk14;
+    f32 m_moveYDelta;
+    f32 m_accelYDelta;
+    f32 m_accelZDelta;
+    f32 m_driftY;
+    u16 m_lifeBase;
+    u16 m_lifeRange;
+    u8 _pad2c[0x30 - 0x2C];
+    f32 m_minX;
+    f32 m_unk34;
+    f32 m_minZ;
+    f32 m_unk3c;
+    f32 m_maxX;
+    f32 m_maxY;
+    f32 m_maxZ;
+    u8 _pad4c[0x50 - 0x4C];
+    u8 m_lineWidth;
+    u8 _pad51[3];
+    f32 m_lengthBase;
+    f32 m_lengthRand;
+    u8 m_blendMode;
+    u8 m_fogIndex;
+    u8 m_lightTarget;
+    u8 _pad63;
+};
 
 #ifdef __cplusplus
 extern "C" {

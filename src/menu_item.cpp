b9430eb7630571121ec523666af8daefee30a180
Zachary Canann <zcanann@Zacharys-Mac-mini.local>
2026-06-05 02:31:57 -0700

    Improve menu cursor pad index matching

--- src/menu_item.cpp
+++ src/menu_item.cpp
@@ -125,7 +125,7 @@ int CMenuPcs::ItemCtrlCur()
     if (blocked) {
         press = 0;
     } else {
-        int padIndex = blocked;
+        int padIndex = 0;
         padIndex &= ~-((__cntlzw((unsigned int)Pad._448_4_) & 0x20) >> 5);
         press = Pad.GetPadInputs()[padIndex].buttonDown[0];
     }
@@ -137,7 +137,7 @@ int CMenuPcs::ItemCtrlCur()
     if (blocked) {
         hold = 0;
     } else {
-        int padIndex = blocked;
+        int padIndex = 0;
         padIndex &= ~-((__cntlzw((unsigned int)Pad._448_4_) & 0x20) >> 5);
         hold = Pad.GetPadInputs()[padIndex].repeatButton;
     }

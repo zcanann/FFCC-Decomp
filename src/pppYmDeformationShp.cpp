37ff8b7beac89591da0704dcca015782637e2e23
AsclepiusBot <asclepiusbot@proton.me>
2026-06-05 09:04:32 -0700

    Improve pppYmDeformationShp render shape (#15626)
    Co-authored-by: Zachary Canann <zcanann@Zacharys-Mac-mini.local>

--- src/pppYmDeformationShp.cpp
+++ src/pppYmDeformationShp.cpp
@@ -155,8 +155,9 @@ void pppRenderYmDeformationShp(pppYmDeformationShp* pppYmDeformationShp_, pppYmD
 
 		if (param_2->m_splitMode == 0) {
 			u8 size = param_2->m_size;
+			s8 orientation = param_2->m_orientation;
 			float quadSize = (float)size;
-			setVertexPos(vertices, (s8)param_2->m_orientation, -quadSize, quadSize, quadSize, -quadSize);
+			setVertexPos(vertices, orientation, -quadSize, quadSize, quadSize, -quadSize);
 			setVertexUV(uvs, kPppYmDeformationShpZero, kPppYmDeformationShpZero, FLOAT_803305f8, FLOAT_803305f8);
 			RenderDeformationShape(object, work, vertices, uvs);
 		} else {

0115fc87f621734558280042ce9ce4155949bc6f
AsclepiusBot <asclepiusbot@proton.me>
2026-06-05 08:57:04 -0700

    Improve menu cursor pad index matching (#15605)
    Co-authored-by: Zachary Canann <zcanann@Zacharys-Mac-mini.local>

--- src/menu_money.cpp
+++ src/menu_money.cpp
@@ -122,7 +122,7 @@ int CMenuPcs::MoneyCtrlCur()
 	if (blocked) {
 		press = 0;
 	} else {
-		int padIndex = blocked;
+		int padIndex = 0;
 		padIndex &= ~-((__cntlzw((unsigned int)Pad._448_4_) & 0x20) >> 5);
 		press = Pad.GetPadInputs()[padIndex].buttonDown[0];
 	}
@@ -134,7 +134,7 @@ int CMenuPcs::MoneyCtrlCur()
 	if (blocked) {
 		hold = 0;
 	} else {
-		int padIndex = blocked;
+		int padIndex = 0;
 		padIndex &= ~-((__cntlzw((unsigned int)Pad._448_4_) & 0x20) >> 5);
 		hold = Pad.GetPadInputs()[padIndex].repeatButton;
 	}

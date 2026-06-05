2445126068a1eab3d5bcbc3fb1b084bc2d3c8a08
asmithbot <asmithbot@proton.me>
2026-06-05 09:06:39 -0700

    Improve mapshadow sdata2 matching (#15643)
    Co-authored-by: Zachary Canann <zacharycanann@gmail.com>

--- src/mapshadow.cpp
+++ src/mapshadow.cpp
@@ -40,23 +40,25 @@ void CMapShadowInsertOctTree(CMapShadow::TARGET mapShadow, COctTree& octTree)
 {
 	CPtrArray<CMapShadow*>* mapShadowArray;
 	CMapShadow* shadow;
+	int target;
 	int i;
 	u32 octTreeMask;
 	Vec pos;
 
+	target = (int)mapShadow;
 	octTree.ClearShadow();
 	if (octTree.GetMapObject()->m_shadowTarget != 0) {
         mapShadowArray = &MapMng.GetMapShadowArray();
 		for (i = 0; i < (u32)mapShadowArray->GetSize(); i++) {
 			octTreeMask = octTree.GetMapObject()->m_shadowTarget;
 			if (((octTreeMask & (1U << i)) != 0) &&
-			    ((shadow = (*mapShadowArray)[i])->m_targetEnabled[(int)mapShadow] != 0) &&
+			    ((shadow = (*mapShadowArray)[i])->m_targetEnabled[target] != 0) &&
 			    (shadow->m_materialMode == 0)) {
 				pos.x = shadow->m_modelA->m_worldMtx[0][3];
 				pos.y = shadow->m_modelA->m_worldMtx[1][3];
 				pos.z = shadow->m_modelA->m_worldMtx[2][3];
 
-				octTree.InsertShadow(i, pos, shadow->m_targetBounds[(int)mapShadow]);
+				octTree.InsertShadow(i, pos, shadow->m_targetBounds[target]);
 			}
 		}
 	}

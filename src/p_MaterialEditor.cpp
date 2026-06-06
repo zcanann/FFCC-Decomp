79821ff49ab9943e12cc11dbd2caf13e0862d335
Zachary Canann <zacharycanann@gmail.com>
2026-06-06 10:04:03 -0700

    Localize editor scratch state

--- src/p_MaterialEditor.cpp
+++ src/p_MaterialEditor.cpp
@@ -33,7 +33,7 @@ CProcessTableCallback CMaterialEditorPcs::m_table_desc1 = {0, 0xFFFFFFFF, reinte
 CProcessTableCallback CMaterialEditorPcs::m_table_desc2 = {0, 0xFFFFFFFF, reinterpret_cast<unsigned int>(calcViewer__18CMaterialEditorPcsFv)};
 CProcessTableCallback CMaterialEditorPcs::m_table_desc3 = {0, 0xFFFFFFFF, reinterpret_cast<unsigned int>(drawViewer__18CMaterialEditorPcsFv)};
 CMaterialEditorPcs MaterialEditorPcs;
-char* q;
+static char* q;
 
 CProcessTable CMaterialEditorPcs::m_table = {
     const_cast<char*>(s_CMaterialEditorPcsViewer),

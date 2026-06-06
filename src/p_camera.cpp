e71c34b204d1958f9d71fe4b16ca7368a3c211f2
Zachary Canann <zcanann@Zacharys-Mac-mini.local>
2026-06-06 02:54:39 -0700

    Fix camera standard projection FOV fallback

--- src/p_camera.cpp
+++ src/p_camera.cpp
@@ -853,8 +853,10 @@ void CCameraPcs::SetStdProjectionMatrix()
 {
     float fov = m_fov;
 
-    if (fov < FLOAT_8032fac8 && static_cast<unsigned int>(System.m_execParam) >= 1) {
-        System.Printf(const_cast<char*>(sCameraInvalidFovFmt), fov);
+    if (fov < FLOAT_8032fac8) {
+        if (static_cast<unsigned int>(System.m_execParam) >= 1) {
+            System.Printf(const_cast<char*>(sCameraInvalidFovFmt), fov);
+        }
         fov = FLOAT_8032fab4;
     }
 

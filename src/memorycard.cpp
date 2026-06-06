58b3a40a9a0d30a0dcc0a9647a12118447f2a6f3
asmithbot <asmithbot@proton.me>
2026-06-06 12:33:19 -0700

    Improve memory card save letter packing (#15971)
    Co-authored-by: Zachary Canann <zacharycanann@gmail.com>

--- src/memorycard.cpp
+++ src/memorycard.cpp
@@ -1622,7 +1622,27 @@ void CMemoryCardMan::MakeSaveData()
         *reinterpret_cast<u32*>(dst + 0xEC) = caravanWork->m_gil;
         memcpy(dst + 0xF0, caravanWork->m_name, 0x10);
         *reinterpret_cast<u32*>(dst + 0x100) = caravanWork->m_letterCount;
-        memcpy(dst + kMemoryCardSaveLetterOffset, caravanWork->m_letters, sizeof(caravanWork->m_letters));
+        u8* letterDst = dst + kMemoryCardSaveLetterOffset;
+        u8* letterSrc = reinterpret_cast<u8*>(caravanWork->m_letters);
+        for (int letter = 0; letter < 100; letter++)
+        {
+            letterDst[0] = ((letterSrc[0] >> 3 & 1) << 3) | (letterDst[0] & 0xF7);
+            *reinterpret_cast<u16*>(letterDst) =
+                (*reinterpret_cast<u16*>(letterSrc) & 0x07FC) | (*reinterpret_cast<u16*>(letterDst) & 0xF803);
+            *reinterpret_cast<u32*>(letterDst) =
+                (*reinterpret_cast<u32*>(letterSrc) & 0x0003FE00) |
+                (*reinterpret_cast<u32*>(letterDst) & 0xFFFC01FF);
+            *reinterpret_cast<u16*>(letterDst + 2) =
+                (*reinterpret_cast<u16*>(letterSrc + 2) & 0x01FF) |
+                (*reinterpret_cast<u16*>(letterDst + 2) & 0xFE00);
+            memcpy(letterDst + 4, letterSrc + 4, 8);
+            letterDst[0] = (letterSrc[0] & 0x80) | (letterDst[0] & 0x7F);
+            letterDst[0] = (letterSrc[0] & 0x40) | (letterDst[0] & 0xBF);
+            letterDst[0] = (letterSrc[0] & 0x20) | (letterDst[0] & 0xDF);
+            letterDst[0] = (letterSrc[0] & 0x10) | (letterDst[0] & 0xEF);
+            letterSrc += sizeof(CCaravanWork::CLetterWork);
+            letterDst += 0xC;
+        }
 
         for (int artifact = 0; artifact < 96; artifact += 3)
         {

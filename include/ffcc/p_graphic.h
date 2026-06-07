8bce7f427e66c3dc245c11afd79576c35ab25d0c
Zachary Canann <zacharycanann@gmail.com>
2026-06-06 21:07:31 -0700

    Localize graphic process callbacks

--- include/ffcc/p_graphic.h
+++ include/ffcc/p_graphic.h
@@ -25,17 +25,6 @@ public:
     };
 
     static CProcessTable m_table;
-    static CProcessTableCallback m_table_desc0;
-    static CProcessTableCallback m_table_desc1;
-    static CProcessTableCallback m_table_desc2;
-    static CProcessTableCallback m_table_desc3;
-    static CProcessTableCallback m_table_desc4;
-    static CProcessTableCallback m_table_desc5;
-    static CProcessTableCallback m_table_desc6;
-    static CProcessTableCallback m_table_desc7;
-    static CProcessTableCallback m_table_desc8;
-    static CProcessTableCallback m_table_desc9;
-
     void Init();
     void Quit();
     int GetTable(unsigned long);

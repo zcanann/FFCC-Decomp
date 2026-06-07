db08ecfd78c520f14d42d7e755dd8dea75d830d1
Zachary Canann <zacharycanann@gmail.com>
2026-06-06 22:31:16 -0700

    Name menu resource strings

--- src/p_menu.cpp
+++ src/p_menu.cpp
@@ -64,46 +64,46 @@ STATIC_ASSERT(offsetof(CMenuPcs, m_singleMenuInitialized) == 0x85A);
 STATIC_ASSERT(offsetof(CMenuPcs, m_singleMenuTextureLoadIndex) == 0x85C);
 STATIC_ASSERT(offsetof(CMenuPcs, m_singleMenuTextureLoadState) == 0x860);
 
-static const char s_win_kazari_801D9D48[] = "win_kazari";
-extern const char s_CManager_801D9D54[] = "CManager";
-extern const char s_CProcess_801D9D60[] = "CProcess";
-static const char s_dvd__smenu__s_tex_801d9d6c[] = "dvd/%smenu/%s.tex";
+static const char sMenuTexWinKazari[] = "win_kazari";
+extern const char sMenuManagerClassName[] = "CManager";
+extern const char sMenuProcessClassName[] = "CProcess";
+static const char sMenuTexturePathFmt[] = "dvd/%smenu/%s.tex";
 extern "C" const char s_p_menu_cpp[] = "p_menu.cpp";
-static const char s_dvd__smenu_gc23_fnt_801d9d8c[] = "dvd/%smenu/gc23.fnt";
-static const char s_dvd__smenu__s_fnt_801d9da0[] = "dvd/%smenu/%s.fnt";
-static const char s_dvd__smenu_gc22_fnt_801d9db4[] = "dvd/%smenu/gc22.fnt";
-static const char s_MenuTexKasoru_80330714[] = "kasoru";
-static const char s_MenuTexPause_8033071C[] = "pause";
-static const char s_MenuTexWin1_0_80330724[] = "win1_0";
-static const char s_MenuTexWin1_1_8033072C[] = "win1_1";
-static const char s_MenuTexWin1_2_80330734[] = "win1_2";
-static const char s_MenuTexWin1_3_8033073C[] = "win1_3";
-static const char s_MenuTexWin1_4_80330744[] = "win1_4";
-static const char s_MenuTexWin1_5_8033074C[] = "win1_5";
-static const char s_MenuTexWin1_6_80330754[] = "win1_6";
-static const char s_MenuTexWin1_7_8033075C[] = "win1_7";
-static const char s_MenuTexWin1_8_80330764[] = "win1_8";
-static const char s_MenuTexWin2_0_8033076C[] = "win2_0";
-static const char s_MenuTexWin2_1_80330774[] = "win2_1";
-static const char s_MenuTexWin2_2_8033077C[] = "win2_2";
-static const char s_MenuTexWin2_3_80330784[] = "win2_3";
-static const char s_MenuTexWin2_4_8033078C[] = "win2_4";
-static const char s_MenuTexWin2_5_80330794[] = "win2_5";
-static const char s_MenuTexWin2_6_8033079C[] = "win2_6";
-static const char s_MenuTexWin2_7_803307A4[] = "win2_7";
-static const char s_MenuTexWin2_8_803307AC[] = "win2_8";
-static const char s_MenuTexButton_803307B4[] = "button";
-static const char s_MenuRegionShibuya_803307BC[] = "shibuya";
-static const char s_MenuRegionFace_803307C4[] = "face";
-static const char s_MenuTexBattle_803307CC[] = "battle";
-static const char s_MenuTexHeart_803307D4[] = "heart";
-static const char s_MenuTexNavi_803307DC[] = "navi";
-static const char s_MenuTexHp0_803307E4[] = "hp0";
-static const char s_MenuTexHp1_803307E8[] = "hp1";
-static const char s_MenuTexHp2_803307EC[] = "hp2";
-static const char s_MenuTexSuna_803307F0[] = "suna";
-static const char s_MenuTexGba_803307F8[] = "gba";
-static const char s_MenuTexBattle2_803307FC[] = "battle2";
+static const char sMenuGc23FontPathFmt[] = "dvd/%smenu/gc23.fnt";
+static const char sMenuFontPathFmt[] = "dvd/%smenu/%s.fnt";
+static const char sMenuGc22FontPathFmt[] = "dvd/%smenu/gc22.fnt";
+static const char sMenuTexKasoru[] = "kasoru";
+static const char sMenuTexPause[] = "pause";
+static const char sMenuTexWin1_0[] = "win1_0";
+static const char sMenuTexWin1_1[] = "win1_1";
+static const char sMenuTexWin1_2[] = "win1_2";
+static const char sMenuTexWin1_3[] = "win1_3";
+static const char sMenuTexWin1_4[] = "win1_4";
+static const char sMenuTexWin1_5[] = "win1_5";
+static const char sMenuTexWin1_6[] = "win1_6";
+static const char sMenuTexWin1_7[] = "win1_7";
+static const char sMenuTexWin1_8[] = "win1_8";
+static const char sMenuTexWin2_0[] = "win2_0";
+static const char sMenuTexWin2_1[] = "win2_1";
+static const char sMenuTexWin2_2[] = "win2_2";
+static const char sMenuTexWin2_3[] = "win2_3";
+static const char sMenuTexWin2_4[] = "win2_4";
+static const char sMenuTexWin2_5[] = "win2_5";
+static const char sMenuTexWin2_6[] = "win2_6";
+static const char sMenuTexWin2_7[] = "win2_7";
+static const char sMenuTexWin2_8[] = "win2_8";
+static const char sMenuTexButton[] = "button";
+static const char sMenuRegionShibuya[] = "shibuya";
+static const char sMenuRegionFace[] = "face";
+static const char sMenuTexBattle[] = "battle";
+static const char sMenuTexHeart[] = "heart";
+static const char sMenuTexNavi[] = "navi";
+static const char sMenuTexHp0[] = "hp0";
+static const char sMenuTexHp1[] = "hp1";
+static const char sMenuTexHp2[] = "hp2";
+static const char sMenuTexSuna[] = "suna";
+static const char sMenuTexGba[] = "gba";
+static const char sMenuTexBattle2[] = "battle2";
 
 extern "C" const f32 FLOAT_80330804 = 0.015625f;
 extern "C" const f32 FLOAT_80330808 = 1.0f;
@@ -326,28 +326,28 @@ void CMenuPcs::create()
     char fontPath[0x80];
     char texPath[0x100];
     static int tTmp[] = {
-        0, reinterpret_cast<int>(const_cast<char*>(s_MenuTexKasoru_80330714)),
-        0, reinterpret_cast<int>(const_cast<char*>(s_MenuTexPause_8033071C)),
-        1, reinterpret_cast<int>(const_cast<char*>(s_MenuTexWin1_0_80330724)),
-        1, reinterpret_cast<int>(const_cast<char*>(s_MenuTexWin1_1_8033072C)),
-        1, reinterpret_cast<int>(const_cast<char*>(s_MenuTexWin1_2_80330734)),
-        1, reinterpret_cast<int>(const_cast<char*>(s_MenuTexWin1_3_8033073C)),
-        1, reinterpret_cast<int>(const_cast<char*>(s_MenuTexWin1_4_80330744)),
-        1, reinterpret_cast<int>(const_cast<char*>(s_MenuTexWin1_5_8033074C)),
-        1, reinterpret_cast<int>(const_cast<char*>(s_MenuTexWin1_6_80330754)),
-        1, reinterpret_cast<int>(const_cast<char*>(s_MenuTexWin1_7_8033075C)),
-        1, reinterpret_cast<int>(const_cast<char*>(s_MenuTexWin1_8_80330764)),
-        1, reinterpret_cast<int>(const_cast<char*>(s_MenuTexWin2_0_8033076C)),
-        1, reinterpret_cast<int>(const_cast<char*>(s_MenuTexWin2_1_80330774)),
-        1, reinterpret_cast<int>(const_cast<char*>(s_MenuTexWin2_2_8033077C)),
-        1, reinterpret_cast<int>(const_cast<char*>(s_MenuTexWin2_3_80330784)),
-        1, reinterpret_cast<int>(const_cast<char*>(s_MenuTexWin2_4_8033078C)),
-        1, reinterpret_cast<int>(const_cast<char*>(s_MenuTexWin2_5_80330794)),
-        1, reinterpret_cast<int>(const_cast<char*>(s_MenuTexWin2_6_8033079C)),
-        1, reinterpret_cast<int>(const_cast<char*>(s_MenuTexWin2_7_803307A4)),
-        1, reinterpret_cast<int>(const_cast<char*>(s_MenuTexWin2_8_803307AC)),
-        1, reinterpret_cast<int>(const_cast<char*>(s_win_kazari_801D9D48)),
-        0, reinterpret_cast<int>(const_cast<char*>(s_MenuTexButton_803307B4))
+        0, reinterpret_cast<int>(const_cast<char*>(sMenuTexKasoru)),
+        0, reinterpret_cast<int>(const_cast<char*>(sMenuTexPause)),
+        1, reinterpret_cast<int>(const_cast<char*>(sMenuTexWin1_0)),
+        1, reinterpret_cast<int>(const_cast<char*>(sMenuTexWin1_1)),
+        1, reinterpret_cast<int>(const_cast<char*>(sMenuTexWin1_2)),
+        1, reinterpret_cast<int>(const_cast<char*>(sMenuTexWin1_3)),
+        1, reinterpret_cast<int>(const_cast<char*>(sMenuTexWin1_4)),
+        1, reinterpret_cast<int>(const_cast<char*>(sMenuTexWin1_5)),
+        1, reinterpret_cast<int>(const_cast<char*>(sMenuTexWin1_6)),
+        1, reinterpret_cast<int>(const_cast<char*>(sMenuTexWin1_7)),
+        1, reinterpret_cast<int>(const_cast<char*>(sMenuTexWin1_8)),
+        1, reinterpret_cast<int>(const_cast<char*>(sMenuTexWin2_0)),
+        1, reinterpret_cast<int>(const_cast<char*>(sMenuTexWin2_1)),
+        1, reinterpret_cast<int>(const_cast<char*>(sMenuTexWin2_2)),
+        1, reinterpret_cast<int>(const_cast<char*>(sMenuTexWin2_3)),
+        1, reinterpret_cast<int>(const_cast<char*>(sMenuTexWin2_4)),
+        1, reinterpret_cast<int>(const_cast<char*>(sMenuTexWin2_5)),
+        1, reinterpret_cast<int>(const_cast<char*>(sMenuTexWin2_6)),
+        1, reinterpret_cast<int>(const_cast<char*>(sMenuTexWin2_7)),
+        1, reinterpret_cast<int>(const_cast<char*>(sMenuTexWin2_8)),
+        1, reinterpret_cast<int>(const_cast<char*>(sMenuTexWinKazari)),
+        0, reinterpret_cast<int>(const_cast<char*>(sMenuTexButton))
     };
     u8* self = reinterpret_cast<u8*>(this);
 
@@ -362,11 +362,11 @@ void CMenuPcs::create()
     memset(m_textureSets, 0, sizeof(m_textureSets));
     memset(m_textures, 0, sizeof(m_textures));
 
-    sprintf(fontPath, const_cast<char*>(s_dvd__smenu_gc22_fnt_801d9db4), Game.GetLangString());
+    sprintf(fontPath, const_cast<char*>(sMenuGc22FontPathFmt), Game.GetLangString());
     loadFont(0, fontPath, 0, 0);
 
     for (int i = 0; i < 2; i++) {
-        sprintf(texPath, const_cast<char*>(s_dvd__smenu__s_tex_801d9d6c), Game.GetLangString(), PTR_s_common[i]);
+        sprintf(texPath, const_cast<char*>(sMenuTexturePathFmt), Game.GetLangString(), PTR_s_common[i]);
 
         CFile::CHandle* fileHandle = File.Open(texPath, 0, CFile::PRI_LOW);
         if (fileHandle != 0) {
@@ -586,20 +586,20 @@ void CMenuPcs::loadFont(int type, char* path, int slot, int tlutMode)
 }
 
 const char* sMenuTextureRegionNameTable[] = {
-    s_MenuRegionShibuya_803307BC, s_MenuRegionFace_803307C4, 0, 0, 0, 0, 0, 0, 0
+    sMenuRegionShibuya, sMenuRegionFace, 0, 0, 0, 0, 0, 0, 0
 };
 
 int sMenuTextureInfoTable[] = {
-    2, reinterpret_cast<int>(const_cast<char*>(s_MenuTexBattle_803307CC)),
-    2, reinterpret_cast<int>(const_cast<char*>(s_MenuTexHeart_803307D4)),
-    3, reinterpret_cast<int>(const_cast<char*>(s_MenuRegionFace_803307C4)),
-    2, reinterpret_cast<int>(const_cast<char*>(s_MenuTexNavi_803307DC)),
-    2, reinterpret_cast<int>(const_cast<char*>(s_MenuTexHp0_803307E4)),
-    2, reinterpret_cast<int>(const_cast<char*>(s_MenuTexHp1_803307E8)),
-    2, reinterpret_cast<int>(const_cast<char*>(s_MenuTexHp2_803307EC)),
-    2, reinterpret_cast<int>(const_cast<char*>(s_MenuTexSuna_803307F0)),
-    2, reinterpret_cast<int>(const_cast<char*>(s_MenuTexGba_803307F8)),
-    2, reinterpret_cast<int>(const_cast<char*>(s_MenuTexBattle2_803307FC))
+    2, reinterpret_cast<int>(const_cast<char*>(sMenuTexBattle)),
+    2, reinterpret_cast<int>(const_cast<char*>(sMenuTexHeart)),
+    3, reinterpret_cast<int>(const_cast<char*>(sMenuRegionFace)),
+    2, reinterpret_cast<int>(const_cast<char*>(sMenuTexNavi)),
+    2, reinterpret_cast<int>(const_cast<char*>(sMenuTexHp0)),
+    2, reinterpret_cast<int>(const_cast<char*>(sMenuTexHp1)),
+    2, reinterpret_cast<int>(const_cast<char*>(sMenuTexHp2)),
+    2, reinterpret_cast<int>(const_cast<char*>(sMenuTexSuna)),
+    2, reinterpret_cast<int>(const_cast<char*>(sMenuTexGba)),
+    2, reinterpret_cast<int>(const_cast<char*>(sMenuTexBattle2))
 };
 
 /*
@@ -618,7 +618,7 @@ void CMenuPcs::loadTexture(char** paths, int textureSetStart, int textureSetCoun
     u8* self = reinterpret_cast<u8*>(this);
 
     for (int i = 0; i < textureSetCount; i++) {
-        sprintf(texPath, const_cast<char*>(s_dvd__smenu__s_tex_801d9d6c), Game.GetLangString(), *paths);
+        sprintf(texPath, const_cast<char*>(sMenuTexturePathFmt), Game.GetLangString(), *paths);
 
         CFile::CHandle* fileHandle = File.Open(texPath, 0, CFile::PRI_LOW);
         if (fileHandle != 0) {
@@ -1544,7 +1544,7 @@ void CMenuPcs::LoadExtraFont(int fontNo, char* fileName)
         m_fonts[fontNo + 2] = 0;
     }
 
-    sprintf(path, const_cast<char*>(s_dvd__smenu__s_fnt_801d9da0), Game.GetLangString(), fileName);
+    sprintf(path, const_cast<char*>(sMenuFontPathFmt), Game.GetLangString(), fileName);
     loadFont(2, path, fontNo + 2, -1);
 }
 
@@ -1629,7 +1629,7 @@ void CMenuPcs::createBattle()
 
     for (int i = 0; i < 2; i++) {
         const char* language = Game.GetLangString();
-        sprintf(path, const_cast<char*>(s_dvd__smenu__s_tex_801d9d6c), language, sMenuTextureRegionNameTable[i]);
+        sprintf(path, const_cast<char*>(sMenuTexturePathFmt), language, sMenuTextureRegionNameTable[i]);
 
         CFile::CHandle* fileHandle = File.Open(path, 0, CFile::PRI_LOW);
         if (fileHandle != 0) {
@@ -1670,7 +1670,7 @@ void CMenuPcs::createBattle()
         m_battleRingMenus[i]->Create();
     }
 
-    sprintf(fontPath, const_cast<char*>(s_dvd__smenu_gc23_fnt_801d9d8c), Game.GetLangString());
+    sprintf(fontPath, const_cast<char*>(sMenuGc23FontPathFmt), Game.GetLangString());
     loadFont(0, fontPath, 1, 1);
 
     for (int i = 0; i < 0x100; i++) {

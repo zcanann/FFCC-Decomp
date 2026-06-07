75b5b0152cdae01b0dec4010e3f6d53a109cd321
Zachary Canann <zacharycanann@gmail.com>
2026-06-07 15:55:49 -0700

    Remove scratch diff helper scripts

--- tools/sdiff.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/zsh
-# sdiff.sh <symbol>  -> show instruction diff for symbol in materialman
-SYM="$1"
-cd /Users/zcanann/Documents/Projects/ffcc-grind-materialman
-tools/objdiff-cli diff -p . -u main/materialman -o - --format json-pretty "$SYM" 2>/dev/null

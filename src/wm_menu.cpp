87f0b0933d6a35eb6c98153456bb1ac40a345c55
Zachary Canann <zcanann@Zacharys-Mac-mini.local>
2026-06-09 19:38:41 -0700

    permute main/wm_menu LoadDat: 86.7882 -> 86.8234

--- src/wm_menu.cpp
+++ src/wm_menu.cpp
@@ -13431,7 +13431,8 @@ int McCtrl::LoadDat()
 	case 5: {
 		unsigned int serialLo = 0;
 		unsigned int serialHi = 0;
-		if (CARDGetSerialNo(m_cardChannel, reinterpret_cast<unsigned long long*>(&serialLo)) != 0) {
+		int __p9 = serialLo;
+		if (CARDGetSerialNo(m_cardChannel, reinterpret_cast<unsigned long long*>(&__p9)) != 0) {
 			MemoryCardMan.McClose();
 			MemoryCardMan.McUnmount(m_cardChannel);
 			MemoryCardMan.DestroyMcBuff();
@@ -13461,13 +13462,13 @@ int McCtrl::LoadDat()
 				MemoryCardMan.DecodeData();
 				MemoryCardMan.McClose();
 				MemoryCardMan.McUnmount(m_cardChannel);
-				if (m_userBuffer == 0) {
+				if (!(m_userBuffer == 0)) {
+					memcpy(m_userBuffer, MemoryCardMan.m_saveBuffer, 0x8BD0);
+					MemoryCardMan.CalcSaveDatHpMax(reinterpret_cast<Mc::SaveDat*>(m_userBuffer));
+				} else {
 					Game.LoadInit();
 					MemoryCardMan.SetLoadData();
 					Game.LoadFinished();
-				} else {
-					memcpy(m_userBuffer, MemoryCardMan.m_saveBuffer, 0x8BD0);
-					MemoryCardMan.CalcSaveDatHpMax(reinterpret_cast<Mc::SaveDat*>(m_userBuffer));
 				}
 				MemoryCardMan.DestroyMcBuff();
 				m_state = 7;

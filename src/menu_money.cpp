02acf9125992540d61c7f4c2f7f4e5e54eaf0dae
Zachary Canann <zcanann@Zacharys-Mac-mini.local>
2026-06-11 08:58:34 -0700

    Fix money digit placement build

--- src/menu_money.cpp
+++ src/menu_money.cpp
@@ -45,7 +45,6 @@ inline void CMenuPcs::MoneySetPlace(int row)
 	digitIndex = 0;
 	place = &s_place[row * 8];
 
-	signed char* place = &s_place[row * 8];
 	do {
 		if ((!started) && (gil >= digitPlace)) {
 			started = 1;
@@ -60,7 +59,6 @@ inline void CMenuPcs::MoneySetPlace(int row)
 			*place = static_cast<signed char>(digit);
 			gil = gil % digitPlace;
 		}
-		place++;
 		digitIndex++;
 		place++;
 		digitPlace /= 10;

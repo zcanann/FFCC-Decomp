0b39178a5e0c9bd21f252eedbc27033792adbd8f
Zachary Canann <zacharycanann@gmail.com>
2026-06-11 09:30:33 -0700

    Complete wmm_str data constants

--- src/wmm_str.cpp
+++ src/wmm_str.cpp
@@ -16,8 +16,9 @@ extern const char sWmmSlotB[] = "Slot B";
 extern const char sWmmSteckplatzB[];
 extern const char sWmmRanuraB[];
 
-extern const float kWmmWindowTextScale;
-extern const double kWmmCenteringHalf;
+extern const float kWmmWindowTextScale = 1.0f;
+extern const double kWmmCenteringHalf = 0.5;
+extern const double kWmmUnsignedToDoubleBias = 4503601774854144.0;
 
 extern const char* s_McStr_en[];
 extern const char* s_McStr_de[];

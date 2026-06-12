435f169bf84c9f70260845bf79b3e04a79221aba
Zachary Canann <zcanann@Zacharys-Mac-mini.local>
2026-06-11 20:10:16 -0700

    wm_menu: DrawCMLife 92.68->92.72 color order a,b,r,g

--- src/wm_menu.cpp
+++ src/wm_menu.cpp
@@ -9446,9 +9446,9 @@ void CMenuPcs::DrawCMLife()
 		const float* p255C = &FLOAT_80331458;
 		GXColor color;
 		color.a = static_cast<unsigned char>(static_cast<int>(alphaF));
-		color.g = static_cast<unsigned char>(static_cast<int>(*p255C * green));
-		color.r = static_cast<unsigned char>(static_cast<int>(*p255C * red));
 		color.b = static_cast<unsigned char>(static_cast<int>(*p255C * blue));
+		color.r = static_cast<unsigned char>(static_cast<int>(*p255C * red));
+		color.g = static_cast<unsigned char>(static_cast<int>(*p255C * green));
 		GXSetChanMatColor(GX_COLOR0A0, color);
 
 		row = slot / 4;

414eda5cb4702f62df37a641be7d22265b5d1e15
Zachary Canann <zacharycanann@gmail.com>
2026-06-11 21:47:44 -0700

    Improve MoneyCtrlCur gil signedness

--- src/menu_money.cpp
+++ src/menu_money.cpp
@@ -181,7 +181,7 @@ int CMenuPcs::MoneyCtrlCur()
 			}
 		} else {
 			if ((hold & 4) != 0) {
-				int gil = caravanWork->m_gil;
+				unsigned int gil = caravanWork->m_gil;
 				if (gil == 0) {
 					Sound.PlaySe(4, 0x40, 0x7F, 0);
 				} else {

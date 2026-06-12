4efc7c8925e2399297916e20985423ca90d55fb4
Zachary Canann <zacharycanann@gmail.com>
2026-06-12 00:39:09 -0700

    Improve mini-game data layout

--- include/ffcc/p_minigame.h
+++ include/ffcc/p_minigame.h
@@ -61,21 +61,34 @@ public:
     void SetNumPlayer();
 
 private:
-    u8 m_work[0x1344];
+    u8 m_work[0x1340];
+    char m_managerTag[4]; // 0x1344
     unsigned char m_managerState; // 0x1348
-    u8 m_work1349[2];
+    u8 m_work1349;
+    unsigned char m_managerMode; // 0x134A
     unsigned char m_playerMask; // 0x134B
-    u8 m_work134C[4];
+    unsigned char m_playerCount; // 0x134C
+    u8 m_work134D;
+    unsigned short m_statusCode; // 0x134E
     unsigned char m_managerIndex; // 0x1350
-    u8 m_work1351[0x6484 - 0x1351];
+    u8 m_work1351[3];
+    unsigned char* m_managerImage; // 0x1354
+    unsigned int m_managerImageSize; // 0x1358
+    unsigned char* m_managerSpImage; // 0x135C
+    unsigned int m_managerSpImageSize; // 0x1360
+    u8 m_work1364[0x6484 - 0x1364];
 
 public:
     unsigned int m_flags; // 0x6484
 
 private:
-    u8 m_work6488[0x6498 - 0x6488];
+    u8 m_work6488[0x6494 - 0x6488];
+    unsigned char m_miniGameReady; // 0x6494
+    unsigned char m_miniGameFailed; // 0x6495
+    u8 m_work6496[2];
     signed char m_miniGameParams[4]; // 0x6498
-    u8 m_work649C[0x64A0 - 0x649C];
+    unsigned char m_managerThreadStop; // 0x649C
+    u8 m_work649D[3];
 };
 
 extern CMiniGamePcs MiniGamePcs;
